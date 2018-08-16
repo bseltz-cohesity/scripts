20c36078296f5dcc8f47772cbc4d35268aa5b4fc
selterb <selterb01@gmail.com>
2018-08-16 16:08:16 -0400

    2018-08-16

--- powershell/protectView/README.md
+++ powershell/protectView/README.md
@@ -1,16 +1,16 @@
-# Add a VM to a Cohesity Protection Job using PowerShell
+# Protect a Cohesity View using PowerShell
 
 Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
 
-This powershell script adds a VM to an existing protection job.
+This powershell script creates a protection job to protect a view.
 
 ## Components
 
-* protectVM.ps1: the main powershell script
+* protectView.ps1: the main powershell script
 * cohesity-api.ps1: the Cohesity REST API helper module
 
 Place both files in a folder together and run the main script like so:
 
 ```powershell
-./protectVM.ps1 -vip mycluster -username admin -jobName 'vm backup' -vmName mongodb
+./protectView.ps1 -vip mycluster -username admin -viewName homedirs -policyName 'Standard Protection'
 ```
---
+++ powershell/protectView/protectView.ps1
@@ -0,0 +1,79 @@
+### usage: ./protectView.ps1 -vip bseltzve01 -username admin -viewName homedirs -policyName 'Standard Protection'
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter(Mandatory = $True)][string]$viewName, #name of VM to protect
+    [Parameter(Mandatory = $True)][string]$policyName #name of the job to add VM to
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+$policy = api get protectionPolicies | Where-Object { $_.name -ieq $policyName }
+if(!$policy){
+    Write-Warning "Policy $policyName not found!"
+    exit
+}
+
+$view = $(api get views).views | Where-Object { $_.name -ieq $viewName }
+if(!$view){
+    Write-Warning "View $viewName not found!"
+    exit
+}
+
+$protectionJob = @{
+    'name' = "$($view.name) backup";
+    'environment' = 'kView';
+    '_envParams' = @{};
+    'viewBoxId' = $view.viewBoxId;
+    'sourceIds' = @();
+    'excludeSourceIds' = @();
+    'vmTagIds' = @();
+    'excludeVmTagIds' = @();
+    '_selectedSources' = @();
+    'policyId' = $policy.id;
+    'priority' = 'kMedium';
+    'alertingPolicy' = @(
+        'kFailure'
+    );
+    'timezone' = 'America/New_York';
+    'incrementalProtectionSlaTimeMins' = 60;
+    'fullProtectionSlaTimeMins' = 120;
+    'qosType' = 'kBackupHDD';
+    '_sourceSpecialParametersMap' = @{};
+    'viewName' = $view.name;
+    '_viewBoxName' = $view.viewBoxName;
+    '_viewSource' = @{
+        'name' = $view.name;
+        'viewProtectionSource' = @{
+            'id' = @{
+                'id' = $view.viewId;
+            }
+        }
+    };
+    'isActive' = $true;
+    '_parentSource' = @{};
+    '_supportsAutoProtectExclusion' = $false;
+    'sourceSpecialParameters' = @();
+    'isDeleted' = $true;
+    '_supportsIndexing' = $false;
+    'indexingPolicy' = @{
+        'disableIndexing' = $true
+    };
+    '_hasFilePathFilters' = $false;
+    'startTime' = @{
+        'hour' = 23;
+        'minute' = 55;
+    }
+}
+
+"Creating Protection Job for $($view.name)..."
+$result = api post protectionJobs $protectionJob
+
