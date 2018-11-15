4c267966987803e264b975ec33d6c9e2f253ffe7
selterb <selterb01@gmail.com>
2018-11-15 05:08:37 -0500

    cloneView

---
+++ python/cloneView/README.md
@@ -0,0 +1,31 @@
+# Clone a Cohesity View using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script clones a view.
+
+## Components
+
+* cloneView.sh: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./cloneView.py -s mycluster -u admin [ -d domain ] -v myview -n newview
+Connected!
+Cloning View myview as newview...
+```
+
+## The Python Helper Module - pyhesity.py
+The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module.
+
+### Installing the Prerequisites
+```bash
+sudo yum install python-requests
+```
+or
+
+```bash
+sudo easy_install requests
+```
---
+++ python/cloneView/cloneView.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""Clone a Cohesity View Using python"""
+
+### usage: ./cloneView.py -s mycluster -u admin -d domain -v myview -n newview
+
+### import pyhesity wrapper module
+from pyhesity import *
+
+### command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-s','--server', type=str, required=True)
+parser.add_argument('-u','--username', type=str, required=True)
+parser.add_argument('-d','--domain',type=str,default='local')
+parser.add_argument('-v','--view', type=str, required=True)
+parser.add_argument('-n','--name', type=str, required=True)
+
+args = parser.parse_args()
+    
+vip = args.server
+username = args.username
+domain = args.domain
+viewName = args.view
+newName = args.name
+
+### authenticate
+apiauth(vip, username, domain)
+
+### search for view to clone
+searchResults = api('get','/searchvms?entityTypes=kView&vmName=%s' % viewName)
+if len(searchResults) == 0: print "View %s not found" % viewName; exit()
+
+### narrow search results to the correct view
+viewResults = [ viewResult for viewResult in searchResults['vms'] if viewResult['vmDocument']['objectName'].lower() == viewName.lower() ]
+if len(viewResults) == 0: print "View %s not found" % viewName; exit()
+viewResult = viewResults[0]
+
+view = api('get','views/%s?includeInactive=True' % viewResult['vmDocument']['objectName'])
+
+taskName = "Clone-View_%s_as_%s" % (viewName, newName)
+
+cloneTask = {
+    "name": taskName,
+    "objects": [
+            {
+                "jobUid": viewResult['vmDocument']['objectId']['jobUid'],
+                "jobId": viewResult['vmDocument']['objectId']['jobId'],
+                "jobInstanceId": viewResult['vmDocument']['versions'][0]['instanceId']['jobInstanceId'],
+                "startTimeUsecs": viewResult['vmDocument']['versions'][0]['instanceId']['jobStartTimeUsecs'],
+                "entity": viewResult['vmDocument']['objectId']['entity']
+            }
+    ],
+    "viewName": newName,
+    "action": 5,
+    "viewParams": {
+        "sourceViewName": view['name'],
+        "cloneViewName": newName,
+        "viewBoxId": view['viewBoxId'],
+        "viewId": viewResult['vmDocument']['objectId']['entity']['id'],
+        "qos": view['qos'],
+        "description": view['description'],
+        "allowMountOnWindows": view['allowMountOnWindows'],
+        "storagePolicyOverride": view['storagePolicyOverride']
+    }
+}
+
+### execute the clone task
+response = api('post','/clone',cloneTask)
+print "Cloning View %s as %s..." % (viewName, newName)
