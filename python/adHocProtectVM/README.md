de45107df32ddcb2ea390a7d9dcae22a0cc488e9
selterb <selterb01@gmail.com>
2019-01-10 15:38:48 -0500

    2019-01-10

---
+++ python/adHocProtectVM/README.md
@@ -0,0 +1,33 @@
+# Backup New VM Once using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script performs an adHovc backup of a new VM. It refreshes a vCenter source, adds a new VM to a protection job, backs up the VM, waits for the backup to finish, then removes the VM from the job. 
+
+## Components
+
+* adHocProtectVM.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+```bash
+bash:~/scripts/python$./adHocProtectVM.py -v mycluster -u myuser -vc vCenter6.mydomain.net -vm wiki -job 'vm backup'
+Connected!
+refreshing vCenter6.mydomain.net...
+adding Wiki to VM Backup job...
+Running vm backup...
+RunID: 7474 Status: kSuccess
+```
+
+## The Python Helper Module - pyhesity.py
+The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module.
+
+### Installing the Prerequisites
+```bash
+sudo yum install python-requests
+```
+or
+
+```bash
+sudo easy_install requests
+```
---
+++ python/adHocProtectVM/adHocProtectVM.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""Discover New VM And Backup Now Once"""
+
+### usage: ./adHocProtectVM.py -v mycluster -u myuser [-d mydomain.net ] -vc vCenter6.mydomain.net -vm wiki -job 'vm backup'
+
+### import pyhesity wrapper module
+from pyhesity import *
+from time import sleep
+
+### command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v','--vip', type=str, required=True)
+parser.add_argument('-u','--username', type=str, required=True)
+parser.add_argument('-d','--domain',type=str,default='local')
+parser.add_argument('-vc','--vcenter', type=str, required=True)
+parser.add_argument('-vm','--vmname', type=str, required=True)
+parser.add_argument('-job','--jobname', type=str, required=True)
+
+args = parser.parse_args()
+    
+vip = args.vip                 #cluster name/ip
+username = args.username       #username to connect to cluster
+domain = args.domain           #domain of username (e.g. local, or AD domain)
+vcenter = args.vcenter         #name of vcenter to find vm on
+vmname = args.vmname           #name of VM to add to protection job
+jobname = args.jobname         #name of protection job to add VM to
+
+### authenticate
+apiauth(vip, username, domain)
+
+### find vCenter
+vCenterId = None
+vCenters = api('get','/entitiesOfType?environmentTypes=kVMware&vmwareEntityTypes=kVCenter')
+for vc in vCenters:
+    if vc['displayName'].lower() == vcenter.lower():
+        vCenterId = vc['id']
+
+if vCenterId is None:
+    print 'Warning: vCenter %s not found!' % vcenter
+    exit()
+
+### refresh vCenter
+print 'refreshing %s...' % vcenter
+result = api('post','protectionSources/refresh/%s' % vCenterId)
+
+foundJob = False
+foundVM = False
+myJob = None
+for job in api('get','protectionJobs'):
+    ### find job
+    if job['name'].lower() == jobname.lower():
+        foundJob = True
+        jobId = job['id']
+        for vm in api('get','protectionSources/virtualMachines?vCenterId=%s' % job['parentSourceId']):
+            ### find VM
+            if vm['name'].lower() == vmname.lower():
+                foundVM = True
+                vmId = vm['id']
+                ### avoid duplicate
+                if vm['id'] in job['sourceIds']:
+                    print 'VM %s already in job %s' % (vm['name'], job['name'])
+                else:
+                    ### record old state of job
+                    myJob = job
+                    mySources = []
+                    mySources.extend(job['sourceIds'])
+                    ### add VM to job
+                    job['sourceIds'].append(vm['id'])
+                    print 'adding %s to %s job...' % (vm['name'], job['name'])
+                    updatedJob = api('put','protectionJobs/%s' % job['id'],job)
+
+### report not founds
+if foundJob == False:
+    print 'Warning: Job %s not found!' % jobname
+    exit()
+if foundVM == False:
+    print 'Warning: VM %s not found!' % vmname
+    exit()
+
+### job data
+jobData = {
+    "copyRunTargets": [],
+    "sourceIds": [
+        vmId
+    ],
+    "runType": "kRegular"
+}
+
+runs = api('get','protectionRuns?jobId=%s' % jobId)
+newRunId = lastRunId = runs[0]['backupRun']['jobRunId']
+
+### wait for existing job run to finish
+finishedStates = ['kCanceled', 'kSuccess', 'kFailure']
+if (runs[0]['backupRun']['status'] not in finishedStates):
+    print "waiting for existing job run to finish..."
+    while (runs[0]['backupRun']['status'] not in finishedStates):
+        sleep(5)
+        runs = api('get','protectionRuns?jobId=%s' % jobId)    
+
+### run protectionJob
+print "Running %s..." % jobname
+api('post',"protectionJobs/run/%s" % jobId, jobData)
+
+### wait for new job run to appear
+while(newRunId == lastRunId):
+    sleep(1)
+    runs = api('get','protectionRuns?jobId=%s' % jobId)
+    newRunId=runs[0]['backupRun']['jobRunId']
+
+### wait for job run to finish
+while(runs[0]['backupRun']['status'] not in finishedStates):
+    sleep(5)
+    runs = api('get','protectionRuns?jobId=%s' % jobId)
+
+### remove vm from job
+if myJob is not None:
+    myJob['sourceIds'] = mySources
+    updatedJob = api('put','protectionJobs/%s' % myJob['id'],myJob)
+
+print "RunID: %s Status: %s" % (newRunId, runs[0]['backupRun']['status'])
