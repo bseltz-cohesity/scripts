1b7b15720f7733f7f21a623374e09ae41a471b6f
selterb <selterb01@gmail.com>
2019-01-19 19:55:49 -0500

    2019-01-19

---
+++ powershell/expungeDataSpillage/README.md
@@ -0,0 +1,69 @@
+# Expunge Data Spillage with PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+## Warning! This script deletes backup data! Make sure you know what you are doing before you run it!  
+
+This powershell script searches for a file, and displays the ProtectionJobs/Objects where the file is stored. You can then select a ProtectionJob/Object and the script will delete all versions of the ProtectionJob/Object where that file exists. The deletion will include the local snapshots plus any remote replicas and archives.
+
+If you run the script without the -delete switch, the script will only display what it would delete. Use the -delete switch to actually perform the deletions.
+
+Deletions will be logged to scriptPath/expungeLog.txt
+
+## Components
+
+* expungeDataSpillage.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together, then we can run the script.
+
+```powershell
+### usage: ./expungeDataSpillage.ps1 -vip mycluster -username admin [ -domain local ] -search 'partial/filepath' [ -delete ]
+```
+
+First, run the script WITHOUT the -delete switch to see what would be deleted.
+
+```powershell
+powershell> ./expungeDataSpillage.ps1 -vip mycluster -username admin [ -domain mydomain ] -search 'jobmonitor.sh'
+Connected!
+Searching for jobmonitor.sh...
+Search Results:
+----
+0: /home/seltzerb/old-jobMonitor.sh
+VM Backup::CentOS1
+----
+1: /home/seltzerb/jobMonitor.sh
+VM Backup::CentOS1
+----
+Please select ID to expunge: 
+```
+
+Select an item from the list by typing the ID (e.g. 0) 
+
+```powershell
+Please select ID to expunge: 0
+Searching for versions to delete...
+Deleting CentOS1 from VM Backup: 01/17/2019 23:30:01
+  Local Snapshot
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/15/2019 23:30:01
+  Local Snapshot
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/14/2019 23:30:01
+  Local Snapshot
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/13/2019 23:30:00
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/12/2019 23:30:01
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/11/2019 23:30:01
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/10/2019 23:30:00
+  Archive on S3
+Deleting CentOS1 from VM Backup: 01/09/2019 23:30:01
+  Archive on S3
+```
+
+Then, if you're happy with what would be deleted, re-run the script again and include the -delete switch. THIS WILL DELETE BACKUP DATA!!!
+
+Please note that there may be some delay before the deletions are reflected in file search results. 
---
+++ powershell/expungeDataSpillage/expungeDataSpillage.ps1
@@ -0,0 +1,126 @@
+### usage: ./expungeDataSpillage.ps1 -vip mycluster -username admin [ -domain local ] -search 'partial/filepath' [ -delete ]
+
+### note: -delete switch actually performs the delete, otherwise just perform a test run
+### processing is logged at <scriptpath>/expungeLog.txt
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter(Mandatory = $True)][string]$search, #file name or path to download
+    [Parameter()][switch]$delete
+)
+
+$scriptdir = Split-Path -parent $PSCommandPath
+$logfile = Join-Path -Path $scriptdir -ChildPath 'expungeLog.txt'
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+$replicaType = @{'1' = 'kLocal'; '2' = 'kReplica'; '3' = 'kArchival'}
+$archiveType = @{'0' = "kCloud"; '1' = "kTape" }
+$jobs = api get protectionJobs
+
+### provide file to search for
+$fileSearch = $search
+"Searching for $fileSearch..."
+$restoreFiles = api get "/searchfiles?filename=$fileSearch"
+
+### display files
+$id = 0
+"Search Results:"
+"----"
+foreach ($restoreFile in $restoreFiles.files){
+    "$($id): $($restoreFile.fileDocument.filename)"
+    "$(($jobs | Where-Object { $_.id -eq $restoreFile.fileDocument.objectId.jobId }).name)::$($restoreFile.fileDocument.objectId.entity.displayName)"
+    "----"
+    $id += 1
+}
+
+### prompt for ID to select one from result set
+$selectedId='x'
+while(!($selectedId -is [int] -and $restoreFiles.files[$selectedId])){
+    $selectedId = read-host -Prompt "Please select ID to expunge"    
+    if($selectedId -ne '' -and ($selectedId -as [int]) -ne $null){ $selectedId = [int]$selectedId }
+}
+
+$restoreFile = $restoreFiles.files[$selectedId]
+
+### gather info for log
+$logItem = "- Started at $(get-date) -------`n"
+$logItem += "FileName: $($restoreFile.fileDocument.filename)`n"
+$logItem += "JobName: $(($jobs | Where-Object { $_.id -eq $restoreFile.fileDocument.objectId.jobId }).name)`n"
+$logItem += "ObjectName: $($restoreFile.fileDocument.objectId.entity.displayName)`n"
+
+### get versions
+$clusterId = $restoreFile.fileDocument.objectId.jobUid.clusterId
+$clusterIncarnationId = $restoreFile.fileDocument.objectId.jobUid.clusterIncarnationId
+$entityId = $restoreFile.fileDocument.objectId.entity.id
+$encodedFileName = [System.Web.HttpUtility]::UrlEncode($restoreFile.fileDocument.filename)
+$jobId = $restoreFile.fileDocument.objectId.jobId
+
+"Searching for versions to delete..."
+$versions = api get "/file/versions?clusterId=$clusterId&clusterIncarnationId=$clusterIncarnationId&entityId=$entityId&filename=$encodedFileName&fromObjectSnapshotsOnly=false&jobId=$jobId"
+
+$runs = api get protectionRuns?jobId=$jobId
+
+### delete protected object from each affected version
+foreach ($version in $versions.versions){
+    $exactRun = $runs | Where-Object {$_.backupRun.jobRunId -eq $version.instanceId.jobInstanceId }
+    "Deleting $($restoreFile.fileDocument.objectId.entity.displayName) from $($exactRun.jobName): $(usecsToDate $exactRun.copyRun[0].runStartTimeUsecs)"
+    $logItem += "Deleting $($restoreFile.fileDocument.objectId.entity.displayName) from $($exactRun.jobName): $(usecsToDate $exactRun.copyRun[0].runStartTimeUsecs)`n"
+    $updateProtectionJobRunsParam = @{
+        'jobRuns' = @(
+            @{
+                'copyRunTargets' = @();
+                'runStartTimeUsecs' = $exactRun.copyRun[0].runStartTimeUsecs;
+                'jobUid' = $exactRun.jobUid;
+                'sourceIds' = @(
+                    $restoreFile.fileDocument.objectId.entity.id
+                )
+            }
+        )
+    }
+    ### delete local snapshot, remote replicas and archive targets
+    foreach ($replica in $version.replicaInfo.replicaVec){
+
+        $newCopyRunTarget = @{
+            'daysToKeep' = 0;
+            'type' = $replicaType[[string]$replica.target.type]
+        }
+        if($replica.target.type -eq 1){
+            "  Local Snapshot"
+            $logItem += "  Local Snapshot`n"
+        }
+        if($replica.target.type -eq 3){
+            $newCopyRunTarget['archivalTarget']=@{}
+            $newCopyRunTarget['archivalTarget']['vaultName'] = $replica.target.archivalTarget.name
+            $newCopyRunTarget['archivalTarget']['vaultId'] = $replica.target.archivalTarget.vaultId
+            $newCopyRunTarget['archivalTarget']['vaultType'] = $archiveType[[string]$replica.target.archivalTarget.type]
+            "  Archive on $($replica.target.archivalTarget.name)"
+            $logItem += "  Archive on $($replica.target.archivalTarget.name)`n"
+        }
+        if($replica.target.type -eq 2){
+            $newCopyRunTarget['replicationTarget']=@{}
+            $newCopyRunTarget['replicationTarget']['clusterId']=$replica.target.replicationTarget.clusterId
+            $newCopyRunTarget['replicationTarget']['clusterName']=$replica.target.replicationTarget.clusterName
+            "  Replica on $($replica.target.replicationTarget.clusterName)"
+            $logItem += "  Replica on $($replica.target.replicationTarget.clusterName)`n"
+        }
+        $updateProtectionJobRunsParam.jobRuns[0].copyRunTargets += $newCopyRunTarget
+    }
+    ### execute the deletion
+    if($delete){
+        api put protectionRuns $updateProtectionJobRunsParam
+    }
+}
+$logItem += "- Ended at $(get-date) -------`n`n"
+#$logItem | Out-File $logfile -Append
+if($delete){
+    $logItem | Out-File $logfile -Append
+}
