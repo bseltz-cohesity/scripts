e463ab998945654c4ae5ae2ec4da38a53a76ce7c
selterb <selterb01@gmail.com>
2019-01-30 14:08:36 -0500

    2019-01-30

--- python/nodeAdd/README.md
+++ python/nodeAdd/README.md
@@ -12,16 +12,17 @@ This python script adds a free node to a cluster.
 Place both files in a folder together and run the main script like so:
 
 ```bash
-bash:~/python$ ./nodeAdd.py -v mycluster -u admin
+bash:~/python$ ./nodeAdd.py -v mycluster -u admin -v 10.1.15.223
 Connected!
+adding VIP 10.1.15.223
 Adding node 215507192131180 to cluster
 Expanding cluster having id 775397535002324 and name mycluster in progress...
 ```
 
 The full syntax of the command is:
 ```bash
-bash:~/python$ ./nodeAdd.py -v mycluster -u admin [ -d mydomain ] \
-    [ -i nodeIP ] [ -p ipmiIP ] [ -n nodeId ]
+bash:~/python$ ./nodeAdd.py -s mycluster -u admin [ -d mydomain ] \
+    [ -i nodeIP ] [ -p ipmiIP ] [ -n nodeId ] [ -v newVIP]
 ```
 
 Syntax notes:
@@ -29,7 +30,10 @@ Syntax notes:
 * domain defaults to local if not specified
 * nodeIP must be already configured on the node if not specified
 * ipmiIP must be already configured on the node if not specified
-* nodeId will default to a free node in the same chassis if possible, otherwise will use the first free node found, if not specified 
+* nodeId will default to a free node in the same chassis if possible, otherwise will use the first free node found, if not specified
+* newVIP is required if a spare VIP doesn't already exist on the cluster
+
+Note: The script does not add the newVIP to DNS. 
 
 ## The Python Helper Module - pyhesity.py
 The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module.
--- python/nodeAdd/nodeAdd.py
+++ python/nodeAdd/nodeAdd.py
@@ -13,24 +13,36 @@ from pyhesity import *
 ### command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v','--vip', type=str, required=True)
+parser.add_argument('-s','--server', type=str, required=True)
 parser.add_argument('-u','--username', type=str, required=True)
 parser.add_argument('-d','--domain',type=str, default='local')
 parser.add_argument('-n','--nodeId', type=int, default=0)
-parser.add_argument('-i','--ipAddress', type=int, default=0)
-parser.add_argument('-p','--ipmiAddress', type=int, default=0)
+parser.add_argument('-i','--ipAddress', type=str, default='')
+parser.add_argument('-p','--ipmiAddress', type=str, default='')
+parser.add_argument('-v','--newVip', type=str, default='')
 
 args = parser.parse_args()
     
-vip = args.vip
+server = args.server
 username = args.username
 domain = args.domain
 nodeId = args.nodeId
 ipAddress = args.ipAddress
 ipmiAddress = args.ipmiAddress
+newVip = args.newVip
+
+### ip validate functions
+def ipToBinary(ip):
+    return ''.join(['{0:08b}'.format(int(octet)) for octet in ip.split('.')])
+
+def ipValid(newIp, oldIp, netMask):
+    cidr = ipToBinary(netMask).index('0')
+    if('1' not in ipToBinary(newIp)[cidr:]): return False
+    if('0' not in ipToBinary(newIp)[cidr:]): return False
+    return ipToBinary(newIp)[0:cidr] == ipToBinary(oldIp)[0:cidr]
 
 ### authenticate
-apiauth(vip, username, domain)
+apiauth(server, username, domain)
 
 ### get cluster config
 clusterStat = api('get','/nexus/cluster/status')
@@ -71,24 +83,55 @@ if selectedNode == 0:
     exit()
 
 else:
-    print "Adding node %i to cluster" % selectedNode['nodeId']
     
-    ### use specified addresses or get existing addresses from node 
-    if ipAddress == 0:
+    ### gather cluster subnet details
+    clusterSubnet = clusterStat['clusterConfig']['proto']['clusterSubnet']['ip']
+    clusterNetMask = clusterStat['clusterConfig']['proto']['clusterSubnet']['netmaskIp4']
+    ipmiSubnet = clusterStat['clusterConfig']['proto']['ipmiConfig']['subnet']['ip']
+    ipmiNetMask = clusterStat['clusterConfig']['proto']['ipmiConfig']['subnet']['netmaskIp4']
+
+    ### validate VIP
+    
+    partitions = api('get','clusterPartitions')
+
+    if(newVip == ''):
+        createVip = False
+        if(len(partitions[0]['nodeIds']) >= len(partitions[0]['vips'])):
+            print "please provide a new VIP"
+            exit()            
+    else:
+        createVip = True
+        if(not ipValid(newVip,clusterSubnet,clusterNetMask)):
+            print "VIP address %s is not in the range of the cluster subnet %s(%s)" % (newVip, clusterSubnet, clusterNetMask)
+            exit()
+
+        if(newVip in partitions[0]['vips']):
+            print "VIP %s is already in use on the cluster. Please select an unused address" % newVip
+            exit()
+
+    ### validate Node IP
+    if ipAddress == '':
         if 'ipAddresses' in selectedNode:
             ipAddress = selectedNode['ipAddresses'][0]
         else:
             print "please provide node ip Address"
             exit()
-    if ipmiAddress == 0:
-        if 'ipmiIp' in selectedNode:
+
+    if(not ipValid(ipAddress,clusterSubnet,clusterNetMask)):
+        print "Node IPAddress %s is not in range of the cluster subnet %s(%s)" % (ipAddress, clusterSubnet, clusterNetMask)
+        exit()
+
+    ### validate ipmi IP
+    if ipmiAddress == '':
+        if 'ipmiIp' in selectedNode and selectedNode['ipmiIp'] != '':
             ipmiAddress = selectedNode['ipmiIp']
         else:
             print "please provide ipmi ip address"
             exit()
 
-    ### calculate subnet
-    # not sure if API will validate the IP address or not...
+    if(not ipValid(ipmiAddress,ipmiSubnet,ipmiNetMask)):
+        print "Node ipmi address %s is not in range of the ipmi subnet %s(%s)" % (ipmiAddress, ipmiSubnet, ipmiNetMask)
+        exit() 
 
     ### new node parameters
     newNodeParams = {
@@ -104,7 +147,15 @@ else:
         "ignoreSwIncompatibility": True
     }
 
+    ### add the vip
+    if(createVip):
+        print "adding VIP %s" % newVip
+        partitions[0]['vips'].append(newVip)
+        vipresult = api('put','/clusterPartitions/%i' % partitions[0]['id'], partitions[0])
+
     ### execute the node add
+    print "Adding node %i to cluster" % selectedNode['nodeId']
+
     result = api('post','/nexus/cluster/expand',newNodeParams)
     if 'message' in result:
         print result['message']
