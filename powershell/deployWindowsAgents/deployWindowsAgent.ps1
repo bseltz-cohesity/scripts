b1beb798eee7caf6b1dedcc96b96b46a3a20efb4
selterb <selterb01@gmail.com>
2019-02-12 09:01:06 -0500

    2019-02-12

--- powershell/deployWindowsAgents/deployWindowsAgent.ps1
+++ powershell/deployWindowsAgents/deployWindowsAgent.ps1
@@ -8,10 +8,12 @@ param (
     [Parameter(Mandatory = $True)][string]$vip, #Cohesity cluster to connect to
     [Parameter(Mandatory = $True)][string]$username, #Cohesity username
     [Parameter()][string]$domain = 'local', #Cohesity user domain name
-    [Parameter(Mandatory = $True)][string]$serverList, #Server to add as physical source
+    [Parameter()][string]$serverList, #Servers to add as physical source
+    [Parameter()][string]$server,
     [Parameter()][switch]$installAgent,
     [Parameter()][switch]$register,
     [Parameter()][switch]$registerSQL,
+    [Parameter()][switch]$sqlCluster,
     [Parameter()][string]$sqlAccount = $null
 )
 
@@ -42,16 +44,32 @@ if($sqlAccount){
     $sqlPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
 }
 
+if($sqlCluster){
+    $entityType = 2
+}else{
+    $entityType = 1
+}
+
+### get protection sources
+$sources = api get protectionSources/registrationInfo
+
 ### download agent installer to local host
-$downloadsFolder = join-path -path $([Environment]::GetFolderPath("UserProfile")) -ChildPath downloads
-$agentFile = $("cohesity-agent-windows" + (api get basicClusterInfo).clusterSoftwareVersion + ".exe")
-$filepath = join-path -path $downloadsFolder -ChildPath $agentFile
-fileDownload 'physicalAgents/download?hostType=kWindows' $filepath
-$remoteFilePath = Join-Path -Path "C:\Windows\Temp" -ChildPath $agentFile
+if ($installAgent) {
+    $downloadsFolder = join-path -path $([Environment]::GetFolderPath("UserProfile")) -ChildPath downloads
+    $agentFile = $("cohesity-agent-windows" + (api get basicClusterInfo).clusterSoftwareVersion + ".exe")
+    $filepath = join-path -path $downloadsFolder -ChildPath $agentFile
+    fileDownload 'physicalAgents/download?hostType=kWindows' $filepath
+    $remoteFilePath = Join-Path -Path "C:\Windows\Temp" -ChildPath $agentFile
+}
 
-#$cred = Get-Credential -Message "Enter credentials to access remote servers"
-
+if($serverList){
 $servers = get-content $serverList
+}elseif($server) {
+    $servers = @($server)
+}else{
+    Write-Warning "No Servers Specified"
+    exit
+}
 
 foreach ($server in $servers){
     $server = $server.ToString()
@@ -68,8 +86,10 @@ foreach ($server in $servers){
         "`tinstalling Cohesity agent..."
         $result = Invoke-Command -Computername $server -ArgumentList $remoteFilePath -ScriptBlock {
             param($remoteFilePath)
-            ([WMICLASS]"\\localhost\ROOT\CIMV2:win32_process").Create("$remoteFilePath /type=allcbt /verysilent /supressmsgboxes /norestart")
-            New-NetFirewallRule -DisplayName 'Cohesity Agent' -Profile 'Domain' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 50051
+            if (! $(Get-Service | Where-Object { $_.Name -eq 'CohesityAgent' })) {
+                ([WMICLASS]"\\localhost\ROOT\CIMV2:win32_process").Create("$remoteFilePath /type=allcbt /verysilent /supressmsgboxes /norestart")
+                New-NetFirewallRule -DisplayName 'Cohesity Agent' -Profile 'Domain' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 50051
+            }
         }
     }
 
@@ -86,7 +106,7 @@ foreach ($server in $servers){
                         'type' = 6;
                         'physicalEntity' = @{
                             'name' = $server;
-                            'type' = 1;
+                            'type' = $entityType;
                             'hostType' = 1
                         }
                     };
@@ -115,15 +135,18 @@ foreach ($server in $servers){
     }
 
     ### register server as SQL
-    if($registerSQL){
-        "`tRegistering as SQL protection source..."
-        $phys = api get protectionSources?environments=kPhysical
-        $sourceId = ($phys.nodes | Where-Object { $_.protectionSource.name -ieq $server }).protectionSource.id
-        if($sourceId){
-        $regSQL = @{"ownerEntity" = @{"id" = $sourceId}; "appEnvVec" = @(3)}
-        $result4 = api post /applicationSourceRegistration $regSQL
-        }else{
-            Write-Warning "$server is not yet registered as a protection source"
+    if ($registerSQL) {
+        if (! $($sources.rootNodes | Where-Object { $_.rootNode.name -eq $server -and $_.applications.environment -eq 'kSQL' })) {
+            "`tRegistering as SQL protection source..."
+            $phys = api get protectionSources?environments=kPhysical
+            $sourceId = ($phys.nodes | Where-Object { $_.protectionSource.name -ieq $server }).protectionSource.id
+            if ($sourceId) {
+                $regSQL = @{"ownerEntity" = @{"id" = $sourceId}; "appEnvVec" = @(3)}
+                $result4 = api post /applicationSourceRegistration $regSQL
+            }
+            else {
+                Write-Warning "$server is not yet registered as a protection source"
+            }
         }
     }
 }
