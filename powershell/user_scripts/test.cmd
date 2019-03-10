4e4cad81a3a0901e869d4578dbe3d6dc6ed59dde
selterb <selterb01@gmail.com>
2019-03-10 19:14:06 -0400

    2019-03-10

---
+++ powershell/user_scripts/test.cmd
@@ -0,0 +1,1 @@
+powershell.exe "& 'c:\program files\cohesity\user_scripts\backupNowAndCopy.ps1' -vip mycluster -username admin -jobName 'File-Based Backup' -replicateTo CohesityVE -keepReplicaFor 5"
