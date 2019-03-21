9bb28f4057e14d546ac249efbbc5585ac4091e5d
selterb <selterb01@gmail.com>
2019-03-21 07:33:03 -0400

    2019-03-21

---
+++ python/jobSeries/README.md
@@ -0,0 +1,77 @@
+# Run a Series of Disabled Jobs Using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script runs a series of disabled jobs, one at a time, leaving them disabled when fihished.
+
+## Components
+
+* jobSeries.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+## Download The Scripts
+
+The script is designed to run from the Cohesity clister. To download and install the script, SSH into the cohesity cluster and run the following commands to download the scripts:
+
+```bash
+mkdir /home/cohesity/scripts
+cd /home/cohesity/scripts
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/jobSeries/jobSeries.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/jobSeries/pyhesity.py
+chmod +x jobSeries.py
+```
+
+Edit the script and configure the settings section:
+
+```python
+### settings
+vip = 'mycluster'
+username = 'myusername'
+domain = 'mydomain.net'
+replicateTo = 'anothercluster'
+keepReplicaFor = 5
+archiveTo = 'archivetarget'
+keepArchiveFor = 5
+jobs = ['Job1', 'Job2', 'Job3']
+```
+
+Note: If you don't want to replicate, set replicateTo = None and the same can be done for arciveTo.
+
+The script will need to use a stored, encrypted password file to authenticate at runtime. To setup this password file, start an interactive python session and run the following commands:
+
+```bash
+$ python
+Python 2.7.10 (default, Oct  6 2017, 22:29:07)
+[GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
+>>>
+>>> from pyhesity import *
+>>> apiauth('mycluster','myusername','mydomain.net',updatepw=True)
+Enter your password: *****
+Confirm your password: *****
+Connected!
+>>>
+>>>
+>>> exit()
+```
+
+Note: you can use the same process to update the stored password if it ever needs to be changed.
+
+## A Note about Timezones
+
+Cohesity clusters are typically set to US/Pacific time, rgardless of their physical location. If you schedule this script to run on a Cohesity cluster, make sure to account for the difference between your time zone and the cluster's timezone. For example, if you want to run the script at 5am eastern time, then schedule it to run at 2am on the cluster.
+
+## Schedule the Script to Run Daily
+
+We can schedule the script to run using cron.
+
+```bash
+crontab -e
+```
+
+Let's say that you want the script to run at 9PM eastern. Remember to adjust to pacific time. Enter the following line in crontab:
+
+```text
+0 21 * * * /home/cohesity/scripts/jobSeries.py
+```
---
+++ python/jobSeries/jobSeries.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+"""Run a Series of Disabled Jobs"""
+
+### import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+from time import sleep
+
+### settings
+vip = 'bseltzve01'
+username = 'admin'
+domain = 'local'
+replicateTo = 'CohesityVE'
+keepReplicaFor = 5
+archiveTo = 'S3'
+keepArchiveFor = 5
+jobs = ['Job1-w2016', 'Job2-w2012c']
+
+### authenticate
+apiauth(vip, username, domain)
+
+finishedStates = ['kCanceled', 'kSuccess', 'kFailure']
+
+### open log
+f = open('log-jobSeries.txt', 'w')
+f.write('started at %s\n' % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+### configure job run task
+copyRunTargets = []
+
+if replicateTo is not None:
+    remote = [remote for remote in api('get', 'remoteClusters') if remote['name'].lower() == replicateTo.lower()]
+    if len(remote) > 0:
+        remote = remote[0]
+        copyRunTargets.append({
+            "type": "kRemote",
+            "daysToKeep": keepReplicaFor,
+            "replicationTarget": {
+                "clusterId": remote['clusterId'],
+                "clusterName": remote['name']
+            }
+        })
+    else:
+        print("Remote Cluster %s not found!" % replicateTo)
+        f.write("Remote Cluster %s not found!\n" % replicateTo)
+        f.close()
+        exit()
+
+if archiveTo is not None:
+    vault = [vault for vault in api('get', 'vaults') if vault['name'].lower() == archiveTo.lower()]
+    if len(vault) > 0:
+        vault = vault[0]
+        copyRunTargets.append({
+            "archivalTarget": {
+                "vaultId": vault['id'],
+                "vaultName": vault['name'],
+                "vaultType": "kCloud"
+            },
+            "daysToKeep": keepArchiveFor,
+            "type": "kArchival"
+        })
+    else:
+        print("Archive target %s not found!" % archiveTo)
+        f.write("Archive target %s not found!\n" % archiveTo)
+        f.close()
+        exit()
+
+runNowTask = {
+    "copyRunTargets": copyRunTargets,
+    "sourceIds": [],
+    "runType": "kRegular"
+}
+
+for jobName in jobs:
+
+    # find job
+    job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobName.lower()]
+    if not job:
+        print("Job '%s' not found" % jobName)
+        f.write("Job '%s' not found\n" % jobName)
+    else:
+
+        # enable job
+        while (api('get', 'protectionJobs/%s' % job[0]['id'])['isPaused'] is True):
+            enable = api('post', 'protectionJobState/%s' % job[0]['id'], {'pause': False})
+            sleep(2)
+
+        # run job
+        runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
+        newRunId = lastRunId = runs[0]['backupRun']['jobRunId']
+        if (runs[0]['backupRun']['status'] in finishedStates):
+            print("Running %s..." % jobName)
+            f.write("Running %s...\n" % jobName)
+            api('post', "protectionJobs/run/%s" % job[0]['id'], runNowTask)
+
+            ### wait for new job run to appear
+            while(newRunId == lastRunId):
+                sleep(1)
+                runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
+                newRunId = runs[0]['backupRun']['jobRunId']
+            print("New Job Run ID: %s" % newRunId)
+            f.write("New Job Run ID: %s\n" % newRunId)
+
+            ### wait for job run to finish
+            while(runs[0]['backupRun']['status'] not in finishedStates):
+                sleep(5)
+                runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
+        else:
+            print("%s already running" % jobName)
+            f.write("%s already running\n" % jobName)
+
+        # disable job
+        while (api('get', 'protectionJobs/%s' % job[0]['id'])['isPaused'] is False):
+            disable = api('post', 'protectionJobState/%s' % job[0]['id'], {'pause': True})
+            sleep(2)
+f.close()
