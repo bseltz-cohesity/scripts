43b57daf4d6574c56dbad7026fd9b2c52bbf7adf
selterb <selterb01@gmail.com>
2019-03-29 12:43:43 -0400

    2019-03-29

--- python/archiveNow/README.md
+++ python/archiveNow/README.md
@@ -32,6 +32,27 @@ Connected!
 archiving snapshot from 2019-03-26 14:55:18...
 ```
 
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -j, --jobname: Name of protection job
+* -r, --rundate: Date and time of protection run to archive, e.g. '2019-03-26 14:37:00'
+* -k, --keepfor: (optional) keepfor X days. Required if there is no archive policy for the job
+* -t, --newtarget: (optional) name of the external target. Required if there is no archive policy for the job
+* -f, --fromtoday: (optional) calculate -k from today instead of from snapshot date
+
+## Notes
+
+If the job has a policy applied with an archival policy element, by default the script will use the target and retention specified in the policy. The retention to be set will be calculated from the snapshot date by default, so if the retention is 10 days, but the snapshot occured 2 days ago, then the retention will be set to 8 days.
+
+Using the -k parameter overrides the retention specified in the policy, or specifies a retention when the job has no archival policy. Again, retention is adjusted based on the age of the snapshot.
+
+Using the -t parameter overrides the archival target specified in the policy, or specifies a target when the job has no archival policy.
+
+Using the -f parameter ignores the snapshot age and sets the retention to k days from today.
+
 ## The Python Helper Module - pyhesity.py
 
 The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module.
--- python/archiveNow/archiveNow.py
+++ python/archiveNow/archiveNow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Archive Now for python"""
 
-# usage: ./archiveNow.py -v mycluster -u myuser -d mydomain.net -j MyJob -r '2019-03-26 14:47:00'
+# usage: ./archiveNow.py -v mycluster -u myuser -d mydomain.net -j MyJob -r '2019-03-26 14:47:00' [ -k 5 ] [ -t S3 ] [ -f ]
 
 # import pyhesity wrapper module
 from pyhesity import *
@@ -10,11 +10,14 @@ from datetime import datetime, timedelta
 # command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)
-parser.add_argument('-u', '--username', type=str, required=True)
-parser.add_argument('-d', '--domain', type=str, default='local')
-parser.add_argument('-j', '--jobname', type=str, required=True)
-parser.add_argument('-r', '--rundate', type=str, required=True)
+parser.add_argument('-v', '--vip', type=str, required=True)       # cluster to connect to
+parser.add_argument('-u', '--username', type=str, required=True)  # username
+parser.add_argument('-d', '--domain', type=str, default='local')  # (optional) domain - defaults to local
+parser.add_argument('-j', '--jobname', type=str, required=True)   # job name
+parser.add_argument('-r', '--rundate', type=str, required=True)   # run date to archive in military format with 00 seconds
+parser.add_argument('-k', '--keepfor', type=int, default=None)    # (optional) will use policy retention if omitted
+parser.add_argument('-t', '--newtarget', type=str, default=None)  # (optional) will use policy target if omitted
+parser.add_argument('-f', '--fromtoday', action='store_true')     # (optional) keepfor x days from today instead of from snapshot date
 
 args = parser.parse_args()
 
@@ -23,6 +26,9 @@ username = args.username
 domain = args.domain
 jobname = args.jobname
 rundate = args.rundate
+keepfor = args.keepfor
+newtarget = args.newtarget
+fromtoday = args.fromtoday
 
 rundateusecs = dateToUsecs(rundate)
 
@@ -32,23 +38,46 @@ apiauth(vip, username, domain)
 # find protection job
 job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobname.lower()]
 if not job:
-    print("Job '%s' not found" % jobName)
+    print("Job '%s' not found" % jobname)
     exit()
 else:
     job = job[0]
 
+target = None
+daysToKeep = None
+
 # get archive policy
 policyId = job['policyId']
 policy = api('get', 'protectionPolicies/%s' % policyId)
-target = policy['snapshotArchivalCopyPolicies'][0]['target']
-daysToKeep = policy['snapshotArchivalCopyPolicies'][0]['daysToKeep']
+if 'snapshotArchivalCopyPolicies' in policy:
+    target = policy['snapshotArchivalCopyPolicies'][0]['target']
+    daysToKeep = policy['snapshotArchivalCopyPolicies'][0]['daysToKeep']
+
+if newtarget:
+    vault = [vault for vault in api('get', 'vaults') if vault['name'].lower() == newtarget.lower()]
+    if len(vault) > 0:
+        vault = vault[0]
+        target = {
+            "vaultId": vault['id'],
+            "vaultName": vault['name'],
+            "vaultType": "kCloud"
+        }
+    else:
+        print('No archive target named %s' % newtarget)
+        exit()
+
+if keepfor:
+    daysToKeep = keepfor
+
+if target is None or daysToKeep is None:
+    print('Job "%s" does not have an archive policy. Please specify a target and retention' % jobname)
+    exit()
 
 # find requested run
 runs = api('get', 'protectionRuns?jobId=%s' % job['id'])
 
 foundRun = False
 for run in runs:
-    existingarchive = False
 
     # zero out seconds for rundate match
     thisrundate = datetime.strptime(usecsToDate(run['copyRun'][0]['runStartTimeUsecs']), "%Y-%m-%d %H:%M:%S")
@@ -56,13 +85,13 @@ for run in runs:
 
     if rundate == thisrundatebase:
         foundRun = True
-        print('archiving snapshot from %s...' % usecsToDate(run['copyRun'][0]['runStartTimeUsecs']))
+        currentExpiry = None
         for copyRun in run['copyRun']:
 
             # resync existing archive run
             if copyRun['target']['type'] == 'kArchival':
                 target = copyRun['target']['archivalTarget']
-                existingarchive = True
+                currentExpiry = copyRun.get('expiryTimeUsecs', 0)
 
             # configure archive task
             archiveTask = {
@@ -80,14 +109,26 @@ for run in runs:
                 ]
             }
 
-            # set retention on new run
-            if existingarchive is False:
-                daysToKeep = daysToKeep - dayDiff(dateToUsecs(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), run['copyRun'][0]['runStartTimeUsecs'])
-                archiveTask['jobRuns'][0]['copyRunTargets'][0]['daysToKeep'] = int(daysToKeep)
+        # if fromtoday is not set, calculate days to keep from snapshot date
+        if fromtoday is False:
+            daysToKeep = daysToKeep - dayDiff(dateToUsecs(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), run['copyRun'][0]['runStartTimeUsecs'])
 
-            # update run
-            if(daysToKeep > 0):
-                result = api('put', 'protectionRuns', archiveTask)
+        # if there's an existing archive and keepfor is specified adjust the retention
+        if keepfor is not None and currentExpiry != 0 and currentExpiry is not None:
+            if currentExpiry != 0 and currentExpiry is not None:
+                daysToKeep = daysToKeep + (dayDiff(run['copyRun'][0]['runStartTimeUsecs'], currentExpiry))
+            archiveTask['jobRuns'][0]['copyRunTargets'][0]['daysToKeep'] = int(daysToKeep)
+
+        # if the current archive was deleted, resync it
+        if currentExpiry == 0:
+            archiveTask['jobRuns'][0]['copyRunTargets'][0]['daysToKeep'] = int(daysToKeep)
+
+        # update run
+        if((daysToKeep > 0 and currentExpiry is None) or (daysToKeep != 0 and currentExpiry is not None)):
+            print('archiving snapshot from %s...' % usecsToDate(run['copyRun'][0]['runStartTimeUsecs']))
+            result = api('put', 'protectionRuns', archiveTask)
+        else:
+            print('Not archiving because expiry time would be in the past or unchanged')
 
 # report if no run was found
 if foundRun is False:
