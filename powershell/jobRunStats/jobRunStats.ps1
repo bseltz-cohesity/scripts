89185a212a3f9149d3046575b8fbad3d93c76ba8
selterb <selterb01@gmail.com>
2019-04-01 10:46:46 -0400

    2019-04-01

--- powershell/jobRunStats/jobRunStats.ps1
+++ powershell/jobRunStats/jobRunStats.ps1
@@ -7,7 +7,9 @@
 param (
     [Parameter(Mandatory = $True)][string]$vip,
     [Parameter(Mandatory = $True)][string]$username,
-    [Parameter()][string]$domain = 'local'
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][switch]$failedOnly,
+    [Parameter()][switch]$lastDay
 )
 
 ### source the cohesity-api helper code
@@ -32,18 +34,22 @@ foreach ($job in $jobs){
     $runs = api get "protectionRuns?jobId=$($job.id)&numRuns=999999"
 
     foreach ($run in $runs){
+        $nowTime = dateToUsecs (get-date)
         $startTime = $run.copyRun[0].runStartTimeUsecs
-        $runId = $run.backupRun.jobRunId
-        $endTime = $run.backupRun.stats.endTimeUsecs
-        $duration = [math]::Round(($endTime - $startTime)/1000000,0)
-        $runType = $run.backupRun.runType.substring(1)
-        $readMBytes = [math]::Round($run.backupRun.stats.totalBytesReadFromSource / $MB, 2)
-        $writeMBytes = [math]::Round($run.backupRun.stats.totalPhysicalBackupSizeBytes / $MB, 2)
-        $logicalMBytes = [math]::Round($run.backupRun.stats.totalLogicalBackupSizeBytes / $MB, 2)
-        $status = $run.backupRun.status.substring(1)
-        $runURL = "https://$vip/protection/job/$jobId/run/$runId/$startTime/protection"
-            
-        "`t{0}`t{1}`t{2}`t{3}`t{4}`t{5}" -f (usecsToDate $startTime), $status, $runType, $duration, $readMBytes, $writeMBytes
-        "$jobName,$jobType,$status,$(usecsToDate $startTime),$runType,$duration,$logicalMBytes,$readMBytes,$writeMBytes,$runURL" | Out-File -FilePath $outfileName -Append
+        if(! $lastDay -or ($lastDay -and ($nowTime - $startTime -le 86400000000))){
+            $runId = $run.backupRun.jobRunId
+            $endTime = $run.backupRun.stats.endTimeUsecs
+            $duration = [math]::Round(($endTime - $startTime)/1000000,0)
+            $runType = $run.backupRun.runType.substring(1)
+            $readMBytes = [math]::Round($run.backupRun.stats.totalBytesReadFromSource / $MB, 2)
+            $writeMBytes = [math]::Round($run.backupRun.stats.totalPhysicalBackupSizeBytes / $MB, 2)
+            $logicalMBytes = [math]::Round($run.backupRun.stats.totalLogicalBackupSizeBytes / $MB, 2)
+            $status = $run.backupRun.status.substring(1)
+            $runURL = "https://$vip/protection/job/$jobId/run/$runId/$startTime/protection"
+            if(! $failedOnly -or ($failedOnly -and $status -ne "Success")){
+                "`t{0}`t{1}`t{2}`t{3}`t{4}`t{5}" -f (usecsToDate $startTime), $status, $runType, $duration, $readMBytes, $writeMBytes
+                "$jobName,$jobType,$status,$(usecsToDate $startTime),$runType,$duration,$logicalMBytes,$readMBytes,$writeMBytes,$runURL" | Out-File -FilePath $outfileName -Append
+            }
+        }
     }
 }
