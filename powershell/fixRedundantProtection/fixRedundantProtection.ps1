2be699672492a535f312bc7b4cf9f6aa5fea3c18
selterb <bseltzer@cohesity.com>
2019-04-11 09:31:46 -0400

    2019-04-11

---
+++ powershell/fixRedundantProtection/fixRedundantProtection.ps1
@@ -0,0 +1,54 @@
+# usage: ./fixRedundantProtection.ps1 -vip myCluster -username myUser -jobName 'My Job' -fix
+
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter(Mandatory = $True)][string]$jobName,
+    [Parameter()][switch]$fix
+)
+
+# source the cohesity-api helper code
+. ./cohesity-api
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+# get protectionJobs
+$jobs = api get protectionJobs?isDeleted=false
+
+# get specified job
+$myJob = $jobs | Where-Object { $_.name -eq $jobName }
+if(!$myJob){
+    Write-Warning "Job $jobName not found!"
+    exit
+}
+
+$redundanciesFound = $false
+foreach ($job in @($jobs | Where-Object { $_.id -ne $myJob.id})){
+    # find resundant source IDs
+    $redundantSourceIds = $job.sourceIds | Where-Object { $_ -in $myJob.sourceIds }
+    if($redundantSourceIds){
+        $redundanciesFound = $True
+        "Found The following objects also protected in job: $($job.name):"
+        # Display name of redundant source
+        foreach ($sourceId in $redundantSourceIds){
+            $source = api get "protectionSources/objects/$sourceId"
+            "  $($source.name)"
+        }
+        if($fix){
+            "  fixing..."
+            # Remove redundant sources from the other job
+            $job.sourceIds = @($job.sourceIds | Where-Object { $_ -notin $myJob.sourceIds })
+            $null = api put "protectionJobs/$($job.id)" $job
+        }
+    }
+}
+
+if(!$redundanciesFound){
+    "No redundancies found"
+}
+
+
