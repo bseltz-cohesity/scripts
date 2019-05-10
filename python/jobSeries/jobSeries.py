2d9f3f442a93cea22ae15dd1e4d10996a54d8de7
selterb <bseltzer@cohesity.com>
2019-05-10 05:45:24 -0400

    2019-05-10

--- python/jobSeries/jobSeries.py
+++ python/jobSeries/jobSeries.py
@@ -8,13 +8,14 @@ from time import sleep
 
 ### settings
 vip = 'mycluster'
-username = 'myusername'
+username = 'myuser'
 domain = 'mydomain.net'
-replicateTo = 'anothercluster'
+keepLocalFor = 5
+replicateTo = None
 keepReplicaFor = 5
-archiveTo = 'archivetarget'
+archiveTo = None
 keepArchiveFor = 5
-jobs = ['Job1', 'Job2', 'Job3']
+jobs = ['Exchange1', 'Exchange2', 'Exchange3']
 
 ### authenticate
 apiauth(vip, username, domain)
@@ -26,7 +27,12 @@ f = open('log-jobSeries.txt', 'w')
 f.write('started at %s\n' % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 ### configure job run task
-copyRunTargets = []
+copyRunTargets = [
+    {
+        "type": "kLocal",
+        "daysToKeep": keepLocalFor
+    }
+]
 
 if replicateTo is not None:
     remote = [remote for remote in api('get', 'remoteClusters') if remote['name'].lower() == replicateTo.lower()]
@@ -71,6 +77,36 @@ runNowTask = {
     "runType": "kRegular"
 }
 
+# wait for any of the jobs are currently running
+for jobName in jobs:
+
+    print("checking if %s is already running..." % jobName)
+
+    # find job
+    job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobName.lower()]
+    if not job:
+        print("Job '%s' not found" % jobName)
+        f.write("Job '%s' not found\n" % jobName)
+    else:
+        runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
+        newRunId = lastRunId = runs[0]['backupRun']['jobRunId']
+        if (runs[0]['backupRun']['status'] not in finishedStates):
+            print("%s already running" % jobName)
+            f.write("%s already running\n" % jobName)
+            print("Existing Job Run ID: %s" % newRunId)
+            f.write("Existing Job Run ID: %s\n" % newRunId)
+
+            # wait for job run to finish
+            while(runs[0]['backupRun']['status'] not in finishedStates):
+                sleep(5)
+                runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
+
+            # disable job
+            while (api('get', 'protectionJobs/%s' % job[0]['id'])['isPaused'] is False):
+                disable = api('post', 'protectionJobState/%s' % job[0]['id'], {'pause': True})
+                sleep(3)
+    del job  # this is just for PEP8 compliance
+
 for jobName in jobs:
 
     # find job
@@ -83,34 +119,38 @@ for jobName in jobs:
         # enable job
         while (api('get', 'protectionJobs/%s' % job[0]['id'])['isPaused'] is True):
             enable = api('post', 'protectionJobState/%s' % job[0]['id'], {'pause': False})
-            sleep(2)
+            sleep(3)
 
         # run job
         runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
         newRunId = lastRunId = runs[0]['backupRun']['jobRunId']
+
         if (runs[0]['backupRun']['status'] in finishedStates):
             print("Running %s..." % jobName)
             f.write("Running %s...\n" % jobName)
             api('post', "protectionJobs/run/%s" % job[0]['id'], runNowTask)
 
-            ### wait for new job run to appear
+            # wait for new job run to appear
             while(newRunId == lastRunId):
-                sleep(1)
+                sleep(3)
                 runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
                 newRunId = runs[0]['backupRun']['jobRunId']
             print("New Job Run ID: %s" % newRunId)
             f.write("New Job Run ID: %s\n" % newRunId)
 
-            ### wait for job run to finish
-            while(runs[0]['backupRun']['status'] not in finishedStates):
-                sleep(5)
-                runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
         else:
             print("%s already running" % jobName)
             f.write("%s already running\n" % jobName)
+            print("Existing Job Run ID: %s" % newRunId)
+            f.write("Existing Job Run ID: %s\n" % newRunId)
+
+        # wait for job run to finish
+        while(runs[0]['backupRun']['status'] not in finishedStates):
+            sleep(5)
+            runs = api('get', 'protectionRuns?jobId=%s' % job[0]['id'])
 
         # disable job
         while (api('get', 'protectionJobs/%s' % job[0]['id'])['isPaused'] is False):
             disable = api('post', 'protectionJobState/%s' % job[0]['id'], {'pause': True})
-            sleep(2)
+            sleep(3)
 f.close()
