618c67dfe305c5a838b3d12dc2b13290d90c2dad
selterb <bseltzer@cohesity.com>
2019-05-22 05:12:50 -0400

    2019-05-22

---
+++ powershell/replicateOldSnapshots/README.md
@@ -0,0 +1,61 @@
+# Replicate Old Snapshots using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script replicates existing local snapshots to a replication target. This is useful if you have recently created an replication target and want to programatically replicate existing local snapshots.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Begin download commands
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell/replicateOldSnapshots/replicateOldSnapshots.ps1).content | Out-File replicateOldSnapshots.ps1; (Get-Content replicateOldSnapshots.ps1) | Set-Content replicateOldSnapshots.ps1
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell/replicateOldSnapshots/cohesity-api.ps1).content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End download commands
+```
+
+## Components
+
+* replicateOldSnapshots.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together, then we can run the script.
+
+First, run the script WITHOUT the -replicate switch to see what would be replicated.
+
+```powershell
+./replicateOldSnapshots.ps1 -vip mycluster -username admin -replicteTo CohesityVE -olderThan 1 -IfExpiringAfter 3
+```
+
+```text
+Connected!
+searching for old snapshots...
+05/19/2019 23:20:00  VM Backup  (expiring in 3 days. skipping...)
+05/20/2019 05:34:46  VM Backup  (expiring in 3 days. skipping...)
+05/20/2019 23:20:00  VM Backup  (would replicate for 4 days)
+05/19/2019 23:40:01  Infrastructure  (expiring in 3 days. skipping...)
+05/20/2019 23:40:01  Infrastructure  (would replicate for 4 days)
+05/20/2019 00:00:01  Oracle Adapter  (expiring in 3 days. skipping...)
+05/21/2019 00:00:01  Oracle Adapter  (would replicate for 4 days)
+05/20/2019 00:20:00  SQL Backup  (expiring in 3 days. skipping...)
+05/21/2019 00:20:01  SQL Backup  (would replicate for 4 days)
+```
+
+Then, if you're happy with the list of snapshots that will be replicated, run the script again and include the -replicate switch. This will execute the replication tasks
+
+```powershell
+./archiveOldSnapshots.ps1 -vip mycluster -username admin -replicteTo CohesityVE -olderThan 1 -IfExpiringAfter 3 -replicate
+```
+
+## Parameters
+
+* -vip: Cohesity cluster to connect to
+* -username: Cohesity username
+* -domain: AD domain name (defaults to local)
+* -jobName: (optional) replicate only specified job (otherwise replicate all jobs)
+* -replicateTo: (optional) name of remote cluster to replicate to
+* -keepFor: days to keep replica (default is same as local) existing age is subtracted
+* -olderThan: (optional) only replicate if older than X days
+* -IfExpiringAfter: (optional) only replicate if there are X or more days left before expiration
+* -replicate: actually replicate (otherwise only a test run)
---
+++ powershell/replicateOldSnapshots/replicateOldSnapshots.ps1
@@ -0,0 +1,112 @@
+### usage: ./replicateOldSnapshots.ps1 -vip mycluster -username admin [ -domain local ] -replicateTo CohesityVE -olderThan 365 [ -IfExpiringAfter 30 ] [ -keepFor 365 ] [ -archive ]
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, # the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, # username (local or AD)
+    [Parameter()][string]$domain = 'local', # local or AD domain
+    [Parameter()][string]$jobName = $null, # name of job to replicate
+    [Parameter(Mandatory = $True)][string]$replicateTo, # name of replication target
+    [Parameter()][string]$olderThan = 0, # archive snapshots older than x days
+    [Parameter()][string]$IfExpiringAfter = 0, # do not archve if the snapshot is going to expire within x days
+    [Parameter()][string]$keepFor = 0, # set archive retention to x days from original backup date
+    [Parameter()][switch]$replicate # actually replicate (otherwise test run)
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+### cluster Id
+$clusterId = (api get cluster).id
+
+### get replication target info
+$remote = api get remoteClusters | Where-Object {$_.name -eq $replicateTo}
+if (!$remote) {
+    Write-Warning "Replication target $replicateTo not found"
+    exit
+}
+
+### get job
+if($jobName){
+    $myjob = api get protectionJobs | Where-Object {$_.name -eq $jobName}
+    if(!$myjob){
+        Write-Warning "Job $jobName not found!" -ForegroundColor Yellow
+        exit
+    }
+}else{
+    $myjob = $null
+}
+
+
+### olderThan days in usecs
+$olderThanUsecs = timeAgo $olderThan days
+
+### find protectionRuns with old local snapshots that are not archived yet and sort oldest to newest
+"searching for old snapshots..."
+foreach ($job in ((api get protectionJobs) | Where-Object{ $_.policyId.split(':')[0] -eq $clusterId })) {
+    if (!$myjob -or ($myjob.name -eq $job.name)){
+        $runs = (api get protectionRuns?jobId=$($job.id)`&numRuns=999999`&runTypes=kRegular`&excludeTasks=true`&excludeNonRestoreableRuns=true) | `
+            Where-Object { $_.backupRun.snapshotsDeleted -eq $false } | `
+            Where-Object { $_.copyRun[0].runStartTimeUsecs -le $olderThanUsecs } | `
+            Where-Object { !('kRemote' -in $_.copyRun.target.type) -or ($_.copyRun | Where-Object { $_.target.type -eq 'kRemote' -and $_.status -in @('kCanceled','kFailed') }) } | `
+            Sort-Object -Property @{Expression = { $_.copyRun[0].runStartTimeUsecs }; Ascending = $True }
+
+        foreach ($run in $runs) {
+
+            $runDate = usecsToDate $run.copyRun[0].runStartTimeUsecs
+            $thisJobName = $run.jobName
+
+            ### calculate daysToKeep
+            $startTimeUsecs = $run.copyRun[0].runStartTimeUsecs
+            if($keepFor -gt 0){
+                $expireTimeUsecs = $startTimeUsecs + ([int]$keepFor * 86400000000)
+            }else{
+                $expireTimeUsecs = $run.copyRun[0].expiryTimeUsecs
+            }
+            $now = dateToUsecs $(get-date)
+            $daysToKeep = [math]::Round(($expireTimeUsecs - $now) / 86400000000) 
+
+            ### create replication task definition
+            $replicationTask = @{
+                'jobRuns' = @(
+                    @{
+                        'copyRunTargets'    = @(
+                            @{
+                                "replicationTarget" = @{
+                                    "clusterId" = $remote.clusterId;
+                                    "clusterName" = $remote.name
+                                };
+                                'daysToKeep'     = [int] $daysToKeep;
+                                'type'           = 'kRemote'
+                            }
+                        );
+                        'runStartTimeUsecs' = $run.copyRun[0].runStartTimeUsecs;
+                        'jobUid'            = $run.jobUid
+                    }
+                )
+            }
+
+            ### If the Local Snapshot is not expiring soon...
+            if ($daysToKeep -gt $IfExpiringAfter) {
+                if ($replicate) {
+                    write-host "Replicating $runDate  $thisJobName for $daysToKeep days" -ForegroundColor Green
+                    ### execute replication task if arcvhive swaitch is set
+                    $null = api put protectionRuns $replicationTask
+                }
+                else {
+                    ### just display what we would do if archive switch is not set
+                    write-host "$runDate  $thisJobName  (would replicate for $daysToKeep days)" -ForegroundColor Green
+                }
+            }
+            ### Otherwise tell us that we're not archiving since the snapshot is expiring soon
+            else {
+                write-host "$runDate  $thisJobName  (expiring in $daysToKeep days. skipping...)" -ForegroundColor Gray
+            }
+        }
+    }
+}
+
