99e8b209a7e3f4f8058ce89d1e60170a597f7671
selterb <bseltzer@cohesity.com>
2019-07-03 09:17:29 -0400

    2019-07-04

--- python/jobRunReport/jobRunReport.py
+++ python/jobRunReport/jobRunReport.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Job Run Report"""
 
-### usage: ./jobRunReport.py -v mycluster -u admin [ -d domain ]
+### usage: ./jobRunReport.py -v mycluster -u admin [-d domain]
 
 ### import pyhesity wrapper module
 from pyhesity import *
@@ -9,12 +9,12 @@ from pyhesity import *
 ### command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v','--vip', type=str, required=True)
-parser.add_argument('-u','--username', type=str, required=True)
-parser.add_argument('-d','--domain',type=str,default='local')
+parser.add_argument('-v', '--vip', type=str, required=True)
+parser.add_argument('-u', '--username', type=str, required=True)
+parser.add_argument('-d', '--domain', type=str, default='local')
 
 args = parser.parse_args()
-    
+
 vip = args.vip
 username = args.username
 domain = args.domain
@@ -23,17 +23,16 @@ domain = args.domain
 apiauth(vip, username, domain)
 
 ### find protectionRuns for last 24 hours
-runs = api('get','protectionRuns?startTimeUsecs=%s' % timeAgo('24','hours'))
+runs = api('get', 'protectionRuns?startTimeUsecs=%s' % timeAgo('24', 'hours'))
 
 seen = {}
-print "{:>20} {:>10}  {:25}".format('JobName','Status ','StartTime')
-print "{:>20} {:>10}  {:25}".format('-------','--------','---------')
+print("{:>20} {:>10}  {:25}".format('JobName', 'Status ', 'StartTime'))
+print("{:>20} {:>10}  {:25}".format('-------', '--------', '---------'))
 
 for run in runs:
     jobName = run['jobName']
     status = run['backupRun']['status']
     startTime = usecsToDate(run['backupRun']['stats']['startTimeUsecs'])
     if jobName not in seen:
-        seen[jobName]=True
-        print "{:>20} {:>10}  {:25}".format(jobName,status,startTime)
-
+        seen[jobName] = True
+        print("{:>20} {:>10}  {:25}".format(jobName, status, startTime))
