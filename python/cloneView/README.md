841f83fbbe435181252f5cd862f9aca8eba9e1ce
selterb <bseltzer@cohesity.com>
2019-08-05 14:35:35 -0400

    2019-08-05

--- python/cloneView/README.md
+++ python/cloneView/README.md
@@ -4,6 +4,16 @@ Warning: this code is provided on a best effort basis and is not in any way offi
 
 This python script clones a view.
 
+## Downloading the Files
+
+Go to the folder where you want to download the files, then run the following commands:
+
+```bash
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/cloneView.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/pyhesity.py
+chmod +x cloneView.py
+```
+
 ## Components
 
 * cloneView.py: the main python script
@@ -12,10 +22,19 @@ This python script clones a view.
 Place both files in a folder together and run the main script like so:
 
 ```bash
-./cloneView.py -s mycluster -u admin [ -d domain ] -v myview -n newview
+./cloneView.py -s mycluster -u admin [ -d domain ] -v myview -n newview [ -w ]
 Connected!
 Cloning View myview as newview...
 ```
+
+## Parameters
+
+* -s, --server: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -v, --view: name of source view to be cloned
+* -n, --newname: name of clone view to create
+* -w, --wait: (optional) wait for completion and report exit status
 
 ## The Python Helper Module - pyhesity.py
 
@@ -31,14 +50,4 @@ or
 
 ```bash
 sudo easy_install requests
-```
-
-### Downloading the Files
-
-Go to the folder where you want to download the files, then run the following commands:
-
-```bash
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/cloneView.py
-curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneView/pyhesity.py
-chmod +x cloneView.py
 ```
--- python/cloneView/cloneView.py
+++ python/cloneView/cloneView.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 """Clone a Cohesity View Using python"""
 
-### usage: ./cloneView.py -s mycluster -u admin -d domain -v myview -n newview
+### usage: ./cloneView.py -s mycluster -u admin -d domain -v myview -n newview -w
 
 ### import pyhesity wrapper module
 from pyhesity import *
+from time import sleep
 
 ### command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--server', type=str, required=True)
-parser.add_argument('-u', '--username', type=str, required=True)
-parser.add_argument('-d', '--domain', type=str, default='local')
-parser.add_argument('-v', '--view', type=str, required=True)
-parser.add_argument('-n', '--name', type=str, required=True)
+parser.add_argument('-s', '--server', type=str, required=True)  # Cohesity cluster name or IP
+parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
+parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
+parser.add_argument('-v', '--view', type=str, required=True)  # name of source view to clone
+parser.add_argument('-n', '--newname', type=str, required=True)  # name of target view to create
+parser.add_argument('-w', '--wait', action='store_true')  # wait for completion
 
 args = parser.parse_args()
 
@@ -21,7 +23,8 @@ vip = args.server
 username = args.username
 domain = args.domain
 viewName = args.view
-newName = args.name
+newName = args.newname
+wait = args.wait
 
 ### authenticate
 apiauth(vip, username, domain)
@@ -61,14 +64,28 @@ cloneTask = {
         "sourceViewName": view['name'],
         "cloneViewName": newName,
         "viewBoxId": view['viewBoxId'],
-        "viewId": viewResult['vmDocument']['objectId']['entity']['id'],
-        "qos": view['qos'],
-        "description": view['description'],
-        "allowMountOnWindows": view['allowMountOnWindows'],
-        "storagePolicyOverride": view['storagePolicyOverride']
+        "viewId": viewResult['vmDocument']['objectId']['entity']['id']
     }
 }
 
 ### execute the clone task
 response = api('post', '/clone', cloneTask)
+
+if 'errorCode' in response:
+    exit(1)
+
 print("Cloning View %s as %s..." % (viewName, newName))
+taskId = response['restoreTask']['performRestoreTaskState']['base']['taskId']
+status = api('get', '/restoretasks/%s' % taskId)
+
+if wait is True:
+    finishedStates = ['kCanceled', 'kSuccess', 'kFailure']
+    while(status[0]['restoreTask']['performRestoreTaskState']['base']['publicStatus'] not in finishedStates):
+        sleep(1)
+        status = api('get', '/restoretasks/%s' % taskId)
+    if(status[0]['restoreTask']['performRestoreTaskState']['base']['publicStatus'] == 'kSuccess'):
+        print('Cloned View Successfully')
+        exit(0)
+    else:
+        print('Clone View ended with state: %s' % status[0]['restoreTask']['performRestoreTaskState']['base']['publicStatus'])
+        exit(1)
