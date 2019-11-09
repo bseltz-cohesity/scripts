4e66d4e2db36fbb266c5f1d7504d183647d92bd6
selterb <bseltzer@cohesity.com>
2019-11-09 04:03:11 -0500

    2019-11-09

---
+++ python/createSMBView/README.md
@@ -0,0 +1,48 @@
+# Create an SMB View using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script creates a new SMB View on Cohesity
+
+## Download the script
+
+Run these commands to download the scripts into your current directory
+
+```bash
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/createSMBView/createSMBView.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/createSMBView/pyhesity.py
+chmod +x createSMBView.py
+```
+
+## Components
+
+* createSMBView.py: the main powershell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+#example
+./createSMBView.py -v mycluster \
+                   -u myusername \
+                   -d mydomain.net \
+                   -n newview1 \
+                   -w mydomain.net\server1 \
+                   -f mydomain.net\admingroup1
+                   -f mydomain.net\admingroup2 \
+                   -r mydomain.net\auditors \
+                   -q 'TestAndDev High' \
+                   -s mystoragedomain
+#end example
+```
+
+## Parameters
+
+* -v, --vip: Cohesity cluster to connect to
+* -u, --username: Cohesity username
+* -d, --domain: (optional) Active Directory domain (defaults to 'local')
+* -n, --viewname: name of new view to create
+* -w, --readwrite: comma separated list of principals to grant read/write access
+* -f, --fullcontrol: comma separated list of principals to grant full control
+* -q, --qospolicy: defaults to 'Backup Target Low' or choose 'Backup Target High', 'TestAndDev High' or 'TestAndDev Low'
+* -s, --storageDomain: name of storage domain to place view data. Defaults to DefaultStorageDomain
---
+++ python/createSMBView/createSMBView.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+"""Create a Cohesity SMB View Using python"""
+
+# usage: ./createSMBView.py -v mycluster \
+#                           -u myusername \
+#                           -d mydomain.net \
+#                           -n newview1 \
+#                           -w mydomain.net\server1 \
+#                           -f mydomain.net\admingroup1
+#                           -f mydomain.net\admingroup2 \
+#                           -r mydomain.net\auditors \
+#                           -q 'TestAndDev High' \
+#                           -s mystoragedomain
+
+# import pyhesity wrapper module
+from pyhesity import *
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)  # Cohesity cluster name or IP
+parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
+parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
+parser.add_argument('-n', '--viewname', type=str, required=True)  # name view to create
+parser.add_argument('-s', '--storagedomain', type=str, default='DefaultStorageDomain')  # name of storage domain to use
+parser.add_argument('-f', '--fullcontrol', action='append', default=[])  # user/group to grant fullControl
+parser.add_argument('-w', '--readwrite', action='append', default=[])  # user/group to grant readWrite
+parser.add_argument('-r', '--readonly', action='append', default=[])  # user/group to grant readOnly
+parser.add_argument('-q', '--qospolicy', type=str, choices=['Backup Target Low', 'Backup Target High', 'TestAndDev High', 'TestAndDev Low'], default='Backup Target Low')  # qos policy
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+viewName = args.viewname
+storageDomain = args.storagedomain
+fullControl = args.fullcontrol
+readWrite = args.readwrite
+reeadOnly = args.readonly
+qosPolicy = args.qospolicy
+
+# authenticate
+apiauth(vip, username, domain)
+
+# find storage domain
+sd = [sd for sd in api('get', 'viewBoxes') if sd['name'].lower() == storageDomain.lower()]
+
+if len(sd) != 1:
+    print("Storage domain %s not found!" % storageDomain)
+    exit()
+
+sdid = sd[0]['id']
+
+# new view parameters
+newView = {
+    "enableSmbAccessBasedEnumeration": True,
+    "enableSmbViewDiscovery": True,
+    "fileDataLock": {
+        "lockingProtocol": "kSetReadOnly"
+    },
+    "fileExtensionFilter": {
+        "isEnabled": False,
+        "mode": "kBlacklist",
+        "fileExtensionsList": []
+    },
+    "securityMode": "kNativeMode",
+    "smbPermissionsInfo": {
+        "ownerSid": "S-1-5-32-544",
+        "permissions": []
+    },
+    "protocolAccess": "kSMBOnly",
+    "subnetWhitelist": [],
+    "qos": {
+        "principalName": qosPolicy
+    },
+    "viewBoxId": sdid,
+    "caseInsensitiveNamesEnabled": True,
+    "storagePolicyOverride": {
+        "disableInlineDedupAndCompression": False
+    },
+    "name": viewName
+}
+
+
+# function to add permissions
+def addPermission(user, perms):
+    (domain, domainuser) = user.split('\\')
+    principal = [principal for principal in api('get', 'activeDirectory/principals?domain=%s&includeComputers=true&search=%s' % (domain, domainuser)) if principal['fullName'].lower() == domainuser.lower()]
+    if len(principal) == 1:
+        permission = {
+            "sid": principal[0]['sid'],
+            "type": "kAllow",
+            "mode": "kFolderSubFoldersAndFiles",
+            "access": perms
+        }
+        newView['smbPermissionsInfo']['permissions'].append(permission)
+    else:
+        print("User %s not found" % user)
+        exit(1)
+
+
+for user in reeadOnly:
+    addPermission(user, 'kReadOnly')
+
+for user in readWrite:
+    addPermission(user, 'kReadWrite')
+
+for user in fullControl:
+    addPermission(user, 'kFullControl')
+
+# create the view
+print("Creating view %s..." % viewName)
+result = api('post', 'views', newView)
