9f63d1df3d6b5ae1221aca4be05e384f34a93d33
selterb <bseltzer@cohesity.com>
2019-11-09 05:23:26 -0500

    2019-11-09

---
+++ powershell/recoverNasList/README.md
@@ -0,0 +1,42 @@
+# Recover a list of NAS Shares using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script recovers a list of protected NAS shares as Cohesity Views.
+
+```powershell
+# Download Commands
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell/recoverNasList/recoverNasList.ps1).content | Out-File recoverNasList.ps1; (Get-Content recoverNasList.ps1) | Set-Content recoverNasList.ps1
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell/cohesity-api/cohesity-api.ps1).content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* recoverNasList.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together. Create a text file for input, with the list of NAS UNC paths to recover, like so:
+
+```text
+\\mynas\share1
+\\mynas\share1
+\\mynas\share3
+```
+
+Then run the script like so:
+
+```powershell
+./recoverNasList.ps1 -vip mycluster -username admin -nasList .\nasList.txt
+```
+
+## Parameters
+
+* -vip: Cohesity cluster to connect to
+* -username: Cohesity logon username
+* -domain: (optional) Cohesity logon domain (defaults to local)
+* -nasList: (optional) defaults to .\nasList.txt
+
+## Notes
+
+This example is for recovering SMB shares as SMB views. A number of settings are hard-coded into this example such as qos policy, SMB browsablility and access based enumeration.
---
+++ powershell/recoverNasList/recoverNasList.ps1
@@ -0,0 +1,78 @@
+# usage: ./recoverNasList.ps1 -vip mycluster -username admin -nasList .\nasList.txt
+
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter()][string]$nasList = './naslist.txt'
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+# get input file
+$nasListFile = Get-Content $nasList
+
+foreach($shareName in $nasListFile){
+
+    # find nas share to recover
+    $shares = api get restore/objects?search=$shareName
+    $exactShares = $shares | Where-Object {$_.objectSnapshotInfo.snapshottedSource.name -ieq $shareName}
+    if(! $exactShares){
+        write-host "Can't find $shareName - skipping..." -ForegroundColor Yellow
+    }else{
+
+        $newViewName = $shareName.replace('\\','').replace('\','_')
+
+        # select latest snapshot to recover
+        $latestsnapshot = ($exactShares | sort-object -property @{Expression={$_.objectSnapshotInfo.versions[0].snapshotTimestampUsecs}; Ascending = $False})[0]
+
+        # new view parameters
+        $nasRecovery = @{
+            "name" = "Recover-$shareName";
+            "objects" = @(
+                @{
+                    "jobId" = $latestsnapshot.objectSnapshotInfo[0].jobId;
+                    "jobUid" = $latestsnapshot.objectSnapshotInfo[0].jobUid;
+                    "jobRunId" = $latestsnapshot.objectSnapshotInfo[0].versions[0].jobRunId;
+                    "startedTimeUsecs" = $latestsnapshot.objectSnapshotInfo[0].versions[0].startedTimeUsecs;
+                    "protectionSourceId" = $latestsnapshot.objectSnapshotInfo[0].snapshottedSource.id
+                }
+            );
+            "type" = "kMountFileVolume";
+            "viewName" = $newViewName;
+            "restoreViewParameters" = @{
+                "qos" = @{
+                    "principalName" = "TestAndDev High"
+                }
+            }
+        }
+
+        "Recovering $shareName as view $newViewName"
+
+        # perform the recovery
+        $result = api post restore/recover $nasRecovery
+
+        if($result){
+
+            # set post recovery view settings
+            do {
+                $newView = (api get views).views | Where-Object { $_.name -eq $newViewName }
+                sleep 1
+            } until ($newView)
+            
+            $newView | setApiProperty -name enableSmbViewDiscovery -value $True
+            $newView | setApiProperty -name enableSmbAccessBasedEnumeration -value $True
+            $newView | setApiProperty -name protocolAccess -value 'kSMBOnly'
+            $newView.qos = @{
+                "principalName" = 'TestAndDev High';
+            }
+            $null = api put views $newView
+        }
+    }
+}
