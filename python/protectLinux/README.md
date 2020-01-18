82b98f22b0a024d28e364a20e5dc84b6a830fbc2
selterb <bseltzer@cohesity.com>
2020-01-18 07:17:52 -0500

    2020-01-18

---
+++ python/protectLinux/README.md
@@ -0,0 +1,60 @@
+# Add Physical Linux Servers to File-based Protection Job Using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script adds physical linux servers to a file-based protection job.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/protectLinux/protectLinux.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x protectLinux.py
+# end download commands
+```
+
+## Components
+
+* protectLinux.py: the main powershell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./protectLinux.py -v mycluster \
+                  -u myuser \
+                  -d mydomain.net \
+                  -j 'My Backup Job' \
+                  -s myserver.mydomain.net \
+                  -l serverlist.txt \
+                  -i /var \
+                  -i /home \
+                  -e /var/log \
+                  -e /home/oracle \
+                  -e *.dbf \
+                  -x excludes.txt
+```
+
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -s, --servername: (optional) name of server to add to the job (use multiple times for multiple)
+* -l, --serverlist: (optional) list of server names in a text file
+* -j, --jobname: name of the job to add the server to
+* -i, --include: file path to include (use multiple times for multiple paths)
+* -x, --exclude: (optional) file path to exclude (use multiple times for multiple paths)
+* -f, --excludefile: (optional) a text file full of exclude file paths
+* -m, --skipnestedmountpoints: (optional) if omitted, nested mount paths are not skipped
+
+## Notes
+
+You can specify the names of servers to add on the command line (-s 'server1.mydomain.net' -s 'server2.mydomain.net), or you can point to a text file containing server names (-l serverlist.txt) or both.
+
+**Warning**: If you specify a server that already exists in the job, it's includes and excludes will be overwritten (existing includes and excludes will be lost). Existing servers that are not specified will be left as is.
+
+You can specify exclusions on the command line (-e /var/log -e /home/oracle), or you can point to a text file containing exclusions (-x excludes.txt) or both.
---
+++ python/protectLinux/protectLinux.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+"""Add Physical Linux Servers to File-based Protection Job Using Python"""
+
+### usage: ./protectLinux.py -v mycluster \
+#                            -u myuser \
+#                            -d mydomain.net \
+#                            -j 'My Backup Job' \
+#                            -s myserver1.mydomain.net \
+#                            -s myserver2.mydomain.net \
+#                            -l serverlist.txt \
+#                            -i /var \
+#                            -i /home \
+#                            -e /var/log \
+#                            -e /home/oracle \
+#                            -e *.dbf \
+#                            -f excludes.txt
+
+### import pyhesity wrapper module
+from pyhesity import *
+
+### command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)
+parser.add_argument('-u', '--username', type=str, required=True)
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-s', '--servername', action='append', type=str)
+parser.add_argument('-l', '--serverlist', type=str)
+parser.add_argument('-j', '--jobname', type=str, required=True)
+parser.add_argument('-i', '--include', action='append', type=str, required=True)
+parser.add_argument('-e', '--exclude', action='append', type=str)
+parser.add_argument('-x', '--excludefile', type=str)
+parser.add_argument('-m', '--skipnestedmountpoints', action='store_true')
+
+args = parser.parse_args()
+
+vip = args.vip                  # cluster name/ip
+username = args.username        # username to connect to cluster
+domain = args.domain            # domain of username (e.g. local, or AD domain)
+servernames = args.servername   # name of server to protect
+serverlist = args.serverlist    # file with server names
+jobname = args.jobname          # name of protection job to add server to
+includes = args.include         # include path
+excludes = args.exclude         # exclude path
+excludefile = args.excludefile  # file with exclude paths
+skipmountpoints = args.skipnestedmountpoints  # skip nested mount points
+
+if servernames is None:
+    servernames = []
+
+# read server file
+if serverlist is not None:
+    f = open(serverlist, 'r')
+    servernames += [s.strip() for s in f.readlines() if s.strip() != '']
+    f.close()
+
+# read exclude file
+if excludefile is not None:
+    f = open(excludefile, 'r')
+    excludes += [e.strip() for e in f.readlines() if e.strip() != '']
+    f.close()
+
+# authenticate to Cohesity
+apiauth(vip, username, domain)
+
+# get job info
+job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobname.lower()]
+if not job:
+    print("Job '%s' not found" % jobname)
+    exit(1)
+
+job = job[0]
+
+# get registered physical servers
+physicalServersRoot = api('get', 'protectionSources/rootNodes?allUnderHierarchy=false&environments=kPhysicalFiles&environments=kPhysical&environments=kPhysical')
+physicalServersRootId = physicalServersRoot[0]['protectionSource']['id']
+physicalServers = api('get', 'protectionSources?allUnderHierarchy=false&id=%s&includeEntityPermissionInfo=true' % physicalServersRootId)[0]['nodes']
+
+if len(servernames) == 0:
+    print('no servers specified')
+    exit()
+
+for servername in servernames:
+    # find server
+    physicalServer = [s for s in physicalServers if s['protectionSource']['name'].lower() == servername.lower() and s['protectionSource']['physicalProtectionSource']['hostType'] == 'kLinux']
+    if not physicalServer:
+        print ("******** %s is not a registered Linux server ********" % servername)
+    else:
+        physicalServer = physicalServer[0]
+        # get sourceSpecialParameters
+        if physicalServer['protectionSource']['id'] in job['sourceIds']:
+            print('updating %s in job %s...' % (servername, jobname))
+            sourceParameters = []
+            for sp in job['sourceSpecialParameters']:
+                if sp['sourceId'] != physicalServer['protectionSource']['id']:
+                    sourceParameters.append(sp)
+        else:
+            print('  adding %s to job %s...' % (servername, jobname))
+            job['sourceIds'].append(physicalServer['protectionSource']['id'])
+            sourceParameters = job['sourceSpecialParameters']
+
+        # create new parameter for this server
+        newParameter = {
+            "physicalSpecialParameters": {
+                "filePaths": []
+            },
+            "sourceId": physicalServer['protectionSource']['id']
+        }
+
+        # add includes to parameter
+        for include in includes:
+            filePath = {
+                "backupFilePath": include,
+                "excludedFilePaths": [],
+                "skipNestedVolumes": skipmountpoints
+            }
+            newParameter['physicalSpecialParameters']['filePaths'].append(filePath)
+
+        # add excludes to parameter
+        for exclude in excludes:
+            thisParent = ''
+            for include in includes:
+                if include in exclude and '/' in exclude:
+                    if len(include) > len(thisParent):
+                        thisParent = include
+            for filePath in newParameter['physicalSpecialParameters']['filePaths']:
+                if thisParent == '' or filePath['backupFilePath'] == thisParent:
+                    filePath['excludedFilePaths'].append(exclude)
+
+        # include new parameter
+        sourceParameters.append(newParameter)
+        job['sourceSpecialParameters'] = sourceParameters
+
+# update job
+result = api('put', 'protectionJobs/%s' % job['id'], job)
