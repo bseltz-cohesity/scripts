24ba03eb3505d79f47e317c9d766adcb2ecbef6c
selterb <bseltzer@cohesity.com>
2020-01-30 05:14:00 -0500

    2020-01-30

---
+++ powershell/sslFix/sslFix.ps1
@@ -0,0 +1,28 @@
+# please paste these commands into your PowerShell session to solve SSL-related problems when using download commands
+
+#C# class to create callback
+$code = @"
+public class SSLHandler
+{
+    public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
+    {
+        return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+    }
+}
+"@
+
+#compile the class
+Add-Type -TypeDefinition $code
+
+#disable checks using new class
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
+#do the request
+try
+{
+    invoke-WebRequest -Uri myurl -UseBasicParsing
+} catch {
+    # do something
+} finally {
+   #enable checks again
+   [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
+}
