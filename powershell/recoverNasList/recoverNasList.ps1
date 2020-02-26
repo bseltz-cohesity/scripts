42513501cc193692c7405ee6b02fd4d35a35c456
selterb <bseltzer@cohesity.com>
2020-02-26 11:51:51 -0500

    2020-02-26

--- powershell/recoverNasList/recoverNasList.ps1
+++ powershell/recoverNasList/recoverNasList.ps1
@@ -6,6 +6,10 @@ param (
     [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
     [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
     [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter()][array]$fullControl,                 # list of users to grant full control
+    [Parameter()][array]$readWrite,                   # list of users to grant read/write
+    [Parameter()][array]$readOnly,                    # list of users to grant read-only
+    [Parameter()][array]$modify,                      # list of users to grant modify
     [Parameter()][string]$nasList = './naslist.txt'
 )
 
@@ -14,6 +18,56 @@ param (
 
 # authenticate
 apiauth -vip $vip -username $username -domain $domain
+
+### get AD info
+$ads = api get activeDirectory
+$sids = @{}
+
+
+function addPermission($username, $perms){
+    if($user.contains('\')){
+        $workgroup, $user = $username.split('\')
+        # find domain
+        $adDomain = $ads | Where-Object { $_.workgroup -eq $workgroup -or $_.domainName -eq $workgroup}
+        if(!$adDomain){
+            write-host "domain $workgroup not found!" -ForegroundColor Yellow
+            exit 1
+        }else{
+            # find domain princlipal/sid
+            $domainName = $adDomain.domainName
+            $principal = api get "activeDirectory/principals?domain=$($domainName)&includeComputers=true&search=$($user)"
+            if(!$principal){
+                write-host "user $($username) not found!" -ForegroundColor Yellow
+            }else{
+                $sid = $principal[0].sid
+                $sids[$username] = $sid
+            }
+        }
+    }else{
+        # find local or wellknown sid
+        $principal = api get "activeDirectory/principals?includeComputers=true&search=$($username)"
+        if(!$principal){
+            write-host "user $($username) not found!" -ForegroundColor Yellow
+        }else{
+            $sid = $principal[0].sid
+            $sids[$username] = $sid
+        }
+    }
+
+    if($sid){
+        $permission = @{
+            "visible" = $True;
+            "sid" = $sid;
+            "type" = "kAllow";
+            "access" = $perms
+        }
+        return $permission
+    }else{
+        Write-Warning "User $user not found"
+        exit 1
+    }
+}
+
 
 # get input file
 $nasListFile = Get-Content $nasList
@@ -28,7 +82,7 @@ foreach($shareName in $nasListFile){
         write-host "Can't find $shareName - skipping..." -ForegroundColor Yellow
     }else{
 
-        $newViewName = $shareName.replace('\\','').replace('\','_')
+        $newViewName = $shareName.split('\')[-1].split('/')[-1]
 
         # select latest snapshot to recover
         $latestsnapshot = ($exactShares | sort-object -property @{Expression={$_.versions[0].snapshotTimestampUsecs}; Ascending = $False})[0]
@@ -62,6 +116,36 @@ foreach($shareName in $nasListFile){
         if($result){
 
             # set post recovery view settings
+
+            # apply share permissions
+            $sharePermissionsApplied = $False
+            $sharePermissions = @()
+
+            foreach($user in $readWrite){
+                $sharePermissionsApplied = $True
+                $sharePermissions += addPermission $user 'kReadWrite'
+                
+            }
+            
+            foreach($user in $fullControl){
+                $sharePermissionsApplied = $True
+                $sharePermissions += addPermission $user 'kFullControl'
+            }
+            
+            foreach($user in $readOnly){
+                $sharePermissionsApplied = $True
+                $sharePermissions += addPermission $user 'kReadOnly'
+            }
+            
+            foreach($user in $modify){
+                $sharePermissionsApplied = $True
+                $sharePermissions += addPermission $user 'kModify'
+            }
+            
+            if($sharePermissionsApplied -eq $False){
+                $sharePermissions += addPermission "Everyone" 'kFullControl'
+            }
+
             do {
                 $newView = (api get views).views | Where-Object { $_.name -eq $newViewName }
                 sleep 1
@@ -70,6 +154,7 @@ foreach($shareName in $nasListFile){
             $newView | setApiProperty -name enableSmbViewDiscovery -value $True
             $newView | setApiProperty -name enableSmbAccessBasedEnumeration -value $True
             $newView | setApiProperty -name protocolAccess -value 'kSMBOnly'
+            $newView | setApiProperty -name sharePermissions -value $sharePermissions
             $newView.qos = @{
                 "principalName" = 'TestAndDev High';
             }
