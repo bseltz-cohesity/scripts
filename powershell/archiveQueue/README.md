74499b8ece558dc6fd07425f314062930b76b47f
selterb <bseltzer@cohesity.com>
2020-03-02 05:46:13 -0500

    2020-03-02

---
+++ powershell/archiveQueue/README.md
@@ -0,0 +1,36 @@
+# List Archive Tasks Using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This PowerShell script lists the currently active archive tasks, sorted oldest to newest
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'archiveQueue'
+$repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell'
+(Invoke-WebRequest -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* archiveQueue.ps1: the main python script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```powershell
+./archiveQueue.ps1 -vip mycluster -username myusername -domain mydomain.net
+```
+
+## Parameters
+
+* -vip: the Cohesity cluster to connect to
+* -username: the cohesity user to login with
+* -domain: (optional) domain of the Cohesity user (defaults to local)
+* -numRuns: (optional) number of runs per job to inspect (defaults to 9999)
---
+++ powershell/replicationQueue/replicationQueue.ps1
@@ -0,0 +1,82 @@
+### process commandline arguments
+[CmdletBinding()]
+param (
+   [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+   [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+   [Parameter()][string]$domain = 'local', #local or AD domain
+   [Parameter()][int]$numRuns = 9999
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+$finishedStates = @('kCanceled', 'kSuccess', 'kFailure')
+
+$nowUsecs = dateToUsecs (get-date)
+
+$runningTasks = @{}
+
+foreach($job in (api get protectionJobs | Where-Object {$_.isDeleted -ne $True -and $_.isActive -ne $false} | Sort-Object -Property name)){
+    $jobId = $job.id
+    $jobName = $job.name
+    "Getting tasks for $jobName"
+    $runs = api get "protectionRuns?jobId=$jobId&numRuns=$numRuns&excludeTasks=true" | Where-Object {$_.copyRun.status -notin $finishedStates }
+    foreach($run in $runs){
+        $runStartTimeUsecs = $run.backupRun.stats.startTimeUsecs
+        foreach($copyRun in $($run.copyRun | Where-Object {$_.status -notin $finishedStates})){
+            $startTimeUsecs = $runStartTimeUsecs
+            $copyType = $copyRun.target.type
+            $status = $copyRun.status
+            if($copyType -eq 'kRemote'){
+                $runningTask = @{
+                    "jobname" = $jobName;
+                    "jobId" = $jobId;
+                    "startTimeUsecs" = $runStartTimeUsecs;
+                    "copyType" = $copyType;
+                    "status" = $status
+                }
+                $runningTasks[$startTimeUsecs] = $runningTask
+            }
+        }
+    }
+}
+
+# display output sorted by oldest first
+if($runningTasks.Keys.Count -gt 0){
+    "`n`nStart Time           Job Name"
+    "----------           --------"
+    foreach($startTimeUsecs in ($runningTasks.Keys | Sort-Object)){
+        $t = $runningTasks[$startTimeUsecs]
+        "{0}   {1} ({2})" -f (usecsToDate $t.startTimeUsecs), $t.jobName, $t.jobId
+        $run = api get "/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=$($t.startTimeUsecs)&id=$($t.jobId)"
+        $runStartTimeUsecs = $run.backupJobRuns.protectionRuns[0].backupRun.base.startTimeUsecs
+        foreach($task in $run.backupJobRuns.protectionRuns[0].copyRun.activeTasks){
+            if($task.snapshotTarget.type -eq 2){
+
+                $noLongerNeeded = ''
+                $daysToKeep = $task.retentionPolicy.numDaysToKeep
+                $usecsToKeep = $daysToKeep * 1000000 * 86400
+                $timePassed = $nowUsecs - $runStartTimeUsecs
+                if($timePassed -gt $usecsToKeep){
+                    $noLongerNeeded = "NO LONGER NEEDED"
+                }
+                "                       Replication Task ID: {0}  {1}" -f $task.taskUid.objectId, $noLongerNeeded
+                foreach($subTask in $task.activeCopySubTasks | Sort-Object {$_.publicStatus} -Descending){
+                    if($subTask.snapshotTarget.type -eq 2){
+                        if($subTask.publicStatus -eq 'kRunning'){
+                            $pct = $subTask.replicationInfo.pctCompleted
+                        }else{
+                            $pct = 0
+                        }
+                        "                       {0} ({1})`t{2}" -f $subTask.publicStatus, $pct, $subTask.entity.displayName
+                    }
+                }
+            }
+        }
+    }
+}else{
+    "`nNo active replication tasks found"
+}
