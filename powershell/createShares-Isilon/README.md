e5f1d0632bd63d0a9dffb7893169a7cd0ed1734d
selterb <bseltzer@cohesity.com>
2020-03-11 12:13:09 -0400

    2020-03-11

---
+++ powershell/createShares-Isilon/README.md
@@ -0,0 +1,41 @@
+# Create Cohesity Shares from Isilon Using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script takes SMB share information (exported from Isilon using the Isilon CLI) and for each share, identifies the matching view on Cohesity, and migrates share permissions and child shares. Note that share-level SMB permissions require Cohesity version 6.4 or later.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'createShares-Isilon'
+$repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell'
+(Invoke-WebRequest -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* createShares-Isilon.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```powershell
+./createShares-Isilon.ps1 -vip mycluster `
+                          -username myusername `
+                          -domain mydomain.net `
+                          -shareDataFilename ./shares.csv `
+                          -sourcePathPrefix '/ifs/myisilon/'
+```
+
+## Parameters
+
+* -vip: Cohesity Cluster to connect to
+* -username: Cohesity username
+* -domain: (optional) Active Directory domain of user (defaults to local)
+* -shareDataFilename: path to csv output file from Isilon
+* -sourcePathPrefix: Isilon root path (relative to Cohesity View)
---
+++ powershell/createShares-Isilon/createShares-Isilon.ps1
@@ -0,0 +1,107 @@
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$shareDataFilename = './shares.csv',
+    [Parameter()][string]$sourcePathPrefix = '/ifs/tpuisilon/'
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+### get AD info
+$ads = api get activeDirectory
+$sids = @{}
+
+### read shares file
+$shareDataFile = get-content $shareDataFilename
+$shareCount = -1
+$shares = @()
+
+foreach($shareData in $shareDataFile){
+    $shareItems = $shareData.split(',')
+    # share line item
+    if($shareItems[1].Contains('/')){
+        $shareCount += 1
+        $rootPath = $shareItems[1].replace($sourcePathPrefix, '')
+        $viewName, $folderPath = $rootPath.split('/', 2)
+        $folderPath = "/$folderPath"
+        $shares += @{'viewName' = $viewName; 'name' = $shareItems[0]; 'folderPath' = $folderPath; 'permissions' = @() }
+        # permission line item    
+    }elseif($shareItems[1] -ne 'Account Type'){
+        if($shareItems[0].substring(0, 4) -ne 'SID:'){
+            $shares[$shareCount]['permissions'] += @{
+                'account'        = $shareItems[0];
+                'accountType'    = $shareItems[1];
+                'permissionType' = $shareItems[3];
+                'permission'     = $shareItems[4]
+            }
+        }
+    }
+}
+
+### create shares
+foreach($share in $shares){
+    write-host "Creating share $($share.name) ($($share.folderPath))"
+
+    $viewParams = @{
+        "viewName"         = $share.viewName;
+        "viewPath"         = $share.folderPath;
+        "aliasName"        = $share.name;
+        "sharePermissions" = @()
+    }
+    
+    foreach($permission in $share.permissions){
+        $sid = $null
+        # already have this sid in the cache
+        if($sids.ContainsKey($permission.account)){
+            $sid = $sids[$permission.account]
+        }else{
+            if($permission.account.contains('\')){
+                $workgroup, $user = $permission.account.split('\')
+                # find domain
+                $adDomain = $ads | Where-Object { $_.workgroup -eq $workgroup }
+                if(!$adDomain){
+                    write-host "domain $workgroup not found!" -ForegroundColor Yellow
+                }else{
+                    # find domain princlipal/sid
+                    $domainName = $adDomain.domainName
+                    $principal = api get "activeDirectory/principals?domain=$($domainName)&includeComputers=true&search=$($user)"
+                    if(!$principal){
+                        write-host "user $($permission.account) not found!" -ForegroundColor Yellow
+                    }else{
+                        $sid = $principal[0].sid
+                        $sids[$permission.account] = $sid
+                    }
+                }
+            }else{
+                # find local or wellknown sid
+                $principal = api get "activeDirectory/principals?includeComputers=true&search=$($permission.account)"
+                if(!$principal){
+                    write-host "user $($permission.account) not found!" -ForegroundColor Yellow
+                }else{
+                    $sid = $principal[0].sid
+                    $sids[$permission.account] = $sid
+                }
+            }
+        }
+        # add permission
+        if($sid){
+            $newPermission = @{
+                "visible" = $true;
+                "type"    = "kAllow";
+                "access"  = $permission.permission.replace('full', 'kFullControl').replace('read', 'kReadOnly').replace('change', 'kModify');
+                "sid"     = $sid
+            }
+            $viewParams.sharePermissions += $newPermission
+        }
+    }
+    # create share
+    $null = api post viewAliases $viewParams
+}
