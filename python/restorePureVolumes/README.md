aa0063caecf6d6d7f980933cc8b70be113172a49
selterb <bseltzer@cohesity.com>
2020-04-10 06:46:31 -0400

    2020-04-10

---
+++ python/restorePureVolumes/README.md
@@ -0,0 +1,63 @@
+# Restore Pure Storage Volumes from Cohesity backups using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script restores Pure Storage volumes from a Cohesity backup.
+
+## Components
+
+* restorePureVolumes.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/restorePureVolumes/restorePureVolumes.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x restorePureVolumes.py
+# end download commands
+```
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+# example
+./restorePureVolumes.py -c mycluster -u myusername -a mypure -v myserver_lun1 -v myserver_lun2 -p restore- -s -0410
+# end example
+```
+
+```text
+Connected!
+Restoring mypure/myserver_lun1 as mypure/restore-myserver_lun1-0410
+Restoring mypure/myserver_lun2 as mypure/restore-myserver_lun2-0410
+```
+
+## Parameters
+
+* -c', '--cluster': Cohesity cluster name or IP
+* -u', '--username': Cohesity Username
+* -d', '--domain': Cohesity User Domain
+* -a', '--purename': name of registered pure array
+* -v', '--volumename': volume name(s) to recover
+* -l', '--volumelist': file of volumes names to recover
+* -p', '--prefix': prefix to apply to recovered volumes
+* -s', '--suffix': suffix to apply to recovered volumes
+
+## The Python Helper Module - pyhesity.py
+
+The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module. You can get a copy and read about it here:
+
+<https://github.com/bseltz-cohesity/scripts/tree/master/python>
+
+### Installing the Prerequisites
+
+```bash
+sudo yum install python-requests
+```
+
+or
+
+```bash
+sudo easy_install requests
+```
---
+++ python/restorePureVolumes/restorePureVolumes.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+"""Restore Pure Storage Volumes Using python"""
+
+# usage: ./restorePureVolumes.py -c mycluster -u myusername -a mypure -v myserver_lun1 -v myserver_lun2 -p restore- -s -0410
+
+# import pyhesity wrapper module
+from pyhesity import *
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--cluster', type=str, required=True)  # Cohesity cluster name or IP
+parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
+parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
+parser.add_argument('-a', '--purename', type=str, required=True)  # name of registered pure array
+parser.add_argument('-v', '--volumename', action='append', type=str)  # volume name(s) to recover
+parser.add_argument('-l', '--volumelist', type=str)  # file of volumes names to recover
+parser.add_argument('-p', '--prefix', type=str, required=True)  # prefix to apply to recovered volumes
+parser.add_argument('-s', '--suffix', type=str, required=True)  # suffix to apply to recovered volumes
+
+args = parser.parse_args()
+
+vip = args.cluster
+username = args.username
+domain = args.domain
+purename = args.purename
+volumes = args.volumename
+volumelist = args.volumelist
+prefix = args.prefix
+suffix = args.suffix
+
+# gather volume list
+if volumes is None:
+    volumes = []
+if volumelist is not None:
+    f = open(volumelist, 'r')
+    volumes += [s.strip() for s in f.readlines() if s.strip() != '']
+    f.close()
+if len(volumes) == 0:
+    print("No volumes specified")
+    exit(1)
+
+# authenticate to Cohesity
+apiauth(vip, username, domain)
+
+
+# volume search function
+def searchvolume(purename, volumename):
+    searchresult = api('get', '/searchvms?entityTypes=kPure&vmName=%s' % volumename)
+    if len(searchresult) == 0 or not ('vms' in searchresult):
+        return None
+    else:
+        volume = [v for v in searchresult['vms'] if v['vmDocument']['objectName'].lower() == volumename.lower() and v['vmDocument']['registeredSource']['displayName'].lower() == purename.lower()]
+        if not volume:
+            return None
+        else:
+            return volume[0]['vmDocument']
+
+
+# validate all volumes exist before starting any restores
+for volumename in volumes:
+    volume = searchvolume(purename, volumename)
+    if volume is None:
+        print("Volume %s/%s not found!" % (purename, volumename))
+        exit(1)
+
+# proceed with restores
+for volumename in volumes:
+    # find volume
+    volume = searchvolume(purename, volumename)
+    if volume is None:
+        print("Volume %s/%s not found!" % (purename, volumename))
+        exit(1)
+    else:
+        # define restore params
+        restoreParams = {
+            'action': 8,
+            'name': 'Recover-pure_Apr_9_2020_2-06pm',
+            'objects': [
+                {
+                    'jobId': volume['objectId']['jobId'],
+                    'jobUid': volume['objectId']['jobUid'],
+                    'entity': volume['objectId']['entity'],
+                    'jobInstanceId': volume['versions'][0]['instanceId']['jobInstanceId'],
+                    'attemptNum': volume['versions'][0]['instanceId']['attemptNum'],
+                    'startTimeUsecs': volume['versions'][0]['instanceId']['jobStartTimeUsecs']
+                }
+            ],
+            'restoreParentSource': volume['registeredSource'],
+            'renameRestoredObjectParam': {
+                'prefix': prefix,
+                'suffix': suffix
+            }
+        }
+        # perform restore
+        print("Restoring %s/%s as %s/%s%s%s" % (purename, volumename, purename, prefix, volumename, suffix))
+        result = api('post', '/restore', restoreParams)
