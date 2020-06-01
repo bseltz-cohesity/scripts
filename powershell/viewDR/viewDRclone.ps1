8cab26010792d8162fe21c273ea0ce6e9b4eac27
selterb <bseltzer@cohesity.com>
2020-06-01 07:09:11 -0400

    2020-06-01

--- powershell/viewDR/viewDRclone.ps1
+++ powershell/viewDR/viewDRclone.ps1
@@ -8,15 +8,27 @@ param (
     [Parameter()][string]$domain = 'local',
     [Parameter()][string]$viewName,
     [Parameter()][string]$viewList,
+    [Parameter()][switch]$all,
     [Parameter()][string]$policyName = $null,
     [Parameter(Mandatory = $True)][string]$inPath
 )
 
 # gather view list
 if($viewList){
-    $views = get-content $viewList
+    $myViews = get-content $viewList
 }elseif($viewName){
-    $views = @($viewName)
+    $myViews = @($viewName)
+}elseif($all){
+    if(test-path $inPath){
+        $files = Get-ChildItem $inPath
+        $myViews = @()
+        foreach($file in $files){
+            $myViews += [string]$file.name
+        }
+    }else{
+        Write-Warning "Can't access $inPath"
+        exit
+    }
 }else{
     Write-Host "No Views Specified" -ForegroundColor Yellow
     exit
@@ -41,25 +53,52 @@ if($policyName){
     }
 }
 
-foreach($viewName in $views){
+if(! (Test-Path $inPath)){
+    Write-Warning "$inPath not found"
+    exit
+}
+
+
+function getViews(){
+    $myViews = @()
+    $views = api get "views?includeInactive=True"
+    $myViews += $views.views
+    $lastResult = $views.lastResult
+    while(! $lastResult){
+        $lastViewId = $views.views[-1].viewId
+        $views = api get "views?maxViewId=$lastViewId&includeInactive=True"
+        $lastResult = $views.lastResult
+        $myViews += $views.views
+    }
+    return $myViews
+}
+
+"Gathering Views..."
+$views = getViews
+
+foreach($viewName in $myViews){
     ### get view metadata from file
-    $inPath = Join-Path -Path $inPath -ChildPath $viewName
-    if(Test-Path $inPath){
-        $metadata = Get-Content $inPath | ConvertFrom-Json
+    $filePath = Join-Path -Path $inPath -ChildPath $viewName
+    if(Test-Path $filePath){
+        $metadata = Get-Content $filePath | ConvertFrom-Json
     }else{
-        Write-Warning "$inPath not found"
-        exit 
+        Write-Warning "$filePath not found"
     }
 
     ### search for view to clone
     $searchResults = api get /searchvms?entityTypes=kView`&vmName=$viewName
     $viewResults = $searchResults.vms | Where-Object { $_.vmDocument.objectName -ieq $viewName }
-    $viewResult = ($viewResults | Sort-Object -Property {$_.vmDocument.versions[0].snapshotTimestampUsecs} -Descending)[0]
-
+    if($viewResults){
+        $viewResult = ($viewResults | Sort-Object -Property {$_.vmDocument.versions[0].snapshotTimestampUsecs} -Descending)[0]
+    }else{
+        Write-Host "$viewName not replicated to this cluster" -ForegroundColor Yellow
+        $viewResult = $null
+    }
+    
     if ($viewResult) {
         
-        $view = api get views/$($viewResult.vmDocument.objectName)?includeInactive=True
-
+        $view = $views | Where-Object name -eq $viewResult.vmDocument.objectName
+        $view = $view[0]
         $cloneTask = @{
             "name"       = "Clone-View_" + $((get-date).ToString().Replace('/', '_').Replace(':', '_').Replace(' ', '_'));
             "objects"    = @(
@@ -85,62 +124,71 @@ foreach($viewName in $views){
 
         if ($cloneOp) {
             "Cloned $viewName"
-            Start-Sleep 1
-            $newView = (api get views).views | Where-Object { $_.name -eq $viewName }
-            $newView.enableSmbViewDiscovery = $metadata.enableSmbViewDiscovery
-            $newView.qos = @{
-                "principalName" = $metadata.qos.principalName;
-            }
-            if($metadata.PSObject.Properties['subnetWhitelist']){
-                if(! $newView.PSObject.Properties['subnetWhiteList']){
-                    $newView | Add-Member -MemberType NoteProperty -Name subnetWhiteList -Value @()
-                }
-                $newView.subnetWhitelist = $metadata.subnetWhiteList
-            }
-            $null = api put views $newView
-            if($metadata.PSObject.Properties['aliases']){
-                write-host "Creating Shares..."
-                foreach($alias in $metadata.aliases){
-                    write-host "`t$($alias.aliasName)"
-                    $viewPath = $alias.viewPath.trimend("/")
-                    $null = api post viewAliases @{'viewName' = $viewName; 'viewPath' = $viewPath; 'aliasName' = $alias.aliasName}
-                }
-            }
-            if($policyName){
-                # protect cloned view
-                $protectionJob = @{
-                    'name' = "$clusterName $($newView.name) backup";
-                    'environment' = 'kView';
-                    'viewBoxId' = $newView.viewBoxId;
-                    'sourceIds' = @();
-                    'excludeSourceIds' = @();
-                    'vmTagIds' = @();
-                    'excludeVmTagIds' = @();
-                    'policyId' = $policy.id;
-                    'priority' = 'kMedium';
-                    'alertingPolicy' = @(
-                        'kFailure'
-                    );
-                    'timezone' = 'America/New_York';
-                    'incrementalProtectionSlaTimeMins' = 60;
-                    'fullProtectionSlaTimeMins' = 120;
-                    'qosType' = 'kBackupHDD';
-                    'viewName' = $newView.name;
-                    'isActive' = $true;
-                    'sourceSpecialParameters' = @();
-                    'indexingPolicy' = @{
-                        'disableIndexing' = $true
-                    };
-                    'startTime' = @{
-                        'hour' = 23;
-                        'minute' = 55;
-                    }
-                }
-                "Creating Protection Job for $($newView.name)..."
-                $null = api post protectionJobs $protectionJob
-            }
         }
-    }else{
-        write-host "View $viewName Not Found" -ForegroundColor Yellow
     }
 }
+
+Start-Sleep 10
+
+"Gathering Views..."
+$views = getViews
+
+foreach($viewName in $myViews){
+    $newView = ($views | Where-Object name -eq $viewName)
+    if($newView){
+        $newView = $newView[0]
+        $newView.enableSmbViewDiscovery = $false #$metadata.enableSmbViewDiscovery
+        $newView.qos = @{
+            "principalName" = $metadata.qos.principalName;
+        }
+        if($metadata.PSObject.Properties['subnetWhitelist']){
+            if(! $newView.PSObject.Properties['subnetWhiteList']){
+                $newView | Add-Member -MemberType NoteProperty -Name subnetWhiteList -Value @()
+            }
+            $newView.subnetWhitelist = $metadata.subnetWhiteList
+        }
+        $null = api put views $newView
+        if($metadata.PSObject.Properties['aliases']){
+            write-host "Creating Shares..."
+            foreach($alias in $metadata.aliases){
+                write-host "`t$($alias.aliasName)"
+                $viewPath = $alias.viewPath.trimend("/")
+                $null = api post viewAliases @{'viewName' = $viewName; 'viewPath' = $viewPath; 'aliasName' = $alias.aliasName}
+            }
+        }
+        if($policyName){
+            # protect cloned view
+            $protectionJob = @{
+                'name' = "$clusterName $($newView.name) backup";
+                'environment' = 'kView';
+                'viewBoxId' = $newView.viewBoxId;
+                'sourceIds' = @();
+                'excludeSourceIds' = @();
+                'vmTagIds' = @();
+                'excludeVmTagIds' = @();
+                'policyId' = $policy.id;
+                'priority' = 'kMedium';
+                'alertingPolicy' = @(
+                    'kFailure'
+                );
+                'timezone' = 'America/New_York';
+                'incrementalProtectionSlaTimeMins' = 60;
+                'fullProtectionSlaTimeMins' = 120;
+                'qosType' = 'kBackupHDD';
+                'viewName' = $newView.name;
+                'isActive' = $true;
+                'sourceSpecialParameters' = @();
+                'indexingPolicy' = @{
+                    'disableIndexing' = $true
+                };
+                'startTime' = @{
+                    'hour' = 23;
+                    'minute' = 55;
+                }
+            }
+            "Creating Protection Job for $($newView.name)..."
+            $null = api post protectionJobs $protectionJob
+        }
+    }
+}
+
--- powershell/viewDR/viewDRdelete.ps1
+++ powershell/viewDR/viewDRdelete.ps1
@@ -1,4 +1,4 @@
-### usage: ./viewDRdeleteAll.ps1 -vip mycluster -username myusername [ -domain mydomain.net ] -inPath \\myserver\mypath
+ ### usage: ./viewDRdelete.ps1 -vip mycluster -username myusername [ -domain mydomain.net ] -inPath \\myserver\mypath
 
 ### process commandline arguments
 [CmdletBinding()]
@@ -7,18 +7,36 @@ param (
     [Parameter(Mandatory = $True)][string]$username,
     [Parameter()][string]$domain = 'local',
     [Parameter()][string]$viewName,
-    [Parameter()][string]$viewList
+    [Parameter()][string]$viewList,
+    [Parameter()][switch]$all,
+    [Parameter()][string]$inPath,
+    [Parameter()][switch]$deleteSnapshots
 )
 
 # gather view list
 if($viewList){
-    $views = get-content $viewList
+    $myviews = get-content $viewList
 }elseif($viewName){
-    $views = @($viewName)
+    $myviews = @($viewName)
+}elseif($all -and $inPath){
+    if(test-path $inPath){
+        $files = Get-ChildItem $inPath
+        $myviews = @()
+        foreach($file in $files){
+            $myviews += $file.name
+        }
+    }else{
+        Write-Warning "Can't access $inPath"
+        exit
+    }
+
 }else{
     Write-Host "No Views Specified" -ForegroundColor Yellow
     exit
 }
+
+$myviews
+exit
 
 ### source the cohesity-api helper code
 . ./cohesity-api
@@ -26,11 +44,30 @@ if($viewList){
 ### authenticate
 apiauth -vip $vip -username $username -domain $domain
 
+### get views
+function getViews(){
+    $myViews = @()
+    $views = api get "views"
+    $myViews += $views.views
+    $lastResult = $views.lastResult
+    while(! $lastResult){
+        $lastViewId = $views.views[-1].viewId
+        $views = api get "views?maxViewId=$lastViewId"
+        $lastResult = $views.lastResult
+        $myViews += $views.views
+    }
+    return $myViews
+}
+
+"Gathering Views..."
+$views = getViews
+
 $confirmed = $false
 
-foreach($viewname in $views){
-    $view = api get "views/$viewname"
+foreach($viewname in $myviews){
+    $view = $views | Where-Object { $_.name -ieq $viewname }
     if($view){
+        $view = $view[0]
         if($confirmed -eq $false){
             write-host "***********************************************" -ForegroundColor Red
             write-host "*** Warning: you are about to delete views! ***" -ForegroundColor Red
@@ -46,7 +83,11 @@ foreach($viewname in $views){
         if($confirmed -eq $True){
             if($view.viewProtection){
                 "deleting protection job $($view.viewProtection.protectionJobs[0].jobName)..."
-                $null = api delete "protectionJobs/$($view.viewProtection.protectionJobs[0].jobId)"
+                if($deleteSnapshots){
+                    $null = api delete "protectionJobs/$($view.viewProtection.protectionJobs[0].jobId)" @{'deleteSnapshots' = $True}
+                }else{
+                    $null = api delete "protectionJobs/$($view.viewProtection.protectionJobs[0].jobId)"
+                }
             }
             "Deleting $viewname"
             $null = api delete "views/$viewname"
