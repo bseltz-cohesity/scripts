2b1f644aede0e919ed2a23cd74c6d00d91984b92
selterb <bseltzer@cohesity.com>
2020-07-01 13:51:57 -0400

    2020-07-01

---
+++ python/backedUpFileList/README.md
@@ -0,0 +1,44 @@
+# Backup Up File List for Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This Python script enumerates the files that are available for restore from the specified server/job. The file list is written to an output text file.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```bash
+# Begin download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/backuedUpFileList/backedUpFileList.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x backupNow.py
+# End download commands
+```
+
+## Components
+
+* backedUpFileList.py: the main PowerShell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place all files in a folder together. then, run the main script like so:
+
+```bash
+./backedUpFileList.py -v mycluster \
+                      -u myuser \
+                      -d mydomain.net \
+                      -s server1.mydomain.net \
+                      -j 'My Backup Job' \
+                      -f '2020-06-30 13:00:00'
+```
+
+## Parameters
+
+* -v, --vip: name of Cohesity cluster to connect to
+* -u, --username: short username to authenticate to the cluster
+* -d, --domain: active directory domain of user (default is local)
+* -i, --useApiKey: use API key for authentication
+* -pwd, --password: (optional) password for Cohesity user
+* -s, --sourceserver: name of server to inspect
+* -j, --jobname: name of protection job to run
+* -f, --filedate: (optional) date to inspect (next backup after date will be inspected)
---
+++ python/backedUpFileList/backedUpFileList.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+"""backed up files list for python"""
+
+# usage: ./backedUpFileList.py -v mycluster \
+#                              -u myuser \
+#                              -d mydomain.net \
+#                              -s server1.mydomain.net \
+#                              -j myjob \
+#                              -f '2020-06-29 12:00:00'
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+from urllib import quote_plus
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)           # cluster to connect to
+parser.add_argument('-u', '--username', type=str, default='helios')   # username
+parser.add_argument('-d', '--domain', type=str, default='local')      # domain - defaults to local
+parser.add_argument('-i', '--useApiKey', action='store_true')         # use API key authentication
+parser.add_argument('-pwd', '--password', type=str, default=None)       # optional password
+parser.add_argument('-s', '--sourceserver', type=str, required=True)  # name of source server
+parser.add_argument('-j', '--jobname', type=str, required=True)        # narrow search by job name
+parser.add_argument('-f', '--filedate', type=str, default=None)       # date to restore from
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+password = args.password
+useApiKey = args.useApiKey
+sourceserver = args.sourceserver
+jobname = args.jobname
+filedate = args.filedate
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+
+search = api('get', '/searchvms?entityTypes=kAcropolis&entityTypes=kAWS&entityTypes=kAWSNative&entityTypes=kAWSSnapshotManager&entityTypes=kAzure&entityTypes=kAzureNative&entityTypes=kFlashBlade&entityTypes=kGCP&entityTypes=kGenericNas&entityTypes=kHyperV&entityTypes=kHyperVVSS&entityTypes=kIsilon&entityTypes=kKVM&entityTypes=kNetapp&entityTypes=kPhysical&entityTypes=kVMware&vmName=%s' % sourceserver)
+
+if 'vms' not in search:
+    print('%s not found' % sourceserver)
+    exit(1)
+
+searchResults = [vm for vm in search['vms'] if vm['vmDocument']['objectName'].lower() == sourceserver.lower()]
+if len(search['vms']) == 0:
+    print('%s not found' % sourceserver)
+    exit(1)
+
+searchResults = [vm for vm in searchResults if vm['vmDocument']['jobName'].lower() == jobname.lower()]
+if len(search['vms']) == 0:
+    print('%s not protected by %s' % (sourceserver, jobname))
+    exit(1)
+
+searchResult = sorted(searchResults, key=lambda result: result['vmDocument']['versions'][0]['snapshotTimestampUsecs'], reverse=True)[0]
+
+doc = searchResult['vmDocument']
+
+if filedate is not None:
+    filedateusecs = dateToUsecs(filedate)
+    versions = [v for v in doc['versions'] if filedateusecs <= v['snapshotTimestampUsecs']]
+    if versions:
+        version = versions[-1]
+    else:
+        print('No backups from the specified date')
+        exit(1)
+else:
+    version = doc['versions'][0]
+
+instance = ("attemptNum=%s&clusterId=%s&clusterIncarnationId=%s&entityId=%s&jobId=%s&jobInstanceId=%s&jobStartTimeUsecs=%s&jobUidObjectId=%s" %
+            (version['instanceId']['attemptNum'],
+             doc['objectId']['jobUid']['clusterId'],
+             doc['objectId']['jobUid']['clusterIncarnationId'],
+             doc['objectId']['entity']['id'],
+             doc['objectId']['jobId'],
+             version['instanceId']['jobInstanceId'],
+             version['instanceId']['jobStartTimeUsecs'],
+             doc['objectId']['jobUid']['objectId']))
+
+fileDateString = datetime.strptime(usecsToDate(version['instanceId']['jobStartTimeUsecs']), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d_%H-%M-%S')
+
+f = open('backedUpFiles-%s-%s.txt' % (sourceserver, fileDateString), 'w')
+
+
+def listdir(dirPath, instance, volumeInfoCookie=None, volumeName=None):
+    thisDirPath = quote_plus(dirPath)
+    if volumeName is not None:
+        dirList = api('get', '/vm/directoryList?%s&dirPath=%s&statFileEntries=false&volumeInfoCookie=%s&volumeName=%s' % (instance, thisDirPath, volumeInfoCookie, volumeName))
+    else:
+        dirList = api('get', '/vm/directoryList?%s&dirPath=%s&statFileEntries=false' % (instance, thisDirPath))
+    if 'entries' in dirList:
+        for entry in sorted(dirList['entries'], key=lambda e: e['name']):
+            if entry['type'] == 'kDirectory':
+                listdir('%s/%s' % (dirPath, entry['name']), instance, volumeInfoCookie, volumeName)
+            else:
+                print(entry['fullPath'])
+                f.write('%s\n' % entry['fullPath'])
+
+
+volumeTypes = [1, 6]
+backupType = doc['backupType']
+if backupType in volumeTypes:
+    volumeList = api('get', '/vm/volumeInfo?%s&statFileEntries=false' % instance)
+    if 'volumeInfos' in volumeList:
+        volumeInfoCookie = volumeList['volumeInfoCookie']
+        for volume in sorted(volumeList['volumeInfos'], key=lambda v: v['name']):
+            volumeName = quote_plus(volume['name'])
+            listdir('/', instance, volumeInfoCookie, volumeName)
+else:
+    listdir('/', instance)
+
+f.close()
