738172f36bc0ddd60b9690441606a12bf41d0ffd
selterb <bseltzer@cohesity.com>
2020-09-03 06:14:09 -0400

    2020-09-03

--- powershell/listProtectedFilePaths/listProtectedFilePaths.ps1
+++ powershell/listProtectedFilePaths/listProtectedFilePaths.ps1
@@ -13,19 +13,37 @@ param (
 apiauth -vip $vip -username $username -domain $domain
 
 $sources = api get protectionSources?environments=kPhysical
-$jobs = api get protectionJobs?environments=kPhysicalFiles
+$jobs = api get "protectionJobs?environments=kPhysicalFiles&environments=kPhysical"
 
 foreach($job in $jobs){
     "`n{0}" -f $job.name
     $jobSources = $job.sourceSpecialParameters
     foreach($source in $jobSources){
         $sourceId = $source.sourceId
-        $sourceName = ($sources[0].nodes | Where-Object {$_.protectionSource.id -eq $sourceId}).protectionSource.name
+        $protectionSource = $sources[0].nodes | Where-Object {$_.protectionSource.id -eq $sourceId}
+        $sourceName = $protectionSource.protectionSource.name
         "`n    $sourceName"
-        foreach($filePath in $source.physicalSpecialParameters.filePaths){
-            "      + {0} (SkipNestedVolumes={1})" -f $filePath.backupFilePath, $filePath.skipNestedVolumes
-            foreach($excludePath in $filePath.excludedFilePaths){
-                "        - {0}" -f $excludePath
+        if($job.environment -eq 'kPhysicalFiles'){
+            foreach($filePath in $source.physicalSpecialParameters.filePaths){
+                "      + {0} (SkipNestedVolumes={1})" -f $filePath.backupFilePath, $filePath.skipNestedVolumes
+                foreach($excludePath in $filePath.excludedFilePaths){
+                    "        - {0}" -f $excludePath
+                }
+            }
+        }else{
+            if($source.physicalSpecialParameters.volumeGuid){
+                foreach($volumeGuid in $source.physicalSpecialParameters.volumeGuid){
+                    $volumeInfo = $protectionSource.protectionSource.physicalProtectionSource.volumes | Where-Object guid -eq $volumeGuid
+                    $volumeLabel = $volumeInfo.label
+                    if($volumeLabel -eq 'System Reserved'){
+                        "      + System Reserved (volume)"
+                    }else{
+                        $volumeMountPoint = $volumeInfo.mountPoints[0]
+                        "      + {0} (volume)" -f $volumeMountPoint
+                    }
+                }
+            }else{
+                "      + All Volumes"
             }
         }
     }
