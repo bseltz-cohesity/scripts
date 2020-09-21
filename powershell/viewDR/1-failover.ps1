fb1a5b6e0b5bfea708c6f54d60e0d099a5cbb666
selterb <bseltzer@cohesity.com>
2020-09-21 07:16:49 -0400

    2020-09-21

--- powershell/viewDR/1-failover.ps1
+++ powershell/viewDR/1-failover.ps1
@@ -1,8 +1,8 @@
 # clone the replicated view backups
 ./viewDRclone.ps1 -vip myDRcluster -username admin [ -domain local ] -inPath \\myDRcluster\viewDR\mycluster -all
 # toggle dfs folder targets
-.\dfsFailover.ps1 -nameSpace \\mydomain.net\shares -shareList .\migratedShares.txt -disableTarget \\myCluster.mydomain.net -enableTarget \\myDRcluster.mydomain.net
+# .\dfsFailover.ps1 -nameSpace \\mydomain.net\shares -shareList .\migratedShares.txt -disableTarget \\myCluster.mydomain.net -enableTarget \\myDRcluster.mydomain.net
 # toggle cnames and SPNs
-.\cnameFailover.ps1 -cname mynas -oldHost mycluster -newHost myDRcluster -domain mydomain.net
+# .\cnameFailover.ps1 -cname mynas -oldHost mycluster -newHost myDRcluster -domain mydomain.net
 # remove views from source
-./viewDRdelete.ps1 -vip mycluster -username admin [ -domain local ] -inPath \\myDRcluster\viewDR\mycluster -all
+# ./viewDRdelete.ps1 -vip mycluster -username admin [ -domain local ] -inPath \\myDRcluster\viewDR\mycluster -all
