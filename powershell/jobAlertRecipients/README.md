9750891286c61758455303a88ee4db519320b495
selterb <bseltzer@cohesity.com>
2020-10-21 15:22:20 -0400

    2020-10-21

---
+++ powershell/jobAlertRecipients/README.md
@@ -0,0 +1,43 @@
+# Add and Remove Job Alert Recipients using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script adds and removes alert email recipients from all protection jobs/groups on a cluster. 
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'jobAlertRecipients'
+$repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell'
+(Invoke-WebRequest -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* jobAlertRecipients.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place the files in a folder together and run the main script like so:
+
+```powershell
+# Command line example
+./jobAlertRecipients.ps1 -vip mycluster `
+                         -username myuser `
+                         -domain mydomain.net `
+                         -addAddress myuser1@mydomain.net, myuser2@mydomain.net `
+                         -removeAddress myuser3@mydomain.net, myuser4@mydomain.net
+# End example
+```
+
+## Parameters
+
+* -vip: Cohesity Cluster to connect to
+* -username: Cohesity username
+* -domain: (optional) Cohesity logon domain (defaults to local)
+* -addAddress: (optional) one or more email addresses to add (comma separated)
+* -removeAddress: (optional) one or more email addresses to add (comma separated)
---
+++ powershell/jobAlertRecipients/jobAlertRecipients.ps1
@@ -0,0 +1,49 @@
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter()][array]$addAddress,
+    [Parameter()][array]$removeAddress
+)
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+$cluster = api get cluster
+"Modifying email recipients:"
+$jobs = api get protectionJobs
+foreach($job in $jobs | Where-Object {$_.policyId.split(":")[0] -eq $cluster.id} | Sort-Object -Property name){
+    "  $($job.name)"
+    $jobEdited = $False
+    if($job.PSObject.Properties['alertingConfig']){
+        foreach($address in $removeAddress){
+            if($address -in $job.alertingConfig.emailAddresses){
+                $job.alertingConfig.emailAddresses = @($job.alertingConfig.emailAddresses | Where-Object {$_ -ne $address})
+                $job.alertingConfig.emailDeliveryTargets = @($job.alertingConfig.emailDeliveryTargets | Where-Object {$_.emailAddress -ne $address})
+                $jobEdited = $True
+            }
+        }
+    }else{
+        setApiProperty -object $job -name alertingConfig -value @{'emailAddresses' = @(); 'emailDeliveryTargets' = @()}
+    }
+    foreach($address in $addAddress){
+        $address = [string]$address
+        if(!($address -in $job.alertingConfig.emailAddresses)){
+            $job.alertingConfig.emailAddresses = $job.alertingConfig.emailAddresses + $address
+            $job.alertingConfig.emailDeliveryTargets = $job.alertingConfig.emailDeliveryTargets + @{
+                "emailAddress"  = $address;
+                "locale"        = "en-us";
+                "recipientType" = "kTo"
+            }
+            $jobEdited = $True
+        }
+    }
+    if($jobEdited -eq $True){
+        $null = api put protectionJobs/$($job.id) $job
+    }
+}
