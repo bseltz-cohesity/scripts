29a67fa86ed118d50ced49ba892618735e112c41
selterb <bseltzer@cohesity.com>
2020-12-15 12:26:51 -0500

    2020-12-15

--- powershell/archiveNow-latest/archiveNow-latest.ps1
+++ powershell/archiveNow-latest/archiveNow-latest.ps1
@@ -47,6 +47,8 @@ if($jobNames){
     $jobs = $jobs | Where-Object name -in $jobNames
 }
 
+$finishedStates = @('kCanceled', 'kSuccess', 'kFailure', 'kWarning')
+
 foreach($job in $jobs){
 
     $jobName = $job.name
@@ -54,59 +56,62 @@ foreach($job in $jobs){
     $runs = (api get protectionRuns?jobId=$($job.id)`&numRuns=999`&runTypes=kRegular`&runTypes=kFull`&excludeTasks=true) | `
         Where-Object { $_.backupRun.snapshotsDeleted -eq $false } | `
         Where-Object { $_.backupRun.status -eq 'kSuccess' -or $_.backupRun.status -eq 'kWarning' } | `
+        Where-Object {@($_.copyRun.status | Where-Object {$finishedStates -notcontains $_}).Count -eq 0} | `
         Sort-Object -Property {$_.copyRun[0].runStartTimeUsecs} -Descending
     if($runs){
         $run = $runs[0]
-        $now = dateToUsecs $(get-date)
+        if($run){
+            $now = dateToUsecs $(get-date)
 
-        # local snapshots stats
-        $startTimeUsecs = $run.copyRun[0].runStartTimeUsecs
-        $expireTimeUsecs = $run.copyRun[0].expiryTimeUsecs
-        $runDate = usecsToDate $startTimeUsecs
-
-        # get jobUid of originating cluster
-        $runDetail = api get "/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=$startTimeUsecs&excludeTasks=true&id=$($run.jobId)"
-        $jobUid = $runDetail[0].backupJobRuns.protectionRuns[0].backupRun.base.jobUid
-
-        # calculate archive expire time
-        if($keepFor){
-            $newExpireTimeUsecs = $startTimeUsecs + ([int]$keepFor * 86400000000)
-        }else{
-            $newExpireTimeUsecs = $expireTimeUsecs
-        }
-        $daysToKeep = [math]::Round(($newExpireTimeUsecs - $now) / 86400000000) 
-        $expireDate = usecsToDate $newExpireTimeUsecs
-
-        # create archive task definition
-        $archiveTask = @{
-            'jobRuns' = @(
-                @{
-                    'copyRunTargets'    = @(
-                        @{
-                            'archivalTarget' = @{
-                                'vaultId'   = $vaultId;
-                                'vaultName' = $vaultName;
-                                'vaultType' = $vaultType
-                            };
-                            'daysToKeep'     = [int] $daysToKeep;
-                            'type'           = 'kArchival'
+            # local snapshots stats
+            $startTimeUsecs = $run.copyRun[0].runStartTimeUsecs
+            $expireTimeUsecs = $run.copyRun[0].expiryTimeUsecs
+            $runDate = usecsToDate $startTimeUsecs
+    
+            # get jobUid of originating cluster
+            $runDetail = api get "/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=$startTimeUsecs&excludeTasks=true&id=$($run.jobId)"
+            $jobUid = $runDetail[0].backupJobRuns.protectionRuns[0].backupRun.base.jobUid
+    
+            # calculate archive expire time
+            if($keepFor){
+                $newExpireTimeUsecs = $startTimeUsecs + ([int]$keepFor * 86400000000)
+            }else{
+                $newExpireTimeUsecs = $expireTimeUsecs
+            }
+            $daysToKeep = [math]::Round(($newExpireTimeUsecs - $now) / 86400000000) 
+            $expireDate = usecsToDate $newExpireTimeUsecs
+    
+            # create archive task definition
+            $archiveTask = @{
+                'jobRuns' = @(
+                    @{
+                        'copyRunTargets'    = @(
+                            @{
+                                'archivalTarget' = @{
+                                    'vaultId'   = $vaultId;
+                                    'vaultName' = $vaultName;
+                                    'vaultType' = $vaultType
+                                };
+                                'daysToKeep'     = [int] $daysToKeep;
+                                'type'           = 'kArchival'
+                            }
+                        );
+                        'runStartTimeUsecs' = $run.copyRun[0].runStartTimeUsecs;
+                        'jobUid'            = @{
+                            'clusterId' = $jobUid.clusterId;
+                            'clusterIncarnationId' = $jobUid.clusterIncarnationId;
+                            'id' = $jobUid.objectId
                         }
-                    );
-                    'runStartTimeUsecs' = $run.copyRun[0].runStartTimeUsecs;
-                    'jobUid'            = @{
-                        'clusterId' = $jobUid.clusterId;
-                        'clusterIncarnationId' = $jobUid.clusterIncarnationId;
-                        'id' = $jobUid.objectId
                     }
-                }
-            )
-        }
-        # submit the archive task
-        if($commit){
-            write-host "Archiving $($jobName) ($runDate) --> $vaultName ($expireDate)"
-            $null = api put protectionRuns $archiveTask
-        }else{
-            write-host "Would archive $($jobName) ($runDate) --> $vaultName ($expireDate)"
+                )
+            }
+            # submit the archive task
+            if($commit){
+                write-host "Archiving $($jobName) ($runDate) --> $vaultName ($expireDate)"
+                $null = api put protectionRuns $archiveTask
+            }else{
+                write-host "Would archive $($jobName) ($runDate) --> $vaultName ($expireDate)"
+            }
         }
     }
 }
