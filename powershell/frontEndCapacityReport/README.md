2d240cffc31af9ddbc6b3855706dae55704c95c9
selterb <bseltzer@cohesity.com>
2021-01-24 06:52:03 -0500

    2021-01-24

--- powershell/frontEndCapacityReport/README.md
+++ powershell/frontEndCapacityReport/README.md
@@ -1,8 +1,8 @@
-# Gather Cloud Archive Direct Size and Transfer Stats
+# Gather Front End Size of Views and Protected Servers
 
 Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
 
-This PowerShell script gathers cloud archive direct size and transfer stats.
+This PowerShell script gathers front end size of views and protected servers.
 
 ## Download the script
 
@@ -10,7 +10,7 @@ Run these commands from PowerShell to download the script(s) into your current d
 
 ```powershell
 # Download Commands
-$scriptName = 'cloudArchiveDirectStats'
+$scriptName = 'frontEndCapacityReport'
 $repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell'
 (Invoke-WebRequest -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
 (Invoke-WebRequest -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
@@ -19,13 +19,13 @@ $repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/pow
 
 ## Components
 
-* cloudArchiveDirectStats.ps1: the main python script
+* frontEndCapacityReport.ps1: the main python script
 * cohesity-api.ps1: the Cohesity REST API helper module
 
 Place both files in a folder together and run the main script like so:
 
 ```bash
-./cloudArchiveDirectStats.ps1 -vip mycluster -username myusername -domain mydomain.net
+./frontEndCapacityReport.ps1 -vip mycluster -username myusername -domain mydomain.net
 ```
 
 The output will be written to a CSV file.
@@ -35,3 +35,4 @@ The output will be written to a CSV file.
 * -vip: the Cohesity cluster to connect to
 * -username: the cohesity user to login with
 * -domain: (optional) domain of the Cohesity user (defaults to local)
+* -unit: (optional) MiB, GiB or TiB (default is GiB)
---
+++ powershell/frontEndCapacityReport/frontEndCapacityReport.ps1
@@ -0,0 +1,61 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][ValidateSet('MiB','GiB','TiB')][string]$unit = 'GiB'
+)
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+$cluster = api get cluster
+$clusterName = $cluster.name
+
+$dateString = (get-date).ToString("yyyy-MM-dd")
+$outfileName = "$clusterName-FETB-$dateString.csv"
+
+$environments = @('kUnknown', 'kVMware', 'kHyperV', 'kSQL', 'kView', 'kPuppeteer', 
+                  'kPhysical', 'kPure', 'kAzure', 'kNetapp', 'kAgent', 'kGenericNas', 
+                  'kAcropolis', 'kPhysicalFiles', 'kIsilon', 'kKVM', 'kAWS', 'kExchange', 
+                  'kHyperVVSS', 'kOracle', 'kGCP', 'kFlashBlade', 'kAWSNative', 'kVCD',
+                  'kO365', 'kO365Outlook', 'kHyperFlex', 'kGCPNative', 'kAzureNative',
+                  'kAD', 'kAWSSnapshotManager', 'kUnknown', 'kUnknown', 'kUnknown', 'kUnknown')
+
+$conversion = @{'MiB' = 1024 * 1024; 'GiB' = 1024 * 1024 * 1024; 'TiB' = 1024 * 1024 * 1024 * 1024}
+
+function toUnits($val){
+    return "{0:n2}" -f ($val/($conversion[$unit]))
+}
+
+"Job Name,Object Name,Object Type,Logical Size ($unit),Unique Size ($unit)" | Out-File -FilePath $outfileName
+
+# gather storage consumed by servers
+$serverReport = api get /reports/objects/storage?msecsBeforeEndTime=0
+foreach($server in $serverReport | Sort-Object -Property jobName, name){
+    $jobName = $server.jobName
+    $serverName = $server.name
+    $serverType = ($environments[$server.entity.type]).Substring(1)
+    if($server.PSObject.Properties['dataPoints']){
+        $logicalBytes = $server.dataPoints[0].logicalSizeBytes
+        $uniqueBytes = $server.dataPoints[0].primaryPhysicalSizeBytes
+        "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $serverName, $serverType, (toUnits $logicalBytes), (toUnits $uniqueBytes) | Tee-Object -FilePath $outfileName -Append    
+    }
+}
+
+$views = api get views
+foreach($view in $views.views | Sort-Object -Property name){
+    $viewName = $view.name
+    $logicalBytes = $view.logicalUsageBytes
+    if($view.PSObject.Properties['viewProtection']){
+        $jobName = $view.viewProtection.protectionJobs[0].jobName
+    }else{
+        $jobName = '-'
+    }
+    "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $viewName, 'View', (toUnits $logicalBytes), 0 | Tee-Object -FilePath $outfileName -Append
+}
+
+"`nOutput written to $outfileName"
