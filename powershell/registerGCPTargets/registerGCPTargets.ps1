43e1fe31769d36cc69da7cef02bfc8c2b57fd09a
selterb <bseltzer@cohesity.com>
2021-01-25 13:17:21 -0500

    2021-01-25

---
+++ powershell/registerGCPTargets/registerGCPTargets.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter()][string]$keyfile = 'gcpkey.txt', # text file containing private key
+    [Parameter(Mandatory = $True)][string]$clientemail, # GCP idenntity
+    [Parameter(Mandatory = $True)][string]$projectid, # GCP project ID
+    [Parameter()][string]$inputfile = 'gcptargets.csv' # CSV input file with targetname, bucketname, tiertype
+)
+
+$gcptargets = Import-Csv -Path $inputfile
+$privatekey = Get-Content -Path $keyfile -Raw
+
+foreach($target in $gcptargets){
+    $myObject = @{
+        "compressionPolicy" = "kCompressionLow";
+        "config" = @{
+            "google" = @{
+                "tierType" = $target.tiertype;
+                "projectId" = $projectid;
+                "clientEmailAddress" = $clientemail;
+                "clientPrivateKey" = "$privatekey"
+            };
+            "bucketName" = $target.bucketname
+        };
+        "dedupEnabled" = $true;
+        "encryptionPolicy" = "kEncryptionStrong";
+        "incrementalArchivesEnabled" = $true;
+        "name" = $target.targetname;
+        "usageType" = "kArchival";
+        "customerManagingEncryptionKeys" = $false;
+        "externalTargetType" = "kGoogle"
+    }
+    "Registering target $($target.targetname) -> $($target.bucketname)"
+    $null = api post vaults $myObject
+}
+
+
