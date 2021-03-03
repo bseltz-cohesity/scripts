4d8a93fb45cbcc12fd232b1339eb7fada3922df3
selterb <bseltzer@cohesity.com>
2021-03-03 12:05:52 -0500

    2021-03-03

---
+++ powershell/jobList/jobList.ps1
@@ -0,0 +1,36 @@
+### usage: .\jobList.ps1 -vip mycluster -username myusername -domain mydomain.net
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'local'
+ )
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+### get protection jobs and policies
+$jobs = api get protectionJobs?isActive=true
+$policies = api get protectionPolicies
+
+$outfile = "$vip-jobList.csv"
+# write-host "Saving Job Retention Settings to $vip-jobRetentionReport.csv"
+"Job Name,Job ID,Job Type,Policy Name" | Out-File -FilePath $outfile
+
+foreach($job in $jobs | Sort-Object -Property name){
+    $jobName = $job.name
+    $jobName
+    $jobId = $job.id
+    $jobType = $job.environment.subString(1) 
+    $policyId = $job.policyId
+    $policy = $policies | Where-Object {$_.id -eq $policyId}
+    $policyName = $policy.name
+    "$jobName,$jobId,$jobType,$policyName" | out-file -FilePath $outfile -Append
+}
+
+write-host "Job List saved to $outfile"
