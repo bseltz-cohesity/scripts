3de8ed983a4e7fbf9c9cea0bd3e05edf6fadc8b0
selterb <bseltzer@cohesity.com>
2021-03-15 08:48:36 -0400

    2021-03-15

--- python/archiveQueue/archiveQueue.py
+++ python/archiveQueue/archiveQueue.py
@@ -13,7 +13,7 @@ parser.add_argument('-d', '--domain', type=str, default='local')
 parser.add_argument('-o', '--canceloutdated', action='store_true')
 parser.add_argument('-q', '--cancelqueued', action='store_true')
 parser.add_argument('-a', '--cancelall', action='store_true')
-parser.add_argument('-n', '--numruns', type=int, default=9999)
+parser.add_argument('-n', '--numruns', type=int, default=500)
 parser.add_argument('-s', '--units', type=str, choices=['MiB', 'GiB'], default='MiB')
 args = parser.parse_args()
 
@@ -54,53 +54,59 @@ for job in jobs:
         jobName = job['name']
         print("Getting tasks for %s" % jobName)
         # find runs with unfinished archive tasks
-        runs = api('get', 'protectionRuns?jobId=%s&numRuns=%s&excludeTasks=true' % (jobId, numruns))
-        if runs is not None:
-            runs = sorted(runs, key=lambda r: r['backupRun']['stats']['startTimeUsecs'])
-            for run in runs:
-                runStartTimeUsecs = run['backupRun']['stats']['startTimeUsecs']
-                if 'copyRun' in run:
-                    for copyRun in run['copyRun']:
-                        # store run details in dictionary
-                        if copyRun['status'] not in finishedStates and copyRun['target']['type'] == 'kArchival':
-                            thisrun = api('get', '/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=%s&id=%s' % (runStartTimeUsecs, jobId))
-                            if 'activeTasks' in thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']:
-                                for task in thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']['activeTasks']:
+        endUsecs = nowUsecs
+        while(1):
+            runs = [r for r in api('get', 'protectionRuns?jobId=%s&numRuns=%s&endTimeUsecs=%s&excludeTasks=true' % (jobId, numruns, endUsecs)) if 'endTimeUsecs' not in r['backupRun']['stats'] or r['backupRun']['stats']['endTimeUsecs'] < endUsecs]
+            if len(runs) > 0:
+                endUsecs = runs[-1]['backupRun']['stats']['startTimeUsecs']
+            else:
+                break
+            if runs is not None:
+                runs = sorted(runs, key=lambda r: r['backupRun']['stats']['startTimeUsecs'])
+                for run in runs:
+                    runStartTimeUsecs = run['backupRun']['stats']['startTimeUsecs']
+                    if 'copyRun' in run:
+                        for copyRun in run['copyRun']:
+                            # store run details in dictionary
+                            if copyRun['status'] not in finishedStates and copyRun['target']['type'] == 'kArchival':
+                                thisrun = api('get', '/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=%s&id=%s' % (runStartTimeUsecs, jobId))
+                                if 'activeTasks' in thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']:
                                     for task in thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']['activeTasks']:
-                                        if task['snapshotTarget']['type'] == 3:
-                                            # determine if run is now older than the intended retention
-                                            noLongerNeeded = ''
-                                            cancelling = ''
-                                            if cancelall is True:
-                                                cancel = True
-                                            else:
-                                                cancel = False
-                                            daysToKeep = task['retentionPolicy']['numDaysToKeep']
-                                            usecsToKeep = daysToKeep * 1000000 * 86400
-                                            timePassed = nowUsecs - runStartTimeUsecs
-                                            if timePassed > usecsToKeep:
-                                                noLongerNeeded = "NO LONGER NEEDED"
-                                                if canceloutdated is True:
+                                        for task in thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']['activeTasks']:
+                                            if task['snapshotTarget']['type'] == 3:
+                                                # determine if run is now older than the intended retention
+                                                noLongerNeeded = ''
+                                                cancelling = ''
+                                                if cancelall is True:
                                                     cancel = True
-                                            transferred = 0
-                                            if 'archivalInfo' in task:
-                                                if 'logicalBytesTransferred' in task['archivalInfo']:
-                                                    transferred = task['archivalInfo']['logicalBytesTransferred']
-                                            if transferred == 0 and cancelqueued is True:
-                                                cancel = True
-                                            if cancel is True:
-                                                cancelling = 'Cancelling'
-                                                cancelTaskParams = {
-                                                    "copyTaskUid": {
-                                                        "clusterIncarnationId": task['taskUid']['clusterIncarnationId'],
-                                                        "id": task['taskUid']['objectId'],
-                                                        "clusterId": task['taskUid']['clusterId']
-                                                    },
-                                                    "jobId": jobId
-                                                }
-                                                result = api('post', 'protectionRuns/cancel/%s' % jobId, cancelTaskParams)
-                                            unitstransferred = round(transferred / multiplier, 2)
-                                            print('                       %s:  %s %s transferred %s %s' % (usecsToDate(runStartTimeUsecs), unitstransferred, units, noLongerNeeded, cancelling))
-                                            f.write('%s,%s,%s\n' % (jobName, (usecsToDate(runStartTimeUsecs)), unitstransferred))
+                                                else:
+                                                    cancel = False
+                                                daysToKeep = task['retentionPolicy']['numDaysToKeep']
+                                                usecsToKeep = daysToKeep * 1000000 * 86400
+                                                timePassed = nowUsecs - runStartTimeUsecs
+                                                if timePassed > usecsToKeep:
+                                                    noLongerNeeded = "NO LONGER NEEDED"
+                                                    if canceloutdated is True:
+                                                        cancel = True
+                                                transferred = 0
+                                                if 'archivalInfo' in task:
+                                                    if 'logicalBytesTransferred' in task['archivalInfo']:
+                                                        transferred = task['archivalInfo']['logicalBytesTransferred']
+                                                if transferred == 0 and cancelqueued is True:
+                                                    cancel = True
+                                                if cancel is True:
+                                                    cancelling = 'Cancelling'
+                                                    cancelTaskParams = {
+                                                        "copyTaskUid": {
+                                                            "clusterIncarnationId": task['taskUid']['clusterIncarnationId'],
+                                                            "id": task['taskUid']['objectId'],
+                                                            "clusterId": task['taskUid']['clusterId']
+                                                        },
+                                                        "jobId": jobId
+                                                    }
+                                                    result = api('post', 'protectionRuns/cancel/%s' % jobId, cancelTaskParams)
+                                                unitstransferred = round(transferred / multiplier, 2)
+                                                print('                       %s:  %s %s transferred %s %s' % (usecsToDate(runStartTimeUsecs), unitstransferred, units, noLongerNeeded, cancelling))
+                                                f.write('%s,%s,%s\n' % (jobName, (usecsToDate(runStartTimeUsecs)), unitstransferred))
 f.close()
 print("output saved to %s" % outfileName)
