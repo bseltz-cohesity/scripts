006c1dc5d0540ffc073e3d4b1b0e4ade93113afe
bseltz-cohesity <bseltzer@cohesity.com>
2021-03-31 06:49:30 -0400

    2021-03-31

--- python/listViews/README.md
+++ python/listViews/README.md
@@ -24,8 +24,20 @@ Place both files in a folder together and run the main script like so:
 ```bash
 #example
 ./listViews.py -v mycluster \
-                   -u myusername \
-                   -d mydomain.net
+               -u myusername \
+               -d mydomain.net
+#end example
+```
+
+or to produce detailed output:
+
+```bash
+#example
+./listViews.py -v mycluster \
+               -u myusername \
+               -d mydomain.net \
+               -s \
+               -x MiB
 #end example
 ```
 
@@ -34,3 +46,5 @@ Place both files in a folder together and run the main script like so:
 * -v, --vip: Cohesity cluster to connect to
 * -u, --username: Cohesity username
 * -d, --domain: (optional) Active Directory domain (defaults to 'local')
+* -s, --showsettings: (optional) prodice detailed output
+* -x, --units: (optional) show values in GiB or MiB (default is GiB)
--- python/listViews/listViews.py
+++ python/listViews/listViews.py
@@ -10,19 +10,67 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--vip', type=str, required=True)  # Cohesity cluster name or IP
 parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
 parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
+parser.add_argument('-s', '--showsettings', action='store_true')
+parser.add_argument('-x', '--units', type=str, choices=['MiB', 'GiB', 'mib', 'gib'], default='GiB')
 
 args = parser.parse_args()
 
 vip = args.vip
 username = args.username
 domain = args.domain
+showsettings = args.showsettings
+units = args.units
+
+multiplier = 1024 * 1024 * 1024
+if units.lower() == 'mib':
+    multiplier = 1024 * 1024
+
+if units == 'mib':
+    units = 'MiB'
+if units == 'gib':
+    units = 'GiB'
 
 # authenticate
 apiauth(vip, username, domain)
 
 views = api('get', 'views')
 if views['count'] > 0:
-    print('\nProto  Name')
-    print('-----  ----')
-    for view in sorted(views['views'], key=lambda v: v['name'].lower()):
-        print(' %-4s  %s' % (view['protocolAccess'][1:].replace('Only', ''), view['name']))
+    if showsettings:
+        for view in sorted(views['views'], key=lambda v: v['name'].lower()):
+            protected = False
+            if 'viewProtection' in view:
+                protected = True
+            print('\n           name: %s' % view['name'])
+            print('    create date: %s' % usecsToDate(view['createTimeMsecs'] * 1000))
+            print(' Storage Domain: %s' % view['viewBoxName'])
+            print('       protocol: %s' % view['protocolAccess'][1:].replace('Only', ''))
+            if 'nfsMountPath' in view:
+                print(' NFS mount path: %s' % view['nfsMountPath'])
+            if 'smbMountPath' in view:
+                print(' SMB mount path: %s' % view['smbMountPath'])
+            if 's3AccessPath' in view:
+                print('  S3 mount path: %s' % view['s3AccessPath'])
+            print('      protected: %s' % protected)
+            print('  logical usage: %s %s' % (round(view['logicalUsageBytes'] / multiplier, 2), units))
+            if 'logicalQuota' in view:
+                print('  logical quota: %s %s' % (int(round(view['logicalQuota']['hardLimitBytes'] / multiplier, 0)), units))
+                print('    quota alert: %s %s' % (int(round(view['logicalQuota']['alertLimitBytes'] / multiplier, 0)), units))
+            print('     QOS Policy: %s' % view['qos']['principalName'])
+            if 'subnetWhitelist' in view:
+                print('      whitelist:')
+                entrynum = 0
+                for entry in view['subnetWhitelist']:
+                    if 'nfsRootSquash' not in entry:
+                        entry['nfsRootSquash'] = 'n/a'
+                    if entrynum > 0:
+                        print('')
+                    print('                 %s/%s' % (entry['ip'], entry['netmaskBits']))
+                    print('                 nfsRootSquash: %s' % entry['nfsRootSquash'])
+                    print('                 nfsAccess: %s' % entry['nfsAccess'][1:])
+                    print('                 smbAccess: %s' % entry['smbAccess'][1:])
+                    entrynum = 1
+    else:
+        print('\nProto  Name')
+        print('-----  ----')
+        for view in sorted(views['views'], key=lambda v: v['name'].lower()):
+            print(' %-4s  %s' % (view['protocolAccess'][1:].replace('Only', ''), view['name']))
