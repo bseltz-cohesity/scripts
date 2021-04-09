23a51532870770bbcd126b6c408ccb95f8db7676
bseltz-cohesity <bseltzer@cohesity.com>
2021-04-09 05:04:47 -0400

    2021-04-09

--- powershell/frontEndCapacityReport/frontEndCapacityReport.ps1
+++ powershell/frontEndCapacityReport/frontEndCapacityReport.ps1
@@ -41,6 +41,9 @@ function toUnits($val){
 "Job Name,Object Name,Object Type,Logical Size ($unit),Unique Size ($unit)" | Out-File -FilePath $outfileName
 
 $uniqueBytesTable = @{}
+$entityLogical = @{}
+
+"Gathering storage statistics..."
 
 # gather unique capacity for servers
 $serverReport = api get /reports/objects/storage?msecsBeforeEndTime=0
@@ -53,36 +56,33 @@ foreach($server in $serverReport | Sort-Object -Property jobName, name){
         }
     }
 }
+$jobs = (api get -v2 "data-protect/protection-groups?isDeleted=false&&includeTenants=true").protectionGroups | Sort-Object -Property name
 
-$jobs = api get protectionJobs?includeLastRunAndStats=true | Where-Object{$_.isDeleted -ne $True} | Sort-Object -Property name
 if($localOnly){
-    $jobs = $jobs | Where-Object {$_.policyId.split(':')[0] -eq $clusterId}
+    $jobs = $jobs | Where-Object isActive -eq $True
 }
 
-# gather capacity for Servers
-foreach($job in $jobs | Where-Object {$_.environment -in $serverEnvironments}){
+# gather capacity for protected objects
+foreach($job in $jobs){
+    $jobId = $job.id
     $jobName = $job.name
     $jobType = $job.environment.Substring(1)
-    foreach($server in $job.lastRun.backupRun.sourceBackupStatus){
-        $serverName = $server.source.name
-        $logicalBytes = $server.stats.totalLogicalBackupSizeBytes
-        if($serverName -in $uniqueBytesTable.Keys){
-            $uniqueBytes = $uniqueBytesTable[$serverName]
-        }else{
-            $uniqueBytes = $logicalBytes
+    $runs = api get -v2 "data-protect/protection-groups/$jobId/runs?includeTenants=true&includeObjectDetails=true&numRuns=5"
+
+    foreach($run in $runs.runs){
+        foreach($server in ($run.objects | Sort-Object -Property {$_.object.name})){
+            $serverName = $server.object.name
+            $logicalBytes = $server.localSnapshotInfo.snapshotInfo.stats.logicalSizeBytes
+            if($serverName -in $uniqueBytesTable.Keys){
+                $uniqueBytes = $uniqueBytesTable[$serverName]
+            }else{
+                $uniqueBytes = $logicalBytes
+            }
+            if($serverName -notin $entityLogical.Keys){
+                $entityLogical[$serverName] = $logicalBytes
+                "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $serverName, $jobType, (toUnits $logicalBytes), (toUnits $uniqueBytes) | Tee-Object -FilePath $outfileName -Append
+            }
         }
-        "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $serverName, $jobType, (toUnits $logicalBytes), (toUnits $uniqueBytes) | Tee-Object -FilePath $outfileName -Append    
-    }
-}
-
-# gather capacity for NAS backups
-foreach($job in $jobs | Where-Object {$_.environment -in $nasEnvironments}){
-    $jobName = $job.name
-    $jobType = $job.environment.Substring(1)
-    foreach($volume in $job.lastRun.backupRun.sourceBackupStatus){
-        $volumeName = $volume.source.name
-        $logicalBytes = $volume.stats.totalLogicalBackupSizeBytes
-        "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $volumeName, $jobType, (toUnits $logicalBytes), (toUnits $logicalBytes) | Tee-Object -FilePath $outfileName -Append    
     }
 }
 
@@ -96,7 +96,10 @@ foreach($view in $views.views | Sort-Object -Property name){
     }else{
         $jobName = '-'
     }
-    "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $viewName, 'View', (toUnits $logicalBytes), (toUnits $logicalBytes) | Tee-Object -FilePath $outfileName -Append
+    if($viewName -notin $entityLogical.Keys){
+        $entityLogical[$viewName] = $logicalBytes
+        "{0},{1},{2},""{3}"",""{4}""" -f $jobName, $viewName, 'View', (toUnits $logicalBytes), (toUnits $logicalBytes) | Tee-Object -FilePath $outfileName -Append
+    }
 }
 
 "`nOutput written to $outfileName"
