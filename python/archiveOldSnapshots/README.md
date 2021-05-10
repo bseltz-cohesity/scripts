d7f11cea63535e587aa2aeeb1eb9a047cce78406
bseltz-cohesity <bseltzer@cohesity.com>
2021-05-10 08:48:31 -0400

    2021-05-10

---
+++ python/archiveOldSnapshots/README.md
@@ -0,0 +1,54 @@
+# Archive Old Snapshots using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script archives old existing snapshots.
+
+## Components
+
+* archiveOldSnapshots.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/archiveOldSnapshots/archiveOldSnapshots.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x archiveOldSnapshots.py
+
+```
+
+Place both files in a folder together and run the main script like so:
+
+This command will archive all unexipired snapshots (from all jobs) that have run in the past 31 days (that haven't already been archived already) and keep them in the archive for 90 days.
+
+```bash
+./archiveOldSnapshots.py -v mycluster \
+                         -u myuser \
+                         -d mydomain.net \ 
+                         -t myS3bucket \
+                         -n 31 \
+                         -k 90
+```
+
+You can filter on local jobs only (-l) or replicated jobs only (-r), or you can provide include or exclude jobs via text files (-j ./includejobs.txt or -x ./excludejobs.txt or both).
+
+By default, the script will only show what it would do. To actually execute the archives, include the -f switch.
+
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -i, --useApiKey: (optional) use API key for authentication
+* -p, --password: (optional) password or API key
+* -k, --keepfor: (optional) keep for X days in the archive
+* -t, --target: name of the external target
+* -n, --daysback: (optional) number of days back to look for snapshots to archive (default is 31)
+* -j, --joblist: (optional) text file of job names to include (default is all jobs)
+* -x, --excludelist: (optional) text file of job names to exclude
+* -l, --localonly: (optional) only archive local jobs
+* -r, --replicasonly: (optional) only archive replica jobs
+* -e, --excludelogs: (optional) do not archive database log backups
+* -f, --force: (optional) perform archives (show only if omitted)
---
+++ python/archiveOldSnapshots/archiveOldSnapshots.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""Archive Now for python - version 2021.05.10"""
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)       # cluster to connect to
+parser.add_argument('-u', '--username', type=str, required=True)  # username
+parser.add_argument('-d', '--domain', type=str, default='local')  # (optional) domain - defaults to local
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-p', '--password', type=str, default=None)
+parser.add_argument('-k', '--keepfor', type=int, required=True)    # (optional) will use policy retention if omitted
+parser.add_argument('-t', '--target', type=str, required=True)  # (optional) will use policy target if omitted
+parser.add_argument('-r', '--replicasonly', action='store_true')
+parser.add_argument('-l', '--localonly', action='store_true')
+parser.add_argument('-f', '--force', action='store_true')
+parser.add_argument('-e', '--excludelogs', action='store_true')
+parser.add_argument('-n', '--daysback', type=int, default=31)
+parser.add_argument('-j', '--joblist', type=str, default=None)
+parser.add_argument('-x', '--excludelist', type=str, default=None)
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+useApiKey = args.useApiKey
+password = args.password
+keepfor = args.keepfor
+target = args.target
+replicasonly = args.replicasonly
+localonly = args.localonly
+force = args.force
+excludelogs = args.excludelogs
+daysback = args.daysback
+joblist = args.joblist
+excludelist = args.excludelist
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+if apiconnected() is False:
+    print('\nFailed to connect to Cohesity cluster')
+    exit(1)
+
+jobnames = []
+excludejobnames = []
+
+jobs = api('get', 'protectionJobs')
+
+if localonly:
+    jobs = [j for j in jobs if 'isActive' not in j or j['isActive'] is True]
+
+if replicasonly:
+    jobs = [j for j in jobs if 'isActive' in j and j['isActive'] is False]
+
+if joblist is not None:
+    f = open(joblist, 'r')
+    jobnames += [s.strip().lower() for s in f.readlines() if s.strip() != '']
+    f.close()
+    jobs = [j for j in jobs if j['name'].lower() in jobnames]
+
+if excludelist is not None:
+    f = open(excludelist, 'r')
+    excludejobnames += [s.strip().lower() for s in f.readlines() if s.strip() != '']
+    f.close()
+    jobs = [j for j in jobs if j['name'].lower() not in excludejobnames]
+
+vault = [vault for vault in api('get', 'vaults') if vault['name'].lower() == target.lower()]
+if len(vault) > 0:
+    vault = vault[0]
+    target = {
+        "vaultId": vault['id'],
+        "vaultName": vault['name'],
+        "vaultType": "kCloud"
+    }
+else:
+    print('external target %s not found' % target)
+    exit(1)
+
+startTimeUsecs = timeAgo(daysback, 'days')
+
+for job in sorted(jobs, key=lambda job: job['name'].lower()):
+    runs = api('get', 'protectionRuns?jobId=%s&startTimeUsecs=%s' % (job['id'], startTimeUsecs))
+    runs = [r for r in runs if r['backupRun']['snapshotsDeleted'] is not True]
+    if excludelogs is True:
+        runs = [r for r in runs if r['backupRun']['runType'] != 'kLog']
+    for run in sorted(runs, key=lambda run: run['backupRun']['stats']['startTimeUsecs']):
+        daysToKeep = keepfor
+        thisrun = api('get', '/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=%s&excludeTasks=true&id=%s' % (run['backupRun']['stats']['startTimeUsecs'], run['jobId']))
+        jobUid = thisrun[0]['backupJobRuns']['protectionRuns'][0]['backupRun']['base']['jobUid']
+
+        thisRunArchived = False
+        for copyRun in run['copyRun']:
+
+            # resync existing archive run
+            if copyRun['target']['type'] == 'kArchival':
+                thistarget = copyRun['target']['archivalTarget']
+                thisstatus = copyRun['status']
+                if thistarget['vaultName'].lower() == target['vaultName'].lower() and thisstatus == 'kSuccess':
+                    thisRunArchived = True
+
+        if thisRunArchived is False:
+            # configure archive task
+            archiveTask = {
+                "jobRuns": [
+                    {
+                        "copyRunTargets": [
+                            {
+                                "archivalTarget": target,
+                                "type": "kArchival"
+                            }
+                        ],
+                        "runStartTimeUsecs": run['copyRun'][0]['runStartTimeUsecs'],
+                        "jobUid": {
+                            "clusterId": jobUid['clusterId'],
+                            "clusterIncarnationId": jobUid['clusterIncarnationId'],
+                            "id": jobUid['objectId']
+                        }
+                    }
+                ]
+            }
+
+            daysToKeep = daysToKeep - dayDiff(dateToUsecs(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), run['copyRun'][0]['runStartTimeUsecs'])
+            archiveTask['jobRuns'][0]['copyRunTargets'][0]['daysToKeep'] = int(daysToKeep)
+
+            # perform archive
+            if force:
+                print('archiving %s (%s) -> %s...' % (job['name'], usecsToDate(run['copyRun'][0]['runStartTimeUsecs']), target['vaultName']))
+                result = api('put', 'protectionRuns', archiveTask)
+            else:
+                print('%s (%s) -> %s' % (job['name'], usecsToDate(run['copyRun'][0]['runStartTimeUsecs']), target['vaultName']))
