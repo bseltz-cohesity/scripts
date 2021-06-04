7f5300e36da5565b99d5042433dd11eba8dabc58
bseltz-cohesity <bseltzer@cohesity.com>
2021-06-04 10:43:34 -0400

    2021-06-04

--- powershell/recoveryPoints/recoveryPoints.ps1
+++ powershell/recoveryPoints/recoveryPoints.ps1
@@ -12,20 +12,20 @@ param (
 apiauth -vip $vip -username $username -domain $domain
 
 $dateString = (get-date).ToString().Replace(' ','_').Replace('/','-').Replace(':','-')
-$outfileName = "RecoverPoints-$dateString.csv"
-"Job Name,Job Type,Protected Object,Recovery Date,Local Expiry,Remote Expiry,Archival Expiry,Run URL" | Out-File -FilePath $outfileName
+$outfileName = "RecoveryPoints-$dateString.csv"
+"Job Name,Job Type,Protected Object,Recovery Date,Local Expiry,Archival Expiry,Archive Target,Run URL" | Out-File -FilePath $outfileName
 
 ### find recoverable objects
 $ro = api get /searchvms
 
-$environments = @('Unknown', 'VMware', 'HyperV', 'SQL', 'View',
-                  'RemoteAdapter', 'Physical', 'Pure', 'Azure', 'Netapp',
-                  'Agent', 'GenericNas', 'Acropolis', 'PhysicalFiles',
-                  'Isilon', 'KVM', 'AWS', 'Exchange', 'HyperVVSS',
-                  'Oracle', 'GCP', 'FlashBlade', 'AWSNative', 'VCD',
-                  'O365', 'O365Outlook', 'HyperFlex', 'GCPNative',
-                  'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown',
-                  'Unknown', 'Unknown', 'Unknown') 
+$environments = @('kUnknown', 'kVMware', 'kHyperV', 'kSQL', 'kView', 'kPuppeteer',
+                'kPhysical', 'kPure', 'kAzure', 'kNetapp', 'kAgent', 'kGenericNas',
+                'kAcropolis', 'kPhysicalFiles', 'kIsilon', 'kKVM', 'kAWS', 'kExchange',
+                'kHyperVVSS', 'kOracle', 'kGCP', 'kFlashBlade', 'kAWSNative', 'kVCD',
+                'kO365', 'kO365Outlook', 'kHyperFlex', 'kGCPNative', 'kAzureNative', 
+                'kAD', 'kAWSSnapshotManager', 'kGPFS', 'kRDSSnapshotManager', 'kUnknown', 'kKubernetes',
+                'kNimble', 'kAzureSnapshotManager', 'kElastifile', 'kCassandra', 'kMongoDB',
+                'kHBase', 'kHive', 'kHdfs', 'kCouchbase', 'kUnknown', 'kUnknown', 'kUnknown')
 
 if($ro.count -gt 0){
     $ro.vms | Sort-Object -Property {$_.vmDocument.jobName}, {$_.vmDocument.objectName } | ForEach-Object {
@@ -46,40 +46,34 @@ if($ro.count -gt 0){
         }
         write-host ("`n{0} ({1}) {2}" -f $jobName, $objType, $objName) -ForegroundColor Green 
         $versionList = @()
-        $doc.versions | ForEach-Object {  
-            $version = $_
+        foreach($version in $doc.versions){
+        # $doc.versions | ForEach-Object {  
+            # $version = $_
             $runId = $version.instanceId.jobInstanceId
             $startTime = $version.instanceId.jobStartTimeUsecs
             $local = 0
             $remote = 0
+            $remoteCluster = ''
             $archive = 0
-
-            $version.replicaInfo.replicaVec | ForEach-Object {
-                if($_.target.type -eq 1){
-                    $local = $_.expiryTimeUsecs
-                }elseif($_.target.type -eq 2){
-                    if($_.expiryTimeUsecs -gt $remote){
-                        $remote = $_.expiryTimeUsecs
-                    }
-                }elseif($_.target.type -eq 3) {
-                    if($_.expiryTimeUsecs -gt $archive){
-                        $archive = $_.expiryTimeUsecs
+            $archiveTarget = ''
+            foreach($replica in $version.replicaInfo.replicaVec){
+                if($replica.target.type -eq 1){
+                    $local = $replica.expiryTimeUsecs
+                }elseif($replica.target.type -eq 3) {
+                    if($replica.expiryTimeUsecs -gt $archive){
+                        $archive = $replica.expiryTimeUsecs
+                        $archiveTarget = $replica.target.archivalTarget.name
                     }
                 }
             }
-            $versionList += @{'RunDate' = $startTime; 'local' = $local; 'remote' = $remote; 'archive' = $archive; 'runId' = $runId; 'startTime' = $startTime}
+            $versionList += @{'RunDate' = $startTime; 'local' = $local; 'archive' = $archive; 'archiveTarget' = $archiveTarget; 'runId' = $runId; 'startTime' = $startTime}
         }
-        write-host "`n`t             RunDate           SnapExpires        ReplicaExpires        ArchiveExpires" -ForegroundColor Blue
+        write-host "`n`t             RunDate           SnapExpires        ArchiveExpires" -ForegroundColor Blue
         foreach($version in $versionList){
             if($version['local'] -eq 0){
                 $local = '-'
             }else{
                 $local = usecsToDate $version['local']
-            }
-            if($version['remote'] -eq 0){
-                $remote = '-'
-            }else{
-                $remote = usecsToDate $version['remote']
             }
             if($version['archive'] -eq 0){
                 $archive = '-'
@@ -87,10 +81,10 @@ if($ro.count -gt 0){
                 $archive = usecsToDate $version['archive']
             }
             $runDate = usecsToDate $version['RunDate']
-            "`t{0,20}  {1,20}  {2,20}  {3,20}" -f $runDate, $local, $remote, $archive
+            "`t{0,20}  {1,20}  {2,20}" -f $runDate, $local, $archive
             
             $runURL = "https://$vip/protection/job/$jobId/run/$($version['runId'])/$($version['startTime'])/protection"
-            "$jobName,$objType,$objName,$runDate,$local,$remote,$archive,$runURL" | Out-File -FilePath $outfileName -Append
+            "$jobName,$objType,$objName,$runDate,$local,$archive,$($version['archiveTarget']),$runURL" | Out-File -FilePath $outfileName -Append
         }
     }
     write-host "`nReport Saved to $outFileName`n" -ForegroundColor Blue
