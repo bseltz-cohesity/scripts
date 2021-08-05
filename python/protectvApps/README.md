2734bffd168629b12b2289df39a777361194ff3a
bseltz-cohesity <bseltzer@cohesity.com>
2021-08-05 12:31:33 -0400

    2021-08-05

---
+++ python/protectvApps/README.md
@@ -0,0 +1,56 @@
+# Protect vCloud Director vApps Using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script protects all the vApps of a vCloud virtual datacenter.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/protectvApps/protectvApps.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x protectvApps.py
+# end download commands
+```
+
+## Components
+
+* protectvApps.py: the main powershell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./protectvApps.py -v mycluster \
+                  -u myuser \
+                  -d mydomain.net \
+                  -j 'My Backup Job' \
+                  -s myvcloud.mydomain.net \
+                  -o myorg \
+                  -c myvdc \
+                  -j myjobname \
+                  -p mypolicy \
+                  -t kvAppTemplate
+```
+
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -k, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password of API key
+* -s, --sourcename: name of registered vCloud source
+* -o, --orgname: name of vCloud organization
+* -c, --vdcname: name of virtual data center
+* -t, --vapptype: (optional) all, kVirtualApp or kvAppTemplate
+* -j, --jobname: name of the job to add the server to
+* -sd, --storagedomain: (optional) name of storage domain to create job in (default is DefaultStorageDomain)
+* -p, --policyname: (optional) name of protection policy to use for new job (only required for new job)
+* -tz, --timezone: (optional) time zone for new job (default is US/Eastern)
+* -st, --starttime: (optional) start time for new job (default is 21:00)
+* -is, --incrementalsla: (optional) incremental SLA minutes (default is 60)
+* -fs, --fullsla: (optional) full SLA minutes (default is 120)
---
+++ python/protectvApps/protectvApps.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+"""Add Physical Linux Servers to File-based Protection Job Using Python"""
+
+### import pyhesity wrapper module
+from pyhesity import *
+
+### command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)
+parser.add_argument('-u', '--username', type=str, required=True)
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-k', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-s', '--sourcename', type=str, required=True)
+parser.add_argument('-o', '--orgname', type=str, required=True)
+parser.add_argument('-c', '--vdcname', type=str, required=True)
+parser.add_argument('-t', '--vapptype', type=str, choices=['all', 'kVirtualApp', 'kvAppTemplate'], default='all')
+parser.add_argument('-j', '--jobname', type=str, required=True)
+parser.add_argument('-sd', '--storagedomain', type=str, default='DefaultStorageDomain')
+parser.add_argument('-p', '--policyname', type=str, default=None)
+parser.add_argument('-tz', '--timezone', type=str, default='US/Eastern')
+parser.add_argument('-st', '--starttime', type=str, default='21:00')
+parser.add_argument('-is', '--incrementalsla', type=int, default=60)    # incremental SLA minutes
+parser.add_argument('-fs', '--fullsla', type=int, default=120)          # full SLA minutes
+
+args = parser.parse_args()
+
+vip = args.vip                        # cluster name/ip
+username = args.username              # username to connect to cluster
+domain = args.domain                  # domain of username (e.g. local, or AD domain)
+password = args.password              # password or API key
+useApiKey = args.useApiKey            # use API key for authentication
+sourcename = args.sourcename          # name of vcd source to protect
+orgname = args.orgname                # name of vcd org to protect
+vdcname = args.vdcname                # name of org vdc to protect
+vapptype = args.vapptype              # type to protect
+jobname = args.jobname                # name of protection job to add server to
+storagedomain = args.storagedomain    # storage domain for new job
+policyname = args.policyname          # policy name for new job
+starttime = args.starttime            # start time for new job
+timezone = args.timezone              # time zone for new job
+incrementalsla = args.incrementalsla  # incremental SLA for new job
+fullsla = args.fullsla                # full SLA for new job
+
+# authenticate to Cohesity
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+
+# get VCD registered source
+sources = [s for s in api('get', 'protectionSources?environments=kVMware') if s['protectionSource']['vmWareProtectionSource']['type'] == 'kvCloudDirector' and s['protectionSource']['name'].lower() == sourcename.lower()]
+if not sources or len(sources) == 0:
+    print('VCD source %s not registered' % sourcename)
+    exit(1)
+else:
+    source = sources[0]
+
+# get VCD Organization
+orgs = [o for o in source['nodes'] if o['protectionSource']['vmWareProtectionSource']['type'] == 'kOrganization' and o['protectionSource']['name'].lower() == orgname.lower()]
+if not orgs or len(orgs) == 0:
+    print('VCD Org %s not found' % orgname)
+    exit(1)
+else:
+    org = orgs[0]
+
+# get VCD Virtual Datacenter
+vdcs = [v for v in org['nodes'] if v['protectionSource']['vmWareProtectionSource']['type'] == 'kVirtualDatacenter' and v['protectionSource']['name'].lower() == vdcname.lower()]
+if not vdcs or len(vdcs) == 0:
+    print("VCD Vdc %s not found" % vdcname)
+    exit(1)
+else:
+    vdc = vdcs[0]
+
+# get vApps
+vapps = vdc['nodes']
+if vapptype != 'all':
+    vapps = [v for v in vapps if v['protectionSource']['vmWareProtectionSource']['type'].lower() == vapptype.lower()]
+
+vappids = [v['protectionSource']['id'] for v in vapps]
+if len(vappids) == 0:
+    print('No vApps found to protect')
+    exit(1)
+
+# get job info
+newJob = False
+job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobname.lower()]
+
+if not job or len(job) == 0:
+
+    newJob = True
+
+    # find protectionPolicy
+    if policyname is None:
+        print('Policy name required for new job')
+        exit(1)
+    policy = [p for p in api('get', 'protectionPolicies') if p['name'].lower() == policyname.lower()]
+    if len(policy) < 1:
+        print("Policy '%s' not found!" % policyname)
+        exit(1)
+    policyid = policy[0]['id']
+
+    # find storage domain
+    sd = [sd for sd in api('get', 'viewBoxes') if sd['name'].lower() == storagedomain.lower()]
+    if len(sd) < 1:
+        print("Storage domain %s not found!" % storagedomain)
+        exit(1)
+    sdid = sd[0]['id']
+
+    # parse starttime
+    try:
+        (hour, minute) = starttime.split(':')
+        hour = int(hour)
+        minute = int(minute)
+        if hour < 0 or hour > 23 or minute < 0 or minute > 59:
+            print('starttime is invalid!')
+            exit(1)
+    except Exception:
+        print('starttime is invalid!')
+        exit(1)
+
+    job = {
+        "policyId": policyid,
+        "environment": "kVMware",
+        "parentSourceId": source['protectionSource']['id'],
+        "LeverageSanTransport": None,
+        "timezone": timezone,
+        "viewBoxId": sdid,
+        "priority": "kLow",
+        "name": jobname,
+        "indexingPolicy": {
+            "allowPrefixes": [
+                "/"
+            ],
+            "disableIndexing": False,
+            "denyPrefixes": [
+                "/$Recycle.Bin",
+                "/Windows",
+                "/Program Files",
+                "/Program Files (x86)",
+                "/ProgramData",
+                "/System Volume Information",
+                "/Users/*/AppData",
+                "/Recovery",
+                "/var",
+                "/usr",
+                "/sys",
+                "/proc",
+                "/lib",
+                "/grub",
+                "/grub2"
+            ]
+        },
+        "sourceIds": vappids,
+        "startTime": {
+            "minute": minute,
+            "hour": hour
+        }
+    }
+
+else:
+    job = job[0]
+    job['sourceIds'] += vappids
+
+# update job
+if newJob is True:
+    print("Creating new Job '%s'" % jobname)
+    result = api('post', 'protectionJobs', job)
+else:
+    print("Updating Job '%s'" % jobname)
+    result = api('put', 'protectionJobs/%s' % job['id'], job)
