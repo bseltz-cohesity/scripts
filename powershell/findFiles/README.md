015e7f9ee35110ccc4dbf52f34e3d24b93417927
bseltz-cohesity <bseltzer@cohesity.com>
2021-08-18 06:23:06 -0400

    2021-08-18

--- powershell/findFiles/README.md
+++ powershell/findFiles/README.md
@@ -25,39 +25,7 @@ $repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/pow
 Place both files in a folder together and run the main script like so:
 
 ```powershell
-./findFiles.ps1 -vip mycluster -username myusername -domain mydomain.net -extension gif
-```
-
-```text
-Found 25 results
-
-Server   Path
-------   ----
-vCenter6 lvol_1/opt/vmware/lib/python2.7/email/test/data/PyBanner048.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/folder.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/idle_16.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/idle_32.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/idle_48.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/minusnode.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/openfolder.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/plusnode.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/python.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/idlelib/Icons/tk.gif
-vCenter6 lvol_1/opt/vmware/lib/python2.7/test/imghdrdata/python.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/css/img/leftArrow.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/css/img/rightArrow.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/indeterminate.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/leftArrow.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/loader-spinner.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/loading_2x.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/loading-image.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/loading.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/Marge-anim-progressbar.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/rightArrow.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/slider-h.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/slider-v.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/img/whiteSpinner.gif
-vCenter6 lvol_1/opt/vmware/share/htdocs/libs/img/loader-spinner.gif
+./findFiles.ps1 -vip mycluster -username myusername -domain mydomain.net -searchString gif -extensionOnly
 ```
 
 ## Parameters
@@ -65,5 +33,8 @@ vCenter6 lvol_1/opt/vmware/share/htdocs/libs/img/loader-spinner.gif
 * -vip: Cohesity cluster to connect to
 * -username: Cohesity username
 * -domain: (optional) Active Directory domain (defaults to 'local')
-* -extension: last few characters of the file extention you are looking for
-* -getMtime: get last modified date of each file
+* -searchString: last few characters of the file extention you are looking for
+* -getMtime: (optional) get last modified date of each file
+* -extensionOnly: (optional) only return results where the searchString is the file extension
+* -throttle: (optional) pause for X seconds between paged results
+* -pageSize: (optional) 1000 or less (default is 1000)
--- powershell/findFiles/findFiles.ps1
+++ powershell/findFiles/findFiles.ps1
@@ -4,8 +4,11 @@ param (
     [Parameter(Mandatory = $True)][string]$vip,
     [Parameter(Mandatory = $True)][string]$username,
     [Parameter()][string]$domain = 'local',
-    [Parameter(Mandatory = $True)][string]$extension,
-    [Parameter()][switch]$getMtime
+    [Parameter(Mandatory = $True)][string]$searchString,
+    [Parameter()][switch]$extensionOnly,
+    [Parameter()][switch]$getMtime,
+    [Parameter()][int]$throttle = 0,
+    [Parameter()][int]$pageSize = 1000
 )
 
 # source the cohesity-api helper code
@@ -20,50 +23,54 @@ $jobs = api get protectionJobs
 
 "JobName,Parent,Object,Path,Last Modified" | Out-File -FilePath foundFiles.csv
 
-$results = api get "/searchfiles?filename=$($extension)"
-$results = api get "restore/files?paginate=true&pageSize=1000&search=$($extension)"
+$results = api get "restore/files?paginate=true&pageSize=$pageSize&search=$($searchString)"
 $oldcookie = $null
 while($True){
     if($results.files.count -gt 0){
-        $output = $results.files | where-object { $_.isFolder -ne $True -and $_.filename -match $extension+'$'} |
-                        Sort-Object -Property {$_.protectionSource.name}, {$_.filename} |
-                        ForEach-Object {
-                            $objectName = $_.protectionSource.name
-                            $fileName = $_.filename
-                            $parentName = ''
-                            $parentId = $_.protectionSource.parentId
-                            $jobId = $_.jobUid.id
-                            $clusterId = $_.jobUid.clusterId
-                            $clusterIncarnationId = $_.jobUid.clusterIncarnationId
-                            $sourceId = $_.protectionSource.id
-                            $jobName = ($jobs | Where-Object id -eq $jobId).name
-                            if($parentId){
-                                $parent = $sources | Where-Object {$_.protectionSource.id -eq $parentId}
-                                if($parent){
-                                    $parentName = $parent.protectionSource.name
-                                }
-                            }
-                            if($getMtime){
-                                $mtime = ''
-
-                                $thisFileName = [System.Web.HttpUtility]::UrlEncode($_.fileName).Replace('%2f%2f','%2F')
-                                $snapshots = api get -v2 "data-protect/objects/$sourceId/protection-groups/$clusterId`:$clusterIncarnationId`:$jobId/indexed-objects/snapshots?indexedObjectName=$thisFileName&includeIndexedSnapshotsOnly=true"
-                                # write-host "data-protect/objects/$sourceId/protection-groups/$clusterId`:$clusterIncarnationId`:$jobId/indexed-objects/snapshots?indexedObjectName=$thisFileName&includeIndexedSnapshotsOnly=false"
-                                $mtimeUsecs = $snapshots.snapshots[0].lastModifiedTimeUsecs
-                                if($mtimeUsecs){
-                                    $mtime = usecsToDate $mtimeUsecs
-                                }
-                            } 
-                            write-host ("{0},{1},{2},{3}" -f $parentName, $objectName, $fileName, $mtime)
-                            "{0},{1},{2},{3},{4}" -f $jobName, $parentName, $objectName, $fileName, $mtime | Out-File -FilePath foundFiles.csv -Append
-                        }
+        $output = $results.files | where-object { $_.isFolder -ne $True } | Sort-Object -Property {$_.protectionSource.name}, {$_.filename}
+        if($extensionOnly){
+            $output = $output | Where-Object {$_.fileName -match $searchString+'$'}
+        }
+        foreach($result in $output){                       
+            $objectName = $result.protectionSource.name
+            $fileName = $result.filename
+            $parentName = ''
+            $parentId = $result.protectionSource.parentId
+            $jobId = $result.jobUid.id
+            $clusterId = $result.jobUid.clusterId
+            $clusterIncarnationId = $result.jobUid.clusterIncarnationId
+            $sourceId = $result.protectionSource.id
+            $jobName = ($jobs | Where-Object id -eq $jobId).name
+            if($parentId){
+                $parent = $sources | Where-Object {$_.protectionSource.id -eq $parentId}
+                if($parent){
+                    $parentName = $parent.protectionSource.name
+                }
+            }
+            if($getMtime){
+                $mtime = ''
+                $thisFileName = [System.Web.HttpUtility]::UrlEncode($result.fileName).Replace('%2f%2f','%2F')
+                $snapshots = api get -v2 "data-protect/objects/$sourceId/protection-groups/$clusterId`:$clusterIncarnationId`:$jobId/indexed-objects/snapshots?indexedObjectName=$thisFileName&includeIndexedSnapshotsOnly=true"
+                if($snapshots.snapshots.Count -gt 0){
+                    $mtimeUsecs = $snapshots.snapshots[0].lastModifiedTimeUsecs
+                    if($mtimeUsecs){
+                        $mtime = usecsToDate $mtimeUsecs
+                    }
+                }
+            } 
+            write-host ("{0},{1},{2},{3}" -f $parentName, $objectName, $fileName, $mtime)
+            "{0},{1},{2},{3},{4}" -f $jobName, $parentName, $objectName, $fileName, $mtime | Out-File -FilePath foundFiles.csv -Append
+        }
     }else{
         break
     }
     if($results.paginationCookie){
+        if($throttle -gt 0){
+            Start-Sleep $throttle
+        }
         $oldcookie = $results.paginationCookie
         while($results.paginationCookie -eq $oldcookie -and $results){
-                $results = api get "restore/files?paginate=true&pageSize=1000&paginationCookie=$($results.paginationCookie)&search=$($extension)"
+                $results = api get "restore/files?paginate=true&pageSize=$pageSize&paginationCookie=$($results.paginationCookie)&search=$($searchString)"
                 if(! $results){
                     "retrying..."
                     Start-Sleep 2
