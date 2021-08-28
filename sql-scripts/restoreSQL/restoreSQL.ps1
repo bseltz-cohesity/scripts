458b13968c99209a6648c39508f521c08f54eeb4
bseltz-cohesity <bseltzer@cohesity.com>
2021-08-28 09:37:58 -0400

    2021-08-28

--- sql-scripts/restoreSQL/restoreSQL.ps1
+++ sql-scripts/restoreSQL/restoreSQL.ps1
@@ -268,7 +268,7 @@ if($targetDB -ne $sourceDB -or $targetServer -ne $sourceServer -or $differentIns
         $ldfFolderFound = $False
         foreach($datafile in $latestdb.vmDocument.objectId.entity.sqlEntity.dbFileInfoVec){
             $path = $datafile.fullPath.subString(0, $datafile.fullPath.LastIndexOf('\'))
-            $fileName = $datafile.fullPath.subString($datafile.fullPath.LastIndexOf('\') + 1)
+            # $fileName = $datafile.fullPath.subString($datafile.fullPath.LastIndexOf('\') + 1)
             if($datafile.type -eq 0){
                 if($mdfFolderFound -eq $False){
                     $mdfFolder = $path
@@ -382,10 +382,18 @@ if($wait -or $progress){
         $status = (api get /restoretasks/$taskId).restoreTask.performRestoreTaskState.base.publicStatus
         if($progress){
             $progressMonitor = api get "/progressMonitors?taskPathVec=restore_sql_$($taskId)&includeFinishedTasks=true&excludeSubTasks=false"
-            $percentComplete = $progressMonitor.resultGroupVec[0].taskVec[0].progress.percentFinished
-            if($percentComplete -gt $lastProgress){
-                "{0} percent complete" -f [math]::Round($percentComplete, 0)
-                $lastProgress = $percentComplete
+            if($progressMonitor.PSObject.Properties['resultGroupVec']){
+                if($progressMonitor.resultGroupVec.Count -gt 0){
+                    if($progressMonitor.resultGroupVec[0].PSObject.Properties['taskVec']){
+                        if($progressMonitor.resultGroupVec[0].taskVec.Count -gt 0){
+                            $percentComplete = $progressMonitor.resultGroupVec[0].taskVec[0].progress.percentFinished
+                            if($percentComplete -gt $lastProgress){
+                                "{0} percent complete" -f [math]::Round($percentComplete, 0)
+                                $lastProgress = $percentComplete
+                            }
+                        }
+                    }
+                }
             }
         }
         if ($status -in $finishedStates){
