47083e5989f3212e356ac8f1d5aedc9a40ce7ffd
bseltz-cohesity <bseltzer@cohesity.com>
2021-09-03 07:31:08 -0400

    2021-09-03

---
+++ python/cloneBackupToView/README.md
@@ -0,0 +1,49 @@
+# Clone Backup Data to a View Using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script clones backup files to an NFS view.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/cloneBackupToView/cloneBackupToView.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x cloneBackupToView.py
+# end download commands
+```
+
+## Components
+
+* cloneBackupToView.py: the main powershell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./cloneBackupToView.py -v mycluster \
+                       -u myuser \
+                       -d mydomain.net \
+                       -j 'My Backup Job' \
+                       -n myviewname \
+                       -w 192.168.1.0,255.255.255.0 \
+                       -w 192.168.2.23
+```
+
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -j, --jobname: (optional) name of the job to clone
+* -n, --viewname: name of view to use or create
+* -q, --qospolicy: (optional) 'Backup Target Low', 'Backup Target High', 'TestAndDev High' or 'TestAndDev Low' (default is 'TestAndDev High')
+* -w, --whitelist: (optional) e.g. '192.168.1.0,255.255.255.0' or '192.168.2.23' (repeast for multiple entries)
+* -x, --deleteview: (optional) delext existing view and exit
+
+## Note
+
+The Protection Job and the View must be in the same Storage Domain.
---
+++ python/cloneBackupToView/cloneBackupToView.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+"""Create a Cohesity NFS View Using python"""
+
+# import pyhesity wrapper module
+from pyhesity import *
+from time import sleep
+from datetime import datetime
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)  # Cohesity cluster name or IP
+parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
+parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
+parser.add_argument('-n', '--viewname', type=str, required=True)  # name view to create
+parser.add_argument('-q', '--qospolicy', type=str, choices=['Backup Target Low', 'Backup Target High', 'TestAndDev High', 'TestAndDev Low'], default='TestAndDev High')  # qos policy
+parser.add_argument('-w', '--whitelist', action='append', default=[])  # ip to whitelist
+parser.add_argument('-x', '--deleteview', action='store_true')  # delete existing view
+parser.add_argument('-j', '--jobname', type=str, default=None)  # name job to clone
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+viewName = args.viewname
+qosPolicy = args.qospolicy
+whitelist = args.whitelist
+deleteview = args.deleteview
+jobname = args.jobname
+
+
+# netmask2cidr
+def netmask2cidr(netmask):
+    bin = ''.join(["{0:b}".format(int(o)) for o in netmask.split('.')])
+    if '0' in bin:
+        cidr = bin.index('0')
+    else:
+        cidr = 32
+    return cidr
+
+
+# authenticate
+apiauth(vip, username, domain)
+
+if deleteview is not True:
+
+    if jobname is None:
+        print('-j, --jobname required!')
+        exit(1)
+
+    # get protection job
+    job = [job for job in api('get', 'protectionJobs') if job['name'].lower() == jobname.lower()]
+    if not job:
+        print("Job '%s' not found" % jobname)
+        exit(1)
+    else:
+        job = job[0]
+        sdid = job['viewBoxId']
+
+existingview = None
+views = api('get', 'views')
+if views['count'] > 0:
+    existingviews = [v for v in views['views'] if v['name'].lower() == viewName.lower()]
+    if(len(existingviews) > 0):
+        existingview = existingviews[0]
+
+if existingview is None and deleteview is not True:
+
+    # new view parameters
+    newView = {
+        "caseInsensitiveNamesEnabled": True,
+        "enableNfsViewDiscovery": True,
+        "enableSmbAccessBasedEnumeration": False,
+        "enableSmbViewDiscovery": True,
+        "fileExtensionFilter": {
+            "isEnabled": False,
+            "mode": "kBlacklist",
+            "fileExtensionsList": []
+        },
+        "protocolAccess": "kNFSOnly",
+        "securityMode": "kNativeMode",
+        "subnetWhitelist": [],
+        "qos": {
+            "principalName": qosPolicy
+        },
+        "name": viewName,
+        "viewBoxId": sdid
+    }
+
+    if len(whitelist) > 0:
+
+        for ip in whitelist:
+            if ',' in ip:
+                (thisip, netmask) = ip.split(',')
+                netmask = netmask.lstrip()
+                cidr = netmask2cidr(netmask)
+            else:
+                thisip = ip
+                netmask = '255.255.255.255'
+                cidr = 32
+
+            newView['subnetWhitelist'].append({
+                "description": '',
+                "nfsAccess": "kReadWrite",
+                "smbAccess": "kReadWrite",
+                "nfsRootSquash": False,
+                "ip": thisip,
+                "netmaskIp4": netmask
+            })
+
+    print("Creating new view %s..." % viewName)
+    result = api('post', 'views', newView)
+    sleep(5)
+    views = api('get', 'views')
+    if views['count'] > 0:
+        existingviews = [v for v in views['views'] if v['name'].lower() == viewName.lower()]
+        if(len(existingviews) > 0):
+            view = existingviews[0]
+
+else:
+    if deleteview is True:
+        if existingview:
+            print("Deleting view %s..." % viewName)
+            result = api('delete', 'views/%s' % viewName)
+        else:
+            print("View %s does not exist" % viewName)
+        exit(0)
+    else:
+        if existingview['viewBoxId'] != job['viewBoxId']:
+            print('View and job must be in the same storage domain!')
+            exit(1)
+        print("Using existing view: %s" % viewName)
+        view = existingview
+
+
+successStates = ['kSuccess', 'kWarning']
+
+# get runs
+runs = [r for r in api('get', 'protectionRuns?jobId=%s' % job['id']) if r['backupRun']['snapshotsDeleted'] is False and r['backupRun']['status'] in successStates]
+if len(runs) > 0:
+    run = runs[0]
+else:
+    print('No runs found for job %s' % jobname)
+    exit(1)
+
+for sourceInfo in run['backupRun']['sourceBackupStatus']:
+    thisObjectName = sourceInfo['source']['name']
+    if sourceInfo['status'] in successStates:
+        sourceView = sourceInfo['currentSnapshotInfo']['viewName']
+        if 'relativeSnapshotDirectory' in sourceInfo['currentSnapshotInfo']:
+            sourcePath = sourceInfo['currentSnapshotInfo']['relativeSnapshotDirectory']
+        else:
+            sourcePath = ''
+
+        starttimeString = datetime.strptime(usecsToDate(run['backupRun']['stats']['startTimeUsecs']), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d_%H-%M-%S')
+        destinationPath = "%s-%s" % (thisObjectName, starttimeString)
+        CloneDirectoryParams = {
+            'destinationDirectoryName': destinationPath,
+            'destinationParentDirectoryPath': '/%s' % view['name'],
+            'sourceDirectoryPath': '/%s/%s' % (sourceView, sourcePath),
+        }
+        folderPath = "%s:/%s/%s" % (vip, viewName, destinationPath)
+        print("Cloning %s backup files to %s" % (thisObjectName, folderPath))
+        result = api('post', 'views/cloneDirectory', CloneDirectoryParams)
