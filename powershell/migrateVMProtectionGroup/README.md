1ca6cf8a368b66311baf5a65a84deb1552cdb8ca
bseltz-cohesity <bseltzer@cohesity.com>
2021-10-17 07:20:48 -0400

    2021-10-17

--- powershell/migrateVMProtectionGroup/README.md
+++ powershell/migrateVMProtectionGroup/README.md
@@ -4,6 +4,16 @@ Warning: this code is provided on a best effort basis and is not in any way offi
 
 This PowerShell script migrates a VMware protection group from one Cohesity cluster to another.
 
+## Notes and Warnings
+
+If you omit the `-deleteOldJob` and `-deleteOldJobAndExit` switches, the job will be left behind on the source cluster, and this is recommended in case the script fails to migrate the job to the target cluster for some reason. If a failure occurs, the script can be run again after the issue has been figured out.
+
+After the migration is successful, you can rerun the script using the `-deleteOldJobAndExit` parameter to delete the old job from the source cluster. Add `-deleteOldSnapshots` if you want to delete the existing backups on the source cluster.
+
+Alternatively, if you're comfortable that the script works fine and migrations are consistently successful, then you can include the `-deleteOldJob` switch in your command, which will delete the old job durng the migration. Again, add `-deleteOldSnapshots` if you want to delete the existing backups on the source cluster.
+
+*DO NOT* use the `-deleteOldJobAndExit` switch until *AFTER* the migration is complete, otherwise the script will simply delete the protection group from the source cluster without the job having been migrated!
+
 ## Download the script
 
 Run these commands from PowerShell to download the script(s) into your current directory
@@ -36,12 +46,27 @@ To update all VMware protection jobs:
                                -jobName myjob
 ```
 
-## Parameters
+## Basic Parameters
 
 * -sourceCluster: name of source cluster to connect to
 * -sourceUser: username for source cluster
-* -sourceDomain: domain for source cluster user (defaults to local)
+* -sourceDomain: (optional) domain for source cluster user (defaults to local)
 * -targetCluster: name of target cluster to connect to
-* -targetUser: username for target cluster
-* -targetDomain: domain for target cluster user (defaults to local)
+* -targetUser: (optional) username for target cluster (defaults to sourceUser)
+* -targetDomain: (optional) domain for target cluster user (defaults to sourceDomain)
 * -jobName: name of job to migrate
+
+## Target Job Parameters
+
+* -prefix: (optional) add prefix to target job name
+* -suffix: (optional) add suffic to target job name
+* -newJobName: (optional) new name for target job (defaults to jobName)
+* -newPolicyName: (optional) new policy name (defaults to same policy name as source job)
+* -newStorageDomainName: (optional) new storage domain name (defaults to same storage domain name as source job)
+* -pauseNewJob: (optional) pause new job
+
+## Cleanup Parameters
+
+* -deleteOldJob: (optional) delete old job (*destructive!*)
+* -deleteOldJobAndExit: (optional) delete old job and exit (*destructive!*)
+* -deleteOldSnapshots: (optional) delete existing snapshots when deleting old job (*destrictive!*)
--- powershell/migrateVMProtectionGroup/migrateVMProtectionGroup.ps1
+++ powershell/migrateVMProtectionGroup/migrateVMProtectionGroup.ps1
@@ -5,36 +5,231 @@ param (
     [Parameter(Mandatory = $True)][string]$sourceUser,
     [Parameter()][string]$sourceDomain = 'local',
     [Parameter(Mandatory = $True)][string]$targetCluster,
-    [Parameter(Mandatory = $True)][string]$targetUser,
-    [Parameter()][string]$targetDomain = 'local',
-    [Parameter(Mandatory = $True)][string]$jobName
+    [Parameter()][string]$targetUser = $sourceUser,
+    [Parameter()][string]$targetDomain = $sourceDomain,
+    [Parameter(Mandatory = $True)][string]$jobName,
+    [Parameter()][string]$prefix = '',
+    [Parameter()][string]$suffix = '',
+    [Parameter()][string]$newJobName = $jobName,
+    [Parameter()][string]$newPolicyName,
+    [Parameter()][string]$newStorageDomainName,
+    [Parameter()][switch]$pauseNewJob,
+    [Parameter()][switch]$deleteOldJob,
+    [Parameter()][switch]$deleteOldJobAndExit,
+    [Parameter()][switch]$deleteOldSnapshots
 )
+
+
+function getObjectById($objectId, $source){
+    $global:_object = $null
+
+    function get_nodes($obj){
+        if($obj.protectionSource.id -eq $objectId){
+            $global:_object = $obj
+            break
+        }     
+        if($obj.PSObject.Properties['nodes']){
+            foreach($node in $obj.nodes){
+                if($null -eq $global:_object){
+                    get_nodes $node
+                }
+            }
+        }
+    }    
+    if($null -eq $global:_object){
+        get_nodes $source
+    }
+    return $global:_object
+}
+
+
+function getObjectByMoRef($moRef, $source){
+    $global:_object_id = $null
+
+    function get_nodes($obj){
+        if($obj.protectionSource.vmWareProtectionSource.id.morItem -eq $moRef -or $obj.protectionSource.vmWareProtectionSource.id.uuid -eq $moRef){
+            $global:_object_id = $obj.protectionSource.id
+            break
+        }    
+        if($obj.PSObject.Properties['nodes']){
+            foreach($node in $obj.nodes){
+                if($null -eq $global:_object_id){
+                    get_nodes $node
+                }
+            }
+        }
+    }
+    
+    if($null -eq $global:_object_id){
+        get_nodes $source
+    }
+
+    return $global:_object_id
+}
+
 
 # source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-apiauth -vip $sourceCluster -username $sourceUser -domain $sourceDomain
+"`nConnecting to source cluster..."
+apiauth -vip $sourceCluster -username $sourceUser -domain $sourceDomain -quiet
+
+if($prefix){
+    $newJobName = "$prefix-$newJobName"
+}
+
+if($suffix){
+    $newJobName = "$newJobName-$suffix"
+}
 
 $job = (api get -v2 'data-protect/protection-groups').protectionGroups | Where-Object name -eq $jobName
 
 if($job){
+
     $oldPolicy = (api get -v2 data-protect/policies).policies | Where-Object id -eq $job.policyId
     $oldStorageDomain = api get viewBoxes | Where-Object id -eq $job.storageDomainId
-    # Read-Host -Prompt "switch clusters if on RT (hit enter if already connected)"
-    apiauth -vip $targetCluster -username $targetUser -domain $targetDomain
-    # update vCenter ID
-    $newVCenter = api get protectionSources?environments=kVMware | Where-Object {$_.protectionSource.name -eq $job.vmwareParams.sourceName}
-    $job.vmwareParams.sourceId = $newVCenter.protectionSource.id
-    # update storage domain id
-    $newStorageDomain = api get viewBoxes | Where-Object name -eq $oldStorageDomain.name
-    $job.storageDomainId = $newStorageDomain.id
-    # update policy id
-    $newPolicy = (api get -v2 data-protect/policies).policies | Where-Object name -eq $oldPolicy.name
-    $job.policyId = $newPolicy.id
-    # update object IDs
-    foreach($vm in $job.vmwareParams.objects){
-        $newVM = api get "protectionSources/virtualMachines?vCenterId=$($job.vmwareParams.sourceId)&names=$($vm.name)"
-        $vm.id = $newVM.id
+
+    # connect to target cluster for sanity check
+    if(!$deleteOldJobAndExit){
+        "Connecting to target cluster..."
+        apiauth -vip $targetCluster -username $targetUser -domain $targetDomain -quiet
+
+        # check for existing job
+        $existingJob = (api get -v2 'data-protect/protection-groups').protectionGroups | Where-Object name -eq $newJobName
+        if($existingJob){
+            Write-Host "job '$newJobName' already exists on target cluster" -ForegroundColor Yellow
+            exit
+        }
+
+        # check for vCenter
+        $newVCenter = api get "protectionSources?environments=kVMware&includeVMFolders=true" | Where-Object {$_.protectionSource.name -eq $job.vmwareParams.sourceName}
+        if(!$newVCenter){
+            write-host "vCenter $($newVCenter.protectionSource.name) is not registered" -ForegroundColor Yellow
+            exit
+        }
+
+        # check for storage domain
+        if($newStorageDomainName){
+            $oldStorageDomain.name = $newStorageDomainName
+        }
+        $newStorageDomain = api get viewBoxes | Where-Object name -eq $oldStorageDomain.name
+        if(!$newStorageDomain){
+            Write-Host "Storage Domain $($oldStorageDomain.name) not found" -ForegroundColor Yellow
+            exit
+        }
+
+        # check for policy
+        if($newPolicyName){
+            $oldPolicy.name -eq $newPolicyName
+        }
+        $newPolicy = (api get -v2 data-protect/policies).policies | Where-Object name -eq $oldPolicy.name
+        if(!$newPolicy){
+            Write-Host "Policy $($oldPolicy.name) not found" -ForegroundColor Yellow
+            exit
+        }
+        apiauth -vip $sourceCluster -username $sourceUser -domain $sourceDomain -quiet
     }
-    api post -v2 data-protect/protection-groups $job
+
+    # gather old job details
+    if(!$deleteOldJobAndExit){
+        "Migrating '$jobName' from $sourceCluster to $targetCluster...`n"
+        $oldVCenter = api get "protectionSources?id=$($job.vmwareParams.sourceId)&environments=kVMware&includeVMFolders=true"
+    }
+
+    # clean up source cluster
+    if($deleteOldJob -or $deleteOldJobAndExit){
+        if($deleteOldJobAndExit){
+            ""
+        }
+        "    Deleting old protection group..."
+        if($deleteOldSnapshots){
+            $delete = 'true'
+        }else{
+            $delete = 'false'
+        }
+        $null = api delete -v2 "data-protect/protection-groups/$($job.id)?deleteSnapshots=$delete"
+        if($deleteOldJobAndExit){
+            "`nCleanup Complete`n"
+            exit
+        }
+    }else{
+        # pause old job
+        "    Pausing old job..."
+        $pauseParams = @{
+            "action" = "kPause";
+            "ids" = @(
+                $job.id
+            )
+        }
+        $null = api post -v2 data-protect/protection-groups/states $pauseParams  
+    }
+
+    # connect to target cluster
+    apiauth -vip $targetCluster -username $targetUser -domain $targetDomain -quiet
+
+    $job.storageDomainId = $newStorageDomain.id
+    $job.policyId = $newPolicy.id
+    $job.vmwareParams.sourceId = $newVCenter.protectionSource.id
+
+    # pause new job
+    if($pauseNewJob){
+        $job.isPaused = $True
+    }else{
+        $job.isPaused = $false
+    }
+
+    # include objects
+    foreach($vm in $job.vmwareParams.objects){
+        $oldObject = getObjectById $vm.id $oldVCenter
+        $newObjectId = getObjectByMoRef $oldObject.protectionSource.vmWareProtectionSource.id.morItem $newVCenter
+        $vm.id = $newObjectId
+    }
+
+    # exclude objects
+    if($job.vmwareParams.PSObject.Properties['excludeObjectIds']){
+        $newExcludeIds = @()
+        foreach($excludeId in $job.vmwareParams.excludeObjectIds){
+            $oldObject = getObjectById $excludeId $oldVCenter
+            $newObjectId = getObjectByMoRef $oldObject.protectionSource.vmWareProtectionSource.id.morItem $newVCenter
+            $newExcludeIds = @($newExcludeIds + $newObjectId)
+        }
+    }
+
+    # include tags
+    if($job.vmwareParams.PSObject.Properties['vmTagIds']){
+        $newTagIds = @()
+        foreach($tag in $job.vmwareParams.vmTagIds){
+            $newTag = @()
+            foreach($tagId in $tag){
+                $oldObject = getObjectById $tagId $oldVCenter
+                $newObjectId = getObjectByMoRef $oldObject.protectionSource.vmWareProtectionSource.id.uuid $newVCenter
+                $newTag = @($newTag + $newObjectId)
+            }
+            $newTagIds = @($newTagIds + ,$newTag)
+        }
+        $job.vmwareParams.vmTagIds = @($newTagIds)
+    }
+
+    # exclude tags
+    if($job.vmwareParams.PSObject.Properties['excludeVmTagIds']){
+        $newTagIds = @()
+        foreach($tag in $job.vmwareParams.excludeVmTagIds){
+            $newTag = @()
+            foreach($tagId in $tag){
+                $oldObject = getObjectById $tagId $oldVCenter
+                $newObjectId = getObjectByMoRef $oldObject.protectionSource.vmWareProtectionSource.id.uuid $newVCenter
+                $newTag = @($newTag + $newObjectId)
+            }
+            $newTagIds = @($newTagIds + ,$newTag)
+        }
+        $job.vmwareParams.excludeVmTagIds = @($newTagIds)
+    }
+
+    # create new job
+    $job.name = $newJobName
+    "    Creating job '$newJobName' on $targetCluster..."
+    $newjob = api post -v2 data-protect/protection-groups $job
+    "`nMigration Complete`n"
+}else{
+    Write-Host "Job $jobName not found" -ForegroundColor Yellow
 }
