a005bdfaa8e33e02b20878469ecd263e7735cf42
bseltz-cohesity <bseltzer@cohesity.com>
2021-11-10 05:48:41 -0500

    2021-11-10

--- powershell/dailyObjectStatus/dailyObjectStatus.ps1
+++ powershell/dailyObjectStatus/dailyObjectStatus.ps1
@@ -4,14 +4,13 @@ param (
     [Parameter(Mandatory = $True)][string]$vip,
     [Parameter(Mandatory = $True)][string]$username,
     [Parameter()][string]$domain = 'local',
-    [Parameter()][int]$numRuns = 2,
     [Parameter()][switch]$yesterdayOnly
 )
 
-# source the cohesity-api helper code
+### source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-# authenticate
+### authenticate
 apiauth -vip $vip -username $username -domain $domain
 
 # date range
@@ -33,37 +32,47 @@ $dateString = get-date -UFormat '%Y-%m-%d'
 $outputfile = $(Join-Path -Path $PSScriptRoot -ChildPath "dailyObjectStatus-$($cluster.name)-$dateString.csv")
 "Job Name,Job Type,Object Name,Status,Last Run,Message" | Out-File -FilePath $outputfile
 
-$jobs = api get protectionJobs | Where-Object {$_.isActive -ne $False -and $_.isDeleted -ne $True}
+$jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&includeTenants=true&includeLastRunInfo=false"
 
-foreach($job in $jobs | Sort-Object -Property name){
-    "`n{0} ({1})`n" -f $job.name, $job.environment.subString(1)
-    $runs = api get "protectionRuns?jobId=$($job.id)&startTimeUsecs=$yesterdayUsecs&endTimeUsecs=$endTimeUsecs&numRuns=$numRuns"
-    $finishedRuns = $runs | Where-Object {$_.backupRun.status -in @('kCanceled', 'kSuccess', 'kFailure', 'kWarning')}
-    $myRun = $null
-    if($finishedRuns.Count -eq 0){
-        # still running
-        if($runs.Count -gt 0){
-            $myRun = $runs[0]
-        }
-    }else{
-        # finished
-        $myRun = $finishedRuns[0]
-    }
-    if($myRun){
-        $runStartTimeUsecs = $myRun.backupRun.stats.startTimeUsecs 
-        foreach($source in $myRun.backupRun.sourceBackupStatus | Sort-Object -Property {$_.source.name}){
-            $sourceName = $source.source.name
-            if($source.source.environment -eq 'kO365' -and $source.source.office365ProtectionSource.PSObject.Properties['primarySMTPAddress']){
-                $sourceName = $source.source.office365ProtectionSource.primarySMTPAddress
+$o365Sources = api get protectionSources?environments=kO365
+$o365Users = ($o365Sources.nodes | Where-Object {$_.protectionSource.office365ProtectionSource.type -eq 'kUsers'}).nodes | Select-Object -Property @{l='id'; e={$_.protectionSource.id}}, @{l='smtpAddress'; e={$_.protectionSource.office365ProtectionSource.primarySMTPAddress}}
+$o365Index = @{}
+$o365Users | ForEach-Object {
+    $o365Index[$_.id] = $_.smtpAddress
+}
+
+foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
+    "{0}" -f $job.name
+    $lastRun = (api get -v2 "data-protect/protection-groups/$($job.id)/runs/?numRuns=2&includeObjectDetails=true&endTimeUsecs=$endTimeUsecs").runs | Where-Object {$_.localBackupInfo.status -ne 'Running'}
+    if($lastRun.Count -gt 0){
+        $lastRun = $lastRun[0]
+        $lastRun.localBackupInfo.status
+        foreach($entity in $lastRun.objects | Sort-Object -Property {$_.object.name}){
+            $objectName = $entity.object.name
+            if($entity.object.environment -eq 'kO365' -and $entity.object.objectType -eq 'kUser'){
+                $altObjectName = $o365Index[$entity.object.id]
+                if($altObjectName -and $altObjectName -ne ''){
+                    $objectName = $altObjectName
+                }
             }
-            if($source.source.environment -eq 'kO365' -and $source.source.office365ProtectionSource.PSObject.Properties['webUrl']){
-                $sourceName = $source.source.office365ProtectionSource.webUrl
+            if($entity.object.environment -eq 'kO365' -and $entity.object.objectType -eq 'kSite'){
+                if($entity.object.PSObject.Properties['sharepointSiteSummary']){
+                    $objectName = "$objectName ($($entity.object.sharepointSiteSummary.siteWebUrl))"
+                }
             }
-            $status = $source.status.subString(1)
-            $message = $source.error
-            "  {0} ({1})" -f $sourceName, $status
-            "{0},{1},{2},{3},{4},""{5}""" -f $job.name, $job.environment.subString(1), $sourceName, $status, (usecsToDate $runStartTimeUsecs), $message | Out-File -FilePath $outputfile -Append
+            $startTimeUsecs = $lastRun.localBackupInfo.startTimeUsecs
+            $status = $entity.localSnapshotInfo.snapshotInfo.status.Substring(1)
+            if($status -eq 'Failed'){
+                $message = $entity.localSnapshotInfo.failedAttempts[-1].message.replace("`n", " ")
+            }elseif($status -eq 'Warning' -or $status -eq 'WaitingForNextAttempt'){
+                $message = $entity.localSnapshotInfo.snapshotInfo.warnings[0].replace("`n", " ")
+            }else{
+                $message = ""
+            }
+            "    {0}" -f $objectName
+            """{0}"",""{1}"",""{2}"",""{3}"",""{4}"",""{5}""" -f $job.name, $job.environment.Substring(1), $objectName, (usecsToDate $startTimeUsecs), $status, $message | Out-File -FilePath $outputfile -Append
         }
     }
 }
-"`nOutput saved to $outputfile`n"
+
+"Output saved to $outputfile"
