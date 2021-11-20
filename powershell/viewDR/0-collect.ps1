6409df77d57f9f0a32063a7336cfc2005440a870
bseltz-cohesity <bseltzer@cohesity.com>
2021-11-20 08:42:45 -0500

    2021-11-20

--- powershell/viewDR/0-collect.ps1
+++
@@ -1,7 +0,0 @@
-$sourceCluster = 'mysourcecluster'
-$userName = 'myusername'
-$domain = 'mydomain.net'
-$metaDataPath = '\\my\unc\path'
-
-# save view metadata to DR share
-.\viewDRcollect.ps1 -vip $sourceCluster -username $userName -domain $domain -outPath $metaDataPath
