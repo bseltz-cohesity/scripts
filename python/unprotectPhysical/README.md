8f96f4d2d1fb0a9e9fd8a0916f2dbaefa7041473
bseltz-cohesity <bseltzer@cohesity.com>
2021-12-03 17:19:52 -0500

    2021-12-03

---
+++ python/unprotectPhysical/README.md
@@ -0,0 +1,43 @@
+# Unprotect Physical Servers Using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script removes physical servers from file-based or block-based protection groups.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/unprotectPhysical/unprotectPhysical.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x unprotectPhysical.py
+# end download commands
+```
+
+## Components
+
+* unprotectPhysical.py: the main powershell script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./unprotectPhysical.py -v mycluster \
+                       -u myuser \
+                       -d mydomain.net \
+                       -s server1.mydomain.net \
+                       -s server2.mydomain.net \
+                       -l serverlist.txt
+```
+
+## Parameters
+
+* -v, --vip: DNS or IP of the Cohesity cluster to connect to
+* -u, --username: username to authenticate to Cohesity cluster
+* -d, --domain: (optional) domain of username, defaults to local
+* -k, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password of API key
+* -s, --servername: (optional) name of server to add to the job (use multiple times for multiple)
+* -l, --serverlist: (optional) list of server names in a text file (one per line)
---
+++ python/unprotectPhysical/unprotectPhysical.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""unprotect physical servers"""
+
+# version 2021-12-03
+
+# import pyhesity wrapper module
+from pyhesity import *
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, required=True)
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-s', '--servername', action='append', type=str)
+parser.add_argument('-l', '--serverlist', type=str)
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+password = args.password
+useApiKey = args.useApiKey
+servernames = args.servername
+serverlist = args.serverlist
+
+paramPaths = {'kFile': 'fileProtectionTypeParams', 'kVolume': 'volumeProtectionTypeParams'}
+
+# gather server list
+if servernames is None:
+    servernames = []
+if serverlist is not None:
+    f = open(serverlist, 'r')
+    servernames += [s.strip() for s in f.readlines() if s.strip() != '']
+    f.close()
+if len(servernames) == 0:
+    print('no servers specified')
+    exit()
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+
+serverfound = {}
+for server in servernames:
+    serverfound[server] = False
+
+jobs = api('get', 'data-protect/protection-groups?isDeleted=false&isActive=true&environments=kPhysical', v=2)
+
+if 'protectionGroups' in jobs and jobs['protectionGroups'] is not None:
+    for job in jobs['protectionGroups']:
+        saveJob = False
+        paramPath = paramPaths[job['physicalParams']['protectionType']]
+
+        for server in servernames:
+            protectedObjectCount = len(job['physicalParams'][paramPath]['objects'])
+            job['physicalParams'][paramPath]['objects'] = [o for o in job['physicalParams'][paramPath]['objects'] if o['name'].lower() != server.lower()]
+            if len(job['physicalParams'][paramPath]['objects']) < protectedObjectCount:
+                print('%s removed from from group: %s' % (server, job['name']))
+                serverfound[server] = True
+                saveJob = True
+
+        if saveJob is True:
+            if len(job['physicalParams'][paramPath]['objects']) == 0:
+                print('0 objects left in %s. Deleting...' % job['name'])
+                result = api('delete', 'data-protect/protection-groups/%s' % job['id'], v=2)
+            else:
+                pass
+                result = api('put', 'data-protect/protection-groups/%s' % job['id'], job, v=2)
+
+for server in servernames:
+    if serverfound[server] is False:
+        print('%s not found in any physical protection group. * * * * * *' % server)
