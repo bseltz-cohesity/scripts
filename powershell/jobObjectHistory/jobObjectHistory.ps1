35b66a1da7b4c8412ec9f785a6ca1f4171a8463b
bseltz-cohesity <bseltzer@cohesity.com>
2021-12-07 18:31:36 -0500

    2021-12-07

--- powershell/jobObjectHistory/jobObjectHistory.ps1
+++ powershell/jobObjectHistory/jobObjectHistory.ps1
@@ -21,18 +21,26 @@ $outputfile = $(Join-Path -Path $PSScriptRoot -ChildPath "jobObjectHistory-$($cl
 $jobs = api get protectionJobs | Where-Object {$_.isActive -ne $False -and $_.isDeleted -ne $True}
 
 foreach($job in $jobs | Sort-Object -Property name){
-    "`n$($job.name)`n" | Tee-Object -FilePath $outputfile -Append
+    "`n$($job.name)`n"
+    $jobNameReported = $False
     $previousSourceList = @()
     $runs = api get "protectionRuns?jobId=$($job.id)&numRuns=$numRuns" | Where-Object {$_.backupRun.snapshotsDeleted -ne $true}
     foreach($run in $runs | Sort-Object -Property {$_.backupRun.stats.startTimeUsecs}){
-        "    $(usecsToDate $run.backupRun.stats.startTimeUsecs)" | Tee-Object -FilePath $outputfile -Append
+        "    $(usecsToDate $run.backupRun.stats.startTimeUsecs)"
         $sourceList = $run.backupRun.sourceBackupStatus.source.name
         if($previousSourceList -ne @()){
             $added = $sourceList | Where-Object {$_ -notin $previousSourceList} | sort
+            $removed = $previousSourceList | Where-Object {$_ -notin $sourceList} | sort
+            if($added.Count -gt 0 -or $removed.Count -gt 0){
+                if($jobNameReported -eq $False){
+                    "`n$($job.name)  (Last Modified by $($job.modifiedByUser) at $(usecsToDate $job.modificationTimeUsecs))`n" | Out-File -FilePath $outputfile -Append
+                    $jobNameReported = $True
+                }
+                "    $(usecsToDate $run.backupRun.stats.startTimeUsecs)" | Out-File -FilePath $outputfile -Append
+            }
             foreach($add in $added){
                 "        ********     Added: $add" | Tee-Object -FilePath $outputfile -Append
             }
-            $removed = $previousSourceList | Where-Object {$_ -notin $sourceList} | sort
             foreach($remove in $removed){
                 "        ********   Removed: $remove" | Tee-Object -FilePath $outputfile -Append
             }
