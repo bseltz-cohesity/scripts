bc633fd3866e6aa06fc5adaedf50c587f8d1ccb6
bseltz-cohesity <bseltzer@cohesity.com>
2022-01-03 15:42:33 -0500

    2022-01-03

--- powershell/refreshSource/refreshSource.ps1
+++ powershell/refreshSource/refreshSource.ps1
@@ -24,11 +24,39 @@ function getObjectId($sourceName){
     return $null
 }
 
+function waitForRefresh($server){
+    $authStatus = ""
+    while($authStatus -ne 'Finished'){
+        $rootFinished = $false
+        $appsFinished = $false
+        Start-Sleep 2
+        $rootNode = (api get "protectionSources/registrationInfo?includeApplicationsTreeInfo=false&environments=kPhysical").rootNodes | Where-Object {$_.rootNode.name -eq $server}
+        if($rootNode.registrationInfo.authenticationStatus -eq 'kFinished'){
+            $rootFinished = $True
+        }
+        if($rootNode.registrationInfo.PSObject.Properties['registeredAppsInfo']){
+            foreach($app in $rootNode.registrationInfo.registeredAppsInfo){
+                if($app.authenticationStatus -eq 'kFinished'){
+                    $appsFinished = $True
+                }else{
+                    $appsFinished = $false
+                }
+            }
+        }else{
+            $appsFinished = $True
+        }
+        if($rootFinished -and $appsFinished){
+            $authStatus = 'Finished'
+        }
+    }
+    return $rootNode.rootNode.id
+}
+
 $objectId = getObjectId $sourceName
 if($objectId){
     write-host "refreshing $sourceName..."
     $result = api post protectionSources/refresh/$($objectId)
-    $result
+    $result = waitForRefresh($sourceName)
 }else{
     write-host "$sourceName not found" -ForegroundColor Yellow
 }
