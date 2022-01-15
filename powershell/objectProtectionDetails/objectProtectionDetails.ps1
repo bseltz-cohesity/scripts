0ae2e9c428652e9baf0c87a18f7e08455711a9f4
bseltz-cohesity <bseltzer@cohesity.com>
2022-01-15 15:24:10 -0500

    2022-01-15

--- powershell/objectProtectionDetails/objectProtectionDetails.ps1
+++ powershell/objectProtectionDetails/objectProtectionDetails.ps1
@@ -64,89 +64,88 @@ $jobs = api get protectionJobs
 $sources = api get protectionSources
 
 foreach($object in $objects){
-# get object ID
-$objectId = getObjectId $object
+    # get object ID
+    $objectId = getObjectId $object
 
-if($null -eq $objectId){
-    Write-Host "`n$object not found" -ForegroundColor Yellow
-    exit
-}else{
-    # find protection job
-    $jobs = $jobs | Where-Object {
-        $objectId -in $_.sourceIds -or
-        $objectId -in $_.sourceSpecialParameters.oracleSpecialParameters.applicationEntityIds -or
-        $objectId -in $_.sourceSpecialParameters.sqlSpecialParameters.applicationEntityIds
-    }
-    if($null -eq $jobs){
-        Write-Host "`n$object not protected" -ForegroundColor Yellow
-        exit
+    if($null -eq $objectId){
+        Write-Host "`n$object not found" -ForegroundColor Yellow
     }else{
-        foreach($job in $jobs){
-            "`n$global:object ($($job.name))`n"
-            "Start Time          End Time            Job Run  Type         Object   Read       Logical Size"
-            "------------------  ------------------  -------  -----------  -------  ---------  ------------"
-            # get protectionRuns
-            $runs = api get "protectionRuns?jobId=$($job.id)&startTimeUsecs=$(dateToUsecs $startDate)&endTimeUsecs=$(dateToUsecs $endDate)&numRuns=999999"
-            if($includeDeleted -eq $false){
-                $runs = $runs | Where-Object { $_.backupRun.snapshotsDeleted -eq $false}
-            }
-            foreach($run in $runs){
-                # runs stats
-                $runStart = $run.backupRun.stats.startTimeUsecs
-                $runEnd = $run.backupRun.stats.endTimeUsecs
-                $runStatus = $run.backupRun.status.subString(1)
-                $runType = $run.backupRun.runType.substring(1).replace('Regular','Incremental')
-                $objLogical = ''
-                $objLogicalUnits = ''
-                $objRead = ''
-                $objReadUnits = ''
-                $objStatus = ''
-                $objStart = (usecsToDate $runStart).ToString("MM/dd/yyyy hh:mmtt")
-                $objEnd = (usecsToDate $runEnd).ToString("MM/dd/yyyy hh:mmtt")
-                if($run.backupRun.PSObject.Properties['warnings']){
-                    $runStatus = 'Warning'
+        # find protection job
+        $jobs = $jobs | Where-Object {
+            $objectId -in $_.sourceIds -or
+            $objectId -in $_.sourceSpecialParameters.oracleSpecialParameters.applicationEntityIds -or
+            $objectId -in $_.sourceSpecialParameters.sqlSpecialParameters.applicationEntityIds
+        }
+        if($null -eq $jobs){
+            Write-Host "`n$object not protected" -ForegroundColor Yellow
+            exit
+        }else{
+            foreach($job in $jobs){
+                "`n$global:object ($($job.name))`n"
+                "Start Time          End Time            Job Run  Type         Object   Read       Logical Size"
+                "------------------  ------------------  -------  -----------  -------  ---------  ------------"
+                # get protectionRuns
+                $runs = api get "protectionRuns?jobId=$($job.id)&startTimeUsecs=$(dateToUsecs $startDate)&endTimeUsecs=$(dateToUsecs $endDate)&numRuns=999999"
+                if($includeDeleted -eq $false){
+                    $runs = $runs | Where-Object { $_.backupRun.snapshotsDeleted -eq $false}
                 }
-                if($run.backupRun.PSObject.Properties['sourceBackupStatus']){
-                    # object stats
-                    foreach($source in $run.backupRun.sourceBackupStatus){
-                        if($source.source.name -eq $object){
-                            $objStatus = $source.status.subString(1)
-                            if($source.PSObject.Properties['warnings']){
-                                $objStatus = 'Warning'
+                foreach($run in $runs){
+                    # runs stats
+                    $runStart = $run.backupRun.stats.startTimeUsecs
+                    $runEnd = $run.backupRun.stats.endTimeUsecs
+                    $runStatus = $run.backupRun.status.subString(1)
+                    $runType = $run.backupRun.runType.substring(1).replace('Regular','Incremental')
+                    $objLogical = ''
+                    $objLogicalUnits = ''
+                    $objRead = ''
+                    $objReadUnits = ''
+                    $objStatus = ''
+                    $objStart = (usecsToDate $runStart).ToString("MM/dd/yyyy hh:mmtt")
+                    $objEnd = (usecsToDate $runEnd).ToString("MM/dd/yyyy hh:mmtt")
+                    if($run.backupRun.PSObject.Properties['warnings']){
+                        $runStatus = 'Warning'
+                    }
+                    if($run.backupRun.PSObject.Properties['sourceBackupStatus']){
+                        # object stats
+                        foreach($source in $run.backupRun.sourceBackupStatus){
+                            if($source.source.name -eq $object){
+                                $objStatus = $source.status.subString(1)
+                                if($source.PSObject.Properties['warnings']){
+                                    $objStatus = 'Warning'
+                                }
+                                $objLogical = $source.stats.totalLogicalBackupSizeBytes
+                                $objLogicalUnits = 'B'
+                                $objRead = $source.stats.totalBytesReadFromSource
+                                $objReadUnits = 'B'
+                                if($objLogical -ge 1073741824){
+                                    $objLogical = [math]::round(($objLogical/1073741824),1)
+                                    $objLogicalUnits = 'GiB'
+                                }elseif ($objLogical -ge 1048576) {
+                                    $objLogical = [math]::round(($objLogical/1048576),1)
+                                    $objLogicalUnits = 'MiB'                                                              
+                                }elseif ($objLogical -ge 1024) {
+                                    $objLogical = [math]::round(($objLogical/1024),1)
+                                    $objLogicalUnits = 'KiB'                                                              
+                                }
+                                if($objRead -ge 1073741824){
+                                    $objRead = [math]::round(($objRead/1073741824),1)
+                                    $objReadUnits = 'GiB'
+                                }elseif ($objRead -ge 1048576) {
+                                    $objRead = [math]::round(($objRead/1048576),1)
+                                    $objReadUnits = 'MiB'                                                              
+                                }elseif ($objRead -ge 1024) {
+                                    $objRead = [math]::round(($objRead/1024),1)
+                                    $objReadUnits = 'KiB'                                                              
+                                }
+                                $objStart = (usecsToDate $source.stats.startTimeUsecs).ToString("MM/dd/yyyy hh:mmtt")
+                                $objEnd = (usecsToDate $source.stats.endTimeUsecs).ToString("MM/dd/yyyy hh:mmtt")
                             }
-                            $objLogical = $source.stats.totalLogicalBackupSizeBytes
-                            $objLogicalUnits = 'B'
-                            $objRead = $source.stats.totalBytesReadFromSource
-                            $objReadUnits = 'B'
-                            if($objLogical -ge 1073741824){
-                                $objLogical = [math]::round(($objLogical/1073741824),1)
-                                $objLogicalUnits = 'GiB'
-                            }elseif ($objLogical -ge 1048576) {
-                                $objLogical = [math]::round(($objLogical/1048576),1)
-                                $objLogicalUnits = 'MiB'                                                              
-                            }elseif ($objLogical -ge 1024) {
-                                $objLogical = [math]::round(($objLogical/1024),1)
-                                $objLogicalUnits = 'KiB'                                                              
-                            }
-                            if($objRead -ge 1073741824){
-                                $objRead = [math]::round(($objRead/1073741824),1)
-                                $objReadUnits = 'GiB'
-                            }elseif ($objRead -ge 1048576) {
-                                $objRead = [math]::round(($objRead/1048576),1)
-                                $objReadUnits = 'MiB'                                                              
-                            }elseif ($objRead -ge 1024) {
-                                $objRead = [math]::round(($objRead/1024),1)
-                                $objReadUnits = 'KiB'                                                              
-                            }
-                            $objStart = (usecsToDate $source.stats.startTimeUsecs).ToString("MM/dd/yyyy hh:mmtt")
-                            $objEnd = (usecsToDate $source.stats.endTimeUsecs).ToString("MM/dd/yyyy hh:mmtt")
                         }
                     }
+                    
+                    "{0,-19} {1,-19} {2,-8} {3,-12} {4,-8} {5,5} {6,-4} {7} {8,2}" -f $objStart, $objEnd, $runStatus, $runType, $objStatus, $objRead, $objReadUnits, $objLogical, $objLogicalUnits
                 }
-                
-                "{0,-19} {1,-19} {2,-8} {3,-12} {4,-8} {5,5} {6,-4} {7} {8,2}" -f $objStart, $objEnd, $runStatus, $runType, $objStatus, $objRead, $objReadUnits, $objLogical, $objLogicalUnits
             }
         }
-    }
-}    
+    }    
 }
