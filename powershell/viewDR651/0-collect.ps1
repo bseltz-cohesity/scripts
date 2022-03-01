50596fee66d6f2318359bdd51b0e908503ce1ab0
bseltz-cohesity <bseltzer@cohesity.com>
2022-03-01 08:33:03 -0500

    2022-03-01

--- powershell/viewDR651/0-collect.ps1
+++ powershell/viewDR651/0-collect.ps1
@@ -6,3 +6,4 @@ $metaDataPath = '\\my\unc\path'
 
 # save view metadata to DR share
 .\viewDRcollect.ps1 -vip $primaryCluster -username $userName -domain $domain -outPath $metaDataPath
+# .\viewDRcollect.ps1 -vip $primaryCluster -username $userName -domain $domain -outPath $metaDataPath -useApiKey
--- powershell/viewDR651/2-failback.ps1
+++ powershell/viewDR651/2-failback.ps1
@@ -15,9 +15,11 @@ $cnameDomain = 'mydomain.net'
 
 # clone the replicated view backups
 .\viewDRclone.ps1 -vip $primaryCluster -username $userName -domain $domain -viewList ./myviews.txt -inPath "$metaDataPath\$primaryCluster" -policyName $policyName
+# .\viewDRclone.ps1 -vip $primaryCluster -username $userName -domain $domain -viewList ./myviews.txt -inPath "$metaDataPath\$primaryCluster" -policyName $policyName -useApiKey
 # toggle dfs folder targets
 .\dfsFailover.ps1 -nameSpace $dfsNameSpace -shareList .\migratedShares.txt -disableTarget $drCluster -enableTarget $primaryCluster
 # toggle cnames and SPNs
 .\cnameFailover.ps1 -cname $cname -oldHost $drCluster -newHost $primaryCluster -domain $cnameDomain
 # remove views from source
 .\viewDRdelete.ps1 -vip $drCluster -username $userName -domain $domain -viewList ./myviews.txt -deleteSnapshots
+# .\viewDRdelete.ps1 -vip $drCluster -username $userName -domain $domain -viewList ./myviews.txt -deleteSnapshots
