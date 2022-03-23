c9d0aa75260e98566ce1da7588a3c06b10de64bd
bseltz-cohesity <bseltzer@cohesity.com>
2022-03-23 11:50:39 -0400

    2022-03-23

--- powershell/isilonCreateCohesityUser/isilonCreateCohesityUser.ps1
+++ powershell/isilonCreateCohesityUser/isilonCreateCohesityUser.ps1
@@ -6,7 +6,8 @@ param (
     [Parameter()][string]$password = $null,  # optional, will be prompted if omitted
     [Parameter()][string]$cohesityUsername = 'cohesity',
     [Parameter()][string]$cohesityPassword = $null,
-    [Parameter()][switch]$createSMBUser
+    [Parameter()][switch]$createSMBUser,
+    [Parameter()][switch]$delete
 )
 
 function isilonAPI($method, $uri, $data=$null){
@@ -74,85 +75,133 @@ $EncodedAuthorization = [System.Text.Encoding]::UTF8.GetBytes($username + ':' + 
 $EncodedPassword = [System.Convert]::ToBase64String($EncodedAuthorization)
 $headers = @{"Authorization"="Basic $($EncodedPassword)"}
 
-"`nCreating API user $cohesityUsername..."
-if(!$cohesityPassword){
-    while($True){
-        $secureString = Read-Host -Prompt "Enter new password for $cohesityUsername" -AsSecureString
-        $cohesityPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( $secureString ))
-        $secureString = Read-Host -Prompt "  Confirm password for $cohesityUsername" -AsSecureString
-        $cohesityPassword2 = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( $secureString ))
-        if($cohesityPassword -eq $cohesityPassword2){
-            break
-        }else{
-            Write-Host "passwords do not match`n" -foregroundcolor Yellow
+$users = isilonAPI get /platform/1/auth/users
+$existingUser = $users.users | Where-Object name -eq $cohesityUsername
+$roles = isilonAPI get /platform/1/auth/roles
+$existingRole = $roles.roles | Where-Object name -eq $cohesityUsername
+$zones = isilonAPI get /platform/1/zones-summary
+
+if($delete){
+    # delete existing role
+    if($existingRole){
+        Write-Host "Deleting role $cohesityUsername..."
+        $deleteRole = isilonAPI delete "/platform/1/auth/roles/$cohesityUsername"
+    }
+    foreach($zoneName in $zones.summary.list){
+        # remove zone user from BackupAdmins
+        $roles = isilonAPI get /platform/7/auth/roles?zone=$zoneName
+        $role = $roles.roles | Where-Object name -eq "BackupAdmin"
+        if($role){
+            $existingMember = $role.members | Where-Object name -eq $cohesityUsername
+            if($existingMember){
+                Write-Host "Removing $cohesityUsername from BackupAdmin role in zone: $zoneName..."
+                $role.members = @($role.members | Where-Object name -ne $cohesityUsername)
+                $role = isilonAPI put /platform/7/auth/roles/BackupAdmin?zone=$zoneName @{"members" = $role.members}
+            }
         }
     }
-}
-
-$userParams = @{
-    "name" = $cohesityUsername;
-    "enabled" = $True;
-    "shell" = "/bin/zsh";
-    "password_expires" = $false;
-    "password" = $cohesityPassword
-}
-
-$newUser = isilonAPI post /platform/1/auth/users $userParams
-
-$roleParams = @{
-    "name" = $cohesityUsername;
-    "members" = @(
-        @{
-            "name" = $cohesityUsername;
-            "type" = "user"
+    # delete existing user
+    if($existingUser){
+        Write-Host "Deleting user $cohesityUsername..."
+        $deleteUser = isilonAPI delete "/platform/1/auth/users/$cohesityUsername"
+    }else{
+        Write-Host "User $cohesityUsername not found"
+    }
+    foreach($zoneName in $zones.summary.list){
+        # delete zone user
+        $users = isilonAPI get "/platform/1/auth/users?zone=$zoneName&provider=lsa-local-provider%3A$zoneName"
+        $user = $users.users | Where-Object name -eq $cohesityUsername
+        if($user){
+            Write-Host "Deleting SMB user $cohesityUsername from zone: $zoneName..."
+            $deleteUser = isilonAPI delete "/platform/1/auth/users/$($cohesityUsername)?zone=$zoneName&provider=lsa-local-provider%3A$zoneName"
         }
-    );
-    "privileges" = @(
-        @{
-            "id" = "ISI_PRIV_LOGIN_PAPI";
-            "name" = "Platform API";
-            "read_only" = $True
-        };
-        @{
-            "id" = "ISI_PRIV_AUTH";
-            "name" = "Auth";
-            "read_only" = $True
-        };
-        @{
-            "id" = "ISI_PRIV_CLUSTER";
-            "name" = "Cluster";
-            "read_only" = $True
-        };
-        @{
-            "id" = "ISI_PRIV_JOB_ENGINE";
-            "name" = "Job Engine";
-            "read_only" = $false
-        };
-        @{
-            "id" = "ISI_PRIV_NETWORK";
-            "name" = "Network";
-            "read_only" = $True
-        };
-        @{
-            "id" = "ISI_PRIV_SMB";
-            "name" = "SMB";
-            "read_only" = $True
-        };
-        @{
-            "id" = "ISI_PRIV_SNAPSHOT";
-            "name" = "Snapshot";
-            "read_only" = $false
-        };
-        @{
-            "id" = "ISI_PRIV_NFS";
-            "name" = "NFS";
-            "read_only" = $false
-        }
-    )
+    }
+    exit
 }
 
-"Creating API role for $cohesityUsername..."
-$newRole = isilonAPI post /platform/1/auth/roles $roleParams
+if(!$existingUser){
+    "`nCreating API user $cohesityUsername..."
+    if(!$cohesityPassword -and !$delete){
+        while($True){
+            $secureString = Read-Host -Prompt "Enter new password for $cohesityUsername" -AsSecureString
+            $cohesityPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( $secureString ))
+            $secureString = Read-Host -Prompt "  Confirm password for $cohesityUsername" -AsSecureString
+            $cohesityPassword2 = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( $secureString ))
+            if($cohesityPassword -eq $cohesityPassword2){
+                break
+            }else{
+                Write-Host "passwords do not match`n" -foregroundcolor Yellow
+            }
+        }
+    }
+    
+    $userParams = @{
+        "name" = $cohesityUsername;
+        "enabled" = $True;
+        "shell" = "/bin/zsh";
+        "password_expires" = $false;
+        "password" = $cohesityPassword
+    }
+    
+    $newUser = isilonAPI post /platform/1/auth/users $userParams
+}
+
+if(!$existingRole){
+    $roleParams = @{
+        "name" = $cohesityUsername;
+        "members" = @(
+            @{
+                "name" = $cohesityUsername;
+                "type" = "user"
+            }
+        );
+        "privileges" = @(
+            @{
+                "id" = "ISI_PRIV_LOGIN_PAPI";
+                "name" = "Platform API";
+                "read_only" = $True
+            };
+            @{
+                "id" = "ISI_PRIV_AUTH";
+                "name" = "Auth";
+                "read_only" = $True
+            };
+            @{
+                "id" = "ISI_PRIV_CLUSTER";
+                "name" = "Cluster";
+                "read_only" = $True
+            };
+            @{
+                "id" = "ISI_PRIV_JOB_ENGINE";
+                "name" = "Job Engine";
+                "read_only" = $false
+            };
+            @{
+                "id" = "ISI_PRIV_NETWORK";
+                "name" = "Network";
+                "read_only" = $True
+            };
+            @{
+                "id" = "ISI_PRIV_SMB";
+                "name" = "SMB";
+                "read_only" = $True
+            };
+            @{
+                "id" = "ISI_PRIV_SNAPSHOT";
+                "name" = "Snapshot";
+                "read_only" = $false
+            };
+            @{
+                "id" = "ISI_PRIV_NFS";
+                "name" = "NFS";
+                "read_only" = $false
+            }
+        )
+    }
+    
+    "Creating API role for $cohesityUsername..."
+    $newRole = isilonAPI post /platform/1/auth/roles $roleParams    
+}
 
 if($createSMBUser){
     $zones = isilonAPI get /platform/1/zones-summary
