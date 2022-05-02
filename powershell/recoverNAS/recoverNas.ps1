f3c00e8e6009ebbb54f59d94fa35108497191a96
bseltz-cohesity <bseltzer@cohesity.com>
2022-05-02 06:47:04 -0400

    2022-05-02

--- powershell/recoverNAS/recoverNas.ps1
+++ powershell/recoverNAS/recoverNas.ps1
@@ -8,6 +8,10 @@ param (
     [Parameter()][string]$domain = 'local', #local or AD domain
     [Parameter(Mandatory = $True)][string]$shareName, #sharename as listed in sources
     [Parameter(Mandatory = $True)][string]$viewName, #name of the view to create
+    [Parameter()][array]$fullControl,                 # list of users to grant full control
+    [Parameter()][array]$readWrite,                   # list of users to grant read/write
+    [Parameter()][array]$readOnly,                    # list of users to grant read-only
+    [Parameter()][array]$modify,                      # list of users to grant modify
     [Parameter()][string]$sourceName = $null
 )
 
@@ -19,6 +23,10 @@ apiauth -vip $vip -username $username -domain $domain
 
 ### hard coding the qos selection
 $qosSetting = 'TestAndDev High'
+
+### get AD info
+$ads = api get activeDirectory
+$sids = @{}
 
 ### find the VM to recover
 $shares = api get restore/objects?search=$shareName
@@ -57,15 +65,94 @@ $nasRecovery = @{
     }
 }
 
+# apply share permissions
+$sharePermissionsApplied = $False
+$sharePermissions = @()
+
+
+function addPermission($user, $perms){
+    if($user.contains('\')){
+        $workgroup, $user = $username.split('\')
+        # find domain
+        $adDomain = $ads | Where-Object { $_.workgroup -eq $workgroup -or $_.domainName -eq $workgroup}
+        if(!$adDomain){
+            write-host "domain $workgroup not found!" -ForegroundColor Yellow
+            exit 1
+        }else{
+            # find domain princlipal/sid
+            $domainName = $adDomain.domainName
+            $principal = api get "activeDirectory/principals?domain=$($domainName)&includeComputers=true&search=$($user)"
+            if(!$principal){
+                write-host "user $($user) not found!" -ForegroundColor Yellow
+            }else{
+                $sid = $principal[0].sid
+                $sids[$user] = $sid
+            }
+        }
+    }else{
+        # find local or wellknown sid
+        $principal = api get "activeDirectory/principals?includeComputers=true&search=$($user)"
+        if(!$principal){
+            write-host "user $($user) not found!" -ForegroundColor Yellow
+        }else{
+            $sid = $principal[0].sid
+            $sids[$user] = $sid
+        }
+    }
+
+    if($sid){
+        $permission = @{
+            "visible" = $True;
+            "sid" = $sid;
+            "type" = "Allow";
+            "access" = $perms
+        }
+        return $permission
+    }else{
+        Write-Warning "User $user not found"
+        exit 1
+    }
+}
+
+
+foreach($user in $readWrite){
+    $sharePermissionsApplied = $True
+    $sharePermissions += addPermission $user 'ReadWrite'
+    
+}
+
+foreach($user in $fullControl){
+    $sharePermissionsApplied = $True
+    $sharePermissions += addPermission $user 'FullControl'
+}
+
+foreach($user in $readOnly){
+    $sharePermissionsApplied = $True
+    $sharePermissions += addPermission $user 'ReadOnly'
+}
+
+foreach($user in $modify){
+    $sharePermissionsApplied = $True
+    $sharePermissions += addPermission $user 'Modify'
+}
+
+if($sharePermissionsApplied -eq $False){
+    "adding everyone"
+    $sharePermissions += addPermission "Everyone" 'FullControl'
+}
+
 "Recovering $shareName as view $viewName"
 
 $result = api post restore/recover $nasRecovery
 if($result){
     sleep 1
-    $newView = (api get views).views | Where-Object { $_.name -eq $viewName }
+    $newView = (api get -v2 file-services/views).views | Where-Object { $_.name -eq $viewName }
+    $newView | setApiProperty -name Category -value 'FileServices'
     $newView | setApiProperty -name enableSmbViewDiscovery -value $True
+    $newView | setApiProperty -name sharePermissions -value @($sharePermissions)
     $newView.qos = @{
         "principalName" = 'TestAndDev High';
     }
-    $null = api put views $newView
+    $newView | ConvertTo-Json -Depth 99
+    $null = api put -v2 file-services/views/$($newView.viewId) $newView
 }
