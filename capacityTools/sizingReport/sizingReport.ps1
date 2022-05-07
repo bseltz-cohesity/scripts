f013c6057858b0e23d8a82f0c87c75bae939a8b0
bseltz-cohesity <bseltzer@cohesity.com>
2022-05-07 08:29:36 -0400

    2022-05-07

--- capacityTools/sizingReport/sizingReport.ps1
+++ capacityTools/sizingReport/sizingReport.ps1
@@ -6,7 +6,8 @@ param (
    [Parameter()][string]$domain = 'local', #local or AD domain
    [Parameter()][ValidateSet('KiB','MiB','GiB','TiB')][string]$unit = 'MiB',
    [Parameter()][int]$daysBack = 7,
-   [Parameter()][Int64]$numRuns = 100
+   [Parameter()][Int64]$numRuns = 100,
+   [Parameter()][Int64]$backDays = 0
 )
 
 ### source the cohesity-api helper code
@@ -22,19 +23,14 @@ apiauth -vip $vip -username $username -domain $domain
 
 $finishedStates = @('Succeeded', 'Canceled', 'Failed', 'Warning')
 
-$today = Get-Date
-
 $cluster = api get cluster
 $dateString = (get-date).ToString('yyyy-MM-dd')
 $outfileName = "Sizing Report-$($cluster.name)-$dateString.csv"
-"Owner,Job Name,Job Type,Source Name,Logical $unit,Peak Read $unit,Last Day Read $unit,Read Over Days $unit,Last Day Written $unit,Written Over Days $unit,Days Collected,Daily Read Change Rate %,Daily Write Change Rate %" | Out-File -FilePath $outfileName
-
-$nowUsecs = dateToUsecs (get-date)
+"""Owner"",""Job Name"",""Job Type"",""Source Name"",""Logical $unit"",""Peak Read $unit"",""Last Day Read $unit"",""Read Over Days $unit"",""Avg Read $unit"",""Last Day Written $unit"",""Written Over Days $unit"",""Avg Written $unit"",""Days Collected"",""Daily Read Change Rate %"",""Daily Write Change Rate %"",""Avg Replica Queue Hours"",""Avg Replica Hours"",""Avg Logical Replicated"",""Avg Physical Replicated""" | Out-File -FilePath $outfileName
 
 $runningTasks = 0
 
-$now = Get-Date
-$nowUsecs = dateToUsecs $now
+$now = (Get-Date).AddDays(-$backDays)
 $daysBackUsecs = dateToUsecs $now.AddDays(-$daysBack)
 
 foreach($job in (api get -v2 "data-protect/protection-groups?isDeleted=false&includeTenants=true").protectionGroups | Sort-Object -Property name){
@@ -44,7 +40,7 @@ foreach($job in (api get -v2 "data-protect/protection-groups?isDeleted=false&inc
     $jobType = $job.environment.Substring(1)
     $stats = @{}
     $owners = @{}
-    $endUsecs = dateToUsecs (Get-Date)
+    $endUsecs = dateToUsecs $now
     while($True){
         if($endUsecs -le $daysBackUsecs){
             break
@@ -72,6 +68,9 @@ foreach($job in (api get -v2 "data-protect/protection-groups?isDeleted=false&inc
                         $logicalBytes = $server.originalBackupInfo.snapshotInfo.stats.logicalSizeBytes
                         $bytesRead = $server.originalBackupInfo.snapshotInfo.stats.bytesRead
                         $bytesWritten = 0
+                        if($server.PSObject.Properties['replicationInfo']){
+                            $bytesWritten = $server.replicationInfo.replicationTargetResults.stats.physicalBytesTransferred
+                        }
                         $owner = $run.originClusterIdentifier.clusterName
                     }else{
                         $logicalBytes = $server.localSnapshotInfo.snapshotInfo.stats.logicalSizeBytes
@@ -79,13 +78,31 @@ foreach($job in (api get -v2 "data-protect/protection-groups?isDeleted=false&inc
                         $bytesWritten = $server.localSnapshotInfo.snapshotInfo.stats.bytesWritten
                         $owner = $cluster.name
                     }
+                    $logicalReplicated = 0
+                    $physicalReplicated = 0
+                    $replicaDelay = 0
+                    $replicaDuration = 0
+                    if($server.PSObject.Properties['replicationInfo']){
+                        $replicaQueuedTime = $server.replicationInfo.replicationTargetResults[0].queuedTimeUsecs
+                        $replicaStartTime = $server.replicationInfo.replicationTargetResults[0].startTimeUsecs
+                        $replicaEndTime = $server.replicationInfo.replicationTargetResults[0].endTimeUsecs
+                        $replicaDelay = ($replicaStartTime - $replicaQueuedTime) / 3600000000
+                        $replicaDuration = ($replicaEndTime - $replicaStartTime) / 3600000000
+                        $server.replicationInfo.replicationTargetResults.stats.logicalBytesTransferred | ForEach-Object {$logicalReplicated += $_}
+                        $server.replicationInfo.replicationTargetResults.stats.physicalBytesTransferred | ForEach-Object {$physicalReplicated += $_}
+                    }
                     if($sourceName -notin $stats.Keys){
                         $stats[$sourceName] = @()
                     }
                     $stats[$sourceName] += @{'startTimeUsecs' = $runStartTimeUsecs;
                                              'dataRead' = $bytesRead;
                                              'dataWritten' = $bytesWritten;
-                                             'logicalSize' = $logicalBytes}
+                                             'logicalSize' = $logicalBytes;
+                                             'replicaDelay' = $replicaDelay;
+                                             'replicaDuration' = $replicaDuration;
+                                             'logicalReplicated' = $logicalReplicated;
+                                             'physicalReplicated' = $physicalReplicated
+                                            }
                     $owners[$sourceName] = $owner
                 }
             }
@@ -99,35 +116,47 @@ foreach($job in (api get -v2 "data-protect/protection-groups?isDeleted=false&inc
         $logicalSize = ($stats[$sourceName].logicalSize | Measure-Object -Maximum).Maximum
 
         # last 24 hours
-        $midnight = get-date -Hour 0 -Minute 0
+        $midnight = (get-date -Hour 0 -Minute 0).AddDays(-$backDays)
         $midnightUsecs = dateToUsecs $midnight
-        $last24Hours = dateToUsecs ($midnight.AddDays(-1))
-        $last24HourStats = $stats[$sourceName] | Where-Object {$_.startTimeUsecs -ge $last24Hours -and $_.startTimeUsecs -lt $midnightUsecs}
-        $last24HoursDataRead = 0
-        $last24HourStats.dataRead | foreach-object{ $last24HoursDataRead += $_ }
-        $last24HoursDataWritten = 0
-        $last24HourStats.dataWritten | ForEach-Object{ $last24HoursDataWritten += $_}
+        $lastDay = dateToUsecs ($midnight.AddDays(-1))
+        $lastDayStats = $stats[$sourceName] | Where-Object {$_.startTimeUsecs -ge $lastDay -and $_.startTimeUsecs -lt $midnightUsecs}
+        $lastDayDataRead = 0
+        $lastDayStats.dataRead | foreach-object{ $lastDayDataRead += $_ }
+        $lastDayDataWritten = 0
+        $lastDayStats.dataWritten | ForEach-Object{ $lastDayDataWritten += $_}
 
         # last X days
-        $lastXDays = dateToUsecs ((get-date -Hour 0 -Minute 0).AddDays(-$daysBack))
-        $lastXDaysStats = $stats[$sourceName] | Where-Object {$_.startTimeUsecs -ge $lastXDays}
-        $lastXDaysDataRead = 0
-        $lastXDaysStats.dataRead | foreach-object{ $lastXDaysDataRead += $_ }
-        $lastXDaysDataWritten = 0
-        $lastXDaysStats.dataWritten | ForEach-Object{ $lastXDaysDataWritten += $_}
-        $peakRead = ($lastXDaysStats.dataRead | Measure-Object -Maximum).Maximum
-
+        $xDays = dateToUsecs ((get-date -Hour 0 -Minute 0).AddDays(-($daysBack + $backDays)))
+        $xDaysStats = $stats[$sourceName] | Where-Object {$_.startTimeUsecs -ge $xDays}
+        $xDaysDataRead = 0
+        $xDaysStats.dataRead | foreach-object{ $xDaysDataRead += $_ }
+        $xDaysDataWritten = 0
+        $xDaysStats.dataWritten | ForEach-Object{ $xDaysDataWritten += $_}
+        $peakRead = ($xDaysStats.dataRead | Measure-Object -Maximum).Maximum
+        $xDaysReplicaDelay = 0
+        $xDaysStats.replicaDelay | foreach-object{ $xDaysReplicaDelay += $_ }
+        $xDaysLogicalReplicated = 0
+        $xDaysStats.logicalReplicated | foreach-object { $xDaysLogicalReplicated += $_ }
+        $xDaysPhysicalReplicated = 0
+        $xDaysStats.physicalReplicated | foreach-object { $xDaysPhysicalReplicated += $_ }
+        $xDaysReplicaDuration = 0
+        $xDaysStats.replicaDuration | foreach-object { $xDaysReplicaDuration += $_ }
         # number of days gathered
         $oldestStat = usecsToDate $stats[$sourceName][-1]['startTimeUsecs']
-        $numDays = ($today - $oldestStat).Days + 1
+        $numDays = ($now - $oldestStat).Days + 1
         if($logicalSize -gt 0){
-            $changeRate = 100 * [math]::Round(($lastXDaysDataRead / $logicalSize) / $daysBack, 2)
-            $writeChangeRate = 100 * [math]::Round(($lastXDaysDataWritten/ $logicalSize) / $daysBack, 2)
+            $changeRate = 100 * [math]::Round(($xDaysDataRead / $logicalSize) / $numDays, 2)
+            $writeChangeRate = 100 * [math]::Round(($xDaysDataWritten/ $logicalSize) / $numDays, 2)
         }else{
             $changeRate = '-'
             $writeChangeRate = '-'
         }
-
-        "{0},{1},{2},{3},""{4}"",""{5}"",""{6}"",""{7}"",""{8}"",""{9}"",{10},{11},{12}" -f $owner, $jobName, $jobType, $sourceName, $(toUnits $logicalSize), $(toUnits $peakRead), $(toUnits $last24HoursDataRead), $(toUnits $lastXDaysDataRead), $(toUnits $last24HoursDataWritten), $(toUnits $lastXDaysDataWritten), $numDays, $changeRate, $writeChangeRate | Out-File -FilePath $outfileName -Append
+        $avgDataRead = [math]::Round($xDaysDataRead / $numDays, 2)
+        $avgDataWritten = [math]::Round($xDaysDataWritten / $numDays, 2)
+        $avgReplicaDelay = [math]::Round($xDaysReplicaDelay / $numDays, 0)
+        $avgReplicaDuration = [math]::Round($xDaysReplicaDuration / $numDays, 0)
+        $avgLogicalReplicated = [math]::Round($xDaysLogicalReplicated / $numDays, 2)
+        $avgPhysicalReplicated = [math]::Round($xDaysPhysicalReplicated / $numDays, 2)
+        """{0}"",""{1}"",""{2}"",""{3}"",""{4}"",""{5}"",""{6}"",""{7}"",""{8}"",""{9}"",""{10}"",""{11}"",""{12}"",""{13}"",""{14}"",""{15}"",""{16}"",""{17}"",""{18}""" -f $owner, $jobName, $jobType, $sourceName, $(toUnits $logicalSize), $(toUnits $peakRead), $(toUnits $lastDayDataRead), $(toUnits $xDaysDataRead), $(toUnits $avgDataRead), $(toUnits $lastDayDataWritten), $(toUnits $xDaysDataWritten), $(toUnits $avgDataWritten), $numDays, $changeRate, $writeChangeRate, $avgReplicaDelay, $avgReplicaDuration, $(toUnits $avgLogicalReplicated), $(toUnits $avgPhysicalReplicated) | Out-File -FilePath $outfileName -Append
     }
 }
