af20e499a080c7f56b0ea7d21939c2b477013289
bseltz-cohesity <bseltzer@cohesity.com>
2022-07-21 05:53:27 -0400

    2022-07-21

---
+++ powershell/updateJob/README.md
@@ -0,0 +1,77 @@
+# Update Protection Job Properties using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script modifies various protection job properties including policy, startTime, timeZone, incremental and full SLAs.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'updateJob'
+$repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/powershell'
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* updateJob.ps1: the main powershell script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together, then we can run the script.
+
+To change the policy the job uses:
+
+```powershell
+./updateJob.ps1 -vip mycluster `
+                -username myusername `
+                -domain mydomain.net `
+                -jobName 'my job 1', 'my job 2' `
+                -policyName 'my new policy'
+```
+
+You can also provide a text file of jobs to change (one job name per line):
+
+```powershell
+./updateJob.ps1 -vip mycluster `
+                -username myusername `
+                -domain mydomain.net `
+                -jobList ./myjobs.txt `
+                -policyName 'my new policy'
+```
+
+Chaning the start time:
+
+```powershell
+./updateJob.ps1 -vip mycluster `
+                -username myusername `
+                -domain mydomain.net `
+                -jobList ./myjobs.txt `
+                -startTime '23:30'
+```
+
+## Authentication Parameters
+
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -emailMfaCode: (optional) send MFA code via email
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Job Parameters
+
+* -jobName: (optional) one or more job names to modify (comma separated)
+* -jobList: (optional) text file of job names to modify (one per line)
+* -policyName: (optional) name of new policy to assign to job
+* -startTime: (optional) e.g. '18:30'
+* -timeZone: (optional) e.g. 'America/New_York'
+* -incrementalProtectionSlaTimeMins: (optional) number of minutes before marking incremental SLA as missed
+* -fullProtectionSlaTimeMins: (optional) number of minutes before marking full SLA as missed
---
+++ powershell/updateJob/updateJob.ps1
@@ -0,0 +1,183 @@
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$vip = 'helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',  # local or AD domain
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password = $null,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode = $null,
+    [Parameter()][switch]$emailMfaCode,
+    [Parameter()][string]$clusterName = $null,
+    [Parameter()][string]$policyName,
+    [Parameter()][array]$jobName,
+    [Parameter()][string]$jobList,
+    [Parameter()][string]$startTime, # e.g. 23:30 for 11:30 PM
+    [Parameter()][string]$timeZone, # e.g. 'America/New_York'
+    [Parameter()][int]$incrementalProtectionSlaTimeMins,
+    [Parameter()][int]$fullProtectionSlaTimeMins
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $mcm -regionid $region
+
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    if($clusterName){
+        $thisCluster = heliosCluster $clusterName
+    }else{
+        write-host "Please provide -clusterName when connecting through helios" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated"
+    exit
+}
+
+# find job params path
+function findObjectParam($o){
+    $jobParams = $o.PSObject.Properties.Name -match 'params'
+    foreach($param in $jobParams){
+        if($o.$param.PSObject.Properties['objects']){
+            $objectParam = $o.$param
+        }elseif($o.$param.PSObject.Properties.Name -match 'params'){
+            $objectParam = findObjectParam $o.$param
+        }
+    }
+    return $objectParam
+}
+
+# gather list from command line params and file
+function gatherList($Param=$null, $FilePath=$null, $Required=$True, $Name='items'){
+    $items = @()
+    if($Param){
+        $Param | ForEach-Object {$items += $_}
+    }
+    if($FilePath){
+        if(Test-Path -Path $FilePath -PathType Leaf){
+            Get-Content $FilePath | ForEach-Object {$items += [string]$_}
+        }else{
+            Write-Host "Text file $FilePath not found!" -ForegroundColor Yellow
+            exit
+        }
+    }
+    if($Required -eq $True -and $items.Count -eq 0){
+        Write-Host "No $Name specified" -ForegroundColor Yellow
+        exit
+    }
+    return ($items | Sort-Object -Unique)
+}
+
+$jobNames = @(gatherList -Param $jobName -FilePath $jobList -Name 'jobs' -Required $True)
+
+$jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&includeTenants=true"
+
+if($jobNames.Count -gt 0){
+    $notfoundJobs = $jobNames | Where-Object {$_ -notin $jobs.protectionGroups.name}
+    if($notfoundJobs){
+        Write-Host "Jobs not found $($notfoundJobs -join ', ')" -ForegroundColor Yellow
+    }
+}
+
+# parse startTime
+if($startTime){
+    $hour, $minute = $startTime.split(':')
+    $tempInt = ''
+    if(! (($hour -and $minute) -or ([int]::TryParse($hour,[ref]$tempInt) -and [int]::TryParse($minute,[ref]$tempInt)))){
+        Write-Host "Please provide a valid start time" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+# get policy ID
+if($policyName){
+    $policies = (api get -v2 data-protect/policies).policies
+    $newPolicy = $policies | Where-Object { $_.name -ieq $policyName }
+    if(!$newPolicy){
+        Write-Warning "Policy $policyName not found!"
+        exit 1
+    }
+}
+
+foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
+    $updateJob = $false
+    if($job.name -in $jobNames){
+        Write-Host "$($job.name)"
+        $jobParams = findObjectParam $job
+        $isCAD = $jobParams.directCloudArchive
+        # update policy
+        if($policyName){
+            $updatePolicy = $True
+            if($isCAD){
+                $existingPolicy = $policies | Where-Object id -eq $job.policyId
+                # validate same vault ID
+                $existingVaultId = $existingPolicy.remoteTargetPolicy.archivalTargets[0].targetId
+                if($True -eq $updatePolicy -and $newPolicy.PSObject.Properties['remoteTargetPolicy'] -and $newPolicy.remoteTargetPolicy.PSObject.Properties['archivalTargets'] -and $newPolicy.remoteTargetPolicy.archivalTargets.Count -eq 1){
+                    $newVaultId = $newPolicy.remoteTargetPolicy.archivalTargets[0].targetId
+                    if($newVaultId -ne $existingVaultId){
+                        $updatePolicy = $false
+                        Write-Host "    can't update policy - archival targets do not match" -ForegroundColor Yellow
+                    }
+                    # validate archive schedule
+                    if($True -eq $updatePolicy -and $newPolicy.remoteTargetPolicy.archivalTargets[0].schedule.unit -ne 'Runs'){
+                        $updatePolicy = $false
+                        Write-Host "    can't update policy - archive must run after every run" -ForegroundColor Yellow
+                    }
+                }else{
+                    $updatePolicy = $false
+                    Write-Host "    can't update policy - archival targets do not match" -ForegroundColor Yellow
+                }
+                # validate no replication
+                if($True -eq $updatePolicy -and $newPolicy.PSObject.Properties['remoteTargetPolicy'] -and $newPolicy.remoteTargetPolicy.PSObject.Properties['replicationTargets'] -and $newPolicy.remoteTargetPolicy.replicationTargets.Count -gt 0){
+                    $updatePolicy = $false
+                    Write-Host "    can't update policy - replication not allowed for CAD policies" -ForegroundColor Yellow
+                }
+                # validate no retries
+                if($True -eq $updatePolicy -and $newPolicy.retryOptions.retries -gt 0){
+                    $updatePolicy = $false
+                    Write-Host "    can't update policy - num retries must be zero for CAD policies" -ForegroundColor Yellow
+                }
+            }
+            if($True -eq $updatePolicy){
+                Write-Host "    updating policy"
+                $updateJob = $True
+                $job.policyId = $newPolicy.id
+            }            
+        }
+        # update starttime
+        if($startTime){
+            Write-Host "    updating start time"
+            $updateJob = $True
+            $job.startTime.hour = [int]$hour
+            $job.startTime.minute = [int]$minute
+        }
+        # update timezone
+        if($timeZone){
+            Write-Host "    updating timezone"
+            $updateJob = $True
+            $job.startTime.timeZone = $timeZone
+        }
+        # update SLAs
+        if($incrementalProtectionSlaTimeMins){
+            Write-Host "    updating SLA (incremental)"
+            $updateJob = $True
+            $incrementalSLA = $job.sla | Where-Object backupRunType -eq 'kIncremental'
+            $incrementalSLA.slaMinutes = [int]$incrementalProtectionSlaTimeMins
+        }
+        if($fullProtectionSlaTimeMins){
+            Write-Host "    updating SLA (full)"
+            $updateJob = $True
+            $fullSLA = $job.sla | Where-Object backupRunType -eq 'kFull'
+            $fullSLA.slaMinutes = [int]$fullProtectionSlaTimeMins
+        }        
+    }
+    if($True -eq $updateJob){
+        $null = api put -v2 data-protect/protection-groups/$($job.id) $job
+    }
+}
