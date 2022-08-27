8905cc518ce7c8084c6aa881a93769bd53066820
bseltz-cohesity <bseltzer@cohesity.com>
2022-08-27 06:16:50 -0400

    2022-08-27

--- python/instantVolumeMount/instantVolumeMountDestroy.py
+++ python/instantVolumeMount/instantVolumeMountDestroy.py
@@ -12,9 +12,15 @@ from pyhesity import *
 # command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)       # cluster to connect to
-parser.add_argument('-u', '--username', type=str, required=True)  # username
-parser.add_argument('-d', '--domain', type=str, default='local')  # (optional) domain - defaults to local
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-t', '--taskid', type=int, required=True)   # job name
 
 args = parser.parse_args()
@@ -22,10 +28,34 @@ args = parser.parse_args()
 vip = args.vip
 username = args.username
 domain = args.domain
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 taskid = args.taskid
 
 # authenticate
-apiauth(vip, username, domain)
+if mcm:
+    apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=True)
+else:
+    if emailmfacode:
+        apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, emailMfaCode=True)
+    else:
+        apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, mfaCode=mfacode)
+
+# if connected to helios or mcm, select to access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
 
 tearDownTask = api('post', '/destroyclone/%s' % taskid)
 print('Tearing down mount points...')
