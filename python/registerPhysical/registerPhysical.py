1cff949106166f87a3ccaf15ebeca196b7f2e09d
bseltz-cohesity <bseltzer@cohesity.com>
2022-09-06 17:22:12 -0400

    2022-09-06

--- python/registerPhysical/registerPhysical.py
+++ python/registerPhysical/registerPhysical.py
@@ -4,9 +4,15 @@ from pyhesity import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)           # cluster to connect to
-parser.add_argument('-u', '--username', type=str, default='helios')   # admin user to do the work
-parser.add_argument('-d', '--domain', type=str, default='local')      # domain of admin user
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-s', '--servername', action='append', type=str)  # server name to register
 parser.add_argument('-l', '--serverlist', type=str, default=None)     # text list of servers to register
 
@@ -15,19 +21,55 @@ args = parser.parse_args()
 vip = args.vip
 username = args.username
 domain = args.domain
-servernames = args.servername
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+servername = args.servername
 serverlist = args.serverlist
 
-# authenticate
-apiauth(vip, username, domain)
 
-# read server file
-if servernames is None:
-    servernames = []
-if serverlist is not None:
-    f = open(serverlist, 'r')
-    servernames += [s.strip() for s in f.readlines() if s.strip() != '']
-    f.close()
+# gather list function
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+# get list of views to protect
+servernames = gatherList(servername, serverlist, name='servers', required=True)
+
+# authenticate
+if mcm:
+    apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=True)
+else:
+    if emailmfacode:
+        apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, emailMfaCode=True)
+    else:
+        apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, mfaCode=mfacode)
+
+# if connected to helios or mcm, select to access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
 
 for server in servernames:
     newSource = {
