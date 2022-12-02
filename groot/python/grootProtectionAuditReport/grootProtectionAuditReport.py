150a84c6eb8f8bd19fa0cc4a4f2ac8eaa84f4b86
bseltz-cohesity <bseltzer@cohesity.com>
2022-12-02 05:07:23 -0500

    2022-12-02

--- groot/python/grootProtectionAuditReport/grootProtectionAuditReport.py
+++ groot/python/grootProtectionAuditReport/grootProtectionAuditReport.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
 parser.add_argument('-u', '--username', type=str, default='helios')
 parser.add_argument('-d', '--domain', type=str, default='local')
-parser.add_argument('-c', '--clustername', action='append', type=str)
+parser.add_argument('-c', '--clustername', type=str)
 parser.add_argument('-mcm', '--mcm', action='store_true')
 parser.add_argument('-i', '--useApiKey', action='store_true')
 parser.add_argument('-pwd', '--password', type=str, default=None)
@@ -22,7 +22,7 @@ args = parser.parse_args()
 vip = args.vip
 username = args.username
 domain = args.domain
-clusternames = args.clustername
+clustername = args.clustername
 mcm = args.mcm
 useApiKey = args.useApiKey
 password = args.password
@@ -31,6 +31,19 @@ days = args.days
 
 # authenticate
 apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt))
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
 
 print('Collecting report data...')
 
