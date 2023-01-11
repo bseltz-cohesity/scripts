eea4b3502a47f16827770d77cadb011543b27a0d
bseltz-cohesity <bseltzer@cohesity.com>
2023-01-11 15:49:23 -0500

    2023-01-11

---
+++ dmaas/powershell/enableSaaSconnectorRT/enableSaaSconnectorRT.ps1
@@ -0,0 +1,46 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$True)][string]$vip,
+    [Parameter()][string]$username = 'admin',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$enable,
+    [Parameter()][switch]$disable,
+    [Parameter()][int]$days = 1
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -noPromptForPassword $noPrompt
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated"
+    exit 1
+}
+
+$dayUsecs = dateToUsecs ((Get-Date).AddDays($days))
+
+if($enable){
+    Write-Host "Enabling support channel..."
+    $result = api put -v2 support-channel-config @{'isEnabled' = $true; 'endTimeUsecs' = $dayUsecs}
+}
+if($disable){
+    Write-Host "Disabling support channel..."
+    $result = api put -v2 support-channel-config @{'isEnabled' = $false; 'endTimeUsecs' = 1673728458000000}
+    exit
+}
+
+$cluster = api get cluster
+$supportToken = ''
+while($supportToken -eq ''){
+    Start-Sleep 5
+    $token = api put users/linuxSupportUserBashShellAccess
+    $supportToken = $token.supportUserToken
+}
+
+Write-Host "Cluster ID: $($cluster.id)"
+Write-host $supportToken
