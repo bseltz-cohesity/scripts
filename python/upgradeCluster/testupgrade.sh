603abc63941540029c6e3bbdd9022c994cb99e59
bseltz-cohesity <bseltzer@cohesity.com>
2023-02-03 10:57:59 -0500

    2023-02-03

--- python/upgradeCluster/testupgrade.sh
+++ python/upgradeCluster/testupgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./upgradeCluster.py -v 10.19.0.202 -u admin -d local -r '6.5.1c_release-20201119_ec194046' -url 'http://10.19.0.67:5000/6.5.1c_release-20201119_ec194046'
+./upgradeCluster.py -v 10.19.0.202 -u admin -d local -r '6.6.0d_u6_release-20221204_c03629f0' -url 'http://10.19.0.67:5000/6.6.0d_u6_release-20221204_c03629f0'
