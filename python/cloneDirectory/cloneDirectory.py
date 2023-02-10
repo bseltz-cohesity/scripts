08756476b4d6f98a33343fd5196310668ef47822
bseltz-cohesity <bseltzer@cohesity.com>
2023-02-09 21:06:26 -0500

    2023-02-09

--- python/cloneDirectory/cloneDirectory.py
+++ python/cloneDirectory/cloneDirectory.py
@@ -10,23 +10,33 @@ from pyhesity import *
 # command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)
-parser.add_argument('-u', '--username', type=str, required=True)
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
 parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-s', '--sourcepath', type=str, required=True)
 parser.add_argument('-t', '--targetpath', type=str, required=True)
-parser.add_argument('-i', '--useApiKey', action='store_true')       # use API key authentication
-parser.add_argument('-pwd', '--password', type=str, default=None)   # optional password
 
 args = parser.parse_args()
 
 vip = args.vip
 username = args.username
 domain = args.domain
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 sourcepath = args.sourcepath
 targetpath = args.targetpath
-password = args.password
-useApiKey = args.useApiKey
 
 sourcepath = sourcepath.replace('\\', '/').replace('//', '/')
 targetpath = targetpath.replace('\\', '/').replace('//', '/')
@@ -48,7 +58,20 @@ if targetpath == '':
     exit()
 
 # authenticate
-apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, noretry=True, quiet=True)
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), emailMfaCode=emailmfacode, mfaCode=mfacode)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
 
 # clone directory params
 CloneDirectoryParams = {
