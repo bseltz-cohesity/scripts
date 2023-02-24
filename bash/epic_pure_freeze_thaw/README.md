bfeccb49daf6979775fcd01056a82cc174a3bdc9
bseltz-cohesity <bseltzer@cohesity.com>
2023-02-24 08:59:41 -0500

    2023-02-24

---
+++ bash/epic_pure_freeze_thaw/README.md
@@ -0,0 +1,94 @@
+# EPIC Pure Freeze Thaw PreScript
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This bash script can be used as a pre-script in a Pure FlashArray volume backup to freeze an EPIC database, snapshot the related Pure volumes, and thaw the database prior to the volume backup starting.
+
+## Download the script
+
+```bash
+# Begin download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/bash/epic_pure_freeze_thaw/epic_pure_freeze_thaw.sh
+chmod +x epic_pure_freeze_thaw.sh
+chmod +x jobRunning.py
+chmod +x backupNow.py
+# End download commands
+```
+
+## On the Script Host (AIX or Linux)
+
+* Create or select a user to run the script. The script requires no special access on the host. Create or identify an ssh key pair that we will use when authenticating to the Pure FlashArray. To create an ssh key pair, use the command:
+
+```bash
+ssh-keygen
+```
+
+* Copy the script onto the host and make the script executable using the command:
+
+```bash
+chmod +x epic_pure_freeze_thaw.sh
+```
+
+## On the Pure FlashArray
+
+Log into the Pure FlashArray and perform the following steps:
+
+* Create a user for the script to use to log into the Pure FlashArray
+* Paste the ssh public key for the AIX / Linux user into the field provided
+* Create a Pure Protection Group and add all EPIC related volumes to the group
+
+## Back on the Script Host
+
+Edit the script and make the following changes:
+
+* Change first line to `#!/bin/ksh` (AIX) or `#!/bin/bash` (Linux)
+* Modify the settings section of the script:
+
+```bash
+testing=1      # 1 = skip freeze/thaw (for testing), 0 = perform freeze/thaw (production)
+PRIVKEY_PATH="-i /root/.ssh/id_rsa"    # path to ssh private key
+PURE_USER="puresnap"                   # user name on Pure FlashArray
+PURE_ARRAY="10.1.1.10"                 # FQDN or IP of Pure FlashArray
+PURE_SRC_PGROUP="EpicProtectionGroup"  # Name of Pure Protection Group on Pure FlashArray
+```
+
+* Uncomment and inspect file system freeze sections of the script where needed
+
+Also, edit the `/etc/ssh/sshd_cohfig` and set:
+
+```bash
+MaxStartups 50:30:150  # the first number must be 24 or higher
+```
+
+And restart sshd:
+
+```bash
+systemctl restart sshd.service  # Linux
+stopsrc -g ssh && startsrc -g ssh  # AIX
+```
+
+## Create a Cohesity Pure FlashArray Protection Group
+
+Create Cohesity protection group and select all of the EPIC-related volumes. Choose or create a protection policy that has retries set to 0.
+
+Under Additional Settings, under the Pre and Post Scripts option, configure the following:
+
+* The hostname or IP of the script host
+* The AIX/Linux username
+* Enable `Pre Script`
+* The full path to the script, e.g. /home/root/scripts/freezethaw.sh
+* Enter a comma-separated list of volume groups (AIX) to freeze (if any), e.g. volgrp1,volgrp2,volgrp3
+* Disable `Continue Backup if Script Fails`
+* Disable `Post Script`
+
+Also, copy the Cluster SSH Public Key provided and add that to the AIX/Linux user's authorized_keys file on the script host.
+
+## Testing the Script
+
+When the Cohesity Protection Group runs, the Pre Script will run once for each protected EPIC volume. You can monitor the script output log on the script host:
+
+```bash
+tail -f /tmp/cohesity_snap.log
+```
+
+The first instance of the script (the leader) will perform the EPIC freeze / Pure snapshots / EPIC thaw while the others will wait for the leader to finish. Once the snapshots are created and the database and file systems are thawed, the Pure volume backups will begin. Note that these snapshots will be deleted at a later time, as configured in the Cohesity protection group (see the `Retain on Pure Storage Array` setting under Additional Settings).
---
+++ bash/epic_pure_freeze_thaw/epic_pure_freeze_thaw.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+
+##########################################################################################
+##
+## Last updated: 2023.02.24 - Brian Seltzer @ Cohesity
+##
+## - change first line to #!/bin/ksh (AIX) or #!/bin/bash (Linux)
+## - edit /etc/ssh/sshd_cohfig: MaxStartups 50:30:150 (first number must be 24 or higher) 
+## - modify parameters below
+## - epic luns must be in pure protection group
+## - pass in comma separated list of volume groups to freeze (AIX) as pre-script parameter
+## - uncomment and inspect file system freeze sections (AIX)
+## - testing=1 means we will not skip freezing, testing=0 means we will freeze
+##
+##########################################################################################
+
+testing=1
+PRIVKEY_PATH="-i /root/.ssh/id_rsa"
+PURE_USER="puresnap"
+PURE_ARRAY="10.12.1.39"
+PURE_SRC_PGROUP="EpicProtectionGroup108"
+
+echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Started" >> /tmp/cohesity_snap.log
+
+if [[ ! -e /tmp/cohesity_snap.log ]]; then
+    touch /tmp/cohesity_snap.log
+fi
+
+if [[ ! -e /tmp/cohesity_SRCLUNS.txt ]]; then
+    touch /tmp/cohesity_SRCLUNS.txt
+fi
+
+###################################
+## Check if main script already run
+###################################
+
+#### If my lock already exists, we're good, exit and perform the backup
+if [[ -e /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY ]]; then
+    echo "Found Lock. Main Script Ran Already"
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed Successfully" >> /tmp/cohesity_snap.log
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    exit 0
+fi
+
+##########################
+## If Not Create Lock File
+##########################
+
+#### Try to take leader role
+mkdirstatus=1
+if [[ ! -e /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX ]]; then
+    mkdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX 
+    mkdirstatus=$?
+fi
+
+#### If not the leader, wait for my lock to appear then exit and perform the backup
+if [[ $mkdirstatus -ne 0 ]]; then
+    echo "Snapshot script already running. Waiting for Main Script to complete"
+    while [[ -e /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX ]]; do
+        sleep 1
+    done 
+    echo "Done Waiting."
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed Successfully" >> /tmp/cohesity_snap.log
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    exit 0
+fi
+
+#################################################
+## I'm the leader, continue to freeze, snap, thaw
+#################################################
+
+echo "$(date) Created Lock /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX" 
+
+############################
+## Generate Source LUN Locks
+############################
+
+#### Get list of volumes in pure protection group
+echo "Generating Source LUN File /tmp/cohesity_SRCLUNS.txt" 
+/usr/bin/ssh ${PRIVKEY_PATH} ${PURE_USER}@${PURE_ARRAY} "purepgroup listobj --type vol ${PURE_SRC_PGROUP}"  > /tmp/cohesity_SRCLUNS.txt
+if [[ $? -ne 0 ]]; then
+    echo "Obtaining Pure Source LUNs Failed"
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Obtaining Pure Source LUNs Failed ****" >> /tmp/cohesity_snap.log
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed with Error ****" >> /tmp/cohesity_snap.log
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX >> /tmp/cohesity_snap.log 2>&1
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    exit 4
+fi
+
+PURE_SRC_LUNS=$(sed -e :a -e '$!N; s/\n/ /; ta' /tmp/cohesity_SRCLUNS.txt)
+echo "PURE_SOURCE_LUNS: $PURE_SRC_LUNS"
+
+#### make volume locks
+for jj in $PURE_SRC_LUNS
+do
+    mkdir -p /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$jj
+done
+
+if [[ ! -e /tmp/$COHESITY_JOB_ID ]]; then
+    mkdir /tmp/$COHESITY_JOB_ID
+    journalStatus=$?
+fi
+
+if [[ $journalStatus -ne 0 ]]; then
+    echo "Found Current Snapshot Suffix  $COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX> in Journal, Exiting."
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed Successfully" >> /tmp/cohesity_snap.log
+    exit 0
+fi
+
+echo "" >> /tmp/cohesity_snap.log
+echo "################# Backup For $(date) #########################" >> /tmp/cohesity_snap.log
+echo "COHESITY_BACKUP_ENTITY:$COHESITY_BACKUP_ENTITY" 
+echo "COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX:$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX" 
+echo "Cohesity Pre Script Starting.\n"
+echo "################# Script Leader ########################"
+echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Running as Leader" >> /tmp/cohesity_snap.log
+echo "$(date) : $COHESITY_BACKUP_ENTITY : Cohesity Pre-Script starting with PID $$" >> /tmp/cohesity_snap.log
+
+##################
+## Freeze Database
+##################
+
+echo "Freezing Database"
+echo "$(date) : $COHESITY_BACKUP_ENTITY : Freezing Database" >> /tmp/cohesity_snap.log
+
+##### Testing
+if [[ $testing -eq 1 ]]; then
+    freeze_status=0
+    fsfreeze_status=0
+else
+	# /opt/scripts/ZBACKUP/freeze.sh
+    freeze_status=$?
+fi
+
+if [[ $freeze_status -ne 0 ]]; then
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Database Freeze Failed: $freeze_status"  >> /tmp/cohesity_snap.log
+    echo "Database Freeze Failed: $freeze_status"
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed with Error ****" >> /tmp/cohesity_snap.log
+	rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX >> /tmp/cohesity_snap.log 2>&1
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    exit 1
+else
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Database Freeze Successful" >> /tmp/cohesity_snap.log
+    echo "Database Freeze Successful."
+fi
+
+#####################
+## Freeze Filesystems
+#####################
+
+##### comma separated list of volgroups to freeze (from script parameter)
+# typeset -i fsfreeze_status=0
+fsfreeze_status=0
+if [[ -n $1 ]]; then
+    volgrps=$(echo $1 | sed 's/,/ /')
+    echo "$(date) : Volumes Groups to Freeze : $volgrps" >> /tmp/cohesity_snap.log
+    # if [[ $testing -ne 1 ]]; then
+    #     for vgs in $volgrps
+    #     do
+    #         for ii in $(lsvgfs $vgs)
+    #         do
+    #             echo "$(date) : Freezing Filesystem: $ii" >> /tmp/cohesity_snap.log
+    #             echo "Freezing Filesystem: $ii"
+    #             chfs -a freeze=300 $ii
+    #             if [[ $? -ne 0 ]]; then
+    #                 (( fsfreeze_status+=1 ))
+    #                 echo "$(date) : $COHESITY_BACKUP_ENTITY : FileSystem Freeze Failed: $ii"  >> /tmp/cohesity_snap.log
+    #                 echo "FileSystem Freeze Failed: $ii"
+    #             fi
+    #         done
+    #     done
+    # fi
+fi
+
+################
+## Freeze Failed
+################
+
+if [[ $fsfreeze_status -gt 0 ]]; then
+
+    echo "Freeze Failed. Rolling Back:"
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Freeze Failed. Rolling Back ****" >> /tmp/cohesity_snap.log
+
+    #### Thaw file systems
+    
+    # if [[ $testing -ne 1 ]]; then
+    #     for vgs in $volgrps
+    #     do
+    #         for ii in $(lsvgfs $vgs)
+    #         do
+    #             echo "$(date) : $COHESITY_BACKUP_ENTITY : Thawing Filesystem: $ii" >> /tmp/cohesity_snap.log
+    #             echo "Thawing Filesystem: $ii"
+    #             chfs -a freeze=off $ii
+    #         done
+    #     done
+    # fi
+
+    #### Thaw database
+    echo "Thawing Database"
+	echo "$(date) : $COHESITY_BACKUP_ENTITY : Thawing Database" >> /tmp/cohesity_snap.log
+    if [[ $testing -ne 1 ]]; then
+	    /opt/scripts/ZBACKUP/thaw.sh
+    fi
+
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed with Error ****" >> /tmp/cohesity_snap.log
+	rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX >> /tmp/cohesity_snap.log 2>&1
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    exit 2
+else
+
+    ####################
+    ## Freeze Successful
+    ####################
+
+    #### create snapshots
+    echo "$(date) : $COHESITY_BACKUP_ENTITY : FileSystem Freeze Successful" >> /tmp/cohesity_snap.log
+    echo "FileSystem Freeze Successful."
+	echo "$(date) : $COHESITY_BACKUP_ENTITY : Snapshotting Pure Volumes" >> /tmp/cohesity_snap.log
+	echo "Running purevol snap."
+	echo "/usr/bin/ssh ${PRIVKEY_PATH} ${PURE_USER}@${PURE_ARRAY} purevol snap --suffix ${COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX} ${PURE_SRC_LUNS}" 
+	/usr/bin/ssh ${PRIVKEY_PATH} ${PURE_USER}@${PURE_ARRAY} "purevol snap --suffix ${COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX} ${PURE_SRC_LUNS}" 
+  	if [[ $? -eq 0 ]]; then
+    	echo "$(date) : $COHESITY_BACKUP_ENTITY : Pure Volume Snaps Successful" >> /tmp/cohesity_snap.log
+    	echo "Pure Volume Snaps Successful" 
+        snap_status=0
+  	else
+    	echo "$(date) : $COHESITY_BACKUP_ENTITY : Pure Volume Snap Failed" >> /tmp/cohesity_snap.log
+    	echo "Pure Volume Snap Failed" 
+		snap_status=5
+  	fi
+
+    #### Thaw database
+	echo "Thawing Database"
+	echo "$(date) : $COHESITY_BACKUP_ENTITY : Thawing Database" >> /tmp/cohesity_snap.log
+
+    if [[ $testing -ne 1 ]]; then
+        /opt/scripts/ZBACKUP/thaw.sh
+    fi
+
+    #### Thaw file systems
+    # if [[ $testing -ne 1 ]]; then
+    #     for vgs in $volgrps
+    #     do
+    #         for ii in $(lsvgfs $vgs)
+    #         do
+    #             echo "$(date) : $COHESITY_BACKUP_ENTITY : Thawing Filesystem: $ii" >> /tmp/cohesity_snap.log
+    #             echo "Thawing Filesystem: $ii"
+    #             chfs -a freeze=off $ii
+    #         done
+    #     done
+    # fi
+
+	rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX >> /tmp/cohesity_snap.log 2>&1
+	echo "Removing Lock: rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX"
+    echo "Removing Lock  /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY. Exiting"
+    rmdir /tmp/$COHESITY_BACKUP_VOLUME_SNAPSHOT_SUFFIX.$COHESITY_BACKUP_ENTITY
+    if [[ $snap_status -eq 0 ]]; then
+        echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed Successfully" >> /tmp/cohesity_snap.log
+    else
+        echo "$(date) : $COHESITY_BACKUP_ENTITY : Script Completed with Error ****" >> /tmp/cohesity_snap.log
+    fi
+    echo "" >> /tmp/cohesity_snap.log
+    exit $snap_status
+fi
+
+exit 0
