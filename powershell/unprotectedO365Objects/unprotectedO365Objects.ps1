44b24918c510dcaaa440382979716dc20e50102d
bseltz-cohesity <bseltzer@cohesity.com>
2023-04-19 04:31:43 -0400

    2023-04-19

--- powershell/unprotectedO365Objects/unprotectedO365Objects.ps1
+++ powershell/unprotectedO365Objects/unprotectedO365Objects.ps1
@@ -106,7 +106,8 @@ $unprotectedObjects = @()
 $jobs = (api get -v2 "data-protect/protection-groups?environments=kO365&isActive=true&isDeleted=false").protectionGroups | Where-Object {$_.office365Params.protectionTypes -eq $objectKtype}
 
 $protectedIndex = @($jobs.office365Params.objects.id | Where-Object {$_ -ne $null})
-$unprotectedIndex = @($jobs.office365Params.excludeObjectIds | Where-Object {$_ -ne $null -and $_ -notin $protectedIndex})
+# $unprotectedIndex = @($jobs.office365Params.excludeObjectIds | Where-Object {$_ -ne $null -and $_ -notin $protectedIndex})
+$unprotectedIndex = @()
 
 $objects = api get "protectionSources?pageSize=50000&nodeId=$($objectsNode.protectionSource.id)&id=$($objectsNode.protectionSource.id)&allUnderHierarchy=false$($queryParam)&useCachedData=false"
 $cursor = $objects.entityPaginationParameters.beforeCursorEntityId
