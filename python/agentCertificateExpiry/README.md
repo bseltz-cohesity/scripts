6e770df2f7600d83cdc6bd1bab2940e5cf719221
bseltz-cohesity <bseltzer@cohesity.com>
2023-04-26 05:40:26 -0400

    2023-04-26

---
+++ python/agentCertificateExpiry/README.md
@@ -0,0 +1,61 @@
+# Report Agent Certificate Expirations using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This script will report certificate expirations for registered agents.
+
+## Components
+
+* agentCertificateExpiry.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/agentCertificateExpiry/agentCertificateExpiry.py
+curl -O https://raw.githubusercontent.com/bseltz-cohesity/scripts/master/python/pyhesity.py
+chmod +x agentCertificateExpiry.py
+# end download commands
+```
+
+```bash
+./agentCertificateExpiry.py -v mycluster \
+                         -u myuser \
+                         -d local \
+                         -ad mydomain.net
+```
+
+## Authentication Parameters
+
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
+* -w, --includewindows: (optional)
+* -x', --expirywarningdate: (optional) default is '2023-06-01 00:00:00'
+
+## The Python Helper Module - pyhesity.py
+
+The helper module provides functions to simplify operations such as authentication, api calls, storing encrypted passwords, and converting date formats. The module requires the requests python module.
+
+### Installing the Prerequisites
+
+```bash
+sudo yum install python-requests
+```
+
+or
+
+```bash
+sudo easy_install requests
+```
---
+++ python/agentCertificateExpiry/agentCertificateExpiry.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""base V2 example"""
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+import codecs
+import os
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
+parser.add_argument('-w', '--includewindows', action='store_true')
+parser.add_argument('-x', '--expirywarningdate', type=str, default='2023-06-01 00:00:00')
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+includewindows = args.includewindows
+expirywarningdate = args.expirywarningdate
+
+expwarningusecs = dateToUsecs(expirywarningdate)
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), emailMfaCode=emailmfacode, mfaCode=mfacode, tenantId=tenant)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+now = datetime.now()
+nowUsecs = dateToUsecs(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# outfile
+cluster = api('get', 'cluster')
+dateString = now.strftime("%Y-%m-%d")
+outfile = 'agentVersions-%s-%s.tsv' % (cluster['name'], dateString)
+f = codecs.open(outfile, 'w')
+
+# headings
+f.write('Cluster Name\tSource Name\tAgent Version\tOS Type\tOS Name\tCert Expires\tExpires Soon\n')
+
+nodes = api('get', 'protectionSources/registrationInfo?environments=kPhysical')
+
+for node in nodes['rootNodes']:
+    name = node['rootNode']['physicalProtectionSource']['name']
+    if 'agents' in node['rootNode']['physicalProtectionSource']:
+        expiringSoon = False
+        version = node['rootNode']['physicalProtectionSource']['agents'][0]['version']
+        hostType = node['rootNode']['physicalProtectionSource']['hostType'][1:]
+        osName = node['rootNode']['physicalProtectionSource']['osName']
+        if includewindows or hostType != 'Windows':
+            certinfo = os.popen('timeout 5 openssl s_client -showcerts -connect %s:50051 </dev/null 2>/dev/null | openssl x509 -noout -subject -dates 2>/dev/null' % name)
+            cilines = certinfo.readlines()
+            if len(cilines) >= 2:
+                expdate = cilines[2]
+                expires = expdate.strip().split('=')[1].replace('  ', ' ')
+                datetime_object = datetime.strptime(expires, '%b %d %H:%M:%S %Y %Z')
+                expiresUsecs = dateToUsecs(datetime_object)
+                if expiresUsecs < expwarningusecs:
+                    expiringSoon = True
+                expires = datetime.strftime(datetime_object, "%m/%d/%Y %H:%M:%S")
+            else:
+                expires = 'unknown'
+            print('%s\t%s\t(%s) %s -> %s' % (name, version, hostType, osName, expires))
+            f.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (cluster['name'], name, version, hostType, osName, expires, expiringSoon))
+
+f.close()
+print('\nOutput saved to %s\n' % outfile)
