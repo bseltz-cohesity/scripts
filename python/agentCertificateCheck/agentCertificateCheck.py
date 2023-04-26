d3b6da7ba593d75756600e52803db694e5059b22
bseltz-cohesity <bseltzer@cohesity.com>
2023-04-26 18:15:02 -0400

    2023-04-26

--- python/agentCertificateCheck/agentCertificateCheck.py
+++ python/agentCertificateCheck/agentCertificateCheck.py
@@ -91,66 +91,66 @@ for clustername in clusternames:
 
     nodes = api('get', 'protectionSources/registrationInfo?environments=kPhysical&allUnderHierarchy=true')
     hosts = api('get', '/nexus/cluster/get_hosts_file')
-
-    for node in nodes['rootNodes']:
-        port = 50051
-        name = node['rootNode']['physicalProtectionSource']['name']
-        testname = name
-        if hosts is not None and 'hosts' in hosts and hosts['hosts'] is not None and len(hosts['hosts']) > 0:
-            ip = [h['ip'] for h in hosts['hosts'] if name.lower() in [d.lower() for d in h['domainName']]]
-            if ip is not None and len(ip) > 0:
-                testname = ip[0]
-        hostType = 'unknown'
-        osName = 'unknown'
-        version = 'unknown'
-        expiringSoon = False
-        expires = 'unknown'
-        errorMessage = 'None'
-        try:
-            if 'agents' in node['rootNode']['physicalProtectionSource']:
-                version = node['rootNode']['physicalProtectionSource']['agents'][0]['version']
-                hostType = node['rootNode']['physicalProtectionSource']['hostType'][1:]
-                osName = node['rootNode']['physicalProtectionSource']['osName']
-                if includewindows is True or hostType != 'Windows':
-                    agentGflag = [f['value'] for f in gflaglist if f['name'] == 'magneto_agent_port_number' % hostType.lower()]
-                    if agentGflag is not None and len(agentGflag) > 0:
-                        port = agentGflag[0]
-                    agentGflag = [f['value'] for f in gflaglist if f['name'] == 'magneto_%s_agent_port_number' % hostType.lower()]
-                    if agentGflag is not None and len(agentGflag) > 0:
-                        port = agentGflag[0]
-                    try:
-                        if 'authenticationErrorMessage' in node['registrationInfo'] and node['registrationInfo']['authenticationErrorMessage'] is not None:
-                            errorMessage = node['registrationInfo']['authenticationErrorMessage'].split(',')[0].split('\n')[0]
-                        if 'refreshErrorMessage' in node['registrationInfo'] and node['registrationInfo']['refreshErrorMessage'] is not None and node['registrationInfo']['refreshErrorMessage'] != '':
-                            errorMessage = node['registrationInfo']['refreshErrorMessage'].split(',')[0].split('\n')[0]
-                    except Exception:
-                        pass
-                    try:
-                        certinfo = os.popen('timeout 5 openssl s_client -showcerts -connect %s:%s </dev/null 2>/dev/null | openssl x509 -noout -subject -dates 2>/dev/null' % (testname, port))
-                        cilines = certinfo.readlines() 
-                        if len(cilines) >= 2:
-                            expdate = cilines[2]
-                            expires = expdate.strip().split('=')[1].replace('  ', ' ')
-                            datetime_object = datetime.strptime(expires, '%b %d %H:%M:%S %Y %Z')
-                            expiresUsecs = dateToUsecs(datetime_object)
-                            if expiresUsecs < expwarningusecs:
-                                expiringSoon = True
-                            expires = datetime.strftime(datetime_object, "%m/%d/%Y %H:%M:%S")
-                        else:
+    if nodes is not None and 'rootNodes' in nodes and nodes['rootNodes'] is not None:
+        for node in nodes['rootNodes']:
+            port = 50051
+            name = node['rootNode']['physicalProtectionSource']['name']
+            testname = name
+            if hosts is not None and 'hosts' in hosts and hosts['hosts'] is not None and len(hosts['hosts']) > 0:
+                ip = [h['ip'] for h in hosts['hosts'] if name.lower() in [d.lower() for d in h['domainName']]]
+                if ip is not None and len(ip) > 0:
+                    testname = ip[0]
+            hostType = 'unknown'
+            osName = 'unknown'
+            version = 'unknown'
+            expiringSoon = False
+            expires = 'unknown'
+            errorMessage = 'None'
+            try:
+                if 'agents' in node['rootNode']['physicalProtectionSource']:
+                    version = node['rootNode']['physicalProtectionSource']['agents'][0]['version']
+                    hostType = node['rootNode']['physicalProtectionSource']['hostType'][1:]
+                    osName = node['rootNode']['physicalProtectionSource']['osName']
+                    if includewindows is True or hostType != 'Windows':
+                        agentGflag = [f['value'] for f in gflaglist if f['name'] == 'magneto_agent_port_number' % hostType.lower()]
+                        if agentGflag is not None and len(agentGflag) > 0:
+                            port = agentGflag[0]
+                        agentGflag = [f['value'] for f in gflaglist if f['name'] == 'magneto_%s_agent_port_number' % hostType.lower()]
+                        if agentGflag is not None and len(agentGflag) > 0:
+                            port = agentGflag[0]
+                        try:
+                            if 'authenticationErrorMessage' in node['registrationInfo'] and node['registrationInfo']['authenticationErrorMessage'] is not None:
+                                errorMessage = node['registrationInfo']['authenticationErrorMessage'].split(',')[0].split('\n')[0]
+                            if 'refreshErrorMessage' in node['registrationInfo'] and node['registrationInfo']['refreshErrorMessage'] is not None and node['registrationInfo']['refreshErrorMessage'] != '':
+                                errorMessage = node['registrationInfo']['refreshErrorMessage'].split(',')[0].split('\n')[0]
+                        except Exception:
+                            pass
+                        try:
+                            certinfo = os.popen('timeout 5 openssl s_client -showcerts -connect %s:%s </dev/null 2>/dev/null | openssl x509 -noout -subject -dates 2>/dev/null' % (testname, port))
+                            cilines = certinfo.readlines() 
+                            if len(cilines) >= 2:
+                                expdate = cilines[2]
+                                expires = expdate.strip().split('=')[1].replace('  ', ' ')
+                                datetime_object = datetime.strptime(expires, '%b %d %H:%M:%S %Y %Z')
+                                expiresUsecs = dateToUsecs(datetime_object)
+                                if expiresUsecs < expwarningusecs:
+                                    expiringSoon = True
+                                expires = datetime.strftime(datetime_object, "%m/%d/%Y %H:%M:%S")
+                            else:
+                                expires = 'unknown'
+                        except Exception:
                             expires = 'unknown'
-                    except Exception:
-                        expires = 'unknown'
-        except Exception:
-            pass
-        if includewindows is True or hostType != 'Windows':
-            if expires == 'unknown':
-                status = 'unreachable'
-            else:
-                if expiringSoon is True:
-                    status = 'impacted'
+            except Exception:
+                pass
+            if includewindows is True or hostType != 'Windows':
+                if expires == 'unknown':
+                    status = 'unreachable'
                 else:
-                    status = 'not impacted'
-            print('%s:%s,%s,(%s) %s -> %s (%s)' % (name, port, version, hostType, osName, expires, status))
-            f.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (cluster['name'], name, status, clusterVersion, orgsenabled, version, port, hostType, osName, expires, errorMessage))
+                    if expiringSoon is True:
+                        status = 'impacted'
+                    else:
+                        status = 'not impacted'
+                print('%s:%s,%s,(%s) %s -> %s (%s)' % (name, port, version, hostType, osName, expires, status))
+                f.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (cluster['name'], name, status, clusterVersion, orgsenabled, version, port, hostType, osName, expires, errorMessage))
 f.close()
 print('\nOutput saved to %s\n' % outfile)
