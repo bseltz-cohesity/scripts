8098c772b2807204940cc61c86d6eb9376d3fe0b
bseltz-cohesity <bseltzer@cohesity.com>
2023-06-01 05:49:52 -0400

    2023-06-01

--- reports/powershell/clusterProtectionRuns/clusterProtectionRuns.ps1
+++ reports/powershell/clusterProtectionRuns/clusterProtectionRuns.ps1
@@ -48,8 +48,8 @@ foreach($v in $vip){
     }
 
     $cluster = api get cluster
-    $jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&includeTenants=true"
-    $sources = api get protectionSources/registrationInfo?includeApplicationsTreeInfo=false
+    $jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&includeTenants=true"
+    $sources = api get "protectionSources/registrationInfo?includeApplicationsTreeInfo=false"
     $policies = api get -v2 data-protect/policies
 
     foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
@@ -59,73 +59,81 @@ foreach($v in $vip){
         if(!$objectType -or $objectType -eq $environment){
             "{0} ({1})" -f $job.name, $environment
             $policyName = ($policies.policies | Where-Object id -eq $job.policyId).name
+            if(!$policyName){
+                $policyName = '-'
+            }
             while($True){
                 $runs = api get -v2 "data-protect/protection-groups/$($job.id)/runs?numRuns=$numRuns&endTimeUsecs=$endUsecs&includeTenants=true&includeObjectDetails=true"
                 foreach($run in $runs.runs){
+                    $localSources = @{}
                     if(! $run.PSObject.Properties['isLocalSnapshotsDeleted']){
                         if($run.PSObject.Properties['localBackupInfo']){
-                            $runType = $run.localBackupInfo.runType
+                            $backupInfo = $run.localBackupInfo
+                            $snapshotInfo = 'localSnapshotInfo'
                         }else{
-                            break
+                            $backupInfo = $run.originalBackupInfo
+                            $snapshotInfo = 'originalBackupInfo'
                         }
+                        $runType = $backupInfo.runType
                         if($includeLogs -or $runType -ne 'kLog'){
-                            $runStartTime = usecsToDate $run.localBackupInfo.startTimeUsecs
+                            $runStartTime = usecsToDate $backupInfo.startTimeUsecs
                             if($days -and $daysBack -gt $runStartTime){
                                 break
                             }
-                            $status = $run.localBackupInfo.status
-                            if($run.localBackupInfo.isSlaViolated){
+                            if($backupInfo.isSlaViolated){
                                 $slaStatus = 'Missed'
                             }else{
                                 $slaStatus = 'Met'
                             }
-                            $runEndTime = $null
-                            $durationMinutes = "{0:n0}" -f ($now - $runStartTime).totalMinutes
-                            if($run.localBackupInfo.PSObject.Properties['endTimeUsecs']){
-                                $runEndTime = usecsToDate $run.localBackupInfo.endTimeUsecs
-                                $durationMinutes = "{0:n0}" -f ($runEndTime - $runStartTime).totalMinutes
-                            }
-                            $logicalSizeBytes = toUnits $run.localBackupInfo.localSnapshotStats.logicalSizeBytes
-                            $bytesWritten = toUnits $run.localBackupInfo.localSnapshotStats.bytesWritten
-                            $bytesRead = toUnits $run.localBackupInfo.localSnapshotStats.bytesRead
                             "    {0} ({1})" -f $runStartTime, $runType
-                            if($days -and $daysBack -gt $runStartTime){
-                                break
+                            foreach($object in $run.objects){
+                                if($environment -in @('kOracle', 'kSQL') -and $object.object.objectType -eq 'kHost'){
+                                    $localSources["$($object.object.id)"] = $object.object.name
+                                }
                             }
-                            if(! $run.PSObject.Properties['isLocalSnapshotsDeleted']){
-                                foreach($object in $run.objects){
-                                    $objectName = $object.object.name
-                                    if($environment -notin @('Oracle', 'SQL') -or ($environment -in @('Oracle', 'SQL') -and $object.object.objectType -ne 'kHost')){
-                                        if($object.object.PSObject.Properties['sourceId']){
+                            foreach($object in $run.objects){
+                                $objectName = $object.object.name
+                                if($environment -notin @('kOracle', 'kSQL') -or ($environment -in @('kOracle', 'kSQL') -and $object.object.objectType -ne 'kHost')){
+                                    if($object.object.PSObject.Properties['sourceId']){
+                                        if($environment -in @('kOracle', 'kSQL')){
+                                            $registeredSourceName = $localSources["$($object.object.sourceId)"]
+                                        }else{
                                             $registeredSource = $sources.rootNodes | Where-Object {$_.rootNode.id -eq $object.object.sourceId}
                                             $registeredSourceName = $registeredSource.rootNode.name
-                                        }else{
+                                        }
+                                        if(!$registeredSourceName){
                                             $registeredSourceName = $objectName
                                         }
-                                        $objectStatus = $object.localSnapshotInfo.snapshotInfo.status
-                                        if($objectStatus -eq 'kSuccessful'){
-                                            $objectStatus = 'kSuccess'
-                                        }
-                                        $objectStartTime = usecsToDate $object.localSnapshotInfo.snapshotInfo.startTimeUsecs
-                                        $objectEndTime = $null
-                                        $objectDurationSeconds = "{0:n0}" -f ($now - $objectStartTime).totalSeconds
-                                        if($object.localSnapshotInfo.snapshotInfo.PSObject.Properties['endTimeUsecs']){
-                                            $objectEndTime = usecsToDate $object.localSnapshotInfo.snapshotInfo.endTimeUsecs
-                                            $objectDurationSeconds = "{0:n0}" -f ($objectEndTime - $objectStartTime).totalSeconds
-                                        }
-                                        $objectLogicalSizeBytes = toUnits $object.localSnapshotInfo.snapshotInfo.stats.logicalSizeBytes
-                                        $objectBytesWritten = toUnits $object.localSnapshotInfo.snapshotInfo.stats.bytesWritten
-                                        $objectBytesRead = toUnits $object.localSnapshotInfo.snapshotInfo.stats.bytesRead
-                                        "        {0}" -f $objectName
-                                        $objectStartTime, $objectEndTime, $objectDurationSeconds, $objectStatus, $slaStatus, 'Active', $objectName, $registeredSourceName, $job.name, $policyName, $environment, $runType, $cluster.name, $objectLogicalSizeBytes, $objectBytesRead, $objectBytesWritten, $tenant -join "`t" | Out-File -FilePath $outfileName -Append
+                                    }else{
+                                        $registeredSourceName = $objectName
                                     }
+                                    $objectStatus = $object.$snapshotInfo.snapshotInfo.status
+                                    if($objectStatus -eq 'kSuccessful'){
+                                        $objectStatus = 'kSuccess'
+                                    }
+                                    $objectStartTime = usecsToDate $object.$snapshotInfo.snapshotInfo.startTimeUsecs
+                                    $objectEndTime = $null
+                                    $objectDurationSeconds = "{0:n0}" -f ($now - $objectStartTime).totalSeconds
+                                    if($object.$snapshotInfo.snapshotInfo.PSObject.Properties['endTimeUsecs']){
+                                        $objectEndTime = usecsToDate $object.$snapshotInfo.snapshotInfo.endTimeUsecs
+                                        $objectDurationSeconds = "{0:n0}" -f ($objectEndTime - $objectStartTime).totalSeconds
+                                    }
+                                    $objectLogicalSizeBytes = toUnits $object.$snapshotInfo.snapshotInfo.stats.logicalSizeBytes
+                                    $objectBytesWritten = toUnits $object.$snapshotInfo.snapshotInfo.stats.bytesWritten
+                                    $objectBytesRead = toUnits $object.$snapshotInfo.snapshotInfo.stats.bytesRead
+                                    "        {0}" -f $objectName
+                                    $objectStartTime, $objectEndTime, $objectDurationSeconds, $objectStatus, $slaStatus, 'Active', $objectName, $registeredSourceName, $job.name, $policyName, $environment, $runType, $cluster.name, $objectLogicalSizeBytes, $objectBytesRead, $objectBytesWritten, $tenant -join "`t" | Out-File -FilePath $outfileName -Append
                                 }
                             }
                         }
                     }
                 }
                 if($runs.runs.Count -eq $numRuns){
-                    $endUsecs = $runs.runs[-1].localBackupInfo.endTimeUsecs - 1
+                    if($runs.runs[-1].PSObject.Properties['localBackupInfo']){
+                        $endUsecs = $runs.runs[-1].localBackupInfo.endTimeUsecs - 1
+                    }else{
+                        $endUsecs = $runs.runs[-1].originalBackupInfo.endTimeUsecs - 1
+                    }
                     if($endUsecs -lt 0 -or $endUsecs -lt $daysBackUsecs){
                         break
                     }
