db1d2c5a3b2a272a874443ba48bd6d58c9f70216
bseltz-cohesity <bseltzer@cohesity.com>
2023-06-14 04:49:51 -0400

    2023-06-14

--- powershell/cloneVM/README.md
+++ powershell/cloneVM/README.md
@@ -38,20 +38,34 @@ Place the files in a folder together and run the main script like so:
               -prefix clone-
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -vip: Cohesity cluster to connect to
-* -username: Cohesity username (e.g. admin)
-* -domain: (optional) Active Directory domain (defaults to 'local')
-* -vmName: Name of VM to recover
-* -prefix: (optional) add a prefix to the VM name during restore
-* -vCenterName: (optional)vCenter protection source to recover to
-* -datacenterName: (optional) name of vSphere data center to recover to
-* -computeResource: (optional) name of vSphere cluster or stand-alone host to recover to
-* -folderName: (optional) name of vSphere folder to recover to
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
+* -vmName: Name of VM to clone
+* -viewName: (optional) view to host cloned VM (default is 'cloneVMs')
+* -prefix: (optional) add a prefix to the VM name during cloning (default is 'clone-')
+* -vCenterName: vCenter to clone to
+* -datacenterName: name of vSphere data center to clone to
+* -computeResource: name of vSphere cluster or stand-alone host to clone to
+* -folderName: name of vSphere folder to clone to
 * -networkName: (optional) VM Network to attach the VM
-* -poweron: (optional) power on the VMs during restore (default is false)
+* -poweron: (optional) power on the VMs during cloning (default is false)
 * -detachNetwork: (optional) leave VM network disconnected (default is false)
+* -showVersions: (optional) show available versions and exit
+* -runId: (optional) specify run ID (from show versions) to clone from
+* -wait: (optional) wait for completion and report result
 
 ## Specifying a Compute Resource
 
@@ -69,7 +83,7 @@ or if your vShere environment contains stand-alone ESX hosts, then use the name 
 
 ## Specifying a Folder
 
-You can specify a folder to restore to using any of the following formats:
+You can specify a folder to clone to using any of the following formats:
 
 * /vCenter.mydomain.net/Datacenters/DataCenter1/vm/MyFolder/MySubFolder
 * vCenter.mydomain.net/Datacenters/DataCenter1/vm/MyFolder/MySubFolder
--- powershell/cloneVM/cloneVM.ps1
+++ powershell/cloneVM/cloneVM.ps1
@@ -1,9 +1,16 @@
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip,
-    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$vip = 'helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
     [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant = $null,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password = $null,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode = $null,
+    [Parameter()][string]$clusterName = $null,
     [Parameter(Mandatory = $True)][string]$vmName,
     [Parameter(Mandatory = $True)][string]$vCenterName,
     [Parameter(Mandatory = $True)][string]$dataCenterName,
@@ -13,16 +20,34 @@ param (
     [Parameter()][string]$viewName = 'cloneVMs',
     [Parameter()][string]$prefix = 'clone-',
     [Parameter()][switch]$powerOn,
-    [Parameter()][switch]$detachNetwork
+    [Parameter()][switch]$showVersions,
+    [Parameter()][int64]$runId,
+    [Parameter()][switch]$detachNetwork,
+    [Parameter()][switch]$wait
 )
 
-### source the cohesity-api helper code
+# source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-### authenticate
-apiauth -vip $vip -username $username -domain $domain
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -heliosAuthentication $mcm -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
 
-### search for latest snapshot of VM
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    if($clusterName){
+        $thisCluster = heliosCluster $clusterName
+    }else{
+        Write-Host "Please provide -clusterName when connecting through helios" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# search for latest snapshot of VM
 $searchResults = api get /searchvms?entityTypes=kVMware`&vmName=$vmName
 $searchResult = $searchResults.vms | Where-Object {$_.vmDocument.objectName -ieq $vmName }
 if(! $searchResult){
@@ -31,8 +56,20 @@ if(! $searchResult){
 }
 
 $latestVM = ($searchResult | sort-object -property @{Expression={$_.vmDocument.versions[0].snapshotTimestampUsecs}; Ascending = $False})[0]
+$versions = $latestVM.vmDocument.versions
 
-### select vCenter
+if($showVersions){
+    $versions | Select-Object -Property @{label='runId'; expression={$_.instanceId.jobInstanceId}}, @{label='runDate'; expression={usecsToDate $_.instanceId.jobStartTimeUsecs}}
+    exit
+}
+
+if($runId){
+    $version = $versions | Where-Object {$_.instanceId.jobInstanceId -eq $runId}
+}else{
+    $version = $versions[0]
+}
+
+# select vCenter
 $vCenterList = api get /entitiesOfType?environmentTypes=kVMware`&vmwareEntityTypes=kVCenter`&vmwareEntityTypes=kStandaloneHost
 $vCenter = $vCenterList | Where-Object { $_.displayName -ieq $vCenterName }
 $vCenterId = $vCenter.id
@@ -42,7 +79,7 @@ if(! $vCenter){
     exit
 }
 
-### select resource pool
+# select resource pool
 $vCenterSource = api get protectionSources?environments=kVMware`&includeVMFolders=true`&excludeTypes=kVirtualMachine | Where-Object {$_.protectionSource.name -eq $vCenterName}
 $dataCenterSource = $vCenterSource.nodes[0].nodes | Where-Object {$_.protectionSource.name -eq $dataCenterName}
 $hostSource = $dataCenterSource.nodes[0].nodes | Where-Object {$_.protectionSource.name -eq $computeResource}
@@ -54,7 +91,7 @@ if(! $resourcePoolId){
     exit
 }
 
-### select VM folder
+# select VM folder
 $vmfolderId = @{}
 
 function walkVMFolders($node, $parent=$null, $fullPath=''){
@@ -96,8 +133,8 @@ $cloneTask = @{
             'jobId' = $latestVM.vmDocument.objectId.jobId;
             'jobUid' = $latestVM.vmDocument.objectId.jobUid;
             'entity' = $latestVM.vmDocument.objectId.entity;
-            'jobInstanceId' = $latestVM.vmDocument.versions[0].instanceId.jobInstanceId;
-            'startTimeUsecs' = $latestVM.vmDocument.versions[0].instanceId.jobStartTimeUsecs
+            'jobInstanceId' = $version.instanceId.jobInstanceId;
+            'startTimeUsecs' = $version.instanceId.jobStartTimeUsecs
         }
     );
     'powerStateConfig' = @{
@@ -133,7 +170,7 @@ if($detachNetwork){
         'disableNetwork' = $False
     }
 }else{
-    ### select network
+    # select network
     if(! $networkName){
         Write-Host "network name required" -ForegroundColor Yellow
         exit
@@ -148,5 +185,30 @@ if($detachNetwork){
     $cloneTask.restoredObjectsNetworkConfig = @{'networkEntity' = $network}
 }
 
-$null = api post /clone $cloneTask
-"Cloning $vmName as $prefix$vmName..."
+$response = api post /clone $cloneTask
+
+if($response){
+    $taskId = $response.restoreTask.performRestoreTaskState.base.taskId
+    "Cloning $vmName as $prefix$vmName..."
+}else{
+    Write-Warning "No Response"
+    exit(1)
+}
+
+if($wait){
+    $status = 'started'
+    $finishedStates = @('kCanceled', 'kSuccess', 'kFailure', 'kWarning')
+    while($status -ne 'completed'){
+        $task = api get "/restoretasks/$($taskId)"
+        $publicStatus = $task.restoreTask.performRestoreTaskState.base.publicStatus
+        if($publicStatus -in $finishedStates){
+            $status = 'completed'
+        }else{
+            sleep 5
+        }
+    }
+    write-host "Clone task completed with status: $publicStatus"
+    if($publicStatus -eq 'kFailure'){
+        write-host "Error Message: $($task.restoreTask.performRestoreTaskState.base.error.errorMsg)"
+    }
+}
