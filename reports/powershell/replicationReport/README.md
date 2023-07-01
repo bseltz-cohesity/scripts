ab0bf5aebe6c79609716f6e7edb0cb9fc7df42fe
bseltz-cohesity <bseltzer@cohesity.com>
2023-07-01 06:18:25 -0400

    2023-07-01

---
+++ reports/powershell/replicationReport/README.md
@@ -0,0 +1,52 @@
+# Report Per Object Replication Stats Using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This PowerShell script generates a report of local snapshots and their expiration dates.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'replicationReport'
+$repoURL = 'https://raw.githubusercontent.com/bseltz-cohesity/scripts/master'
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/reports/powershell/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/powershell/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* replicationReport.ps1: the main python script
+* cohesity-api.ps1: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script, using a local account to authenticate:
+
+```powershell
+./replicationReport.ps1 -vip mycluster `
+                        -username admin `
+                        -domain local
+```
+
+## Authentication Parameters
+
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
+* -jobName: (optional) one or more job names to inspect (comma separated)
+* -jobList: (optional) text file of job names to inspect (one per line)
+* -unit: (optional) display sizes in GiB or MiB (default is MiB)
+* -daysBack: (optional) number of days back to inspect (default is 7)
+* -backDays: (optional) skip the latest X days (default is 0)
+* -numRuns: (optional) page size for retrieving runs (default is 100)
---
+++ reports/powershell/replicationReport/replicationReport.ps1
@@ -0,0 +1,138 @@
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][string]$clusterName,
+    [Parameter()][array]$jobName,
+    [Parameter()][string]$jobList,
+    [Parameter()][ValidateSet('MiB','GiB')][string]$unit = 'MiB',
+    [Parameter()][int]$daysBack = 7,
+    [Parameter()][Int64]$numRuns = 100,
+    [Parameter()][Int64]$backDays = 0
+)
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+$conversion = @{'Kib' = 1024; 'MiB' = 1024 * 1024; 'GiB' = 1024 * 1024 * 1024; 'TiB' = 1024 * 1024 * 1024 * 1024}
+function toUnits($val){
+    return "{0:n0}" -f ($val/($conversion[$unit]))
+}
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -heliosAuthentication $mcm -tenant $tenant -noPromptForPassword $noPrompt
+
+# select helios/mcm managed cluster
+if($USING_HELIOS -and !$region){
+    if($clusterName){
+        $thisCluster = heliosCluster $clusterName
+    }else{
+        write-host "Please provide -clusterName when connecting through helios" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated"
+    exit 1
+}
+
+# gather list from command line params and file
+function gatherList($Param=$null, $FilePath=$null, $Required=$True, $Name='items'){
+    $items = @()
+    if($Param){
+        $Param | ForEach-Object {$items += $_}
+    }
+    if($FilePath){
+        if(Test-Path -Path $FilePath -PathType Leaf){
+            Get-Content $FilePath | ForEach-Object {$items += [string]$_}
+        }else{
+            Write-Host "Text file $FilePath not found!" -ForegroundColor Yellow
+            exit
+        }
+    }
+    if($Required -eq $True -and $items.Count -eq 0){
+        Write-Host "No $Name specified" -ForegroundColor Yellow
+        exit
+    }
+    return ($items | Sort-Object -Unique)
+}
+
+$jobNames = @(gatherList -Param $jobName -FilePath $jobList -Name 'jobs' -Required $false)
+
+$jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&includeTenants=true"
+
+if($jobNames.Count -gt 0){
+    $notfoundJobs = $jobNames | Where-Object {$_ -notin $jobs.protectionGroups.name}
+    if($notfoundJobs){
+        Write-Host "Jobs not found $($notfoundJobs -join ', ')" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+$cluster = api get cluster
+$dateString = (get-date).ToString('yyyy-MM-dd')
+$objectFileName = "replicationReport-perObject-$($cluster.name)-$dateString.csv"
+"""Job Name"",""Job Type"",""Start Time"",""Source Name"",""Replication Delay Sec"",""Replication Duration Sec"",""Logical Replicated $unit"",""Physical Replicated $unit"",""Target Cluster""" | Out-File -FilePath $objectFileName
+
+$now = (Get-Date).AddDays(-$backDays)
+$daysBackUsecs = dateToUsecs $now.AddDays(-$daysBack)
+
+foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
+    if($jobNames.Count -eq 0 -or $job.name -in $jobNames){
+        $jobId = $job.id
+        $jobName = $job.name
+        "$jobName"
+        $jobType = $job.environment.Substring(1)
+        $endUsecs = dateToUsecs $now
+        while($True){
+            if($endUsecs -le $daysBackUsecs){
+                break
+            }
+            $runs = api get -v2 "data-protect/protection-groups/$jobId/runs?endTimeUsecs=$endUsecs&includeTenants=true&includeObjectDetails=True&numRuns=$numRuns"
+            if($runs.runs.Count -gt 0){
+                $endUsecs = $runs.runs[-1].localBackupInfo.startTimeUsecs - 1
+            }else{
+                break
+            }
+            foreach($run in $runs.runs){
+                if($run.PSObject.Properties['originalBackupInfo']){
+                    $runStartTimeUsecs = $run.originalBackupInfo.startTimeUsecs
+                }else{
+                    $runStartTimeUsecs = $run.localBackupInfo.startTimeUsecs
+                }
+                if($runStartTimeUsecs -lt $daysBackUsecs){
+                    break
+                }
+                # per object stats
+                foreach($server in ($run.objects | Sort-Object -Property {$_.object.name})){
+                    $sourceName = $server.object.name
+                    if(!($run.environment -eq 'kAD' -and $server.object.objectType -eq 'kDomainController')){
+                        if($server.PSObject.Properties['replicationInfo']){
+                            foreach($target in $server.replicationInfo.replicationTargetResults){
+                                $remoteCluster = $target.clusterName
+                                $replicaQueuedTime = $target.queuedTimeUsecs
+                                $replicaStartTime = $target.startTimeUsecs
+                                $replicaEndTime = $target.endTimeUsecs
+                                $replicaDelay = [math]::Round(($replicaStartTime - $replicaQueuedTime) / 1000000)
+                                $replicaDuration = [math]::Round(($replicaEndTime - $replicaStartTime) / 1000000)
+                                $logicalReplicated = toUnits $target.stats.logicalBytesTransferred
+                                $physicalReplicated = toUnits $target.stats.physicalBytesTransferred
+                                """$jobName"",""$jobType"",""$(usecsToDate $runStartTimeUsecs)"",""$sourceName"",""$replicaDelay"",""$replicaDuration"",""$logicalReplicated"",""$physicalReplicated"",""$remoteCluster""" | Out-File -FilePath $objectFileName -Append
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+"`nOutput saved to: {0}`n" -f $objectFileName
