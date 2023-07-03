48757861ccff16bdc6fd7e80687824350dd11504
bseltz-cohesity <bseltzer@cohesity.com>
2023-07-03 16:02:04 -0400

    2023-07-03

---
+++ reports/python/storagePerObjectReport/storagePerObjectReport.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+"""Storage Report for Python"""
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+import codecs
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-of', '--outfolder', type=str, default='.')
+parser.add_argument('-n', '--numruns', type=int, default=1000)
+parser.add_argument('-f', '--vmfullpct', type=float, default=0.75)
+parser.add_argument('-x', '--units', type=str, choices=['MiB', 'GiB', 'mib', 'gib'], default='GiB')  # units
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+folder = args.outfolder
+numruns = args.numruns
+units = args.units
+vmfullpct = args.vmfullpct
+multiplier = 1024 * 1024 * 1024
+if units.lower() == 'mib':
+    multiplier = 1024 * 1024
+
+if units == 'mib':
+    units = 'MiB'
+if units == 'gib':
+    units = 'GiB'
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+print('Collecting report data...')
+
+cluster = api('get', 'cluster?fetchStats=true')
+
+try:
+    clusterReduction = round(cluster['stats']['usagePerfStats']['dataInBytes'] / cluster['stats']['usagePerfStats']['dataInBytesAfterReduction'], 1)
+except Exception:
+    clusterReduction = 1
+
+jobs = api('get', 'protectionJobs')
+title = 'Storage Report for %s' % cluster['name']
+
+now = datetime.now()
+nowUsecs = dateToUsecs(now.strftime("%Y-%m-%d %H:%M:%S"))
+datestring = now.strftime("%Y-%m-%d")
+csvfileName = '%s/storagePerObjectReport-%s-%s.csv' % (folder, cluster['name'], datestring)
+csv = codecs.open(csvfileName, 'w', 'utf-8')
+csv.write('"Job Name","Environment","Source Name","Object Name","Data Written %s","Raw Consumed %s","Reduction"\n' % (units, units))
+
+jobs = api('get', 'data-protect/protection-groups?isDeleted=false&includeTenants=true', v=2)
+storageDomains = api('get', 'viewBoxes')
+
+sourceNames = {}
+
+for job in sorted(jobs['protectionGroups'], key=lambda job: job['name'].lower()):
+    if job['environment'] not in ['kView', 'kRemoteAdapter']:
+
+        # get resiliency factor
+        resiliencyFactor = 0
+        if 'storageDomainId' in job:
+            sd = [v for v in storageDomains if v['id'] == job['storageDomainId']]
+            if sd is not None and len(sd) > 0:
+                if 'erasureCodingInfo' in sd[0]['storagePolicy']:
+                    r = sd[0]['storagePolicy']['erasureCodingInfo']
+                    resiliencyFactor = float(r['numDataStripes'] + r['numCodedStripes']) / r['numDataStripes']
+                else:
+                    if sd[0]['storagePolicy']['numFailuresTolerated'] == 0:
+                        resiliencyFactor = 1
+                    else:
+                        resiliencyFactor = 2
+        objects = {}
+        print(job['name'])
+        v1JobId = job['id'].split(':')[2]
+
+        # get reduction factor
+        stats = api('get', 'stats/consumers?consumerType=kProtectionRuns&consumerIdList=%s' % v1JobId)
+        if 'statsList' in stats and stats['statsList'] is not None:
+            dataIn = stats['statsList'][0]['stats'].get('dataInBytes', 0)
+            dataInAfterDedup = stats['statsList'][0]['stats'].get('dataInBytesAfterDedup', 0)
+            dataWritten = stats['statsList'][0]['stats'].get('dataWrittenBytes', 0)
+            if dataInAfterDedup > 0 and dataWritten > 0:
+                dedup = round(float(dataIn) / dataInAfterDedup, 1)
+                compression = round(float(dataInAfterDedup) / dataWritten, 1)
+                reduction = round((float(dataIn) / dataInAfterDedup) * (float(dataInAfterDedup) / dataWritten), 1)
+            else:
+                reduction = 1
+        else:
+            reduction = clusterReduction
+        if reduction == 0:
+            reduction = 1
+        endUsecs = nowUsecs
+
+        # get protection runs in retention
+        while 1:
+            runs = api('get', 'data-protect/protection-groups/%s/runs?numRuns=%s&endTimeUsecs=%s&includeTenants=true&includeObjectDetails=true&excludeNonRestorableRuns=true' % (job['id'], numruns, endUsecs), v=2)
+            if len(runs['runs']) > 0:
+                if 'localBackupInfo' in runs['runs'][-1]:
+                    endUsecs = runs['runs'][-1]['localBackupInfo']['startTimeUsecs'] - 1
+                else:
+                    endUsecs = runs['runs'][-1]['originalBackupInfo']['startTimeUsecs'] - 1
+            else:
+                break
+            for run in runs['runs']:
+                if 'isLocalSnapshotsDeleted' not in run:
+
+                    # per object stats
+                    if 'objects' in run and run['objects'] is not None and len(run['objects']) > 0:
+                        for object in [o for o in run['objects'] if o['object']['environment'] != job['environment']]:
+                            sourceNames[object['object']['id']] = object['object']['name']
+                        for object in [o for o in run['objects']]:  # if o['object']['environment'] == job['environment']]:
+                            try:
+                                if 'localSnapshotInfo' in object:
+                                    snap = object['localSnapshotInfo']
+                                else:
+                                    snap = object['originalBackupInfo']
+                                if object['object']['name'] not in objects and not (job['environment'] == 'kAD' and object['object']['environment'] == 'kAD') and not (job['environment'] in ['kSQL', 'kOracle'] and object['object']['objectType'] == 'kHost'):
+                                    objects[object['object']['name']] = {}
+                                    if 'sourceId' in object['object']:
+                                        objects[object['object']['name']]['sourceId'] = object['object']['sourceId']
+                                    objects[object['object']['name']]['logical'] = snap['snapshotInfo']['stats']['logicalSizeBytes']
+                                    if job['environment'] == 'kVMware':
+                                        objects[object['object']['name']]['logical'] = int(float(vmfullpct) * objects[object['object']['name']]['logical'])
+                                    objects[object['object']['name']]['bytesWritten'] = 0
+                                # print('    %s  %s' % (object['object']['name'], snap['snapshotInfo']['stats']['logicalSizeBytes']))
+                                if snap['snapshotInfo']['stats']['logicalSizeBytes'] > objects[object['object']['name']]['logical']:
+                                    objects[object['object']['name']]['logical'] = snap['snapshotInfo']['stats']['logicalSizeBytes']
+                                if 'bytesWritten' in snap['snapshotInfo']['stats']:
+                                    objects[object['object']['name']]['bytesWritten'] += snap['snapshotInfo']['stats']['bytesWritten']
+                                else:
+                                    objects[object['object']['name']]['bytesWritten'] += snap['snapshotInfo']['stats']['bytesRead'] / reduction
+                            except Exception:
+                                pass
+        # process output
+        for object in sorted(objects.keys()):
+            if 'logical' in objects[object] and 'bytesWritten' in objects[object]:
+                # print('    %s  %s' % (object, objects[object]['logical']))
+                reducedData = round(((objects[object]['logical'] / reduction) + objects[object]['bytesWritten']) / multiplier, 1)
+                reducedDataWithResiliency = reducedData * resiliencyFactor
+                sourceName = ''
+                if 'sourceId' in objects[object]:
+                    if objects[object]['sourceId'] in sourceNames:
+                        sourceName = sourceNames[objects[object]['sourceId']]
+                    else:
+                        source = api('get', 'protectionSources?id=%s' % objects[object]['sourceId'])
+                        if source and len(source) > 0:
+                            sourceName = source[0]['protectionSource']['name']
+                            sourceNames[objects[object]['sourceId']] = sourceName
+                else:
+                    sourceName = object
+                csv.write('"%s","%s","%s","%s","%s","%s","%s"\n' % (job['name'], job['environment'], sourceName, object, reducedData, reducedDataWithResiliency, reduction))
+
+# views
+views = api('get', 'file-services/views?maxCount=2000&includeTenants=true&includeStats=true&includeProtectionGroups=true&includeInactive=true', v=2)
+if 'views' in views and views['views'] is not None and len(views['views']) > 0:
+    for view in views['views']:
+        try:
+            jobName = view['viewProtection']['protectionGroups'][0]['groupName']
+        except Exception:
+            jobName = ''
+        sourceName = view['storageDomainName']
+        viewName = view['name']
+        print(viewName)
+        dataIn = view['stats']['dataUsageStats'].get('dataInBytes', 0)
+        dataInAfterDedup = view['stats']['dataUsageStats'].get('dataInBytesAfterDedup', 0)
+        dataWritten = view['stats']['dataUsageStats'].get('dataWrittenBytes', 0)
+        consumption = view['stats']['dataUsageStats'].get('localTotalPhysicalUsageBytes', 0)
+        if dataInAfterDedup > 0 and dataWritten > 0:
+            dedup = round(float(dataIn) / dataInAfterDedup, 1)
+            compression = round(float(dataInAfterDedup) / dataWritten, 1)
+            reduction = round((float(dataIn) / dataInAfterDedup) * (float(dataInAfterDedup) / dataWritten), 1)
+        else:
+            reduction = 1
+        csv.write('"%s","%s","%s","%s","%s","%s","%s"\n' % (jobName, 'kView', sourceName, viewName, round(dataWritten / multiplier, 1), round(consumption / multiplier, 1), reduction))
+
+csv.close()
+print('\nOutput saved to %s\n' % csvfileName)
