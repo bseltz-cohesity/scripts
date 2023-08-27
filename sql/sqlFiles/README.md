5f945b51bfb6bf1ac02688ac76e1edf422f9a4f5
bseltz-cohesity <bseltzer@cohesity.com>
2023-08-27 05:41:16 -0400

    2023-08-27

--- sql/sqlFiles/README.md
+++ sql/sqlFiles/README.md
@@ -32,10 +32,20 @@ Place both files in a folder together and run the main script like so:
                -dbName MSSQLSERVER/mydb
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -vip: Cohesity Cluster to connect to
-* -username: Cohesity username
-* -domain: Active Directory domain of user (defaults to local)
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
 * -serverName: Server name to query
 * -dbName: Database to query (e.g. mydb or myInstance/mydb)
--- sql/sqlFiles/sqlFiles.ps1
+++ sql/sqlFiles/sqlFiles.ps1
@@ -1,10 +1,16 @@
 # process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter()][string]$vip = 'helios.cohesity.com',   # the cluster to connect to (DNS name or IP)
-    [Parameter()][string]$username = 'helios',           # username (local or AD)
-    [Parameter()][string]$domain = 'local',              # local or AD domain
-    [Parameter()][string]$clusterName = $null,           # helios cluster to access 
+    [Parameter()][string]$vip = 'helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant = $null,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password = $null,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode = $null,
+    [Parameter()][string]$clusterName = $null,
     [Parameter(Mandatory = $True)][string]$serverName,   # protection source where the DB was backed up
     [Parameter(Mandatory = $True)][string]$dbName        # name of the source DB we want to restore
 )
@@ -13,7 +19,22 @@ param (
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
 # authenticate
-apiauth -vip $vip -username $username -domain $domain
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -heliosAuthentication $mcm -tenant $tenant -noPromptForPassword $noPrompt
+
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    if($clusterName){
+        $thisCluster = heliosCluster $clusterName
+    }else{
+        Write-Host "Please provide -clusterName when connecting through helios" -ForegroundColor Yellow
+        exit 1
+    }
+}
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
 
 # handle source instance name e.g. instance/dbname
 if($dbName.Contains('/')){
