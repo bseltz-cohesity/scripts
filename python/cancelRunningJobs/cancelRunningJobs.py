3c4f9a3b0e63f98d7e1a308a44d284f1e4d1a8d3
bseltz-cohesity <bseltzer@cohesity.com>
2023-10-09 06:02:06 -0400

    2023-10-09

---
+++ python/cancelRunningJobs/cancelRunningJobs.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+"""cancel running jobs"""
+
+# import pyhesity wrapper module
+from pyhesity import *
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-j', '--jobname', action='append', type=str)
+parser.add_argument('-l', '--joblist', type=str)
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+jobnames = args.jobname
+joblist = args.joblist
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, tenantId=tenant)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        print('-clustername is required when connecting to Helios or MCM')
+        exit()
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+
+# gather server list
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+jobnames = gatherList(jobnames, joblist, name='jobs', required=False)
+
+jobs = api('get', 'protectionJobs?isDeleted=false&isActive=true')
+
+# catch invalid job names
+notfoundjobs = [n for n in jobnames if n.lower() not in [j['name'].lower() for j in jobs]]
+if len(notfoundjobs) > 0:
+    print('Jobs not found: %s' % ', '.join(notfoundjobs))
+
+if jobs is None:
+    print('no jobs found')
+    exit()
+
+runningStates = ['kAccepted', 'kRunning']
+canceled = 0
+print('')
+
+for job in sorted(jobs, key=lambda job: job['name'].lower()):
+    if len(jobnames) == 0 or job['name'].lower() in [j.lower() for j in jobnames]:
+
+        print('%s' % job['name'])
+
+        runs = api('get', 'protectionRuns?jobId=%s&numRuns=20&excludeTasks=true' % job['id'])
+        if runs is not None and len(runs) > 0:
+            for run in runs:
+                if run['backupRun']['status'] in runningStates:
+                    print('    %s (canceling...)' % usecsToDate(run['backupRun']['stats']['startTimeUsecs']))
+                    result = api('post', 'protectionRuns/cancel/%s' % job['id'], {"jobRunId": run['backupRun']['jobRunId']})
+                    canceled = 1
+
+if canceled == 0:
+    print('\nnothing to cancel\n')
+else:
+    print('')
