f87e4467957c36a8eff99b0cd5092bc7399b0f50
bseltz-cohesity <bseltzer@cohesity.com>
2023-10-11 05:16:06 -0400

    2023-10-11

---
+++ reports/powershell/archiveVersionReport/archiveVersionReport.ps1
@@ -0,0 +1,81 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$vip = 'helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant = $null,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password = $null,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode = $null,
+    [Parameter()][string]$clusterName = $null
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authentication =============================================
+# demand clusterName for Helios/MCM
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios/MCM" -ForegroundColor Yellow
+    exit 1
+}
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -heliosAuthentication $mcm -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
+
+# outfile
+$cluster = api get cluster
+$dateString = (get-date).ToString('yyyy-MM-dd')
+$outfileName = "archiveVersions-$($cluster.name)-$dateString.csv"
+
+# headings
+"Job Name,Policy,Targets,Archive Version" | Out-File -FilePath $outfileName -Encoding utf8
+
+$jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true"
+$policies = api get -v2 "data-protect/policies"
+$vaults = api get vaults
+
+foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
+    Write-Host $job.name
+    $policy = $policies.policies | Where-Object id -eq $job.policyId
+    $caVersion = ''
+    $targets = @()
+    foreach($archiveTarget in $policy.remoteTargetPolicy.archivalTargets){
+        $vault = $vaults | Where-Object id -eq $archiveTarget.targetId
+        $targets = @($targets + $vault.name | Sort-Object -Unique)
+        if($vault.isForeverIncrementalArchiveEnabled -eq $True){
+            if($caVersion -in @('', 'v2')){
+                $caVersion = 'v2'
+            }else{
+                $caVersion = 'mixed'
+            }
+        }else{
+            if($caVersion -in @('', 'v1')){
+                $caVersion = 'v1'
+            }else{
+                $caVersion = 'mixed'
+            }
+        }
+    }
+    "{0},{1},{2},{3}" -f $job.name, $policy.name, $($targets -join ' '), $caVersion | Out-File -FilePath $outfileName -Append
+}
+
+"`nOutput saved to $outfilename`n"
