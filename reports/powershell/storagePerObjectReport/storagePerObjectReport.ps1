c457fb4e19178d3cf9991d796cf7f578a35162af
bseltz-cohesity <bseltzer@cohesity.com>
2024-01-08 17:57:13 -0500

    2024-01-08

--- reports/powershell/storagePerObjectReport/storagePerObjectReport.ps1
+++ reports/powershell/storagePerObjectReport/storagePerObjectReport.ps1
@@ -18,6 +18,9 @@ param (
     [Parameter()][switch]$dbg
 )
 
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
 $conversion = @{'KiB' = 1024; 'MiB' = 1024 * 1024; 'GiB' = 1024 * 1024 * 1024; 'TiB' = 1024 * 1024 * 1024 * 1024; 'KB' = 1000; 'MB' = 1000 * 1000; 'GB' = 1000 * 1000 * 1000; 'TB' = 1000 * 1000 * 1000 * 1000}
 
 function toUnits($val){
