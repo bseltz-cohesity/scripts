2ee81e5a6f46dba43e85402bc6e2a983088ba7bd
bseltz-cohesity <bseltzer@cohesity.com>
2024-03-14 03:46:48 -0400

    2024-03-14

--- sql/powershell/sqlFileList/README.md
+++ sql/powershell/sqlFileList/README.md
@@ -32,9 +32,19 @@ Place all files in a folder together. And run the script like so:
 # end example
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -vip: name or IP of Cohesity cluster
-* -username: name of user to connect to Cohesity
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
 * -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
 * -protectedOnly: (optional) only output protected databases
--- sql/powershell/sqlFileList/sqlFileList.ps1
+++ sql/powershell/sqlFileList/sqlFileList.ps1
@@ -1,27 +1,61 @@
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
-    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
-    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][string]$clusterName,
     [Parameter()][switch]$protectedOnly
 )
 
 ### source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-### authenticate
-apiauth -vip $vip -username $username -domain $domain
+# authentication =============================================
+# demand clusterName for Helios/MCM
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios/MCM" -ForegroundColor Yellow
+    exit 1
+}
 
-"""Server Name"",""DB Name"",""Protected"",""File Path"",""File Extension"",""Size Bytes"",""File Type""" | Tee-Object -Path sqlFiles.csv
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -heliosAuthentication $mcm -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
+
+Write-Host "`nGathering SQL file info...`n"
+
+$cluster = api get cluster
+$outfileName = "sqlFiles-$($cluster.name).csv"
+
+"""Server Name"",""DB Name"",""Protected"",""File Path"",""File Extension"",""Size Bytes"",""File Type""" | Out-File -FilePath $outfileName -Encoding utf8
 
 $sqlHosts = api get "protectionSources/registrationInfo?includeApplicationsTreeInfo=false&environments=kSQL"
 
 foreach($sqlHost in $sqlHosts.rootNodes | Sort-Object -Property {$_.rootNode.name}){
     $source = api get protectionSources?id=$($sqlHost.rootNode.id)
     $serverName = $source.protectionSource.name
+    Write-Host "$serverName"
     foreach($instance in $source.applicationNodes | Sort-Object -Property {$_.protectionSource.name}){
-        # $instanceName = $instance.protectionSource.name
         foreach($db in $instance.nodes | Sort-Object -Property {$_.protectionSource.name}){
             $dbName = $db.protectionSource.name
             $protected = $False
@@ -35,9 +69,12 @@ foreach($sqlHost in $sqlHosts.rootNodes | Sort-Object -Property {$_.rootNode.nam
                     if($dbFileName.Contains('.')){
                         $ext = $dbFileName.split('.')[-1]
                     }
-                    """{0}"",""{1}"",""{2}"",""{3}"",""{4}"",""{5}"",""{6}""" -f $serverName, $dbName, $protected, $dbFile.fullPath, $ext, $dbFile.sizeBytes, $dbFile.fileType | Tee-Object -Path sqlFiles.csv -Append
+                    Write-Host "    $dbName"
+                    """{0}"",""{1}"",""{2}"",""{3}"",""{4}"",""{5}"",""{6}""" -f $serverName, $dbName, $protected, $dbFile.fullPath, $ext, $dbFile.sizeBytes, $dbFile.fileType | Out-File -FilePath $outfileName -Append
                 }
             }
         }
     }
 }
+
+Write-Host "`nOutput saved to $outfilename`n"
