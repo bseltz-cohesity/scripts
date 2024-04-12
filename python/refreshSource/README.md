42d6c5212fd74569a92b54698bc6ac4610d0900e
bseltz-cohesity <bseltzer@cohesity.com>
2024-04-12 10:45:06 -0400

    2024-04-12

--- python/refreshSource/README.md
+++ python/refreshSource/README.md
@@ -30,9 +30,20 @@ Place both files in a folder together and run the main script like so:
                     -n myserver1.mydomain.net
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -v, --vip: DNS or IP of the Cohesity cluster to connect to
-* -u, --username: username to authenticate to Cohesity cluster
-* -d, --domain: (optional) domain of username, defaults to local
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
 * -n, --sourcename: name of protection source to refresh (repeat for multiple sources)
+* -l, --sourcelist: text file of protection sources to refresh (one per line)
--- python/refreshSource/refreshSource.py
+++ python/refreshSource/refreshSource.py
@@ -8,24 +8,74 @@ from time import sleep
 ### command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)            # cluster to connect to
-parser.add_argument('-u', '--username', type=str, default='helios')    # username
-parser.add_argument('-d', '--domain', type=str, default='local')       # domain - defaults to local
-parser.add_argument('-i', '--useApiKey', action='store_true')          # use API key authentication
-parser.add_argument('-pwd', '--password', type=str, default=None)      # optional password
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-n', '--sourcename', type=str, action='append')   # optional name of vcenter
-
+parser.add_argument('-l', '--sourcelist', type=str)
 args = parser.parse_args()
 
 vip = args.vip
 username = args.username
 domain = args.domain
-password = args.password
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
 useApiKey = args.useApiKey
-sourceNames = args.sourcename
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+sourcenames = args.sourcename
+sourcelist = args.sourcelist
+
+
+# gather server list
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+sourceNames = gatherList(sourcenames, sourcelist, name='sources', required=True)
+
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
 
 # authenticate
-apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
 
 if sourceNames is None:
     print('No sources specified')
@@ -58,14 +108,12 @@ def waitForRefresh(sourcename):
             for app in rootNode[0]['registrationInfo']['registeredAppsInfo']:
                 if app['authenticationStatus'] == 'kFinished':
                     appsFinished = True
-                    # return rootNode[0]['rootNode']['id']
                 else:
                     appsFinished = False
         else:
             appsFinished = True
         if rootFinished is True and appsFinished is True:
             authStatus = 'Finished'
-    return rootNode[0]['rootNode']['id']
 
 
 for sourcename in sourceNames:
