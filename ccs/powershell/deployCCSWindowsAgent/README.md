a693055cb4397c7fd79f446ae8cb8e368843178f
bseltz-cohesity <bseltzer@cohesity.com>
2024-05-16 10:53:09 -0400

    2024-05-16

---
+++ ccs/powershell/deployCCSWindowsAgent/README.md
@@ -0,0 +1,71 @@
+# Remotely Deploy Cohesity CCS Windows Agent using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script installs and configures the Cohesity CCS Windows agent on remote hosts. This is useful when deploying the agent on new hosts. After the agent is installed, Cohesity can push out aget upgrades from the UI.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download commands
+(Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/cohesity/community-automation-samples/main/ccs/powershell/deployCCSWindowsAgent/deployCCSWindowsAgent.ps1).content | Out-File deployCCSWindowsAgent.ps1; (Get-Content deployCCSWindowsAgent.ps1) | Set-Content deployCCSWindowsAgent.ps1
+(Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/deployCCSWindowsAgent/UserRights.psm1).content | Out-File UserRights.psm1; (Get-Content UserRights.psm1) | Set-Content UserRights.psm1
+(Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/cohesity-api/cohesity-api.ps1).content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End download commands
+```
+
+## Components
+
+* [deployCCSWindowsAgent.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/deployCCSWindowsAgent/deployCCSWindowsAgent.ps1): the main powershell script
+* [cohesity-api.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/cohesity-api/cohesity-api.ps1): the Cohesity REST API helper module
+* [UserRights.psm1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/deployCCSWindowsAgent/UserRights.psm1): Module for setting logon as a service right (see attribution below)
+* servers.txt: text file containing server names to deploy to
+
+Place all files in a folder together, then we can run the script.
+
+Note: The script must be run by a user who has rights to perform actions on the remote servers
+
+The script can perform several steps, which can be run individually or together. The steps are:
+
+* -installAgent: This step copies the agent installer to the remote system, runs the installer and opens the necessary port on the firewall.
+
+* -register: This step registers the host as a Cohesity protection source (physical server). This step requires the agent to be installed and firewall port open first.
+
+* -registerAD: This step registers the host as an Active Directory domain controller. This step requires the server to be registered as a protection source first.
+
+* -registerSQL: This step registers the protection source as a SQL server. This step requires the server to be registered as a protection source first.
+
+* -serviceAccount: This step sets the Cohesity agent to logon with a specific account (the script will prompt for the password). The script will also grant the account the SEServiceLogon right.
+
+It would be common to use all steps together as shown below if you are deploying to stand-alone SQL servers or SQL AAG nodes. For SQL Failover Clusters, just use -installAgent and -sqlAccount, then register the SQL cluster manually in the UI (I may add code in the future to handle clusters).
+
+```powershell
+# example
+.\deployCCSWindowsAgent.ps1 -username myuser@mydomain.net -region us-west-2 -serverList .\sqlServers.txt -installAgent -saasConnector 'my saas connector' -register -registerSQL -serviceAccount mydomain.net\myuser
+# end example
+```
+
+## Authentication Parameters
+
+* -username: name of user to connect to Cohesity
+* -region: your AD domain (defaults to local)
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+
+## Other Parameters
+
+* -serverList: (optional) path to file containing servernames to deploy to
+* -server: (optional) name of one server to deploy to
+* -storePassword: (optional) store service account password (encrypted) for later script runs
+* -installAgent: (optional) install the Cohesity agent
+* -register: (optional) register server as a Cohesity physical protection source
+* -registerSQL: (optional) register server as a MSSQL protection source (requires -register or previous registration)
+* -sqlCluster: (optional) register server as a MSSQL Failover Cluster node (requires -register or previous registration)
+* -serviceAccount: (optional) set Cohesity agent to run using a service account
+* -filePath : (optional) use existing agent file (will download latest by default)
+
+## Attributions
+
+* [UserRights.psm1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/deployCCSWindowsAgent/UserRights.psm1): was downloaded from: <https://gallery.technet.microsoft.com/scriptcenter/Grant-Revoke-Query-user-26e259b0> thanks to Tony MCP: <https://social.technet.microsoft.com/profile/tony%20mcp/> just what I was looking for!
---
+++ ccs/powershell/deployCCSWindowsAgent/deployCCSWindowsAgent.ps1
@@ -0,0 +1,179 @@
+### usage: ./deployWindowsAgent.ps1 -vip bseltzve01 -username admin -serverList ./servers.txt [ -installAgent ] [ -register ] [ -registerSQL ] [ -serviceAccount mydomain.net\myuser ]
+### provide a list of servers in a text file
+### specify any of -installAgent -register -registerSQL -serviceAccount -storePassword
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$True)][string]$username,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter(Mandatory=$True)][string]$region,
+    [Parameter()][string]$serverList, #Servers to add as physical source
+    [Parameter()][string]$serverName,
+    [Parameter()][switch]$storePassword,
+    [Parameter()][switch]$installAgent,
+    [Parameter()][switch]$register,
+    [Parameter()][switch]$registerSQL,
+    [Parameter()][switch]$sqlCluster,
+    [Parameter()][string]$serviceAccount = $null,
+    [Parameter()][string]$filePath,
+    [Parameter()][string]$saasConnector = $null
+)
+
+if($register -and !$saasConnector){
+    Write-Host "-saasConnector required to register" -ForegroundColor Yellow
+    exit
+}
+
+if($serverList){
+    $servers = get-content $serverList
+    }elseif($serverName) {
+        $servers = @($serverName)
+    }else{
+        Write-Host "No Servers Specified" -ForegroundColor Yellow
+        exit
+    }
+    
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath userRights.psm1)
+
+### function to set service account
+Function Set-ServiceAcctCreds([string]$strCompName,[string]$strServiceName,[string]$newAcct,[string]$newPass){
+    $filter = 'Name=' + "'" + $strServiceName + "'" + ''
+    $service = Get-WMIObject -ComputerName $strCompName -Authentication PacketPrivacy -namespace "root\cimv2" -class Win32_Service -Filter $filter
+    $service.Change($null,$null,$null,$null,$null,$null,$newAcct,$newPass)
+    $service.StopService()
+    while ($service.Started){
+      Start-Sleep 2
+      $service = Get-WMIObject -ComputerName $strCompName -Authentication PacketPrivacy -namespace "root\cimv2" -class Win32_Service -Filter $filter
+    }
+    $service.StartService()
+}
+
+# authentication =============================================
+apiauth -username $username -passwd $password -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+# end authentication =========================================
+
+$sessionUser = api get sessionUser
+$tenantId = $sessionUser.profiles[0].tenantId
+
+### get sqlAccount Password
+if($serviceAccount){
+    $sqlPassword = Get-CohesityAPIPassword -vip windows -username $serviceAccount
+    if(!$sqlPassword){
+        $securePassword = Read-Host -AsSecureString -Prompt "Enter password for $serviceAccount"
+        $sqlPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR( $securePassword ))
+        if($storePassword){
+            Set-CohesityAPIPassword -vip windows -username $serviceAccount -pwd $sqlPassword
+        }
+    }
+}
+
+if($sqlCluster){
+    $entityType = 2
+}else{
+    $entityType = 1
+}
+
+$remoteFilePath = ''
+### download agent installer to local host
+if ($installAgent) {
+    if(!$filePath){
+        $downloadsFolder = join-path -path $([Environment]::GetFolderPath("UserProfile")) -ChildPath downloads
+        $images = api get -mcmv2 data-protect/agents/images?platform=Windows
+        $downloadURL = $images.agents[0].downloadURL
+        $fileName = "cohesity-agent.exe"
+        $filePath = join-path -path $downloadsFolder -ChildPath $fileName
+        Write-Host "Downloading Windows agent to $filepath..."
+        fileDownload -uri $downloadURL -fileName $filepath
+    }else{
+        $fileName = Split-Path $filePath -leaf
+    }
+    
+    $remoteFilePath = Join-Path -Path "C:\Windows\Temp" -ChildPath $fileName
+}
+
+foreach ($server in $servers){
+    $server = $server.ToString()
+    "managing Cohesity Agent on $server"
+
+    ### install Cohesity Agent
+    if ($installAgent) {
+
+        ### copy agent installer to server
+        "`tcopying agent installer..."
+        Copy-Item $filePath \\$server\c$\Windows\Temp
+
+        ### install agent and open firewall port
+        "`tinstalling Cohesity agent..."
+        $null = Invoke-Command -Computername $server -ArgumentList $remoteFilePath -ScriptBlock {
+            param($remoteFilePath)
+            if (! $(Get-Service | Where-Object { $_.Name -eq 'CohesityAgent' })) {
+                ([WMICLASS]"\\localhost\ROOT\CIMV2:win32_process").Create("$remoteFilePath /verySilent /norestart")
+                New-NetFirewallRule -DisplayName 'Cohesity Agent' -Profile 'Domain' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 50051
+            }
+        }
+    }
+
+    ### register server as physical source
+    if($register){
+        "`tRegistering as Cohesity protection source..."
+        $sources = api get -mcmv2 "data-protect/sources?regionIds=$region&environments=kPhysical"
+        $saasConnectors = api get -mcmv2 "rigelmgmt/rigel-groups?regionIds=$region&tenantId=$tenantId&fetchConnectorGroups=true"
+        $sc = $saasConnectors.rigelGroups | Where-Object {$_.groupName -eq $saasConnector}
+        if(! $sc){
+            Write-Host "SaaS connector $saasConnector not found" -ForegroundColor Yellow
+            exit
+        }
+        $source = $sources.sources | Where-Object { $_.name -ieq $server }
+        if(!$source){
+            $newSource = @{                                                                                                    "environment" = "kPhysical";
+                "physicalParams" = @{
+                    "endpoint" = $server;
+                    "physicalType" = "kHost";
+                    "hostType" = "kWindows"
+                };
+                "connectionId" = $sc.groupId
+            }
+            $result = api post -mcmv2 data-protect/sources/registrations $newSource -region $sc.regionId
+            Start-Sleep 10
+        }    
+    }
+
+    ### set service account
+    if($serviceAccount){
+        "`tSetting CohesityAgent Service Logon Account..."
+        Grant-UserRight -Computer $server -User $serviceAccount -Right SeServiceLogonRight
+        $null = Set-ServiceAcctCreds $server 'CohesityAgent' $serviceAccount $sqlPassword
+    }
+
+    ### register server as SQL
+    if ($registerSQL) {
+        if (! $($sources.rootNodes | Where-Object { $_.rootNode.name -eq $server -and $_.applications.environment -eq 'kSQL' })) {
+            $sources = api get -mcmv2 "data-protect/sources?environments=kPhysical&regionId=$region"
+            $source = $sources.sources | Where-Object { $_.name -ieq $server }
+            if ($source) {
+                Write-Host "`tRegistering as SQL protection source..."
+                $regSQL = @{
+                    "protectionSourceId" = $source.sourceInfoList[0].sourceId;
+                    "applications" = @(
+                        "kSQL"
+                    );
+                    "hasPersistentAgent" = $true
+                }
+                $result = api post protectionSources/applicationServers $regSQL -region $source.sourceInfoList[0].regionId
+            }
+            else {
+                Write-Host "$server is not yet registered as a protection source" -foregroundcolor Yellow
+            }
+        }
+    }
+}
