ecc63fd9f72478522eab70b057cdc505b78e3690
bseltz-cohesity <bseltzer@cohesity.com>
2024-05-22 11:12:09 -0400

    2024-05-22

--- python/unprotectPhysicalServer/README.md
+++ python/unprotectPhysicalServer/README.md
@@ -32,12 +32,21 @@ Place both files in a folder together and run the main script like so:
                              -l serverlist.txt
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -v, --vip: DNS or IP of the Cohesity cluster to connect to
-* -u, --username: username to authenticate to Cohesity cluster
-* -d, --domain: (optional) domain of username, defaults to local
-* -k, --useApiKey: (optional) use API key for authentication
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
+* -i, --useApiKey: (optional) use API key for authentication
 * -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
 * -n, --servername: (optional) name of server to add to the job (use multiple times for multiple)
 * -l, --serverlist: (optional) list of server names in a text file (one per line)
--- python/unprotectPhysicalServer/unprotectPhysicalServer.py
+++ python/unprotectPhysicalServer/unprotectPhysicalServer.py
@@ -5,15 +5,22 @@
 
 # import pyhesity wrapper module
 from pyhesity import *
+from sys import exit
 
 # command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
 parser.add_argument('-u', '--username', type=str, default='helios')
 parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
 parser.add_argument('-i', '--useApiKey', action='store_true')
 parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-n', '--servername', action='append', type=str)
 parser.add_argument('-l', '--serverlist', type=str)
 
@@ -22,8 +29,14 @@ args = parser.parse_args()
 vip = args.vip
 username = args.username
 domain = args.domain
-password = args.password
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
 useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 servernames = args.servername
 serverlist = args.serverlist
 
@@ -40,8 +53,26 @@ if len(servernames) == 0:
     print('no servers specified')
     exit()
 
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
+
 # authenticate
-apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
 
 serverfound = {}
 for server in servernames:
