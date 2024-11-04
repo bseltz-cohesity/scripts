eb2c25ebc563022971d3a7eb6596174dab50b3ee
bseltz-cohesity <bseltzer@cohesity.com>
2024-11-04 10:03:39 -0500

    2024-11-04

--- python/pauseResumeJobs/pauseResumeJobs.py
+++ python/pauseResumeJobs/pauseResumeJobs.py
@@ -18,6 +18,7 @@ parser.add_argument('-i', '--useApiKey', action='store_true')
 parser.add_argument('-pwd', '--password', type=str, default=None)
 parser.add_argument('-np', '--noprompt', action='store_true')
 parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-j', '--jobName', action='append', type=str)
 parser.add_argument('-l', '--jobList', type=str)
 parser.add_argument('-r', '--resume', action='store_true')
@@ -35,17 +36,32 @@ useApiKey = args.useApiKey
 password = args.password
 noprompt = args.noprompt
 mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 jobNames = args.jobName
 jobList = args.jobList
 pause = args.pause
 resume = args.resume
 
-### authenticate
-apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey)
-
-if apiconnected() is False:
-    print('\nFailed to connect to Cohesity cluster')
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
     exit(1)
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
 
 
 # gather server list
