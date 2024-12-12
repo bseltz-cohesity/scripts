ecd19ebe08618d33be501877e876b4218c3da6bb
bseltz-cohesity <bseltzer@cohesity.com>
2024-12-12 13:38:47 -0500

    2024-12-12

---
+++ python/replicateOldSnapshotsV2/README.md
@@ -0,0 +1,75 @@
+# Replicate Old Snapshots using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script replicates old existing snapshots.
+
+Note: there's a new experimental V2 version of the script that uses the V2 API. Feedback is welcome.
+
+## Components
+
+* [replicateOldSnapshotsV2.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/replicateOldSnapshotsV2/replicateOldSnapshotsV2.py): the main python script
+* [pyhesity.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity/pyhesity.py): the Cohesity REST API helper module
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/replicateOldSnapshotsV2/replicateOldSnapshotsV2.py
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity.py
+chmod +x replicateOldSnapshotsV2.py
+
+```
+
+Place both files in a folder together and run the main script like so:
+
+This command will replicate all unexipired snapshots (from all jobs) that have run in the past 31 days (that haven't already been replicated already) and keep them in the replicate for 90 days. First run the command without the -c (--commit) switch, to see what it would do:
+
+```bash
+./replicateOldSnapshotsV2.py -v mycluster \
+                             -u myuser \
+                             -d mydomain.net \ 
+                             -r othercluster \
+                             -j 'some job'
+```
+
+Then if you are happy with what it will do, add the -c (--commit) switch:
+
+```bash
+./replicateOldSnapshotsV2.py -v mycluster \
+                             -u myuser \
+                             -d mydomain.net \ 
+                             -r othercluster \
+                             -j 'some job' \
+                             -x
+```
+
+By default, the script will only show what it would do. To actually execute the replication, include the -c switch.
+
+## Authentication Parameters
+
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
+* -k, --keepfor: (optional) keep for X days in the replica
+* -r, --remotecluster: name of the target cluster
+* -j, --jobname: (optional) name of job to replicate (repeat for multiple jobs or use joblist)
+* -l, --joblist: (optional) text file of job names to include (one per line) default is all jobs
+* -e, --excludelogs: (optional) do not replicate database log backups
+* -x, --commit: (optional) perform replications (show only if omitted)
+* -resync, --resync: (optional) re-replicate to same cluster (skip previously replicated if omitted)
+* -numruns, --numruns: (optional) number of runs per API query (default is 1000)
+* -ri, --runid: (optional) specify run ID to replicate
+* -n, --newerthan: (optional) replicate runs newer than X days ago
+* -o, --olderthan: (optional) replicate runs older than X days ago
---
+++ python/replicateOldSnapshotsV2/replicateOldSnapshotsV2.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+"""replicate old snapshots v2"""
+
+# version 2024-12-12b
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+
+# command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-em', '--emailmfacode', action='store_true')
+parser.add_argument('-k', '--keepfor', type=int, default=0)   # number of days to retain
+parser.add_argument('-x', '--commit', action='store_true')        # perform replication
+parser.add_argument('-resync', '--resync', action='store_true')        # perform replication
+parser.add_argument('-r', '--remotecluster', type=str, required=True)  # cluster to replicate to
+parser.add_argument('-j', '--jobname', action='append', type=str)  # one or more job names
+parser.add_argument('-l', '--joblist', type=str, required=False)   # text file of job names
+parser.add_argument('-e', '--excludelogs', action='store_true')   # exclude log backups
+parser.add_argument('-numruns', '--numruns', type=int, default=1000)
+parser.add_argument('-ri', '--runid', type=int, default=None)
+parser.add_argument('-n', '--newerthan', type=int, default=0)     # number of days back to search for snapshots to archive
+parser.add_argument('-o', '--olderthan', type=int, default=0)     # number of days back to search for snapshots to archive
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+keepfor = args.keepfor
+remotecluster = args.remotecluster
+jobnames = args.jobname
+joblist = args.joblist
+excludelogs = args.excludelogs
+commit = args.commit
+resync = args.resync
+numruns = args.numruns
+runid = args.runid
+newerthan = args.newerthan
+olderthan = args.olderthan
+
+
+# gather server list
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+jobnames = gatherList(jobnames, joblist, name='jobs', required=False)
+
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
+
+# get cluster Id
+clusterId = api('get', 'cluster')['id']
+
+newerthanUsecs = timeAgo(newerthan, 'days')
+olderthanUsecs = timeAgo(olderthan, 'days')
+
+# get replication target info
+remote = None
+remoteClusters = api('get', 'remote-clusters', v=2)
+if remoteClusters is not None and 'remoteClusters' in remoteClusters and len(remoteClusters['remoteClusters']) > 0:
+    remote = [r for r in remoteClusters['remoteClusters'] if r['clusterName'].lower() == remotecluster.lower()]
+if remote is None or len(remote) == 0:
+    print('remote cluster %s not found' % remotecluster)
+    exit(1)
+else:
+    remote = remote[0]
+
+jobs = api('get', 'data-protect/protection-groups', v=2)
+jobs = [j for j in jobs['protectionGroups']]  # if 'isDeleted' not in j or j['isDeleted'] is not True]
+policies = api('get', 'data-protect/policies', v=2)
+
+# catch invalid job names
+notfoundjobs = [n for n in jobnames if n.lower() not in [j['name'].lower() for j in jobs]]
+if len(notfoundjobs) > 0:
+    print('Jobs not found: %s' % ', '.join(notfoundjobs))
+    exit(1)
+
+now = datetime.now()
+nowUsecs = dateToUsecs(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+for job in sorted(jobs, key=lambda job: job['name'].lower()):
+    if len(jobnames) == 0 or job['name'].lower() in [j.lower() for j in jobnames]:
+        print('%s' % job['name'])
+        if 'isActive' in job and job['isActive'] == True and keepfor == 0:
+            policy = [p for p in policies['policies'] if p['id'] == job['policyId']]
+            if len(policy) > 0:
+                policy = policy[0]
+                if 'remoteTargetPolicy' in policy and policy['remoteTargetPolicy'] is not None and 'replicationTargets' in policy['remoteTargetPolicy'] and policy['remoteTargetPolicy']['replicationTargets'] is not None:
+                    replicationTarget = [t for t in policy['remoteTargetPolicy']['replicationTargets'] if t['targetType'] == 'RemoteCluster' and t['remoteTargetConfig']['clusterName'].lower() == remotecluster.lower()]
+                    if len(replicationTarget) > 0:
+                        frequent = [t for t in replicationTarget if t['schedule']['unit'] == 'Runs']
+                        if len(frequent) == 0:
+                            frequent = [t for t in replicationTarget if t['schedule']['unit'] == 'Days']
+                        if len(frequent) == 0:
+                            frequent = [t for t in replicationTarget if t['schedule']['unit'] == 'Weeks']
+                        if len(frequent) == 0:
+                            frequent = [t for t in replicationTarget if t['schedule']['unit'] == 'Months']
+                        if len(frequent) == 0:
+                            frequent = [t for t in replicationTarget if t['schedule']['unit'] == 'Years']
+                        if len(frequent) > 0:
+                            retention = frequent[0]['retention']
+                            retentionduration = retention['duration']
+                            retentionunit = retention['unit']
+                            if retentionunit == 'Weeks':
+                                retentionduration = retentionduration * 7
+                            if retentionunit == 'Months':
+                                retentionduration = retentionduration * 30
+                            if retentionunit == 'Years':
+                                retentionduration = retentionduration * 365
+                            print('  ** using policy retention of %s days **' % retentionduration)
+                            keepfor = retentionduration
+        jobident = job['id'].split(':')
+        jobuid = {
+            "clusterId": int(jobident[0]),
+            "clusterIncarnationId": int(jobident[1]),
+            "id": int(jobident[2])
+        }
+        runstoreplicate = {}
+        endUsecs = nowUsecs
+        if olderthan > 0:
+            endUsecs = olderthanUsecs
+        while 1:
+            runs = api('get', 'data-protect/protection-groups/%s/runs?numRuns=%s&endTimeUsecs=%s&includeTenants=true' % (job['id'], numruns, endUsecs), v=2)
+            if len(runs['runs']) > 0:
+                endUsecs = int(runs['runs'][-1]['id'].split(':')[1]) - 1
+            else:
+                break
+            runs = [r for r in runs['runs'] if 'isLocalSnapshotsDeleted' not in r or r['isLocalSnapshotsDeleted'] is not True]
+            if runs is not None and len(runs) > 0:
+                runs = [r for r in runs if ('localBackupInfo' in r and 'endTimeUsecs' in r['localBackupInfo']) or ('originalBackupInfo' in r and 'endTimeUsecs' in r['originalBackupInfo'])]
+            if runs is not None and len(runs) > 0 and excludelogs is True:
+                runs = [r for r in runs if ('localBackupInfo' in r and r['localBackupInfo']['runType'] != 'kLog') or ('originalBackupInfo') in r and r['originalBackupInfo']['runType'] != 'kLog']
+            if runs is not None and len(runs) > 0:
+                for run in sorted(runs, key=lambda run: run['id']):
+                    if runid is not None and run['protectionGroupInstanceId'] != runid:
+                        continue
+                    daysToKeep = keepfor
+                    startdateusecs = int(run['id'].split(':')[1])
+                    startdate = usecsToDate(startdateusecs)
+                    if newerthan > 0 and startdateusecs < newerthanUsecs:
+                        continue
+                    replicated = False
+                    needsResync = False
+                    if 'replicationInfo' in run:
+                        if 'replicationTargetResults' in run['replicationInfo'] and len(run['replicationInfo']['replicationTargetResults']) > 0:
+                            for replicationTargetResult in run['replicationInfo']['replicationTargetResults']:
+                                if 'clusterId' in replicationTargetResult and replicationTargetResult['clusterId'] ==  remote['clusterId'] and replicationTargetResult['status'] in ['Succeeded', 'Running', 'Accepted', 'Canceling'] and ('expiryTimeUsecs' not in replicationTargetResult or replicationTargetResult['expiryTimeUsecs'] > 0):
+                                    replicated = True
+                                    if resync and replicationTargetResult['clusterId'] == remote['clusterId'] and replicationTargetResult['status'] == 'Succeeded':
+                                        replicated = False
+                                        needsResync = True
+
+                    if replicated is False:
+                        if commit:
+                            if needsResync is False:
+                                startTimeUsecs = startdateusecs
+                                if keepfor > 0:
+                                    expireTimeUsecs = startTimeUsecs + (int(keepfor * 86400000000))
+                                else:
+                                    thisrun = api('get', '/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=%s&excludeTasks=true&id=%s' % (startdateusecs, jobuid['id']))
+                                    expireTimeUsecs = thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']['finishedTasks'][0]['expiryTimeUsecs']
+
+                                daysToKeep = int(round((expireTimeUsecs - nowUsecs) / 86400000000, 0))
+                                if daysToKeep <= 0:
+                                    daysToKeep = 1
+                                replicationTask = {
+                                    "updateProtectionGroupRunParams": [
+                                        {
+                                            "runId": run['id'],
+                                            "replicationSnapshotConfig": {
+                                                "newSnapshotConfig": [
+                                                    {
+                                                        "id": remote['clusterId'],
+                                                        "name": remote['clusterName'],
+                                                        "retention": {
+                                                            "unit": "Days",
+                                                            "duration": int(daysToKeep)
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    ]
+                                }
+                            else:
+                                if keepfor == 0:
+                                    daysToKeep = 0
+                                else:
+                                    startTimeUsecs = startdateusecs
+                                    thisrun = api('get', '/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=%s&excludeTasks=true&id=%s' % (startdateusecs, jobuid['id']))
+                                    expireTimeUsecs = thisrun[0]['backupJobRuns']['protectionRuns'][0]['copyRun']['finishedTasks'][0]['expiryTimeUsecs']
+                                    retentionDays = int(round((expireTimeUsecs - startTimeUsecs) / 86400000000))
+                                    daysToKeep = keepfor - retentionDays
+                                    if daysToKeep < 1:
+                                        daysToKeep = 1
+                                replicationTask = {
+                                    "updateProtectionGroupRunParams": [
+                                        {
+                                            "runId": run['id'],
+                                            "replicationSnapshotConfig": {
+                                               "updateExistingSnapshotConfig": [
+                                                    {
+                                                        "id": remote['clusterId'],
+                                                        "name": remote['clusterName'],
+                                                        "resync": True,
+                                                        "daysToKeep": int(daysToKeep)
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    ]
+                                }
+                            print('  Replicating  %s' % startdate)
+                            runstoreplicate[startdateusecs] = replicationTask
+                        else:
+                            print('  Would replicate  %s (%s)' % (startdate, run['protectionGroupInstanceId']))
+                    else:
+                        print('  Already replicated  %s' % startdate)
+        if len(runstoreplicate.keys()) > 0:
+            print('  Committing replications...')
+        for rundate in sorted(runstoreplicate.keys()):
+            result = api("put", "data-protect/protection-groups/%s/runs" % job['id'], runstoreplicate[rundate], v=2)
