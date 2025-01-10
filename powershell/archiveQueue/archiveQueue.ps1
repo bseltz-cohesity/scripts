fe18c10cecbd086dfc5b484440ce7c4cbc4970ec
bseltz-cohesity <bseltzer@cohesity.com>
2025-01-10 06:23:01 -0500

    2025-01-10

--- powershell/archiveQueue/archiveQueue.ps1
+++ powershell/archiveQueue/archiveQueue.ps1
@@ -34,6 +34,12 @@ if ('' -ne $jobList){
 
 ### source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+if($cohesity_api.api_version -lt '2025.01.10'){
+    Write-Host "This script requires cohesity-api.ps1 version 2025.01.10 or later" -foregroundColor Yellow
+    Write-Host "Please download it from https://github.com/cohesity/community-automation-samples/tree/main/powershell/cohesity-api" -ForegroundColor Yellow
+    exit
+}
+
 
 $conversion = @{'MiB' = 1024 * 1024; 'GiB' = 1024 * 1024 * 1024; 'TiB' = 1024 * 1024 * 1024 * 1024}
 function toUnits($val){
@@ -65,95 +71,88 @@ foreach($job in (api get protectionJobs | Where-Object {$_.isDeleted -ne $True} 
         $endUsecs = dateToUsecs (Get-Date)
         $archiveTasksFound = $false
         $breakOut = $false
-        while($True){
+        Get-Runs -jobId $jobId -numRuns $numRuns -includeRunning | Foreach-Object {
+            $run = $_
             if($breakOut){
                 break
             }
-            $runs = api get "protectionRuns?jobId=$jobId&numRuns=$numRuns&endTimeUsecs=$endUsecs&excludeTasks=true&excludeNonRestorableRuns=true"
-            if($runs){
-                $endUsecs = $runs[-1].backupRun.stats.startTimeUsecs - 1
-            }else{
-                break
-            }
-            foreach($run in $runs){
-                $runStartTimeUsecs = $run.backupRun.stats.startTimeUsecs
-                foreach($copyRun in ($run.copyRun | Where-Object {$_.target.type -eq 'kArchival'})){
-                    $archiveTasksFound = $True
-                    $target = $copyRun.target.archivalTarget.vaultName
-                    $status = $copyRun.status #.subString(1)
-                    $startTimeUsecs = $copyRun.stats.startTimeUsecs
-                    $endTimeUsecs = $copyRun.stats.endTimeUsecs
-                    $noLongerNeeded = ''
-                    $cancelling = ''
-                    $cancel = $false
-                    $expiryTimeUsecs = $copyRun.expiryTimeUsecs
-                    if($copyRun.stats.logicalBytesTransferred){
-                        $transferred = $copyRun.stats.logicalBytesTransferred
-                    }else{
-                        $transferred = 0
-                    }
-                    if($copyRun.stats.physicalBytesTransferred){
-                        $physicalTransferred = $copyRun.stats.physicalBytesTransferred
-                    }else{
-                        $physicalTransferred = 0
-                    }
-                    if($copyRun.stats.isIncremental -eq $False){
-                        $referenceFull = '(Reference Full)'
-                    }else{
-                        $referenceFull = ''
-                    }
+            $runStartTimeUsecs = $run.backupRun.stats.startTimeUsecs
+            foreach($copyRun in ($run.copyRun | Where-Object {$_.target.type -eq 'kArchival'})){
+                $archiveTasksFound = $True
+                $target = $copyRun.target.archivalTarget.vaultName
+                $status = $copyRun.status
+                $startTimeUsecs = $copyRun.stats.startTimeUsecs
+                $endTimeUsecs = $copyRun.stats.endTimeUsecs
+                $noLongerNeeded = ''
+                $cancelling = ''
+                $cancel = $false
+                $expiryTimeUsecs = $copyRun.expiryTimeUsecs
+                if($copyRun.stats.logicalBytesTransferred){
+                    $transferred = $copyRun.stats.logicalBytesTransferred
+                }else{
+                    $transferred = 0
+                }
+                if($copyRun.stats.physicalBytesTransferred){
+                    $physicalTransferred = $copyRun.stats.physicalBytesTransferred
+                }else{
+                    $physicalTransferred = 0
+                }
+                if($copyRun.stats.isIncremental -eq $False){
+                    $referenceFull = '(Reference Full)'
+                }else{
+                    $referenceFull = ''
+                }
 
-                    if($copyRun.status -notin $finishedStates){
-                        # cancel outdated
-                        if($cancelOutdated){
-                            $thisrun = api get "/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=$($runStartTimeUsecs)&id=$($jobId)"
-                            foreach($task in $thisrun.backupJobRuns.protectionRuns[0].copyRun.activeTasks){
-                                if($task.snapshotTarget.type -eq 3){
-                                    $daysToKeep = $task.retentionPolicy.numDaysToKeep - $daysTilExpire
-                                    $usecsToKeep = $daysToKeep * 1000000 * 86400
-                                    $timePassed = $nowUsecs - $runStartTimeUsecs
-                                    if($timePassed -gt $usecsToKeep){
-                                        $noLongerNeeded = "(NO LONGER NEEDED)"
-                                        if($cancelOutdated -or $cancelAll){
-                                            $cancel = $True
-                                            $cancelling = '(Cancelling)'
-                                        }
+                if($copyRun.status -notin $finishedStates){
+                    # cancel outdated
+                    if($cancelOutdated){
+                        $thisrun = api get "/backupjobruns?allUnderHierarchy=true&exactMatchStartTimeUsecs=$($runStartTimeUsecs)&id=$($jobId)"
+                        foreach($task in $thisrun.backupJobRuns.protectionRuns[0].copyRun.activeTasks){
+                            if($task.snapshotTarget.type -eq 3){
+                                $daysToKeep = $task.retentionPolicy.numDaysToKeep - $daysTilExpire
+                                $usecsToKeep = $daysToKeep * 1000000 * 86400
+                                $timePassed = $nowUsecs - $runStartTimeUsecs
+                                if($timePassed -gt $usecsToKeep){
+                                    $noLongerNeeded = "(NO LONGER NEEDED)"
+                                    if($cancelOutdated -or $cancelAll){
+                                        $cancel = $True
+                                        $cancelling = '(Cancelling)'
                                     }
                                 }
                             }
                         }
+                    }
 
-                        if($transferred -eq 0 -and ($cancelQueued -or $cancelAll)){
-                            $cancel = $True
-                            $cancelling = '(Cancelling)'
-                        }
+                    if($transferred -eq 0 -and ($cancelQueued -or $cancelAll)){
+                        $cancel = $True
+                        $cancelling = '(Cancelling)'
+                    }
 
-                        "        {0,25}:    ({1} $unit)    {2}  {3}  {4}" -f (usecsToDate $runStartTimeUsecs), (toUnits $transferred), $referenceFull, $noLongerNeeded, $cancelling
-                        "{0}`t{1}`t{2}`t{3}`t{4}`t{5}`t{6}`t{7}" -f $jobId, $jobName, (usecsToDate $runStartTimeUsecs), (toUnits $transferred), (toUnits $physicalTransferred), $status, $target, (usecsToDate $startTimeUsecs) | Out-File -FilePath $outfileName -Append
-                        $runningTasks += 1
-                        # cancel archive task
-                        if($cancel -eq $True){
-                            $cancelTaskParams = @{
-                                "jobId"       = $jobId;
-                                "copyTaskUid" = $copyRun.taskUid
-                            }
-                            $null = api post "protectionRuns/cancel/$($jobId)" $cancelTaskParams 
+                    "        {0,25}:    ({1} $unit)    {2}  {3}  {4}" -f (usecsToDate $runStartTimeUsecs), (toUnits $transferred), $referenceFull, $noLongerNeeded, $cancelling
+                    "{0}`t{1}`t{2}`t{3}`t{4}`t{5}`t{6}`t{7}" -f $jobId, $jobName, (usecsToDate $runStartTimeUsecs), (toUnits $transferred), (toUnits $physicalTransferred), $status, $target, (usecsToDate $startTimeUsecs) | Out-File -FilePath $outfileName -Append
+                    $runningTasks += 1
+                    # cancel archive task
+                    if($cancel -eq $True){
+                        $cancelTaskParams = @{
+                            "jobId"       = $jobId;
+                            "copyTaskUid" = $copyRun.taskUid
                         }
+                        $null = api post "protectionRuns/cancel/$($jobId)" $cancelTaskParams 
+                    }
+                }else{
+                    if($showFinished -and $expiryTimeUsecs -gt $nowUsecs){
+                        "        {0,25}:    ({1} $unit)    {2}  {3}" -f (usecsToDate $runStartTimeUsecs), (toUnits $transferred), $status, $referenceFull
+                        "{0}`t{1}`t{2}`t{3}`t{4}`t{5}`t{6}`t{7}`t{8}" -f $jobId, $jobName, (usecsToDate $runStartTimeUsecs), (toUnits $transferred), (toUnits $physicalTransferred), $status, $target, (usecsToDate $startTimeUsecs), (usecsToDate $endTimeUsecs) | Out-File -FilePath $outfileName -Append
                     }else{
-                        if($showFinished -and $expiryTimeUsecs -gt $nowUsecs){
-                            "        {0,25}:    ({1} $unit)    {2}  {3}" -f (usecsToDate $runStartTimeUsecs), (toUnits $transferred), $status, $referenceFull
-                            "{0}`t{1}`t{2}`t{3}`t{4}`t{5}`t{6}`t{7}`t{8}" -f $jobId, $jobName, (usecsToDate $runStartTimeUsecs), (toUnits $transferred), (toUnits $physicalTransferred), $status, $target, (usecsToDate $startTimeUsecs), (usecsToDate $endTimeUsecs) | Out-File -FilePath $outfileName -Append
-                        }else{
-                            if($quickScan){
-                                $breakOut = $True
-                                break
-                            }
+                        if($quickScan){
+                            $breakOut = $True
+                            break
                         }
                     }
                 }
-                if($breakOut){
-                    break
-                }
+            }
+            if($breakOut){
+                break
             }
             if($quickScan -and $archiveTasksFound -eq $false){
                 $breakOut = $True
