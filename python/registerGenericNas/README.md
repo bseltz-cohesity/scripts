65e6e86236508596fc44b3fe74f990e7b0bdde2e
bseltz-cohesity <bseltzer@cohesity.com>
2025-02-08 06:49:27 -0500

    2025-02-08

--- python/registerGenericNas/README.md
+++ python/registerGenericNas/README.md
@@ -27,16 +27,28 @@ Place both files in a folder together and run the main script like so:
 ./registerGenericNas.ps1 -v mycluster \
                          -u myuser \
                          -d mydomain.net \
-                         -m \\myserver\myshare \
-                         -m \\myserver\myshare2 \
+                         -m '\\myserver\myshare' \
+                         -m '\\myserver\myshare2' \
                          -s 'mydomain.net\myuser'
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -v, --vip: DNS or IP of the Cohesity cluster to connect to
-* -u, --username: username to authenticate to Cohesity cluster
-* -d, --domain: (optional) domain of username, defaults to local
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
 * -m, --mountpath: (optional) generic NAS mount path to protect (repeat for multiple)
 * -f, --mountpathfile: (optional) text file with NAS mount paths to protect (one per line)
 * -s, --smbusername: (optional) smb username (e.g. mydomain.net\myuser)
+* -sp, --smbpassword: (optional) smb password (will be prompted if omitted)
--- python/registerGenericNas/registerGenericNas.py
+++ python/registerGenericNas/registerGenericNas.py
@@ -16,40 +16,81 @@ import getpass
 ### command line arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--vip', type=str, required=True)       # Cohesity cluster name or IP
-parser.add_argument('-u', '--username', type=str, required=True)  # Cohesity Username
-parser.add_argument('-d', '--domain', type=str, default='local')  # Cohesity User Domain
-parser.add_argument('-m', '--mountpath', action='append', type=str)  # mount path
-parser.add_argument('-f', '--mountpathfile', type=str)               # mount paths in text file
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true'),
+parser.add_argument('-p', '--mountpath', action='append', type=str)  # mount path
+parser.add_argument('-l', '--mountlist', type=str)               # mount paths in text file
 parser.add_argument('-s', '--smbusername', type=str)                 # smb username
-
+parser.add_argument('-sp', '--smbpassword', type=str)
 args = parser.parse_args()
 
 vip = args.vip
 username = args.username
 domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 mountpaths = args.mountpath
-mountpathfile = args.mountpathfile
+mountlist = args.mountlist
 smbusername = args.smbusername
+smbpassword = args.smbpassword
 
 # get smb password
-smbpwd = None
-if smbusername is not None:
-    smbpwd = getpass.getpass("Enter the password for %s: " % smbusername)
+if smbusername is not None and smbpassword is None:
+    smbpassword = getpass.getpass("Enter the password for %s: " % smbusername)
 
-# gather mountpaths
-if mountpaths is None:
-    mountpaths = []
-if mountpathfile is not None:
-    f = open(mountpathfile, 'r')
-    mountpaths += [e.strip() for e in f.readlines() if e.strip() != '']
-    f.close()
-if len(mountpaths) == 0:
-    print('No mount paths specified!')
+# gather server list
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+mountpaths = gatherList(mountpaths, mountlist, name='mount paths', required=True)
+
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
     exit(1)
 
 # authenticate
-apiauth(vip, username, domain)
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
 
 smbdomain = None
 for mountpath in mountpaths:
@@ -68,7 +109,7 @@ for mountpath in mountpaths:
             'nasMountCredentials': {
                 'protocol': protocol,
                 'username': smbusername,
-                'password': smbpwd
+                'password': smbpassword
             }
         }
         if smbdomain is not None:
