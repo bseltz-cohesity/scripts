5728a803d90f381c566815f9641a1f27e076d2e8
bseltz-cohesity <bseltzer@cohesity.com>
2025-02-12 05:01:29 -0500

    2025-02-12

--- reports/helios-other/powershell/heliosProtectedObjectReport/heliosProtectedObjectReport.ps1
+++ reports/helios-other/powershell/heliosProtectedObjectReport/heliosProtectedObjectReport.ps1
@@ -17,25 +17,28 @@ $outfileName = "heliosProtectedObjectReport-$dateString.csv"
 "`nCluster Name,Job Name,Environment,Object Name,Object Type,Parent,Policy Name,Frequency (Minutes),Last Backup,Last Status,Job Paused,Alert Recipients" | Out-File -FilePath $outfileName
 
 foreach($cluster in heliosClusters){
+    Write-Host ''
     heliosCluster $cluster
 
     $policies = api get -v2 "data-protect/policies"
-    $jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true"
-    $sources = api get protectionSources
+    $jobs = api get -v2 "data-protect/protection-groups?isDeleted=false&isActive=true&onlyReturnBasicSummary=true"
+    $sources = api get "protectionSources?numLevels=2&excludeTypes=kDatastore,kVirtualMachine,kVirtualApp,kStoragePod,kNetwork,kDistributedVirtualPortgroup,kTagCategory,kTag&useCachedData=true"
 
     $objects = @{}
 
-    "`nGathering Job Info from $($cluster.name)..."
+    # "`nGathering Job Info from $($cluster.name)..."
     foreach($job in $jobs.protectionGroups | Sort-Object -Property name){
         "    $($job.name)"
         $policy = $policies.policies | Where-Object id -eq $job.policyId
         $runs = api get -v2 "data-protect/protection-groups/$($job.id)/runs?includeObjectDetails=true&numRuns=7"
-        if('kLog' -in $runs.runs.localBackupInfo.runType){
-            $runDates = ($runs.runs | Where-Object {$_.localBackupInfo.runType -eq 'kLog'}).localBackupInfo.startTimeUsecs
+        if($runs.runs[0].PSObject.Properties['localBackupInfo']){
+            $runDates = @($runs.runs.localBackupInfo.startTimeUsecs)
+            $lastStatus = $runs.runs[0].localBackupInfo.status
         }else{
-            $runDates = $runs.runs.localBackupInfo.startTimeUsecs
+            $runDates = @($runs.runs.archivalInfo.archivalTargetResults.startTimeUsecs)
+            $lastStatus = $runs.runs[0].archivalInfo.archivalTargetResults.status
         }
-        $lastStatus = $runs.runs[0].localBackupInfo.status
+        
         foreach($run in $runs.runs){
             foreach($object in $run.objects.object){
                 if($object.id -notin $objects.Keys){
@@ -77,10 +80,12 @@ foreach($cluster in heliosClusters){
             $object.parent = $parent.name
             if($object.runDates.count -gt 1){
                 $frequency = [math]::Round((($object.runDates[0] - $object.runDates[-1]) / ($object.runDates.count - 1)) / (1000000 * 60))
+                $lastRunDate = usecsToDate $object.runDates[0]
             }else{
                 $frequency = '-'
+                $lastRunDate = '-'
             }
-            $lastRunDate = usecsToDate $object.runDates[0]
+            
             if(!$parent){
                 $object.parent = '-'
             }
