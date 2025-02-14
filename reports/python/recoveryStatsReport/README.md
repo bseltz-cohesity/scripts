14391ed23ec8e7e577e6e27db55016454e966423
bseltz-cohesity <bseltzer@cohesity.com>
2025-02-14 10:11:35 -0500

    2025-02-14

---
+++ reports/python/recoveryStatsReport/README.md
@@ -0,0 +1,67 @@
+# Generate Recovery Stats Report using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script creates a report of esitmated object size and data transferred durning recent recoveries.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/reports/python/recoveryStatsReport/recoveryStatsReport.py
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity.py
+chmod +x recoveryStatsReport.py
+# end download commands
+```
+
+## Components
+
+* recoveryStatsReport.py: the main python script
+* pyhesity.py: the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+# example
+./recoveryStatsReport.py -v mycluster -u myusername -d mydomain.net
+# end example
+```
+
+To report on multiple clusters:
+
+```bash
+# example
+./recoveryStatsReport.py -v mycluster1 -v mycluster2 -u myusername -d mydomain.net
+# end example
+```
+
+To connect through Helios:
+
+```bash
+# example
+./recoveryStatsReport.py -u myuser@mydomain.net -c mycluster1 -c mycluster2
+# end example
+```
+
+## Parameters
+
+## Authentication Parameters
+
+* -v, --vip: (optional) one or more names or IPa of Cohesity clustera to connect to (repeat for multiple) default is helios.cohesity.com
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) one or more helios/mcm clusters to connect to (repeat for multiple)
+* -m, --mfacode: (optional) MFA code for authentication
+
+## Other Parameters
+
+* -y, --daysback: (optional) include recoveries from the last X days (default is 7)
+* -of, --outfolder: (optional) where to write report html (default is current directory)
+* -f, --outfilename: (optional) default is recoveryStatsReport.csv
+* -x, --unit: (optional) KiB, MiB, GiB, or TiB] (default is GiB)
---
+++ reports/python/recoveryStatsReport/recoveryStatsReport.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+from pyhesity import *
+from datetime import datetime
+import codecs
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', action='append', type=str)
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', action='append', type=str)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-y', '--daysback', type=int, default=7)
+parser.add_argument('-of', '--outfolder', type=str, default='.')
+parser.add_argument('-f', '--outfilename', type=str, default='recoveryStatsReport.csv')
+parser.add_argument('-x', '--units', type=str, choices=['MiB', 'GiB', 'mib', 'gib'], default='GiB')
+
+args = parser.parse_args()
+
+vips = args.vip
+username = args.username
+domain = args.domain
+clusternames = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+folder = args.outfolder
+daysback = args.daysback
+outfilename = args.outfilename
+units = args.units
+
+if vips is None or len(vips) == 0:
+    vips = ['helios.cohesity.com']
+
+multiplier = 1024 * 1024 * 1024
+if units.lower() == 'mib':
+    multiplier = 1024 * 1024
+
+if units == 'mib':
+    units = 'MiB'
+if units == 'gib':
+    units = 'GiB'
+
+now = datetime.now()
+nowusecs = dateToUsecs()
+midnight = datetime.combine(now, datetime.min.time())
+midnightusecs = dateToUsecs(midnight)
+tonightusecs = midnightusecs + 86399000000
+tonightMsecs = int(round(midnightusecs + 86400000000) / 1000)
+beforeusecs = midnightusecs - (daysback * 86400000000) + 86400000000
+
+# outfile
+dateString = now.strftime("%Y-%m-%d")
+outfilename = '%s/%s-%s.csv' % (folder, outfilename, dateString)
+f = codecs.open(outfilename, 'w')
+f.write('"Cluster","Recovery Task Name","Recovery Task ID","Recovery Task Start Time","Recovery Task End Time","Duration (Sec)","Recovery Type","Object Name","Logical Size (%s)","Front End Size (%s)","Data Transferred (%s)","Status","Username"\n' % (units, units, units))
+
+def reportCluster():
+    cluster = api('get', 'cluster')
+    recoveries = api('get', 'data-protect/recoveries?startTimeUsecs=%s&includeTenants=true&endTimeUsecs=%s' % (beforeusecs, tonightusecs), v=2)
+    if recoveries is None or 'recoveries' not in recoveries or recoveries['recoveries'] is None or len(recoveries['recoveries']) == 0:
+        return None
+
+    statsEntities = api('get', 'statistics/entities?maxEntities=1000&schemaName=kMagnetoRestoreTaskStats&metricName=kNumBytesWritten')
+
+    logicalSizes = {}
+    frontEndSizes = {}
+
+    for recovery in recoveries['recoveries']:
+        thisRecovery = api('get', 'data-protect/recoveries/%s?includeTenants=true' % recovery['id'], v=2)
+        if 'endTimeUsecs' not in thisRecovery:
+            continue
+        print('%s (%s)' %(thisRecovery['name'], thisRecovery['recoveryAction']))
+        recoveryUser = thisRecovery['creationInfo']['userName']
+        recoveryStart = usecsToDate(thisRecovery['startTimeUsecs'])
+        recoveryEnd = usecsToDate(thisRecovery['endTimeUsecs'])
+        duration = (int(round(((thisRecovery['endTimeUsecs'] - thisRecovery['startTimeUsecs']) / 1000000))))
+        recoveryStatus = thisRecovery['status']
+        recoveryId = thisRecovery['id']
+        v1TaskId = int(recoveryId.split(':')[-1])
+        statsEntity = [e for e in statsEntities if e['entityId']['entityId']['data']['int64Value'] == v1TaskId]
+        if statsEntity is not None and len(statsEntity) > 0:
+            statsEntity = statsEntity[0]
+        else:
+            statsEntity = None
+        # find environment params
+        paramskeys = [k for k in thisRecovery.keys() if k.endswith('Params')]
+        if paramskeys is not None and len(paramskeys) > 0:
+            paramskey = paramskeys[0]
+        else:
+            print('paramskey not found')
+        params = thisRecovery[paramskey]
+        for object in params['objects']:
+            objectName = object['objectInfo']['name']
+            if 'status' in object:
+                objectStatus = object['status']
+            else:
+                objectStatus = thisRecovery['status']
+            print('    %s' % objectName)
+            objectId = object['objectInfo']['id']
+            environment = object['objectInfo']['environment']
+            logicalSize = 0
+            frontEndSize = 0
+            # get logical size
+            if objectId not in logicalSizes.keys():
+                search = api('get','/searchvms?entityIds=%s' % objectId)
+                if search is not None and 'vms' in search and len(search['vms']) > 0:
+                    logicalSize = search['vms'][0]['vmDocument']['versions'][0]['logicalSizeBytes']
+                    entityProps = [p for p in search['vms'][0]['vmDocument']['objectId']['entity'].keys() if p.endswith('Entity')]
+                    if entityProps is not None and len(entityProps) > 0:
+                        entityProp = entityProps[0]
+                        entity = search['vms'][0]['vmDocument']['objectId']['entity'][entityProp]
+                        if 'frontEndSizeInfo' in entity and entity['frontEndSizeInfo'] is not None and 'sizeBytes' in entity['frontEndSizeInfo'] and entity['frontEndSizeInfo']['sizeBytes'] is not None:
+                            if entity['frontEndSizeInfo']['sizeBytes'] > 0:
+                                frontEndSize = entity['frontEndSizeInfo']['sizeBytes']
+                logicalSizes[objectId] = logicalSize
+                if frontEndSize == 0 and logicalSize > 0:
+                    frontEndSize = logicalSize
+                frontEndSizes[objectId] = frontEndSize
+            else:
+                logicalSize = logicalSizes[objectId]
+                frontEndSize = frontEndSizes[objectId]
+            # print('      %s' % frontEndSize)
+            totalTransferred = 0
+            if statsEntity is not None:
+                startDate = usecsToDateTime(thisRecovery['startTimeUsecs'])
+                morning = datetime.combine(startDate, datetime.min.time())
+                morningMsecs = int(round(dateToUsecs(morning) / 1000))
+                stats = api('get','statistics/timeSeriesStats?endTimeMsecs=%s&entityId=%s&metricName=kNumBytesWritten&metricUnitType=0&range=month&rollupFunction=average&rollupIntervalSecs=180&schemaName=kMagnetoRestoreTaskStats&startTimeMsecs=%s' % (tonightMsecs, v1TaskId, morningMsecs))
+                if stats is not None and 'dataPointVec' in stats:
+                    for dataPoint in stats['dataPointVec']:
+                        totalTransferred += dataPoint['data']['int64Value']
+            else:
+                if totalTransferred == 0:
+                    totalTransferred = frontEndSize
+            if objectStatus == 'Failed':
+                totalTransferred = 0
+            totalTransferred = round(totalTransferred / multiplier, 1)
+            frontEndSize = round(frontEndSize / multiplier, 1)
+            logicalSize = round(logicalSize / multiplier, 1)
+            f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], thisRecovery['name'], thisRecovery['id'], recoveryStart, recoveryEnd, duration, thisRecovery['recoveryAction'], objectName, logicalSize, frontEndSize, totalTransferred, objectStatus, recoveryUser))
+
+for vip in vips:
+
+    # authentication =========================================================
+    apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, quiet=True)
+
+    # exit if not authenticated
+    if apiconnected() is False:
+        print('authentication failed')
+        continue
+
+    # if connected to helios or mcm, select access cluster
+    if mcm or vip.lower() == 'helios.cohesity.com':
+        if clusternames is None or len(clusternames) == 0:
+            clusternames = [c['name'] for c in heliosClusters()]
+        for clustername in clusternames:
+            heliosCluster(clustername)
+            if LAST_API_ERROR() != 'OK':
+                continue
+            reportCluster()
+    else:
+        reportCluster()
+
+f.close()
+print('Output saved to %s' % outfilename)
+
