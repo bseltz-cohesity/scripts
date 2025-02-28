0b04c2bcbfe178e1e6d23132426dc28d9117430f
bseltz-cohesity <bseltzer@cohesity.com>
2025-02-28 07:47:15 -0500

    2025-02-28

---
+++ python/registerCassandra/README.md
@@ -0,0 +1,65 @@
+# Register Cassandra Protection Sources using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script registers Cassandra protection sources.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/registerCassandra/registerCassandra.py
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity.py
+chmod +x registerCassandra.py
+# end download commands
+```
+
+## Components
+
+* [registerCassandra.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/registerCassandra/registerCassandra.py): the main python script
+* [pyhesity.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity/pyhesity.py): the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./registerCassandra.py -v mycluster \
+                     -u myusername \
+                     -d mydomain.net \
+                     -n cassandra1.mydomain.net,192.168.1.101
+```
+
+## Authentication Parameters
+
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
+* -n, --servername: name of server:port to register (repeat for multiple)
+* -l, --serverlist: text file containing list of servers to register (one per line)
+* -cd, --configdir: cassandra configuration directory
+* -dc, --datacenter: (optional) datacenter name (repeat for multiple)
+* -cl, --commitlog: (optional) commit logs backup directory
+* -dd, --dseconfigdir: (optional) DSE configuration directory
+* -dt, --dsetieredstorage: (optional) DSE tiered storage is present
+* -da, --dseauthenticator: (optional) DSE authenticator is present
+* -dn, --dsesolrnode: (optional) DSE Solr node IP address (repeat for mutiple)
+* -dp, --dsesolrport: (optional) DSE Solr port
+* -su, --sshusername: SSH username
+* -sp, --sshpassword: (optional) SSH password (will be prompted if omitted)
+* -ju, --jmxusername: (optional) JMX username
+* -jp, --jmxpassword: (optional) JMX password (will be prompted if omitted)
+* -cu, --cassandrausername: (optional) cassandra username
+* -cp, --cassandrapassword: (optional) cassandra password (will be prompted if omitted)
+* -kp, --kerberosprincipal: (optional) kerberos principal
---
+++ python/registerCassandra/registerCassandra.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+
+from pyhesity import *
+from getpass import getpass
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
+parser.add_argument('-n', '--servername', action='append', type=str)
+parser.add_argument('-l', '--serverlist', type=str, default=None)
+parser.add_argument('-cd', '--configdir', type=str, required=True)
+parser.add_argument('-dc', '--datacenter', action='append', type=str)
+parser.add_argument('-cl', '--commitlog', type=str, default=None)
+parser.add_argument('-dd', '--dseconfigdir', type=str, default=None)
+parser.add_argument('-dt', '--dsetieredstorage', action='store_true')
+parser.add_argument('-da', '--dseauthenticator', action='store_true')
+parser.add_argument('-dn', '--dsesolrnode', action='append', type=str)
+parser.add_argument('-dp', '--dsesolrport', type=int, default=None)
+parser.add_argument('-su', '--sshusername', type=str, required=True)
+parser.add_argument('-sp', '--sshpassword', type=str, default=None)
+parser.add_argument('-ju', '--jmxusername', type=str, default=None)
+parser.add_argument('-jp', '--jmxpassword', type=str, default=None)
+parser.add_argument('-cu', '--cassandrausername', type=str, default=None)
+parser.add_argument('-cp', '--cassandrapassword', type=str, default=None)
+parser.add_argument('-kp', '--kerberosprincipal', type=str, default=None)
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+
+servername = args.servername
+serverlist = args.serverlist
+
+datacenters = args.datacenter
+configdir = args.configdir
+commitlog = args.commitlog
+dseconfigdir = args.dseconfigdir
+dsetieredstorage = args.dsetieredstorage
+dseauthenticator = args.dseauthenticator
+dsesolrnodes = args.dsesolrnode
+dsesolrport = args.dsesolrport
+
+sshusername = args.sshusername
+sshpassword = args.sshpassword
+jmxusername = args.jmxusername
+jmxpassword = args.jmxpassword
+cassandrausername = args.cassandrausername
+cassandrapassword = args.cassandrapassword
+kerberosprincipal = args.kerberosprincipal
+
+# gather list function
+def gatherList(param=None, filename=None, name='items', required=True):
+    items = []
+    if param is not None:
+        for item in param:
+            items.append(item)
+    if filename is not None:
+        f = open(filename, 'r')
+        items += [s.strip() for s in f.readlines() if s.strip() != '']
+        f.close()
+    if required is True and len(items) == 0:
+        print('no %s specified' % name)
+        exit()
+    return items
+
+
+# get list of views to protect
+servers = gatherList(servername, serverlist, name='servers', required=True)
+
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
+
+if sshpassword is None:
+    sshpassword = getpass("\nEnter SSH password: ")
+
+if jmxusername is not None and jmxpassword is None:
+    jmxpassword = getpass("\nEnter JMX password: ")
+
+if cassandrausername is not None and cassandrapassword is None:
+    cassandrapassword = getpass("\nEnter cassandra password: ")
+
+for server in servers:
+    server = server.replace(' ', '')
+    seeds = server.split(',')
+    seedip = seeds[-1]
+    sourcename = seeds[0]
+    registeredSource = [r for r in (api('get', 'protectionSources/registrationInfo?environments=kCassandra'))['rootNodes'] if r['rootNode']['name'].lower() == sourcename.lower()]
+    if registeredSource is not None and len(registeredSource) > 0:
+        print('%s is already registered' % server)
+    else:
+        newSource = {
+            "environment": "kCassandra",
+            "name": sourcename,
+            "cassandraParams": {
+                "seedNode": seedip,
+                "configDirectory": configdir,
+                "sshPasswordCredentials": {
+                    "username": sshusername,
+                    "password": sshpassword
+                },
+                "sshPrivateKeyCredentials": None,
+                "jmxCredentials": None,
+                "cassandraCredentials": None,
+                "dataCenterNames": [],
+                "commitLogBackupLocation": "",
+                "dseConfigurationDirectory": dseconfigdir,
+                "isDseAuthenticator": False,
+                "isDseTieredStorage": False,
+                "dseSolrInfo": None,
+                "kerberosPrincipal": None
+            }
+        }
+        if kerberosprincipal is not None:
+            newSource['cassandraParams']['kerberosPrincipal'] = kerberosprincipal
+        if commitlog is not None:
+            newSource['cassandraParams']['commitLogBackupLocation'] = commitlog
+        if datacenters is not None and len(datacenters) > 0:
+            newSource['cassandraParams']['dataCenterNames'] = datacenters
+        if dseauthenticator is True:
+            newSource['cassandraParams']['isDseAuthenticator'] = True
+        if dsetieredstorage is True:
+            newSource['cassandraParams']['isDseTieredStorage'] = True
+        if dsesolrnodes is not None and len(dsesolrnodes) > 0:
+            newSource['cassandraParams']['dseSolrInfo'] = {
+                "solrNodes": dsesolrnodes,
+                "solrPort": dsesolrport
+            }
+        if jmxusername is not None:
+            newSource['cassandraParams']['jmxCredentials'] = {
+                "username": jmxusername,
+                "password": jmxpassword
+            }
+        if cassandrausername is not None:
+            newSource['cassandraParams']['cassandraCredentials'] = {
+                "username": cassandrausername,
+                "password": cassandrapassword
+            }
+
+        result = api('post', 'data-protect/sources/registrations', newSource, v=2)
+        if 'id' in result:
+            print('Registering %s' % sourcename)
