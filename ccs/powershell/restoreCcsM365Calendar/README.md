7caf197628b9aeb086149a6ac2b8ed0ef32b34ab
bseltz-cohesity <bseltzer@cohesity.com>
2025-03-18 15:14:57 -0400

    2025-03-18

---
+++ ccs/powershell/restoreCcsM365Calendar/README.md
@@ -0,0 +1,56 @@
+# Restore Ccs M365 Calendars using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script restores Ccs M365 Calendars.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'restoreCcsM365Calendar'
+$repoURL = 'https://raw.githubusercontent.com/cohesity/community-automation-samples/main'
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/ccs/powershell/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/powershell/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* [restoreCcsM365Calendar.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/ccs/powershell/restoreCcsM365Calendar/restoreCcsM365Calendar.ps1): the main powershell script
+* [cohesity-api.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/cohesity-api/cohesity-api.ps1): the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```powershell
+./restoreCcsM365Calendar.ps1 -mailboxName user1.mydomain.onmicrosoft.com, user2.mydomain.onmicrosoft.com
+```
+
+## Basic Parameters
+
+* -username: (optional) used for password storage only (default is 'Ccs')
+* -mailboxName: (optional) one or more mailbox names (comma separated)
+* -mailboxList: (optional) text file of mailbox names (one per line)
+* -pageSize: (optional) limit number of objects returned pr page (default is 1000)
+* -useMBS:  (optional) use Microsoft 365 Backup Storage
+
+## Alternate Location Parameters
+
+* -targetSource: (optional) registered M365 protection source to restore to
+* -targetMailbox: (optional) mailbox name to restore to
+* -recoverDate: (optional) restore latest snashot on or before this date (default is latest backup)
+* -folderPrefix: (optional) folder prefix when restoring to alternate mailbox
+
+## Authenticating to Ccs
+
+Ccs uses an API key for authentication. To acquire an API key:
+
+* log onto Ccs
+* click Settings -> access management -> API Keys
+* click Add API Key
+* enter a name for your key
+* click Save
+
+Immediately copy the API key (you only have one chance to copy the key. Once you leave the screen, you can not access it again). When running a Ccs compatible script for the first time, you will be prompted for a password. Enter the API key as the password.
---
+++ ccs/powershell/restoreCcsM365Calendar/restoreCcsM365Calendar.ps1
@@ -0,0 +1,223 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$username = 'DMaaS',
+    [Parameter()][array]$mailboxName,  # optional names of mailboxes protect
+    [Parameter()][string]$mailboxList = '',  # optional textfile of mailboxes to protect
+    [Parameter()][datetime]$recoverDate,
+    [Parameter()][string]$targetSource,
+    [Parameter()][string]$targetMailbox,
+    [Parameter()][switch]$listDates,
+    [Parameter()][string]$folderPrefix = 'restore',
+    [Parameter()][int]$pageSize = 1000,
+    [Parameter()][switch]$useMBS
+)
+
+
+# gather list from command line params and file
+function gatherList($Param=$null, $FilePath=$null, $Required=$True, $Name='items'){
+    $items = @()
+    if($Param){
+        $Param | ForEach-Object {$items += $_}
+    }
+    if($FilePath){
+        if(Test-Path -Path $FilePath -PathType Leaf){
+            Get-Content $FilePath | ForEach-Object {$items += [string]$_}
+        }else{
+            Write-Host "Text file $FilePath not found!" -ForegroundColor Yellow
+            exit
+        }
+    }
+    if($Required -eq $True -and $items.Count -eq 0){
+        Write-Host "No $Name specified" -ForegroundColor Yellow
+        exit
+    }
+    return ($items | Sort-Object -Unique)
+}
+
+
+$objectNames = @(gatherList -Param $mailboxName -FilePath $mailboxList -Name 'mailboxes' -Required $True)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -username $username
+
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+$sessionUser = api get sessionUser
+$tenantId = $sessionUser.profiles[0].tenantId
+$regions = api get -mcmv2 dms/tenants/regions?tenantId=$tenantId
+$regionList = $regions.tenantRegionInfoList.regionId -join ','
+$selectedRegion = $null
+$selectedRegionObject = $null
+$targetMailboxName = $null
+$targetMailboxId = $null
+$targetParentId = $null
+
+foreach($objName in $objectNames){
+    $search = api get -v2 "data-protect/search/objects?searchString=$objName&regionIds=$regionList&o365ObjectTypes=kO365Exchange,kUser&isProtected=true&environments=kO365&includeTenants=true&count=$pageSize"
+    $exactMatch = $search.objects | Where-Object name -eq $objName
+    if(! $exactMatch){
+        Write-Host "$objName not found" -ForegroundColor Yellow
+    }else{
+        $x = 0
+        foreach($result in $exactMatch | Where-Object {$_.name -eq $objName}){
+            $x += 1
+            foreach($objectProtectionInfo in $result.objectProtectionInfos){
+                
+                $objectId = $objectProtectionInfo.objectId
+                $objectRegionId = $objectProtectionInfo.regionId
+                if($selectedRegion -eq $null){
+                    $selectedRegion = $objectRegionId
+                    $selectedRegionObject = $objName
+                    if($targetSource -and !$targetMailboxId){
+                        if(!$targetMailbox){
+                            Write-Host "-targetMailbox is required" -ForegroundColor Yellow
+                            exit
+                        }
+                        $rootSource = api get "protectionSources/rootNodes?environments=kO365" -region $objectRegionId | Where-Object {$_.protectionSource.name -eq $targetSource}
+                        if(!$rootSource){
+                            Write-Host "$targetSource not found" -ForegroundColor Yellow
+                            exit
+                        }
+                        $targetParentId = $rootSource[0].protectionSource.id
+                        $source = api get "protectionSources?id=$($rootSource[0].protectionSource.id)&excludeOffice365Types=kMailbox,kUser,kGroup,kSite,kPublicFolder,kTeam,kO365Exchange,kO365OneDrive,kO365Sharepoint&allUnderHierarchy=false" -region $objectRegionId
+                        $usersNode = $source.nodes | Where-Object {$_.protectionSource.name -eq 'Users'}
+                        $users = api get "protectionSources?pageSize=$pageSize&nodeId=$($usersNode.protectionSource.id)&id=$($usersNode.protectionSource.id)&hasValidMailbox=true&allUnderHierarchy=false" -region $objectRegionId
+                        while(1){
+                            foreach($node in $users.nodes){
+                                if($node.protectionSource.name -eq $targetMailbox){
+                                    $targetMailboxName = $node.protectionSource.name
+                                    $targetMailboxId = $node.protectionSource.id
+                                    break
+                                }
+                            }
+                            $cursor = $users.nodes[-1].protectionSource.id
+                            $users = api get "protectionSources?pageSize=$pageSize&nodeId=$($usersNode.protectionSource.id)&id=$($usersNode.protectionSource.id)&hasValidMailbox=true&allUnderHierarchy=false&afterCursorEntityId=$cursor" -region $objectRegionId
+                            if(!$users.PSObject.Properties['nodes'] -or $users.nodes.Count -eq 1){
+                                break
+                            }
+                        }
+                        if(!$targetMailboxId){
+                            Write-Host "$targetMailbox not found" -ForegroundColor Yellow
+                            exit
+                        }
+                    }
+                }else{
+                    if($objectRegionId -ne $selectedRegion){
+                        Write-Host "$objName is in a different region than $selectedRegionObject and must be restored separately" -ForegroundColor Yellow
+                        continue
+                    }
+                }
+                if($useMBS){
+                    $snapshots = api get -v2 "data-protect/objects/$objectId/snapshots?snapshotActions=RecoverMailboxCSM&objectActionKeys=kO365ExchangeCSM&regionId=$objectRegionId"
+                }else{
+                    $snapshots = api get -v2 "data-protect/objects/$objectId/snapshots?objectActionKeys=kO365Exchange&regionId=$objectRegionId"
+                }
+                
+                $snapshots = $snapshots.snapshots | Sort-Object -Property runStartTimeUsecs -Descending
+                if($snapshots -and $snapshots.Count -gt 0){
+                    if($listDates){
+                        Write-Host "RecoverDates Available:"
+                        $snapshots | Format-Table -Property @{label="Recovery Date"; expression={usecsToDate $_.runStartTimeUsecs}}
+                        exit
+                    }
+                    if($recoverDate){
+                        $recoverDateUsecs = dateToUsecs ($recoverDate.AddMinutes(1))
+                    
+                        $snapshots = $snapshots | Where-Object runStartTimeUsecs -lt $recoverDateUsecs
+                        if($snapshots -and $snapshots.Count -gt 0){
+                            $snapshot = $snapshots[0]
+                            $snapshotId = $snapshot.id
+                        }else{
+                            Write-Host "No snapshots available for $objName"
+                        }
+                    }else{
+                        $snapshot = $snapshots[0]
+                        $snapshotId = $snapshot.id
+                    }
+                    $calendarQuery = @{
+                        "objectType" = "Emails";
+                        "emailParams" = @{
+                            "types" = @(
+                                "Folder"
+                            );
+                            "folderNames" = @(
+                                "Calendar"
+                            );
+                            "o365Params" = @{
+                                "domainIds" = @();
+                                "mailboxIds" = @(
+                                    $objectId
+                                )
+                            }
+                        }
+                    }
+                    $calendarSearch = api post -v2 "data-protect/search/indexed-objects?regionId=$objectRegionId" $calendarQuery
+                    $calendar = $calendarSearch.emails | Where-Object {$_.folderName -eq 'Calendar'}
+                    if($calendar){
+                        $calendarId = $calendar[0].id
+                    }else{
+                        Write-Host "No calendar for $objName" -ForegroundColor Yellow
+                        continue
+                    }
+                    
+                    $dateString = Get-Date -UFormat '%b_%d_%Y_%H-%M%p'
+                    $restoreParams = @{
+                        "name" = "Recover_Mailboxes_$dateString";
+                        "snapshotEnvironment" = "kO365";
+                        "office365Params" = @{
+                            "recoveryAction" = "RecoverMailbox";
+                            "recoverMailboxParams" = @{
+                                "continueOnError" = $true;
+                                "objects" = @(
+                                    @{
+                                        "mailboxParams" = @{
+                                            "recoverFolders" = @(
+                                                @{
+                                                    "key" = [Int]$calendarId;
+                                                    "recoverEntireFolder" = $true;
+                                                    "itemIds" = @()
+                                                }
+                                            );
+                                            "recoverEntireMailbox" = $False
+                                        };
+                                        "ownerInfo" = @{
+                                            "snapshotId" = $snapshotId
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                    if($useMBS){
+                        $restoreParams.office365Params.recoveryAction = "RecoverMailboxCSM"
+                    }
+                    if($targetSource){
+                        Write-Host "Restoring $objName to $targetMailboxName ($($folderPrefix)-$($objName))"
+                        $restoreParams.office365Params.recoverMailboxParams['targetMailbox'] = @{
+                            "targetFolderPath" = "$($folderPrefix)-$($objName)";
+                            "id" = [int64]$targetMailboxId;
+                            "name" = "$targetMailboxName";
+                            "parentSourceId" = [int64]$targetParentId
+                        }
+                    }else{
+                        Write-Host "Restoring $objName"
+                    }
+                    # $restoreParams | toJson
+                    $null = api post -v2 "data-protect/recoveries?regionId=$objectRegionId" $restoreParams
+                }else{
+                    Write-Host "No snapshots available for $objName"
+                }
+            }
+        }
+        if($x -eq 0){
+            Write-Host "$objName not found" -ForegroundColor Yellow
+        }
+    }
+}
