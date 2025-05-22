532145e031b11cf5ebf914009c3b8a5cd82bc8fc
bseltz-cohesity <bseltzer@cohesity.com>
2025-05-22 13:42:34 -0400

    2025-05-22

--- reports/python/physicalBackupPathsHistoryReport/physicalBackupPathsHistoryReport.py
+++ reports/python/physicalBackupPathsHistoryReport/physicalBackupPathsHistoryReport.py
@@ -6,6 +6,7 @@ from pyhesity import *
 import os
 import codecs
 import argparse
+from time import sleep
 
 # command line arguments
 parser = argparse.ArgumentParser()
@@ -74,61 +75,77 @@ f = codecs.open(outfile, 'w')
 f.write('"Cluster","Protection Group","Start Time","End Time","Status","Server","Directive File","Selected Path","Confirmed"\n')
 
 
-def getCluster():
+def getCluster(thiscluster):
 
-    cluster = api('get', 'cluster')
+    gotcluster = False
+    while gotcluster is False:
+        x = 0
+        cluster = api('get', 'cluster')
+        if cluster is None or 'name' not in cluster or cluster['name'] is None:
+            print('waiting for cluster connection...')
+            sleep(30)
+            x += 1
+            if x >= 10:
+                print('failed to connect to cluster %s' % thiscluster)
+                return None
+        else:
+            gotcluster = True
+    
     print('\n%s' % cluster['name'])
-    jobs = api('get', 'data-protect/protection-groups?environments=kPhysical&isActive=true&isDeleted=false&includeTenants=true', v=2)
+    try:
+        jobs = api('get', 'data-protect/protection-groups?environments=kPhysical&isActive=true&isDeleted=false&includeTenants=true', v=2)
 
-    if jobs is not None and 'protectionGroups' in jobs and jobs['protectionGroups'] is not None:
-        for job in sorted(jobs['protectionGroups'], key=lambda job: job['name'].lower()):
-            if job['physicalParams']['protectionType'] == 'kFile':
-                v1JobId = job['id'].split(':')[2]
-                thisJob = api('get', 'protectionJobs/%s' % v1JobId)
-                startTimeUsecs = daysBackUsecs
-                confirmStartTimeUsecs = startTimeUsecs
-                if thisJob is not None and 'modificationTimeUsecs' in thisJob and thisJob['modificationTimeUsecs'] is not None and thisJob['modificationTimeUsecs'] > startTimeUsecs:
-                    confirmStartTimeUsecs = thisJob['modificationTimeUsecs']
-                    print('  %s:  *** modified on %s ***' % (job['name'], usecsToDate(thisJob['modificationTimeUsecs'])))
-                else:
-                    print('  %s' % job['name'])
-                runs = api('get', 'data-protect/protection-groups/%s/runs?numRuns=%s&includeTenants=true&startTimeUsecs=%s&includeObjectDetails=true' % (job['id'], numruns, startTimeUsecs), v=2)
-                countedRuns = 0
-                if len(runs['runs']) > 0:
-                    for run in runs['runs']:
-                        countedRuns += 1
-                        confirmed = True
-                        if run['localBackupInfo']['startTimeUsecs'] < confirmStartTimeUsecs:
-                            confirmed = False
-                        if 'isLocalSnapshotsDeleted' not in run or run['isLocalSnapshotsDeleted'] is not True:
-                            runStartTime = usecsToDate(run['localBackupInfo']['startTimeUsecs'])
-                            runEndTime = '-'
-                            if 'endTimeUsecs' in run['localBackupInfo']:
-                                runEndTime = usecsToDate(run['localBackupInfo']['endTimeUsecs'])
-                            if 'objects' in job['physicalParams']['fileProtectionTypeParams'] and job['physicalParams']['fileProtectionTypeParams']['objects'] is not None and len(job['physicalParams']['fileProtectionTypeParams']['objects']) > 0:
-                                for obj in sorted(job['physicalParams']['fileProtectionTypeParams']['objects'], key=lambda obj: obj['name'].lower()):
-                                    if len(servernames) == 0 or obj['name'].lower() in [s.lower() for s in servernames]:
-                                        status = run['localBackupInfo']['status']
-                                        runobject = [o for o in run['objects'] if o['object']['id'] == obj['id']]
-                                        if runobject is not None and len(runobject) > 0:
-                                            status = runobject[0]['localSnapshotInfo']['snapshotInfo']['status'][1:]
-                                            usesDirective = False
-                                            if 'metadataFilePath' in obj and obj['metadataFilePath'] is not None:
-                                                usesDirective = True
-                                                f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, obj['metadataFilePath'], confirmed))
-                                            elif 'filePaths' in obj and obj['filePaths'] is not None and len(obj['filePaths']) > 0:
-                                                for filepath in sorted(obj['filePaths'], key=lambda filepath: filepath['includedPath'].lower()):
-                                                    f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, filepath['includedPath'], confirmed))
+        if jobs is not None and 'protectionGroups' in jobs and jobs['protectionGroups'] is not None:
+            for job in sorted(jobs['protectionGroups'], key=lambda job: job['name'].lower()):
+                if job['physicalParams']['protectionType'] == 'kFile':
+                    v1JobId = job['id'].split(':')[2]
+                    thisJob = api('get', 'protectionJobs/%s' % v1JobId)
+                    startTimeUsecs = daysBackUsecs
+                    confirmStartTimeUsecs = startTimeUsecs
+                    if thisJob is not None and 'modificationTimeUsecs' in thisJob and thisJob['modificationTimeUsecs'] is not None and thisJob['modificationTimeUsecs'] > startTimeUsecs:
+                        confirmStartTimeUsecs = thisJob['modificationTimeUsecs']
+                        print('  %s:  *** modified on %s ***' % (job['name'], usecsToDate(thisJob['modificationTimeUsecs'])))
+                    else:
+                        print('  %s' % job['name'])
+                    runs = api('get', 'data-protect/protection-groups/%s/runs?numRuns=%s&includeTenants=true&startTimeUsecs=%s&includeObjectDetails=true' % (job['id'], numruns, startTimeUsecs), v=2)
+                    countedRuns = 0
+                    if len(runs['runs']) > 0:
+                        for run in runs['runs']:
+                            countedRuns += 1
+                            confirmed = True
+                            if run['localBackupInfo']['startTimeUsecs'] < confirmStartTimeUsecs:
+                                confirmed = False
+                            if 'isLocalSnapshotsDeleted' not in run or run['isLocalSnapshotsDeleted'] is not True:
+                                runStartTime = usecsToDate(run['localBackupInfo']['startTimeUsecs'])
+                                runEndTime = '-'
+                                if 'endTimeUsecs' in run['localBackupInfo']:
+                                    runEndTime = usecsToDate(run['localBackupInfo']['endTimeUsecs'])
+                                if 'objects' in job['physicalParams']['fileProtectionTypeParams'] and job['physicalParams']['fileProtectionTypeParams']['objects'] is not None and len(job['physicalParams']['fileProtectionTypeParams']['objects']) > 0:
+                                    for obj in sorted(job['physicalParams']['fileProtectionTypeParams']['objects'], key=lambda obj: obj['name'].lower()):
+                                        if len(servernames) == 0 or obj['name'].lower() in [s.lower() for s in servernames]:
+                                            status = run['localBackupInfo']['status']
+                                            runobject = [o for o in run['objects'] if o['object']['id'] == obj['id']]
+                                            if runobject is not None and len(runobject) > 0:
+                                                status = runobject[0]['localSnapshotInfo']['snapshotInfo']['status'][1:]
+                                                usesDirective = False
+                                                if 'metadataFilePath' in obj and obj['metadataFilePath'] is not None:
+                                                    usesDirective = True
+                                                    f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, obj['metadataFilePath'], confirmed))
+                                                elif 'filePaths' in obj and obj['filePaths'] is not None and len(obj['filePaths']) > 0:
+                                                    for filepath in sorted(obj['filePaths'], key=lambda filepath: filepath['includedPath'].lower()):
+                                                        f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, filepath['includedPath'], confirmed))
+                                                else:
+                                                    f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, 'unknown', confirmed))
+                                                    print('******* debug output ********')
+                                                    display(obj)
+                                                    print('*****************************')
                                             else:
-                                                f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], usesDirective, 'unknown', confirmed))
-                                                print('******* debug output ********')
-                                                display(obj)
-                                                print('*****************************')
-                                        else:
-                                            f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], 'unknown', 'unknown', confirmed))
-                if countedRuns == 0:
-                    print('      ***** no runs reported since modification date *****')
-
+                                                f.write('"%s","%s","%s","%s","%s","%s","%s","%s","%s"\n' % (cluster['name'], job['name'], runStartTime, runEndTime, status, obj['name'], 'unknown', 'unknown', confirmed))
+                    if countedRuns == 0:
+                        print('      ***** no runs reported since modification date *****')
+    except Exception:
+        print('failed to get data from cluster %s' % thiscluster)
+        return None
 
 for vip in vips:
 
@@ -148,9 +165,9 @@ for vip in vips:
             heliosCluster(clustername)
             if LAST_API_ERROR() != 'OK':
                 continue
-            getCluster()
+            getCluster(clustername)
     else:
-        getCluster()
+        getCluster(vip)
 
 
 f.close()
