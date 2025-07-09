f18ddff45693e391c93539034fbc40f5fdb7a292
bseltz-cohesity <bseltzer@cohesity.com>
2025-07-09 12:39:28 -0400

    2025-07-09

---
+++ ccs/powershell/legalHoldCCS/README.md
@@ -0,0 +1,68 @@
+# Add/Remove Legal Hold from CCS Objects using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script adds or removes legal hold from CCS backups.
+
+Note: this script is a work in progress. As of now, it will add/remove legal hold on M365 mailboxes and onedrive objects only.
+
+## Download the script
+
+Run these commands from PowerShell to download the script(s) into your current directory
+
+```powershell
+# Download Commands
+$scriptName = 'legalHoldCCS'
+$repoURL = 'https://raw.githubusercontent.com/cohesity/community-automation-samples/main'
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/ccs/powershell/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/powershell/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* [legalHoldCCS.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/ccs/powershell/legalHoldCCS/legalHoldCCS.ps1): the main powershell script
+* [cohesity-api.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/cohesity-api/cohesity-api.ps1): the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+Add legal hold to M365 mailboxes backed up on 2025-07-08:
+
+```powershell
+./legalHoldCCS.ps1 -date '2025-07-08' -addHold
+```
+
+Add legal hold to M365 onedrives backed up on 2025-07-08:
+
+```powershell
+./legalHoldCCS.ps1 -date '2025-07-08' -addHold -objectType onedrive
+```
+
+Show onedrives with legal hold enabled:
+
+```powershell
+./legalHoldCCS.ps1 -date '2025-07-08' -objectType onedrive -showTrue
+```
+
+## Parameters
+
+* -username: (optional) used for password storage only (default is 'ccs')
+* -password: (optional) enter API key (will be prompted if omitted and not already stored)
+* -noPrompt: (optional) do not prompt for password
+* -date: date of backups e.g. '2025-07-08'
+* -addHold: (optional) add legal hold
+* -removeHold: (optional) remove legal hold
+* -showTrue: (optional) show objects that are on legal hold
+* -showFalse: (optional) show objects that are not on legal hold
+
+## Authenticating to Ccs
+
+Ccs uses an API key for authentication. To acquire an API key:
+
+* log onto Ccs
+* click Settings -> access management -> API Keys
+* click Add API Key
+* enter a name for your key
+* click Save
+
+Immediately copy the API key (you only have one chance to copy the key. Once you leave the screen, you can not access it again). When running a Ccs compatible script for the first time, you will be prompted for a password. Enter the API key as the password.
---
+++ ccs/powershell/legalHoldCCS/legalHoldCCS.ps1
@@ -0,0 +1,113 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$username = 'ccs',
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][ValidateSet('mailbox','onedrive')][string]$objectType = 'mailbox',
+    [Parameter()][string]$date,
+    [Parameter()][switch]$addHold,
+    [Parameter()][switch]$removeHold,
+    [Parameter()][switch]$showTrue,
+    [Parameter()][switch]$showFalse
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -username $username -passwd $password -noPromptForPassword $noPrompt # -regionid $region 
+
+$startDate = dateToUsecs $date
+
+if(! $startDate -or $startDate -eq 0){
+    Write-Host "Invalid date specified. Should be in format '2025-07-09'" -ForegroundColor Yellow
+    exit 1
+}
+$endDate = $startDate + 86400000000
+
+$queryParams = @{
+    "statsParams" = @{
+        "attributes" = @(
+            "Status";
+            "ActivityType"
+        )
+    };
+    "fromTimeUsecs" = $startDate;
+    "toTimeUsecs" = $endDate;
+    "environments" = @(
+        "kO365"
+    );
+    "archivalRunParams" = @{
+        "protectionEnvironmentTypes" = @(
+            "kO365Exchange";
+            "kO365ExchangeCSM"
+        )
+    };
+    "backupRunParams" = @{
+        "protectionEnvironmentTypes" = @(
+            "kO365Exchange";
+            "kO365ExchangeCSM"
+        )
+    };
+    "activityTypes" = @(
+        "ArchivalRun";
+        "BackupRun"
+    );
+    "excludeStats" = $true
+}
+
+if($objectType -eq 'onedrive'){
+    $queryParams.archivalRunParams.protectionEnvironmentTypes = @("kO365OneDrive","kO365OneDriveCSM")
+    $queryParams.backupRunParams.protectionEnvironmentTypes = @("kO365OneDrive","kO365OneDriveCSM")
+}
+
+$sessionUser = api get sessionUser
+$tenantId = $sessionUser.profiles[0].tenantId
+$regions = api get -mcmv2 dms/tenants/regions?tenantId=$tenantId
+$regionList = $regions.tenantRegionInfoList.regionId -join ','
+
+$activities = api post -mcmv2 "data-protect/objects/activity?regionIds=$($regionList)" $queryParams
+
+foreach($activity in $activities.activity | Sort-Object -Property {$_.object.name}){
+    $objectId = $activity.object.id
+    $startTimeUsecs = $activity.timestampUsecs
+    if($addHold -and $activity.archivalRunParams.onLegalHold -eq $False){
+        $holdParams =  @{
+            "targetObjectRuns" = @(
+                @{
+                    "id" = "$objectId";
+                    "runStartTimeUsecs" = $startTimeUsecs
+                }
+            );
+            "environment" = "kO365";
+            "legalHold" = "Enable"
+        }
+        Write-Host "Adding legal hold to $($activity.object.name) ($(usecsToDate $startTimeUsecs))"
+        $result = api put -mcmv2 "data-protect/objects/runs/metadata?regionIds=$($activity.regionId)" $holdParams
+    }elseif($removeHold -and $activity.archivalRunParams.onLegalHold -eq $True){
+        $holdParams =  @{
+            "targetObjectRuns" = @(
+                @{
+                    "id" = "$objectId";
+                    "runStartTimeUsecs" = $startTimeUsecs
+                }
+            );
+            "environment" = "kO365";
+            "legalHold" = "Release"
+        }
+        Write-Host "Removing legal hold from $($activity.object.name) ($(usecsToDate $startTimeUsecs))"
+        $result = api put -mcmv2 "data-protect/objects/runs/metadata?regionIds=$($activity.regionId)" $holdParams
+    }else{
+        $showMe = $True
+        if($showFalse -and $activity.archivalRunParams.onLegalHold -eq $True){
+            $showMe = $False
+        }
+        if($showTrue -and $activity.archivalRunParams.onLegalHold -eq $False){
+            $showMe = $False
+        }
+        if($showMe -eq $True){
+            Write-Host "$($activity.object.name) ($(usecsToDate $startTimeUsecs)) on hold = $($activity.archivalRunParams.onLegalHold)"
+        }       
+    }
+}
