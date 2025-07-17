c9e18362b115752cc51909823ffb4a083a01b114
bseltz-cohesity <bseltzer@cohesity.com>
2025-07-17 09:02:22 -0400

    2025-07-17

---
+++ python/backedUpFileListJSON/README.md
@@ -0,0 +1,87 @@
+# Backup Up File List JSON for Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This Python script enumerates the files that are available for restore from the specified server/job. The file list is written to stdout as JSON.
+
+## Download the script
+
+Run these commands from a terminal to download the script(s) into your current directory
+
+```bash
+# Begin download commands
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/backedUpFileListJSON/backedUpFileListJSON.py
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity.py
+chmod +x backedUpFileListJSON.py
+# End download commands
+```
+
+## Components
+
+* [backedUpFileListJSON.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/backedUpFileListJSON/backedUpFileListJSON.py): the main python script
+* [pyhesity.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity/pyhesity.py): the Cohesity REST API helper module
+
+Place all files in a folder together. then, run the main script like so:
+
+To list what versions are available:
+
+```bash
+./backedUpFileListJSON.py -v mycluster \
+                          -u myuser \
+                          -d mydomain.net \
+                          -s server1.mydomain.net \
+                          -j 'My Backup Job' \
+                          -l
+```
+
+To use a specific job run ID:
+
+```bash
+./backedUpFileListJSON.py -v mycluster \
+                          -u myuser \
+                          -d mydomain.net \
+                          -s server1.mydomain.net \
+                          -j 'My Backup Job' \
+                          -r 123456
+```
+
+To choose the backup at or after the specified file date:
+
+```bash
+./backedUpFileListJSON.py -v mycluster \
+                          -u myuser \
+                          -d mydomain.net \
+                          -s server1.mydomain.net \
+                          -j 'My Backup Job' \
+                          -f '2020-06-30 13:00:00'
+```
+
+## Authentication Parameters
+
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+
+## Mandatory Parameters
+
+* -s, --sourceserver: name of server to inspect (repeat for multiple)
+* -j, --jobname: name of protection job to run
+
+## Other Parameters
+
+* -l, --showversions: (optional) show available versions
+* -t, --start: (optional) filter on versions after date
+* -e, --end: (optional) filter on versions before date
+* -r, --runid: (optional) use specific run ID
+* -f, --filedate: (optional) date to inspect (next backup after date will be inspected)
+* -p, --startpath: (optional) start listing files at path (default is /)
+* -n, --noindex: (optional) do not use the index (otherwise index usage will be automatic)
+* -x, --forceindex: (optional) force use the index (otherwise index usage will be automatic)
+* -nt, --newerthan: (optional) show files added/modified in the last X days
+* -ext, --extension: (optional) only show files that end with the specified extension
---
+++ python/backedUpFileListJSON/backedUpFileListJSON.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python
+"""backed up files list for python"""
+
+# version 2025.01.23
+
+# import pyhesity wrapper module
+from pyhesity import *
+from datetime import datetime
+from sys import exit
+import codecs
+import argparse
+
+try:
+    from urllib.parse import quote_plus
+except Exception:
+    from urllib import quote_plus
+
+# command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-s', '--sourceserver', type=str, action='append')  # name of source server
+parser.add_argument('-j', '--jobname', type=str, required=True)       # narrow search by job name
+parser.add_argument('-l', '--showversions', action='store_true')      # show available snapshots
+parser.add_argument('-k', '--listfiles', action='store_true')         # show fils in snapshots
+parser.add_argument('-t', '--start', type=str, default=None)          # show snapshots after date
+parser.add_argument('-e', '--end', type=str, default=None)            # show snapshots before date
+parser.add_argument('-r', '--runid', type=int, default=None)          # choose specific job run id
+parser.add_argument('-f', '--filedate', type=str, default=None)       # show snapshots after date
+parser.add_argument('-p', '--startpath', type=str, default='/')       # show files under this path
+parser.add_argument('-n', '--noindex', action='store_true')           # do not use librarian
+parser.add_argument('-x', '--forceindex', action='store_true')           # do not use librarian
+# parser.add_argument('-ss', '--showstats', action='store_true')        # show file last modified date and size
+parser.add_argument('-nt', '--newerthan', type=int, default=0)        # show files newer than X days
+parser.add_argument('-ext', '--extension', type=str, default=None) 
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+sourceservers = args.sourceserver
+jobname = args.jobname
+showversions = args.showversions
+start = args.start
+end = args.end
+runid = args.runid
+filedate = args.filedate
+listfiles = args.listfiles
+startpath = args.startpath
+noindex = args.noindex
+showstats = True  # args.showstats
+newerthan = args.newerthan
+forceIndex = args.forceindex
+extension = args.extension
+
+responseJSON = {'error':'', 'files': [], 'fileCount': 0, 'totalBytes': 0}
+if showversions is True:
+    responseJSON = {'error':'', 'versions': []}
+
+def listdir(dirPath, instance, sourceserver, thisrunid, volumeInfoCookie=None, volumeName=None, cookie=None):
+    global responseJSON
+    # global fileCount
+    # global totalSize
+    thisDirPath = quote_plus(dirPath).replace('%2F%2F', '%2F')
+    if cookie is not None:
+        if volumeName is not None:
+            dirList = api('get', '/vm/directoryList?%s%s&statFileEntries=%s&dirPath=%s&volumeInfoCookie=%s&volumeName=%s&cookie=%s' % (instance, useLibrarian, statfile, thisDirPath, volumeInfoCookie, volumeName, cookie))
+        else:
+            dirList = api('get', '/vm/directoryList?%s%s&statFileEntries=%s&dirPath=%s&cookie=%s' % (instance, useLibrarian, statfile, thisDirPath, cookie))
+    else:
+        if volumeName is not None:
+            dirList = api('get', '/vm/directoryList?%s%s&statFileEntries=%s&dirPath=%s&volumeInfoCookie=%s&volumeName=%s' % (instance, useLibrarian, statfile, thisDirPath, volumeInfoCookie, volumeName))
+        else:
+            dirList = api('get', '/vm/directoryList?%s%s&statFileEntries=%s&dirPath=%s' % (instance, useLibrarian, statfile, thisDirPath))
+    if dirList and 'entries' in dirList:
+        for entry in sorted(dirList['entries'], key=lambda e: e['name']):
+            try:
+                if entry['type'] == 'kDirectory':
+                    listdir('%s/%s' % (dirPath, entry['name']), instance, sourceserver, thisrunid, volumeInfoCookie, volumeName)
+                else:
+                    if extension is None or entry['fullPath'].lower().endswith(extension):
+                        if statfile is True:
+                            filesize = entry['fstatInfo']['size']
+                            mtime = usecsToDate(entry['fstatInfo']['mtimeUsecs'])
+                            mtimeusecs = entry['fstatInfo']['mtimeUsecs']
+                            if newerthan == 0 or mtimeusecs > timeAgo(newerthan, 'days'):                            
+                                responseJSON['fileCount'] += 1
+                                responseJSON['totalBytes'] += filesize
+                                # fileCount += 1
+                                # totalSize += filesize
+                                responseJSON['files'].append({
+                                    'sourceServer': sourceserver,
+                                    'fullPath': entry['fullPath'],
+                                    'mtime': mtime,
+                                    'bytes': filesize,
+                                    'runId': thisrunid
+                                })
+                                # print('%s (%s) [%s bytes]' % (entry['fullPath'], mtime, filesize))
+                                # f.write('%s (%s) [%s bytes]\n' % (entry['fullPath'], mtime, filesize))
+                                # csv.write('"%s","%s","%s"\n' % (entry['fullPath'], mtime, filesize))
+                        # else:
+                        #     fileCount += 1
+                        #     print('%s' % entry['fullPath'])
+                        #     f.write('%s\n' % entry['fullPath'])
+                        #     csv.write('"%s","",""\n' % entry['fullPath'])
+            except Exception:
+                pass
+    if dirList and 'cookie' in dirList:
+        listdir('%s' % dirPath, instance, sourceserver, thisrunid, volumeInfoCookie, volumeName, dirList['cookie'])
+
+def showFiles(doc, version):
+    global useLibrarian
+    # global fileCount
+    # global totalSize
+    # filecount = 0
+    # totalSize = 0
+
+    if 'numEntriesIndexed' not in version or version['numEntriesIndexed'] == 0:
+        useLibrarian = ''  # False
+    else:
+        if 'indexingStatus' not in version or version['indexingStatus'] != 2:
+            useLibrarian = ''  # False
+        else:
+            useLibrarian = '&useLibrarian=true'
+    if forceIndex and 'indexingStatus' in version and version['indexingStatus'] == 2:
+        useLibrarian = '&useLibrarian=true'
+    if noindex:
+        useLibrarian = ''  # False
+    if 'attemptNum' in version['instanceId']:
+        attemptNum = version['instanceId']['attemptNum']
+    else:
+        attemptNum = 0
+    thisrunid = version['instanceId']['jobInstanceId']
+    instance = ("attemptNum=%s&clusterId=%s&clusterIncarnationId=%s&entityId=%s&jobId=%s&jobInstanceId=%s&jobStartTimeUsecs=%s&jobUidObjectId=%s" %
+                (attemptNum,
+                    doc['objectId']['jobUid']['clusterId'],
+                    doc['objectId']['jobUid']['clusterIncarnationId'],
+                    doc['objectId']['entity']['id'],
+                    doc['objectId']['jobId'],
+                    version['instanceId']['jobInstanceId'],
+                    version['instanceId']['jobStartTimeUsecs'],
+                    doc['objectId']['jobUid']['objectId']))
+
+    # fileDateString = datetime.strptime(usecsToDate(version['instanceId']['jobStartTimeUsecs']), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d_%H-%M-%S')
+
+    # fsourceserver = sourceserver.replace('/', '-').replace('\\', '-')
+    # f = codecs.open('backedUpFiles-%s-%s-%s.txt' % (fsourceserver, version['instanceId']['jobInstanceId'], fileDateString), 'w', 'utf-8')
+    # csv = codecs.open('backedUpFiles-%s-%s-%s.csv' % (fsourceserver, version['instanceId']['jobInstanceId'], fileDateString), 'w', 'utf-8')
+    # csv.write('"Path","Last Modified","Bytes"\n')
+    volumeTypes = [1, 6]
+    backupType = doc['backupType']
+    if backupType in volumeTypes:
+        volumeList = api('get', '/vm/volumeInfo?%s&statFileEntries=%s' % (instance, statfile))
+        if 'volumeInfos' in volumeList:
+            volumeInfoCookie = volumeList['volumeInfoCookie']
+            for volume in sorted(volumeList['volumeInfos'], key=lambda v: v['name']):
+                volumeName = quote_plus(volume['name'])
+                listdir(startpath, instance, sourceserver, thisrunid, volumeInfoCookie, volumeName)
+    else:
+        listdir(startpath, instance, sourceserver, thisrunid)
+    # if statfile is True:
+    #     unitsize = round(totalSize / 1024, 2)
+    #     unit = 'KiB'
+    #     if unitsize > 1024:
+    #         unitsize = round(unitsize / 1024, 2)
+    #         unit = 'MiB'
+    #     if unitsize > 1024:
+    #         unitsize = round(unitsize / 1024, 2)
+    #         unit = 'GiB'
+    #     if unitsize > 1024:
+    #         unitsize = round(unitsize / 1024, 2)
+    #         unit = 'TiB'
+        # print('\n%s files found (%s bytes) [%s %s]' % (fileCount, totalSize, unitsize, unit))
+    # else:
+        # print('\n%s files found' % fileCount)
+    # f.close()
+    # csv.close()
+
+
+if sourceservers is None or len(sourceservers) == 0:
+    responseJSON['error'] = '--sourceserver is required'
+    display(responseJSON)
+    # print('--sourceserver is required')
+    exit(1)
+
+if showstats is True or newerthan > 0:
+    statfile = True
+else:
+    statfile = False
+
+useLibrarian = '&useLibrarian=true'  # True
+# fileCount = 0
+# totalSize = 0
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, quiet=True)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    if clustername is not None:
+        heliosCluster(clustername)
+    else:
+        responseJSON['error'] = ('-clustername is required when connecting to Helios or MCM')
+        display(responseJSON)
+        exit(1)
+
+# exit if not authenticated
+if apiconnected() is False:
+    responseJSON['error'] = ('authentication failed')
+    display(responseJSON)
+    exit(1)
+
+for sourceserver in sourceservers:
+    # print('\n============================\n %s\n============================\n' % sourceserver)
+
+    search = api('get', '/searchvms?entityTypes=kView&entityTypes=kAcropolis&entityTypes=kAWS&entityTypes=kAWSNative&entityTypes=kAWSSnapshotManager&entityTypes=kAzure&entityTypes=kAzureNative&entityTypes=kFlashBlade&entityTypes=kGCP&entityTypes=kGenericNas&entityTypes=kHyperV&entityTypes=kHyperVVSS&entityTypes=kIsilon&entityTypes=kKVM&entityTypes=kNetapp&entityTypes=kPhysical&entityTypes=kVMware&vmName=%s' % sourceserver)
+
+    if 'vms' not in search:
+        responseJSON['error'] = ('no backups found for %s' % sourceserver)
+        # display(responseJSON)
+        continue
+
+    searchResults = [vm for vm in search['vms'] if vm['vmDocument']['objectName'].lower() == sourceserver.lower()]
+
+    if len(searchResults) == 0:
+        responseJSON['error'] = ('no backups found for %s' % sourceserver)
+        # display(responseJSON)
+        continue
+
+    altJobName = 'old name: %s' % jobname.lower()
+    altJobName2 = '%s (old name' % jobname.lower()
+    searchResults = [vm for vm in searchResults if vm['vmDocument']['jobName'].lower() == jobname.lower() or altJobName in vm['vmDocument']['jobName'].lower() or altJobName2 in vm['vmDocument']['jobName'].lower()]
+
+    if len(searchResults) == 0:
+        responseJSON['error'] = ('%s not protected by %s' % (sourceserver, jobname))
+        # display(responseJSON)
+        continue
+
+    searchResults = [r for r in searchResults if 'versions' in r['vmDocument'] and len(r['vmDocument']['versions']) > 0]
+
+    if len(searchResults) == 0:
+        responseJSON['error'] = ('No backups available for %s in %s' % (sourceserver, jobname))
+        # display(responseJSON)
+        continue
+
+    allVersions = []
+    for searchResult in searchResults:
+        for version in searchResult['vmDocument']['versions']:
+            version['doc'] = searchResult['vmDocument']
+            allVersions.append(version)
+    allVersions = sorted(allVersions, key=lambda r: r['snapshotTimestampUsecs'], reverse=True)
+
+    if showversions or start is not None or end is not None or listfiles:
+        if start is not None:
+            startusecs = dateToUsecs(start)
+            allVersions = [v for v in allVersions if startusecs <= v['snapshotTimestampUsecs']]
+        if end is not None:
+            endusecs = dateToUsecs(end)
+            allVersions = [v for v in allVersions if endusecs >= v['snapshotTimestampUsecs']]
+        if listfiles:
+            for version in allVersions:
+                # print("\n==============================")
+                # print("   runId: %s" % version['instanceId']['jobInstanceId'])
+                # print(" runDate: %s" % usecsToDate(version['instanceId']['jobStartTimeUsecs']))
+                # print("==============================\n")
+                showFiles(version['doc'], version)
+        else:
+            # print('%10s  %s' % ('runId', 'runDate'))
+            # print('%10s  %s' % ('-----', '-------'))
+            for version in allVersions:
+                responseJSON['versions'].append({
+                    'runId': version['instanceId']['jobInstanceId'],
+                    'startTime': usecsToDate(version['instanceId']['jobStartTimeUsecs'])
+                })
+                # print('%10d  %s' % (version['instanceId']['jobInstanceId'], usecsToDate(version['instanceId']['jobStartTimeUsecs'])))
+        continue
+
+    # select version
+    if runid is not None:
+        # select version with matching runId
+        versions = [v for v in allVersions if runid == v['instanceId']['jobInstanceId']]
+        if len(versions) == 0:
+            responseJSON['error'] = ('Run ID not found')
+            # display(responseJSON)
+            continue
+        else:
+            version = versions[0]
+            showFiles(version['doc'], version)
+    elif filedate is not None:
+        # select version just after requested date
+        filedateusecs = dateToUsecs(filedate)
+        versions = [v for v in allVersions if filedateusecs <= v['snapshotTimestampUsecs']]
+        if versions:
+            version = versions[-1]
+            showFiles(version['doc'], version)
+        else:
+            responseJSON['error'] = ('No backups from the specified date')
+            # display(responseJSON)
+            continue
+    else:
+        # just use latest version
+        version = allVersions[0]
+        showFiles(version['doc'], version)
+
+display(responseJSON)
