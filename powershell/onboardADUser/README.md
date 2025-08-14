f98f8604a91dceeebcd0149fefb1aee34072873c
bseltz-cohesity <bseltzer@cohesity.com>
2025-08-14 10:51:26 -0400

    2025-08-14

---
+++ powershell/onboardADUser/README.md
@@ -0,0 +1,84 @@
+# Onboard Active Directory Users and Groups using PowerShell
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This powershell script adds or removes Active Directory Users and Groups from Access Management, and can also generate an API key for a user.
+
+## Download the scripts
+
+Run these commands from PowerShell to download the script(s) into your current directory.
+
+```powershell
+# Download Commands
+$scriptName = 'onboardADUser'
+$repoURL = 'https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell'
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/$scriptName/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+(Invoke-WebRequest -UseBasicParsing -Uri "$repoUrl/cohesity-api/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+# End Download Commands
+```
+
+## Components
+
+* [onboardADUser.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/onboardADUser/onboardADUser.ps1): the main PowerShell script
+* [cohesity-api.ps1](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/powershell/cohesity-api/cohesity-api.ps1): the Cohesity REST API helper module
+
+## Examples
+
+To add a user or group:
+
+```powershell
+./onboardADUser.ps1 -vip mycluster `
+                    -username myuser `
+                    -domain mydomain.net `
+                    -principalName myADuser `
+                    -adDomain mydomain.net `
+                    -role operator, viewer
+```
+
+To remove a user:
+
+```powershell
+./onboardADUser.ps1 -vip mycluster `
+                    -username myuser `
+                    -domain mydomain.net `
+                    -principalName myADuser `
+                    -adDomain mydomain.net `
+                    -remove
+```
+
+## Authentication Parameters
+
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -emailMfaCode: (optional) send MFA code via email
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
+* -adDomain: Active Directory FQDN
+* -principalName: (optional) one or more user/group names (comma separated)
+* -principalList: (optional) text file of user/group names (one per line)
+* -role: (optional) one or more role names/labels (comma separated)
+* -remove: (optional) remove the specified user/group
+* -generateApiKey: (optional) generate new API key for user (not applicable for groups)
+
+## Authenticating to Helios
+
+The Test/Wrapper scripts can be configured to log onto clusters directly, or log onto Helios.
+
+Helios uses an API key for authentication. To acquire an API key:
+
+* log onto Helios
+* click the gear icon -> access management -> API Keys
+* click Add API Key
+* enter a name for your key
+* click Save
+
+Immediately copy the API key (you only have one chance to copy the key. Once you leave the screen, you can not access it again). When running a Helios compatible script for the first time, you will be prompted for a password. Enter the API key as the password.
---
+++ powershell/onboardADUser/onboardADUser.ps1
@@ -0,0 +1,166 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$mcm,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][switch]$emailMfaCode,
+    [Parameter()][string]$clusterName,
+    [Parameter(Mandatory=$True)][string]$adDomain,
+    [Parameter()][array]$principalName,
+    [Parameter()][string]$principalList,
+    [Parameter()][array]$role,
+    [Parameter()][switch]$remove,
+    [Parameter()][switch]$generateApiKey
+)
+
+# gather list from command line params and file
+function gatherList($Param=$null, $FilePath=$null, $Required=$True, $Name='items'){
+    $items = @()
+    if($Param){
+        $Param | ForEach-Object {$items += $_}
+    }
+    if($FilePath){
+        if(Test-Path -Path $FilePath -PathType Leaf){
+            Get-Content $FilePath | ForEach-Object {$items += [string]$_}
+        }else{
+            Write-Host "Text file $FilePath not found!" -ForegroundColor Yellow
+            exit
+        }
+    }
+    if($Required -eq $True -and $items.Count -eq 0){
+        Write-Host "No $Name specified" -ForegroundColor Yellow
+        exit
+    }
+    return ($items | Sort-Object -Unique)
+}
+
+$principalNames = @(gatherList -Param $principalName -FilePath $principalList -Name 'principals' -Required $True)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authentication =============================================
+# demand clusterName for Helios/MCM
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios/MCM" -ForegroundColor Yellow
+    exit 1
+}
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $mcm -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios/mcm managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
+
+# get roles
+$roles = api get "roles"
+$myRoles = @()
+foreach($roleName in $role){
+    $thisRole = $roles | Where-Object {$_.name -eq $roleName -or $_.label -eq $roleName}
+    if(!$thisRole){
+        Write-Host "Role $roleName not found" -ForegroundColor Yellow
+        exit 1
+    }
+    $myRoles = @($myRoles + $thisRole)
+}
+
+# get active directories
+$ads = api get activeDirectory
+$ad = $ads | Where-Object {$_.domainName -eq $adDomain}
+if(! $ad){
+    Write-Host "AD Domain $adDomain not found" -ForegroundColor Yellow
+    exit 1
+}
+$users = api get "users"
+$groups = api get "groups"
+
+foreach($thisPrincipal in $principalNames){
+    # find principal
+    $existingUser = $users | Where-Object {$_.username -eq $thisPrincipal -and $_.domain -eq $adDomain}
+    $existingGroup = $groups | Where-Object {$_.name -eq $thisPrincipal -and $_.domain -eq $adDomain}
+    if(!$remove){
+        if(!($existingUser -or $existingGroup)){
+            if($role -eq $null -or $role.Count -eq 0){
+                Write-Host "At least one role is required" -ForegroundColor Yellow
+                exit 1
+            }
+            $search = api get "activeDirectory/principals?domain=$($ad.domainName)&includeComputers=true&search=$thisPrincipal"
+            $principal = $search | Where-Object {$_.principalName -eq $thisPrincipal}
+            if(! $principal){
+                Write-Host "AD principal $thisPrincipal not found in AD domain $adDomain" -ForegroundColor Yellow
+                continue
+            }
+            $newPrincipalParams = @(@{
+                "principalName" = $principal.principalName;
+                "objectClass" = $principal.objectClass;
+                "roles" = @($myRoles.name);
+                "domain" = $principal.domain;
+                "restricted" = $false
+            })
+            Write-Host "Adding $($ad.domainName)/$($principal.principalName)"
+            $existingUser = api post "activeDirectory/principals" $newPrincipalParams
+        }else{
+            Write-Host "$($ad.domainName)/$($thisPrincipal) already exists"
+        }
+        if($generateApiKey){
+            if($existingUser){
+                if($existingUser.PSObject.Properties['username']){
+                    $keyName = $existingUser.username
+                }else{
+                    $keyName = $existingUser.principalName
+                }
+                $newKeyParams = @{
+                    'isActive' = $True;
+                    'user' = $existingUser;
+                    'name' = "$($keyName)-$((dateToUsecs) / 1000000)"
+                }
+                $newKey = api post "users/$($existingUser.sid)/apiKeys/" $newKeyParams
+                Write-Host "$($ad.domainName)/$($thisPrincipal) new API key: $($newKey.key)"
+            }elseif($existingGroup){
+                Write-Host "It's not possible to create an API key for a group" -ForegroundColor Yellow
+                continue
+            }
+        }
+    }else{
+        if($existingUser){
+            Write-Host "Removing $($ad.domainName)/$thisPrincipal"
+            $deleteParams = @{
+                "domain" = $ad.domainName;
+                "users" = @(
+                    $existingUser.username
+                )
+            }
+            $null = api delete users $deleteParams
+        }elseif($existingGroup){
+            Write-Host "Removing $($ad.domainName)/$thisPrincipal"
+            $deleteParams = @{
+                "domain" = $ad.domainName;
+                "names" = @(
+                    $existingGroup.name
+                )
+            }
+            $null = api delete groups $deleteParams
+        }else{
+            Write-Host "$($ad.domainName)/$thisPrincipal not found"
+        }
+    }
+}
