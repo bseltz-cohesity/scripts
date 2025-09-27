a02e364ac568c0d292745c077803867f4bd1da79
bseltz-cohesity <bseltzer@cohesity.com>
2025-09-26 21:16:26 -0400

    2025-09-26

--- powershell/expungeDataSpillage/expungeDataSpillage.ps1
+++ powershell/expungeDataSpillage/expungeDataSpillage.ps1
@@ -47,7 +47,7 @@ if ($delete) {
     log "--------------------------"
 }
 
-$archiveType = @{'0' = "kCloud"; '1' = "kTape" }
+$archiveType = @{'0' = "kCloud"; '1' = "kTape"; '2' = "kNAS" }
 
 $clusters = @()
 $instanceList = @()
