ef15994018c349968ca8244725f93ba251d86d9e
bseltz-cohesity <bseltzer@cohesity.com>
2026-01-27 12:53:22 -0500

    2026-01-27

--- powershell/migrateSQLProtectionGroup/migrateSQLProtectionGroup.ps1
+++ powershell/migrateSQLProtectionGroup/migrateSQLProtectionGroup.ps1
@@ -54,16 +54,19 @@ function waitForAppRefresh($server){
     return $rootNode.rootNode.id
 }
 
-function Restart-Service([string]$CompName,[string]$ServiceName){
-    $filter = 'Name=' + "'" + $ServiceName + "'" + ''
-    $service = Get-WMIObject -ComputerName $CompName -Authentication PacketPrivacy -namespace "root\cimv2" -class Win32_Service -Filter $filter
-    $service.StopService()
+Function Restart-Service([string]$compName,[string]$serviceName){
+    $filter = 'Name=' + "'" + $serviceName + "'" + ''
+    $service = Get-CimInstance -ComputerName $compName -ClassName Win32_Service -Filter $filter
+    $null = Invoke-CimMethod -InputObject $service -MethodName StopService
     while ($service.Started){
       Start-Sleep 2
-      $service = Get-WMIObject -ComputerName $CompName -Authentication PacketPrivacy -namespace "root\cimv2" -class Win32_Service -Filter $filter
+      $service = Get-CimInstance -ComputerName $compName -ClassName Win32_Service -Filter $filter
     }
-    $service.StartService()
-    Start-Sleep 2
+    Invoke-CimMethod -InputObject $service -MethodName StartService
+    while (! $service.Started){
+      Start-Sleep 2
+      $service = Get-CimInstance -ComputerName $compName -ClassName Win32_Service -Filter $filter
+    }
 }
 
 function Detach-Agent([string]$server){
