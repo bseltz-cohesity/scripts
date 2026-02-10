0550c56548a1e20a8a694689260e261674b21ba3
bseltz-cohesity <bseltzer@cohesity.com>
2026-02-10 04:26:11 -0500

    2026-02-10

--- python/legalHold/README.md
+++ python/legalHold/README.md
@@ -42,17 +42,19 @@ To remove legal hold from a specific run (by run date):
 * -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
 * -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
 * -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
 * -i, --useApiKey: (optional) use API key for authentication
 * -pwd, --password: (optional) password or API key
 * -np, --noprompt: (optional) do not prompt for password
 * -mcm, --mcm: (optional) connect through MCM
 * -c, --clustername: (optional) helios/mcm cluster to connect to
 * -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
 
 ## Other Parameters
 
 * -j, --jobname: protection job name
-* -id, --runid: (optional) run ID to operate on
+* -id, --runid: (optional) run ID to operate on (repeat for multiple)
 * -rl, --runidlist: (optional) text file containing run IDs (one per line)
 * -dt, --rundate: (optional) run date to operate on (e.g. '2024-01-03 23:30')
 * -a, --addhold: (optional) add legal holds
--- python/legalHold/legalHold.py
+++ python/legalHold/legalHold.py
@@ -11,12 +11,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
 parser.add_argument('-u', '--username', type=str, default='helios')
 parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
 parser.add_argument('-c', '--clustername', type=str, default=None)
 parser.add_argument('-mcm', '--mcm', action='store_true')
 parser.add_argument('-i', '--useApiKey', action='store_true')
 parser.add_argument('-pwd', '--password', type=str, default=None)
 parser.add_argument('-np', '--noprompt', action='store_true')
 parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
 parser.add_argument('-j', '--jobname', type=str, required=True)
 parser.add_argument('-n', '--numruns', type=int, default=1000)
 parser.add_argument('-a', '--addhold', action='store_true')
@@ -24,7 +26,7 @@ parser.add_argument('-r', '--removehold', action='store_true')
 parser.add_argument('-p', '--pushtoreplicas', action='store_true')
 parser.add_argument('-l', '--includelogs', action='store_true')
 parser.add_argument('-y', '--daysback', type=int, default=None)
-parser.add_argument('-id', '--runid', type=int, default=None)
+parser.add_argument('-id', '--runid', action='append', type=int, default=None)
 parser.add_argument('-rl', '--runidlist', type=str)
 parser.add_argument('-dt', '--rundate', type=str, default=None)
 
@@ -33,12 +35,14 @@ args = parser.parse_args()
 vip = args.vip
 username = args.username
 domain = args.domain
+tenant = args.tenant
 clustername = args.clustername
 mcm = args.mcm
 useApiKey = args.useApiKey
 password = args.password
 noprompt = args.noprompt
 mfacode = args.mfacode
+emailmfacode = args.emailmfacode
 jobname = args.jobname
 numruns = args.numruns
 addhold = args.addhold
@@ -80,21 +84,26 @@ if daysback is not None:
     daysBackUsecs = timeAgo(daysback, 'days')
     tail = '&startTimeUsecs=%s' % daysBackUsecs
 
-# authenticate
-apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode)
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
 
-# if connected to helios or mcm, select access cluster
-if mcm or vip.lower() == 'helios.cohesity.com':
-    if clustername is not None:
-        heliosCluster(clustername)
-    else:
-        print('-clustername is required when connecting to Helios or MCM')
-        exit()
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
 
 # exit if not authenticated
 if apiconnected() is False:
     print('authentication failed')
     exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
 
 now = datetime.now()
 nowUsecs = dateToUsecs(now.strftime("%Y-%m-%d %H:%M:%S"))
