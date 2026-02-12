8ec57f46e5e00948a117c6329e0eadd51b5d6f24
bseltz-cohesity <bseltzer@cohesity.com>
2026-02-12 08:18:55 -0500

    2026-02-12

---
+++ python/registerMongoDBOpsManager/README.md
@@ -0,0 +1,65 @@
+# Register MongoDB Ops Manager Protection Sources using Python
+
+Warning: this code is provided on a best effort basis and is not in any way officially supported or sanctioned by Cohesity. The code is intentionally kept simple to retain value as example code. The code in this repository is provided as-is and the author accepts no liability for damages resulting from its use.
+
+This python script registers a MongoDB Ops Manager protection sources.
+
+## Download the script
+
+You can download the scripts using the following commands:
+
+```bash
+# download commands
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/registerMongoDBOpsManager/registerMongoDBOpsManager.py
+curl -O https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity.py
+chmod +x registerMongoDBOpsManager.py
+# end download commands
+```
+
+## Components
+
+* [registerMongoDBOpsManager.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/registerMongoDBOpsManager/registerMongoDBOpsManager.py): the main python script
+* [pyhesity.py](https://raw.githubusercontent.com/cohesity/community-automation-samples/main/python/pyhesity/pyhesity.py): the Cohesity REST API helper module
+
+Place both files in a folder together and run the main script like so:
+
+```bash
+./registerMongoDBOpsManager.py -v mycluster \
+                               -u myusername \
+                               -d mydomain.net \
+                               -n myopsmanager.mydomain.net \
+                               -p 8080 \
+                               -pubfile mypublickey.pem \
+                               -privfile myprivatekey.pem
+```
+
+## Authentication Parameters
+
+* -v, --vip: (optional) DNS or IP of the Cohesity cluster to connect to (default is helios.cohesity.com)
+* -u, --username: (optional) username to authenticate to Cohesity cluster (default is helios)
+* -d, --domain: (optional) domain of username (defaults to local)
+* -t, --tenant: (optional) multi-tenancy tenant name
+* -i, --useApiKey: (optional) use API key for authentication
+* -pwd, --password: (optional) password or API key
+* -np, --noprompt: (optional) do not prompt for password
+* -mcm, --mcm: (optional) connect through MCM
+* -c, --clustername: (optional) helios/mcm cluster to connect to
+* -m, --mfacode: (optional) MFA code for authentication
+* -e, --emailmfacode: (optional) send MFA code via email
+
+## Other Parameters
+
+* -n, --hostname: name of source and primary host to register
+* -p, --port: TCP port number
+* -certfile, --certificatefile: (optional) path to file containing SSL certificate
+* -pubfile, --publickeyfile: (optional) path to file containing public key
+* -privfile, --privatekeyfile: (optional) path to file containing private key
+* -certificate, --certificate: (optional) certificate (string)
+* -pubkey, --publickey: (optional) public key (string)
+* -privkey, --privatekey: (optional) private key (string)
+
+## Notes
+
+You must provide the public and private keys using one of the command line parameters (for each key, there is a parameter to use a string or a file).
+
+To enable SSL, provide a certificate using one of the command line parameters (there is a parameter to use a string or a file).
---
+++ python/registerMongoDBOpsManager/registerMongoDBOpsManager.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+from pyhesity import *
+from getpass import getpass
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--vip', type=str, default='helios.cohesity.com')
+parser.add_argument('-u', '--username', type=str, default='helios')
+parser.add_argument('-d', '--domain', type=str, default='local')
+parser.add_argument('-t', '--tenant', type=str, default=None)
+parser.add_argument('-c', '--clustername', type=str, default=None)
+parser.add_argument('-mcm', '--mcm', action='store_true')
+parser.add_argument('-i', '--useApiKey', action='store_true')
+parser.add_argument('-pwd', '--password', type=str, default=None)
+parser.add_argument('-np', '--noprompt', action='store_true')
+parser.add_argument('-m', '--mfacode', type=str, default=None)
+parser.add_argument('-e', '--emailmfacode', action='store_true')
+parser.add_argument('-n', '--hostname', required=True, type=str)
+parser.add_argument('-p', '--port', required=True, type=int)
+parser.add_argument('-certfile', '--certificatefile', type=str, default=None)
+parser.add_argument('-pubfile', '--publickeyfile', type=str, default=None)
+parser.add_argument('-privfile', '--privatekeyfile', type=str, default=None)
+parser.add_argument('-certificate', '--certificate', type=str, default=None)
+parser.add_argument('-pubkey', '--publickey', type=str, default=None)
+parser.add_argument('-privkey', '--privatekey', type=str, default=None)
+
+args = parser.parse_args()
+
+vip = args.vip
+username = args.username
+domain = args.domain
+tenant = args.tenant
+clustername = args.clustername
+mcm = args.mcm
+useApiKey = args.useApiKey
+password = args.password
+noprompt = args.noprompt
+mfacode = args.mfacode
+emailmfacode = args.emailmfacode
+
+hostname = args.hostname
+port = args.port
+
+certificatefile = args.certificatefile
+publickeyfile = args.publickeyfile
+privatekeyfile = args.privatekeyfile
+certificate = args.certificate
+publickey = args.publickey
+privatekey = args.privatekey
+
+# authentication =========================================================
+# demand clustername if connecting to helios or mcm
+if (mcm or vip.lower() == 'helios.cohesity.com') and clustername is None:
+    print('-c, --clustername is required when connecting to Helios or MCM')
+    exit(1)
+
+# authenticate
+apiauth(vip=vip, username=username, domain=domain, password=password, useApiKey=useApiKey, helios=mcm, prompt=(not noprompt), mfaCode=mfacode, emailMfaCode=emailmfacode, tenantId=tenant)
+
+# exit if not authenticated
+if apiconnected() is False:
+    print('authentication failed')
+    exit(1)
+
+# if connected to helios or mcm, select access cluster
+if mcm or vip.lower() == 'helios.cohesity.com':
+    heliosCluster(clustername)
+    if LAST_API_ERROR() != 'OK':
+        exit(1)
+# end authentication =====================================================
+
+usessl = False
+if certificate is not None or certificatefile is not None:
+    usessl = True
+    if certificatefile is not None:
+        c = open(certificatefile, 'r')
+        certificate = c.read()
+        c.close()
+
+if publickey is not None or publickeyfile is not None:
+    if publickeyfile is not None:
+        p = open(publickeyfile, 'r')
+        publickey = p.read()
+        p.close()
+
+if privatekey is not None or privatekeyfile is not None:
+    if privatekeyfile is not None:
+        k = open(privatekeyfile, 'r')
+        privatekey = k.read()
+        k.close()
+
+if privatekey is None:
+    print('private key required')
+    exit(1)
+if publickey is None:
+    print('publick key required')
+    exit(1)
+
+sourcename = '%s:%s' % (hostname, port)
+registeredSource = None
+registeredSources = api('get', 'protectionSources/registrationInfo?environments=kMongoDBPhysical')
+if registeredSources is not None and 'rootNodes' in registeredSources and registeredSources['rootNodes'] is not None and len(registeredSources['rootNodes']) > 0:
+    registeredSource = [r for r in registeredSources['rootNodes'] if r['rootNode']['name'].lower() == sourcename.lower()]
+if registeredSource is not None and len(registeredSource) > 0:
+    print('%s already registered' % sourcename)
+    exit(0)
+
+newSource = {
+    "environment": "kMongoDBPhysical",
+    "mongodbOpsParams": {
+        "hostname": hostname,
+        "port": port,
+        "isSSlRequired": usessl,
+        "publicKey": publickey,
+        "privateKey": privatekey
+    }
+}
+
+if usessl is True:
+    newSource['mongodbOpsParams']['caCertificate'] = certificate
+
+result = api('post', 'data-protect/sources/registrations', newSource, v=2)
+if result is not None and 'id' in result:
+    print('Registering %s' % sourcename)
+else:
+    print('An error occurred')
