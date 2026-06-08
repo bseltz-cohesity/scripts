71474cb681629e9cff06d04d1cc80cf402395e87
bseltz-cohesity <bseltzer@cohesity.com>
2026-06-08 12:18:33 -0400

    2026-06-08

--- powershell/viewList/viewList.ps1
+++ powershell/viewList/viewList.ps1
@@ -1,18 +1,49 @@
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip,
-    [Parameter(Mandatory = $True)][string]$username,
-    [Parameter()][string]$domain = 'local'
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$helios,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][switch]$emailMfaCode,
+    [Parameter()][string]$clusterName
 )
 
 ### source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-### authenticate
-apiauth -vip $vip -username $username -domain $domain
+# authentication =============================================
+# demand clusterName for Helios
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios" -ForegroundColor Yellow
+    exit 1
+}
 
-$ignore = @('madrox:', 'magneto_', 'icebox_', 'AUDIT_', 'yoda_', 'cohesity_download_', 'COHESITY_HELIOS_')
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $helios -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
+
+# $ignore = @('madrox:', 'magneto_', 'icebox_', 'AUDIT_', 'yoda_', 'cohesity_download_', 'COHESITY_HELIOS_')
+$ignore = @()
 
 $views = api get "views?_includeTenantInfo=true&allUnderHierarchy=true&includeInternalViews=true"
 foreach ($view in $views.views){
