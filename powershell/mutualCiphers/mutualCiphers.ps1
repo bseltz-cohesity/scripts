9137f16d6438b311969feb1327645faf6f50bc2d
bseltz-cohesity <bseltzer@cohesity.com>
2026-06-23 05:46:15 -0400

    2026-06-23

---
+++ powershell/mutualCiphers/mutualCiphers.ps1
@@ -0,0 +1,60 @@
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$helios,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][switch]$emailMfaCode,
+    [Parameter()][string]$clusterName
+)
+
+if($PSVersionTable.PSEdition -ne 'Desktop'){
+    Write-Host "This script will only work on PowerShell Desktop edition" -ForegroundColor Yellow
+    exit 1
+}
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authentication =============================================
+# demand clusterName for Helios
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios" -ForegroundColor Yellow
+    exit 1
+}
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $helios -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
+
+$clusterCiphers = api get -v2 security/ciphers
+$clientCiphers = Get-TlsCipherSuite
+$multualCiphers = $clusterCiphers.enabledCiphers | Where-Object {$_ -in $clientCiphers.name }
+
+if(!$multualCiphers){
+    Write-Host "No mutual ciphers found" -ForegroundColor Yellow
+    exit 1
+}else{
+    Write-Host "Mutual Ciphers:`n==============="
+    $multualCiphers
+}
