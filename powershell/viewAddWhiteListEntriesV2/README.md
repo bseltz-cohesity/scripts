09b87c5184b55722e456b783057a8f1757ec7763
bseltz-cohesity <bseltzer@cohesity.com>
2026-09-09 08:07:59 -0400

    2026-09-09

--- powershell/viewAddWhiteListEntriesV2/README.md
+++ powershell/viewAddWhiteListEntriesV2/README.md
@@ -36,12 +36,22 @@ Place both files in a folder together and run the main script like so:
 #end example
 ```
 
-## Parameters
+## Authentication Parameters
 
-* -vip: Cohesity cluster to connect to
-* -username: Cohesity username
-* -domain: (optional) Active Directory domain (defaults to 'local')
-* -tenantId: (optional) tenant to impersonate
+* -vip: (optional) name or IP of Cohesity cluster (defaults to helios.cohesity.com)
+* -username: (optional) name of user to connect to Cohesity (defaults to helios)
+* -domain: (optional) your AD domain (defaults to local)
+* -useApiKey: (optional) use API key for authentication
+* -password: (optional) will use cached password or will be prompted
+* -noPrompt: (optional) do not prompt for password
+* -tenant: (optional) organization to impersonate
+* -mcm: (optional) connect through MCM
+* -mfaCode: (optional) TOTP MFA code
+* -emailMfaCode: (optional) send MFA code via email
+* -clusterName: (optional) cluster to connect to when connecting through Helios or MCM
+
+## Other Parameters
+
 * -viewNames: (optional) names of new views to modify (comma separated)
 * -viewList: (optional) text file of views to modify (one per line)
 * -ips: (optional) cidrs to add, examples: 192.168.1.3/32, 192.168.2.0/24 (comma separated)
--- powershell/viewAddWhiteListEntriesV2/viewAddWhiteListEntriesV2.ps1
+++ powershell/viewAddWhiteListEntriesV2/viewAddWhiteListEntriesV2.ps1
@@ -1,17 +1,24 @@
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip,       # the cluster to connect to (DNS name or IP)
-    [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
-    [Parameter()][string]$domain = 'local',           # local or AD domain
-    [Parameter()][string]$tenantId = $null,           # tenant ID to impersonate
+    [Parameter()][string]$vip='helios.cohesity.com',
+    [Parameter()][string]$username = 'helios',
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$tenant,
+    [Parameter()][switch]$useApiKey,
+    [Parameter()][string]$password,
+    [Parameter()][switch]$noPrompt,
+    [Parameter()][switch]$helios,
+    [Parameter()][string]$mfaCode,
+    [Parameter()][switch]$emailMfaCode,
+    [Parameter()][string]$clusterName,
     [Parameter()][array]$viewNames,                   # names of view to modify (comma separated)
-    [Parameter()][string]$viewList,                   # optional textfile of views to modify
-    [Parameter()][array]$ips,                         # optional cidrs to add (comma separated)
-    [Parameter()][string]$ipList,                     # optional textfile of cidrs to add
-    [Parameter()][switch]$rootSquash,                 # whether whitelist entries should use root squash
-    [Parameter()][switch]$allSquash,                  # whether whitelist entries should use all squash
-    [Parameter()][switch]$readOnly                    # grant only read access
+    [Parameter()][string]$viewList,                    # optional textfile of views to modify
+    [Parameter()][array]$ips,                          # optional cidrs to add (comma separated)
+    [Parameter()][string]$ipList,                      # optional textfile of cidrs to add
+    [Parameter()][switch]$rootSquash,                  # whether whitelist entries should use root squash
+    [Parameter()][switch]$allSquash,                   # whether whitelist entries should use all squash
+    [Parameter()][switch]$readOnly                     # grant only read access
 )
 
 # gather list from command line params and file
@@ -46,8 +53,30 @@ if($readOnly){
 ### source the cohesity-api helper code
 . $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
 
-### authenticate
-apiauth -vip $vip -username $username -domain $domain -tenantId $tenantId
+# authentication =============================================
+# demand clusterName for Helios
+if(($vip -eq 'helios.cohesity.com' -or $mcm) -and ! $clusterName){
+    Write-Host "-clusterName required when connecting to Helios" -ForegroundColor Yellow
+    exit 1
+}
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -passwd $password -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $helios -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
+
+# exit on failed authentication
+if(!$cohesity_api.authorized){
+    Write-Host "Not authenticated" -ForegroundColor Yellow
+    exit 1
+}
+
+# select helios managed cluster
+if($USING_HELIOS){
+    $thisCluster = heliosCluster $clusterName
+    if(! $thisCluster){
+        exit 1
+    }
+}
+# end authentication =========================================
 
 function newWhiteListEntry($cidr, $perm){
     $ip, $netbits = $cidr -split '/'
@@ -80,11 +109,11 @@ foreach($viewName in $viewsToModify){
         Write-Host "View $viewName not found" -ForegroundColor Yellow
     }else{
         Write-Host $view.name
-    
+
         if(! $view.PSObject.Properties['subnetWhitelist']){
             setApiProperty -object $view -name 'subnetWhitelist' -value @()
         }
-        
+
         foreach($cidr in $ipsToAdd){
             Write-Host "    $cidr"
             $ip, $netbits = $cidr -split '/'
